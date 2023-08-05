070bbbc336017d6fc6456a4f0d01a86a0626166b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:21 +0000

    Add package prophecy-libs/1.1.10/prophecy-libs-1.1.10.tar.gz

---
+++ packages/prophecy-libs/prophecy-libs-1.1.10.tar.gz/prophecy-libs-1.1.10/prophecy/test/base_test_case.py
@@ -0,0 +1,16 @@
+import unittest
+from pyspark.sql import SparkSession
+import glob
+
+class BaseTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        jarDependencies = glob.glob("/opt/docker/lib/*.jar")
+        cls.spark = SparkSession.builder.master("local").appName("init")\
+            .config("spark.jars", ','.join(jarDependencies))\
+            .getOrCreate()
+        cls.maxUnequalRowsToShow = 5
+
+    def setup(self):
+        self.spark = BaseTestCase.spark
+        self.maxUnequalRowsToShow = BaseTestCase.maxUnequalRowsToShow
