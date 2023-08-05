1578c069d16185a32087df9c9b12db315673f73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package easyTDV/1.1.2/easyTDV-1.1.2-py3-none-any.whl

---
+++ packages/easyTDV/easyTDV-1.1.2-py3-none-any.whl/easyTDV/DeploymentNomenclature.py
@@ -0,0 +1,33 @@
+
+class DeploymentNomenclature:
+    def __init__(self, job_id, bucket, region):
+        self.job_id = job_id
+        self.bucket = bucket
+        self.region = region
+
+    def get_s3_lbd_location(self):
+        return f"domaine=repository/table=lambda-deployment/jobid={self.job_id}/lambda_deployment.zip"
+
+    def get_s3_model_location(self):
+        pass
+
+    def get_lbd_deployment_name(self):
+        return f"lbd-request-processor-{self.job_id}"
+
+    def get_api_name(self):
+        return f"api-predection-{self.job_id}"
+
+    def get_deployment_stack_name(self):
+        return f"deployment-stack-{self.job_id}"
+
+    def get_role_deployment_name(self):
+        return f"Deployment-Role-{self.job_id}"
+
+    def get_s3_stack_template_location(self):
+        s3_key = f"domaine=repository/table=template_stack/jobid={self.job_id}/template_deployment_stack.json"
+        s3_url = f"https://{self.bucket}.s3.{self.region}.amazonaws.com/{s3_key}"
+        return {
+            "s3_key": s3_key,
+            "s3_url": s3_url
+        }
+
