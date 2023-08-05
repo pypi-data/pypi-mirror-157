11f5b8a480fc486ad8989746aa96040e346080d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:09 +0000

    Add package nester-monstrosity878/1.3.0/nester_monstrosity878-1.3.0-py3-none-any.whl

---
+++ packages/nester-monstrosity878/nester_monstrosity878-1.3.0-py3-none-any.whl/src/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup( 
+	name = 'nester', 
+	version = '1.3.0', 
+	py_modules = ['nester'],
+	author = 'ObscureIT',
+	author_email = 'akif.khan1704@gmail.com',
+	url = 'https://github.com/obscureIT/headfirstPythonPractice',
+	description = 'A simple printer of nested lists',
+)
