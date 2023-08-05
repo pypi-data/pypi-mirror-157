0abab7bb97b042207ba7d005c91e4ddc092ba89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:16 +0000

    Add package elasticai.creator/0.10.1/elasticai.creator-0.10.1-py3-none-any.whl

---
+++ packages/elasticai.creator/elasticai.creator-0.10.1-py3-none-any.whl/elasticai/creator/vhdl/language_testbench.py
@@ -0,0 +1,25 @@
+"""
+The module contains classes and functions for generating vhdl code similar to the language module
+This module includes CodeGenerator that are only used by the vhdl testbenches
+"""
+import abc
+from abc import abstractmethod
+from typing import Iterator
+
+
+class TestBenchBase(abc.ABC):
+    simulation_start_msg = 'report "======Simulation Start======" severity Note'
+    simulation_end_msgs = (
+        'report "======Simulation Success======" severity Note',
+        'report "Please check the output message." severity Note',
+        "wait",
+    )
+
+    @abstractmethod
+    def _body(self) -> Iterator[str]:
+        ...
+
+    def __iter__(self) -> Iterator[str]:
+        yield self.simulation_start_msg
+        yield from self._body()
+        yield from self.simulation_end_msgs
