bd805f0a26971b2d8875519cd746384d4bd5af01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:16 +0000

    Add package pytest-railflow-testrail-reporter/0.2.5/pytest-railflow-testrail-reporter-0.2.5.tar.gz

---
+++ packages/pytest-railflow-testrail-reporter/pytest-railflow-testrail-reporter-0.2.5.tar.gz/pytest-railflow-testrail-reporter-0.2.5/setup.py
@@ -0,0 +1,46 @@
+# import os
+# import re
+import io
+
+from setuptools import setup
+
+setup(
+    name="pytest-railflow-testrail-reporter",
+    version="0.2.5",
+    description="Generate json reports along with specified metadata defined in test markers.",
+    long_description=io.open("README.md", encoding="utf-8", errors="ignore").read(),
+    long_description_content_type='text/markdown',
+    author="Railflow",
+    # author_email=u"",
+    url=u"https://github.com/railflow/railflow-pytest-plugin",
+    license="MIT License",
+    license_file="LICENSE",
+    packages=["pytest_railflow_testrail_reporter"],
+    entry_points={
+        "pytest11": [
+            "pytest_railflow_testrail_reporter = pytest_railflow_testrail_reporter.plugin"
+        ]
+    },
+    install_requires=["pytest"],
+    keywords="py.test pytest json railflow report",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",  # change to Production/Stable
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        " License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
