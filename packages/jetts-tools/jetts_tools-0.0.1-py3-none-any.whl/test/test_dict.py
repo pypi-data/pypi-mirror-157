b7cdf401977077ab8494456f874bd0e98d225a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:47 +0000

    Add package jetts-tools/0.0.1/jetts_tools-0.0.1-py3-none-any.whl

---
+++ packages/jetts-tools/jetts_tools-0.0.1-py3-none-any.whl/test/test_dict.py
@@ -0,0 +1,20 @@
+from jettools import dict
+import unittest
+
+
+class TestGetNestedKey(unittest.TestCase):
+    def test_should_return_none_if_no_keys(self):
+        d = {'level1': {'level1': 2}}
+        self.assertEqual(dict.get(d, []), None)
+
+    def test_should_grab_val_if_just_string(self):
+        d = {'level1': 3}
+        self.assertEqual(dict.get(d, 'level1'), 3)
+
+    def test_should_get_nested_value_if_exists(self):
+        d = {'level1': {'level2': 2}}
+        self.assertEqual(dict.get(d, ['level1', 'level2']), 2)
+
+    def test_should_return_none_if_fake_levels(self):
+        d = {'level1': {'level2': 2}}
+        self.assertEqual(dict.get(d, ['level2', 'level3']), None)
