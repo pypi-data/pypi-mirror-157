30841801258bd439ddeb45d14803ee977c87bfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:44 +0000

    Add package shrike/1.28.2/shrike-1.28.2.tar.gz

---
+++ packages/shrike/shrike-1.28.2.tar.gz/shrike-1.28.2/shrike/pipeline/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+Python utilities that help users to manage, validate
+and submit AML pipelines
+"""
+
+
+from .pipeline_helper import AMLPipelineHelper  # noqa: F401
+from .federated_learning import FederatedPipelineBase, StepOutput  # noqa: F401
+from .ray_actor import ray_actor_on_shrike, b64_decode, b64_encode  # noqa: F401
