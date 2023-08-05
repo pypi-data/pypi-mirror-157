16f864cf9eabe968aa87b541ac669d7c92d8c7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:16 +0000

    Add package pyupdater-cos-plugin/0.1.1/pyupdater-cos-plugin-0.1.1.tar.gz

---
+++ packages/pyupdater-cos-plugin/pyupdater-cos-plugin-0.1.1.tar.gz/pyupdater-cos-plugin-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['uploader']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cos-python-sdk-v5>=1.9.20,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pyupdater-cos-plugin',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': '杨志勇',
+    'author_email': 'eye.zhi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
