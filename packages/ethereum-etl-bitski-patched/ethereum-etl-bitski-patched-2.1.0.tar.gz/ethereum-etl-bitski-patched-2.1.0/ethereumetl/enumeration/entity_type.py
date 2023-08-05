740d5bdef203d5819d39e7bbe4b80660afc7564d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:29 +0000

    Add package ethereum-etl-bitski-patched/2.1.0/ethereum-etl-bitski-patched-2.1.0.tar.gz

---
+++ packages/ethereum-etl-bitski-patched/ethereum-etl-bitski-patched-2.1.0.tar.gz/ethereum-etl-bitski-patched-2.1.0/ethereumetl/enumeration/entity_type.py
@@ -0,0 +1,13 @@
+class EntityType:
+    BLOCK = 'block'
+    TRANSACTION = 'transaction'
+    RECEIPT = 'receipt'
+    LOG = 'log'
+    TOKEN_TRANSFER = 'token_transfer'
+    TOKEN_TRANSFER_V2 = 'token_transfer_v2'
+    TRACE = 'trace'
+    CONTRACT = 'contract'
+    TOKEN = 'token'
+
+    ALL_FOR_STREAMING = [BLOCK, TRANSACTION, LOG, TOKEN_TRANSFER, TOKEN_TRANSFER_V2, TRACE, CONTRACT, TOKEN]
+    ALL_FOR_INFURA = [BLOCK, TRANSACTION, LOG, TOKEN_TRANSFER, TOKEN_TRANSFER_V2]
