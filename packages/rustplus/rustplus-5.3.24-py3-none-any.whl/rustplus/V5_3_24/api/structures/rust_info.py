9bd63be356b02d47a0ed67434f2085f7ab685c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:52 +0000

    Add package rustplus/5.3.24/rustplus-5.3.24-py3-none-any.whl

---
+++ packages/rustplus/rustplus-5.3.24-py3-none-any.whl/rustplus/V5_3_24/api/structures/rust_info.py
@@ -0,0 +1,63 @@
+class RustInfo:
+    def __init__(self, data) -> None:
+        self._url: str = data.url
+        self._name: str = data.name
+        self._map: str = data.map
+        self._size: int = data.mapSize
+        self._players: int = data.players
+        self._max_players: int = data.maxPlayers
+        self._queued_players: int = data.queuedPlayers
+        self._seed: int = data.seed
+        self._wipe_time: int = data.wipeTime
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def map(self) -> str:
+        return self._map
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def players(self) -> int:
+        return self._players
+
+    @property
+    def max_players(self) -> int:
+        return self._max_players
+
+    @property
+    def queued_players(self) -> int:
+        return self._queued_players
+
+    @property
+    def seed(self) -> int:
+        return self._seed
+
+    @property
+    def wipe_time(self) -> int:
+        return self._wipe_time
+
+    def __str__(self) -> str:
+        return (
+            "RustInfo[url={}, name={}, map={}, size={}, players={}, max_players={}, queued_players={}, seed={}, "
+            "wipe_time={}]".format(
+                self._url,
+                self._name,
+                self._map,
+                self._size,
+                self._players,
+                self._max_players,
+                self._queued_players,
+                self._seed,
+                self._wipe_time,
+            )
+        )
