52883a4a0ef65976565a3acf5dea07650e493b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:07 +0000

    Add package django-multi-token/0.1.5/django-multi-token-0.1.5.tar.gz

---
+++ packages/django-multi-token/django-multi-token-0.1.5.tar.gz/django-multi-token-0.1.5/tokens/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+
+from rest_framework.authentication import TokenAuthentication
+import binascii
+import os
+
+
+class MultiToken(models.Model):
+    """
+    The default authorization token model.
+    """
+
+    key = models.CharField("Key", max_length=40, primary_key=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="custom_auth_token",
+        on_delete=models.CASCADE,
+        verbose_name="User",
+    )
+    created = models.DateTimeField("Created", auto_now_add=True)
+
+    class Meta:
+        # Work around for a bug in Django:
+        # https://code.djangoproject.com/ticket/19422
+        #
+        # Also see corresponding ticket:
+        # https://github.com/tomchristie/django-rest-framework/issues/705
+        # abstract = 'rest_framework.authtoken' not in settings.INSTALLED_APPS
+        verbose_name = "Multi Token"
+        verbose_name_plural = "Multi Tokens"
+        ordering = ("-created",)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(MultiToken, self).save(*args, **kwargs)
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __str__(self):
+        return self.key
+
+
+class MultiTokenAuthentication(TokenAuthentication):
+
+    model = MultiToken
