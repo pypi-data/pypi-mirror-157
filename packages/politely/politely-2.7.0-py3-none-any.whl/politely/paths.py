a794323f1eaea2fc07f7315ad9aff351918570b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:21 +0000

    Add package politely/2.7.0/politely-2.7.0-py3-none-any.whl

---
+++ packages/politely/politely-2.7.0-py3-none-any.whl/politely/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# directories
+LIB_DIR = Path(__file__).resolve().parent
+RESOURCES_DIR = LIB_DIR / "resources"
+
+# files
+HONORIFICS_YAML = RESOURCES_DIR / "honorifics.yaml"
+RULES_YAML = RESOURCES_DIR / "rules.yaml"
