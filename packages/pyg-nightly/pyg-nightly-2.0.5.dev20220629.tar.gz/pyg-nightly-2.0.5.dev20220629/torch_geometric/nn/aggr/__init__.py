a11acdeb3f4cdc331cd655bce34f130b4a222829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:35 +0000

    Add package pyg-nightly/2.0.5.dev20220629/pyg-nightly-2.0.5.dev20220629.tar.gz

---
+++ packages/pyg-nightly/pyg-nightly-2.0.5.dev20220629.tar.gz/pyg-nightly-2.0.5.dev20220629/torch_geometric/nn/aggr/__init__.py
@@ -0,0 +1,31 @@
+from .base import Aggregation
+from .multi import MultiAggregation
+from .basic import (
+    MeanAggregation,
+    SumAggregation,
+    MaxAggregation,
+    MinAggregation,
+    MulAggregation,
+    VarAggregation,
+    StdAggregation,
+    SoftmaxAggregation,
+    PowerMeanAggregation,
+)
+from .lstm import LSTMAggregation
+from .set2set import Set2Set
+
+__all__ = classes = [
+    'Aggregation',
+    'MultiAggregation',
+    'MeanAggregation',
+    'SumAggregation',
+    'MaxAggregation',
+    'MinAggregation',
+    'MulAggregation',
+    'VarAggregation',
+    'StdAggregation',
+    'SoftmaxAggregation',
+    'PowerMeanAggregation',
+    'LSTMAggregation',
+    'Set2Set',
+]
