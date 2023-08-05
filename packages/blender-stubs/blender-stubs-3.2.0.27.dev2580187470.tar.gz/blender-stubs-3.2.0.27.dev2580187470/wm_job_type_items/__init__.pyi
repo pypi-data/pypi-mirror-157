4a7bc9fbe8c260043b02789e6018a2a3b733937c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:17 +0000

    Add package blender-stubs/3.2.0.27.dev2580187470/blender-stubs-3.2.0.27.dev2580187470.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev2580187470.tar.gz/blender-stubs-3.2.0.27.dev2580187470/wm_job_type_items/__init__.pyi
@@ -0,0 +1,23 @@
+"""
+
+
+Wm Job Type Items
+^^^^^^^^^^^^^^^^^
+
+:RENDER:          
+  Regular rendering.
+
+:RENDER_PREVIEW:  
+  Rendering previews.
+
+:OBJECT_BAKE:     
+  Object Baking.
+
+:COMPOSITE:       
+  Compositing.
+
+.. _rna-enum-wm-job-type-items:
+
+"""
+
+import typing
