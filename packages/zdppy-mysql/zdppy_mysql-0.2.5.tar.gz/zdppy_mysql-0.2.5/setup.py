fbc35b1cd2725d6ba5b2c686d2484f0dacaf6b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:16 +0000

    Add package zdppy-mysql/0.2.5/zdppy_mysql-0.2.5.tar.gz

---
+++ packages/zdppy-mysql/zdppy_mysql-0.2.5.tar.gz/zdppy_mysql-0.2.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zdppy_mysql', 'zdppy_mysql.constants']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'zdppy-mysql',
+    'version': '0.2.5',
+    'description': '使用python操作MySQL,同步版本,支持事务',
+    'long_description': None,
+    'author': 'zhangdapeng',
+    'author_email': 'pygosuperman@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
