8f7eaab7a1e1ccb774fbfc0258c6200922668d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:53 +0000

    Add package vetiver/0.1.5/vetiver-0.1.5.tar.gz

---
+++ packages/vetiver/vetiver-0.1.5.tar.gz/vetiver-0.1.5/vetiver/__init__.py
@@ -0,0 +1,21 @@
+"""vetiver - Python parallel to R vetiver package"""
+
+__version__ = "0.1.5"
+__author__ = "Isabel Zimmerman <isabel.zimmerman@rstudio.com>"
+__all__ = []
+
+import importlib  # noqa
+from .ptype import *  # noqa
+from .vetiver_model import VetiverModel  # noqa
+from .server import VetiverAPI, vetiver_endpoint, predict  # noqa
+from .mock import get_mock_data, get_mock_model  # noqa
+from .pin_read_write import vetiver_pin_write  # noqa
+from .attach_pkgs import *  # noqa
+from .meta import *  # noqa
+from .write_docker import write_docker  # noqa
+from .write_fastapi import write_app  # noqa
+from .handlers._interface import create_handler, InvalidModelError  # noqa
+from .handlers.base import VetiverHandler  # noqa
+from .handlers.sklearn import SKLearnHandler  # noqa
+from .handlers.torch import TorchHandler  # noqa
+from .rsconnect import deploy_rsconnect
---
+++ packages/vetiver/vetiver-0.1.5.tar.gz/vetiver-0.1.5/vetiver/handlers/base.py
@@ -0,0 +1,81 @@
+from abc import ABCMeta
+
+from ..ptype import vetiver_create_ptype
+from ..meta import _model_meta
+
+
+class VetiverHandler(metaclass=ABCMeta):
+    """Base handler class for creating VetiverModel of different type.
+
+    Parameters
+    ----------
+    model :
+        a trained model
+    ptype_data :
+        An object with information (data) whose layout is to be determined.
+    """
+
+    def __init__(self, model, ptype_data):
+        self.model = model
+        self.ptype_data = ptype_data
+
+    def describe(self):
+        """Create description for model"""
+        desc = f"{self.model.__class__} model"
+        return desc
+
+    def create_meta(
+        user: list = None,
+        version: str = None,
+        url: str = None,
+        required_pkgs: list = [],
+    ):
+        """Create metadata for sklearn model"""
+        meta = _model_meta(user, version, url, required_pkgs)
+
+        return meta
+
+    def construct_ptype(self):
+        """Create data prototype for torch model
+
+        Parameters
+        ----------
+        ptype_data : pd.DataFrame, np.ndarray, or None
+            Training data to create ptype
+
+        Returns
+        -------
+        ptype : pd.DataFrame or None
+            Zero-row DataFrame for storing data types
+        """
+        ptype = vetiver_create_ptype(self.ptype_data)
+        return ptype
+
+    def handler_startup():
+        """Include required packages for prediction
+
+        The `handler_startup` function executes when the API starts. Use this
+        function for tasks like loading packages.
+        """
+        ...
+
+    def handler_predict(self, input_data, check_ptype):
+        """Generates method for /predict endpoint in VetiverAPI
+
+        The `handler_predict` function executes at each API call. Use this
+        function for calling `predict()` and any other tasks that must be executed
+        at each API call.
+
+        Parameters
+        ----------
+        input_data:
+            Data used to generate prediction
+        check_ptype:
+            If type should be checked against `ptype` or not
+
+        Returns
+        -------
+        prediction
+            Prediction from model
+        """
+        ...
