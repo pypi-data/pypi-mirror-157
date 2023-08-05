30689bb4dbb1355aa356455b5006c0987ab575fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:00 +0000

    Add package nodegam/0.2.0/nodegam-0.2.0-py3-none-any.whl

---
+++ packages/nodegam/nodegam-0.2.0-py3-none-any.whl/nodegam/__init__.py
@@ -0,0 +1,12 @@
+from . import nn_utils
+from . import odst
+from . import utils
+from . import data
+from . import trainer
+from . import recorder
+from . import arch
+from . import gams
+from . import mypreprocessor
+
+
+__version__ = '0.2.0'
