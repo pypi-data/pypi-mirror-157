c49605b4625ecba4176eb7dbf9e1706d86d7d223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:59 +0000

    Add package jsonteng/1.1.0/jsonteng-1.1.0-py3-none-any.whl

---
+++ packages/jsonteng/jsonteng-1.1.0-py3-none-any.whl/jsonteng/tags/tag_map.py
@@ -0,0 +1,61 @@
+# Copyright 2019 VMware, Inc.
+# SPDX-License-Indentifier: Apache-2.0
+
+from collections import OrderedDict
+
+from .at_tag import AtTag
+from .for_each_tag import ForEachTag
+from .exists_tag import ExistsTag
+from .len_tag import LenTag
+from .one_of_tag import OneOfTag
+from .to_bool_tag import ToBoolTag
+from .to_float_tag import ToFloatTag
+from .to_int_tag import ToIntTag
+from .to_null_tag import ToNullTag
+
+_tag_class_map = {
+    AtTag.name: AtTag,
+    ExistsTag.name: ExistsTag,
+    ForEachTag.name: ForEachTag,
+    LenTag.name: LenTag,
+    OneOfTag.name: OneOfTag,
+    ToIntTag.name: ToIntTag,
+    ToFloatTag.name: ToFloatTag,
+    ToBoolTag.name: ToBoolTag,
+    ToNullTag.name: ToNullTag
+}
+
+
+def get_tag_map(tag_resolver):
+    """
+    Return a tag map of known tags.
+    :param tag_resolver: Tag resolved used by tags.
+    :type tag_resolver: 'TagResolver'
+    :return: A map of tags.
+    :rtype: 'dict'
+    """
+    tag_map = OrderedDict()
+    for tag_name, tag in _tag_class_map.items():
+        tag_map[tag_name] = tag(tag_resolver)
+    return tag_map
+
+
+def add_tag(tag_name, tag):
+    """
+    Add a tag to the tag collection.
+    :param tag_name: Tag name.
+    :type tag_name: 'str'
+    :param tag: Tag class.
+    :type tag: 'TagBase'
+    :return:
+    """
+    _tag_class_map[tag_name] = tag
+
+
+def get_tag_names():
+    """
+    Get all tag names.
+    :return: Tag names.
+    :rtype: 'list'
+    """
+    return _tag_class_map.keys()
