6034e476b0b7e49d55b61dd145123e255291fbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:28 +0000

    Add package akhilnester/1.0.0/akhilnester-1.0.0.tar.gz

---
+++ packages/akhilnester/akhilnester-1.0.0.tar.gz/akhilnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		name = "akhilnester",
+		version = "1.0.0",
+		py_modules = ["akhilnester"],
+		author = "akhiltoday",
+		author_email = "support@akhiltoday.com",
+		url = "https://akhiltoday.com",
+		description = "A simple printer of nested lists",
+	)
