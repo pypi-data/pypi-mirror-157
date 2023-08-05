ff683ec2c9acf82290ca5107ea9b237402650788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:45 +0000

    Add package datappkit/0.0.6/datappkit-0.0.6-py3-none-any.whl

---
+++ packages/datappkit/datappkit-0.0.6-py3-none-any.whl/datappkit/common/config.py
@@ -0,0 +1,24 @@
+import os
+
+from datappkit.utils import file_utils
+
+
+_t_filename = os.environ['HOME'] + os.sep + ".datapp-kit.session"
+_token = ''
+
+
+def debug():
+    return os.getenv('DATAPP_KIT_DEBUG', 'off') == 'on'
+
+
+def set_authorization(jwt):
+    global _token
+    _token = jwt
+    return file_utils.write_file(_t_filename, jwt)
+
+
+def authorization():
+    global _token
+    if not _token:
+        _token = file_utils.read_file(_t_filename)
+    return _token
