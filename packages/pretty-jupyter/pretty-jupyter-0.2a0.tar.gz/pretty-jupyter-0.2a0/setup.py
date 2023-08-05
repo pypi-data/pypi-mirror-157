72cd83f400ad59c0fe3cbae60b67badb80aad500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:48 +0000

    Add package pretty-jupyter/0.2a0/pretty-jupyter-0.2a0.tar.gz

---
+++ packages/pretty-jupyter/pretty-jupyter-0.2a0.tar.gz/pretty-jupyter-0.2a0/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+import glob
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# this is done only to copy to the nbconvert's expected target directory
+# so 'jupyter nbconvert' recognizes our new template
+data_files = []
+data_files.append(
+    ("share/jupyter/nbconvert/templates/pj", [
+        "src/pretty_jupyter/templates/pj/conf.json",
+        "src/pretty_jupyter/templates/pj/index.html.j2",
+        "src/pretty_jupyter/templates/pj/base.html.j2"])
+)
+data_files.append(
+    ("share/jupyter/nbconvert/templates/pj/static", [
+        "src/pretty_jupyter/templates/pj/static/pj.js",
+        "src/pretty_jupyter/templates/pj/static/pj.css"
+    ])
+)
+
+setuptools.setup(
+    name='pretty-jupyter',
+    author="Jan Palasek",
+    version='0.2a0',
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={
+        "": "src"
+    },
+    install_requires=[
+        "click",
+        "ipython",
+        "nbconvert"
+    ],
+    packages=setuptools.find_packages("src"),
+    include_package_data=True,
+    data_files=data_files,
+    entry_points={
+        "console_scripts": ["pretty-jupyter = pretty_jupyter.__main__:cli"]
+    }
+)
