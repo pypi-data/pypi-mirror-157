ce0de50961b1010962d55aad35fb43c1d5cb7393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:28 +0000

    Add package autonogame/1.2.0a0/autonogame-1.2.0a0.tar.gz

---
+++ packages/autonogame/autonogame-1.2.0a0.tar.gz/autonogame-1.2.0a0/setup.py
@@ -0,0 +1,42 @@
+from distutils.util import convert_path
+from setuptools import find_packages, setup
+
+module_name = "autonogame"
+main_ns = {}
+ver_path = convert_path(f"src/{module_name}/version.py")
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setup(
+    name=module_name,
+    version=main_ns["__version__"],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    license="MIT",
+    description="Ogame bot",
+    keywords=["ogame", "bot", "hack", "script"],
+    long_description_content_type="text/markdown",
+    long_description=open("README.md", encoding="utf8").read(),
+    install_requires=[
+        "cryptography==37.0.2",
+        "ogame==8.4.0.22",
+    ],
+    url="https://github.com/tassoneroberto/autonogame",
+    author="Roberto Tassone",
+    author_email="roberto.tassone@proton.me",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    entry_points={
+        "console_scripts": [
+            "autonogame = autonogame.gui:main",
+        ],
+    },
+)
---
+++ packages/autonogame/autonogame-1.2.0a0.tar.gz/autonogame-1.2.0a0/src/autonogame/auth.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import logging
+import traceback
+from typing import Union
+
+from autonogame.bot import OgameBot
+from autonogame.config import Config
+from autonogame.crypto import Crypto
+from ogame import OGame
+
+logging.getLogger(__name__)
+logging.root.setLevel(logging.INFO)
+
+
+class Auth(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.config = Config()
+        self.crypto = Crypto()
+
+    def load_accounts(self) -> list[OgameBot]:
+        logging.info("Loading accounts...")
+        logged_in_accounts = []
+        stored_accounts = self.config.get_stored_accounts()
+
+        for account in stored_accounts:
+            account_bot = self.login_client(
+                account["universe"],
+                account["email"],
+                account["password"],
+                account["token"],
+            )
+            if account_bot:
+                logged_in_accounts.append(account_bot)
+                logging.info(f"Account `{account['email']}` loaded")
+        return logged_in_accounts
+
+    def login_client(
+        self,
+        account_universe,
+        account_email,
+        account_password,
+        account_token=None,
+    ) -> Union[OgameBot, None]:
+        empire = self.login(
+            universe=account_universe,
+            email=account_email,
+            password=account_password,
+            token=account_token,
+        )
+
+        if empire:
+            logging.info(f"Account `{account_email}` logged in")
+            credentials = {
+                "universe": account_universe,
+                "email": account_email,
+                "password": account_password,
+                "token": empire.token,
+            }
+            self.config.store_credentials(credentials)
+
+            return OgameBot(empire)
+
+        return None
+
+    def login(self, universe, email, password, token=None):
+        try:
+            return OGame(
+                universe=universe,
+                username=email,
+                password=password,
+                token=token,
+            )
+        except Exception:
+            logging.error(traceback.print_exc())
+
+    def logout(self, account_bot: OgameBot):
+        self.config.delete_account(
+            universe=account_bot.empire.universe,
+            username=account_bot.empire.username,
+        )
+        logging.info(
+            f"Deleted credentials for account `{account_bot.empire.username}`"
+        )
