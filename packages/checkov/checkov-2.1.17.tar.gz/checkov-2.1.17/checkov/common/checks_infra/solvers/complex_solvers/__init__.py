ffcd6c523ad2a6c9c5acf2a3e541236f036d9440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:33 +0000

    Add package checkov/2.1.17/checkov-2.1.17.tar.gz

---
+++ packages/checkov/checkov-2.1.17.tar.gz/checkov-2.1.17/checkov/common/checks_infra/solvers/complex_solvers/__init__.py
@@ -0,0 +1,2 @@
+from checkov.common.checks_infra.solvers.complex_solvers.or_solver import OrSolver  # noqa
+from checkov.common.checks_infra.solvers.complex_solvers.and_solver import AndSolver  # noqa
