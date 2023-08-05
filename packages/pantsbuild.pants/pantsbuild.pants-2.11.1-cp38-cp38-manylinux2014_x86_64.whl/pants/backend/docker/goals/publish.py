3db58f14f63da1fb315f43fccff3b64eb8deadd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:28 +0000

    Add package pantsbuild.pants/2.11.1/pantsbuild.pants-2.11.1-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.11.1-cp38-cp38-manylinux2014_x86_64.whl/pants/backend/docker/goals/publish.py
@@ -0,0 +1,93 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass
+from itertools import chain
+from typing import cast
+
+from pants.backend.docker.goals.package_image import BuiltDockerImage
+from pants.backend.docker.subsystems.docker_options import DockerOptions
+from pants.backend.docker.target_types import DockerImageRegistriesField, DockerImageSkipPushField
+from pants.backend.docker.util_rules.docker_binary import DockerBinary
+from pants.core.goals.publish import (
+    PublishFieldSet,
+    PublishOutputData,
+    PublishPackages,
+    PublishProcesses,
+    PublishRequest,
+)
+from pants.engine.environment import Environment, EnvironmentRequest
+from pants.engine.process import InteractiveProcess, InteractiveProcessRequest
+from pants.engine.rules import Get, MultiGet, collect_rules, rule
+
+logger = logging.getLogger(__name__)
+
+
+class PublishDockerImageRequest(PublishRequest):
+    pass
+
+
+@dataclass(frozen=True)
+class PublishDockerImageFieldSet(PublishFieldSet):
+    publish_request_type = PublishDockerImageRequest
+    required_fields = (DockerImageRegistriesField,)
+
+    registries: DockerImageRegistriesField
+    skip_push: DockerImageSkipPushField
+
+    def get_output_data(self) -> PublishOutputData:
+        return PublishOutputData(
+            {
+                "publisher": "docker",
+                "registries": self.registries.value or (),
+                **super().get_output_data(),
+            }
+        )
+
+
+@rule
+async def push_docker_images(
+    request: PublishDockerImageRequest, docker: DockerBinary, options: DockerOptions
+) -> PublishProcesses:
+    tags = tuple(
+        chain.from_iterable(
+            cast(BuiltDockerImage, image).tags
+            for pkg in request.packages
+            for image in pkg.artifacts
+        )
+    )
+
+    if request.field_set.skip_push.value:
+        return PublishProcesses(
+            [
+                PublishPackages(
+                    names=tags,
+                    description=f"(by `{request.field_set.skip_push.alias}` on {request.field_set.address})",
+                ),
+            ]
+        )
+
+    env = await Get(Environment, EnvironmentRequest(options.env_vars))
+    interactive_processes = await MultiGet(
+        Get(InteractiveProcess, InteractiveProcessRequest(process))
+        for process in docker.push_image(tags, env)
+    )
+    return PublishProcesses(
+        [
+            PublishPackages(
+                names=(tag,),
+                process=process,
+            )
+            for tag, process in zip(tags, interactive_processes)
+        ]
+    )
+
+
+def rules():
+    return (
+        *collect_rules(),
+        *PublishDockerImageFieldSet.rules(),
+    )
---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.11.1-cp38-cp38-manylinux2014_x86_64.whl/pants/core/goals/lint.py
@@ -0,0 +1,300 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import itertools
+import logging
+from dataclasses import dataclass
+from typing import Any, ClassVar, Iterable, cast
+
+from pants.core.goals.style_request import (
+    StyleRequest,
+    determine_specified_tool_names,
+    only_option_help,
+    style_batch_size_help,
+    write_reports,
+)
+from pants.core.util_rules.distdir import DistDir
+from pants.engine.console import Console
+from pants.engine.engine_aware import EngineAwareParameter, EngineAwareReturnType
+from pants.engine.fs import EMPTY_DIGEST, Digest, SpecsSnapshot, Workspace
+from pants.engine.goal import Goal, GoalSubsystem
+from pants.engine.process import FallibleProcessResult
+from pants.engine.rules import Get, MultiGet, collect_rules, goal_rule
+from pants.engine.target import FieldSet, Targets
+from pants.engine.unions import UnionMembership, union
+from pants.option.option_types import IntOption, StrListOption
+from pants.util.collections import partition_sequentially
+from pants.util.logging import LogLevel
+from pants.util.memo import memoized_property
+from pants.util.meta import frozen_after_init
+from pants.util.strutil import strip_v2_chroot_path
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class LintResult(EngineAwareReturnType):
+    exit_code: int
+    stdout: str
+    stderr: str
+    partition_description: str | None = None
+    report: Digest = EMPTY_DIGEST
+
+    @classmethod
+    def from_fallible_process_result(
+        cls,
+        process_result: FallibleProcessResult,
+        *,
+        partition_description: str | None = None,
+        strip_chroot_path: bool = False,
+        report: Digest = EMPTY_DIGEST,
+    ) -> LintResult:
+        def prep_output(s: bytes) -> str:
+            return strip_v2_chroot_path(s) if strip_chroot_path else s.decode()
+
+        return cls(
+            exit_code=process_result.exit_code,
+            stdout=prep_output(process_result.stdout),
+            stderr=prep_output(process_result.stderr),
+            partition_description=partition_description,
+            report=report,
+        )
+
+    def metadata(self) -> dict[str, Any]:
+        return {"partition": self.partition_description}
+
+
+@frozen_after_init
+@dataclass(unsafe_hash=True)
+class LintResults(EngineAwareReturnType):
+    """Zero or more LintResult objects for a single linter.
+
+    Typically, linters will return one result. If they no-oped, they will return zero results.
+    However, some linters may need to partition their input and thus may need to return multiple
+    results. For example, many Python linters will need to group by interpreter compatibility.
+    """
+
+    results: tuple[LintResult, ...]
+    linter_name: str
+
+    def __init__(self, results: Iterable[LintResult], *, linter_name: str) -> None:
+        self.results = tuple(results)
+        self.linter_name = linter_name
+
+    @property
+    def skipped(self) -> bool:
+        return bool(self.results) is False
+
+    @memoized_property
+    def exit_code(self) -> int:
+        return next((result.exit_code for result in self.results if result.exit_code != 0), 0)
+
+    def level(self) -> LogLevel | None:
+        if self.skipped:
+            return LogLevel.DEBUG
+        return LogLevel.ERROR if self.exit_code != 0 else LogLevel.INFO
+
+    def message(self) -> str | None:
+        if self.skipped:
+            return f"{self.linter_name} skipped."
+        message = self.linter_name
+        message += (
+            " succeeded." if self.exit_code == 0 else f" failed (exit code {self.exit_code})."
+        )
+
+        def msg_for_result(result: LintResult) -> str:
+            msg = ""
+            if result.stdout:
+                msg += f"\n{result.stdout}"
+            if result.stderr:
+                msg += f"\n{result.stderr}"
+            if msg:
+                msg = f"{msg.rstrip()}\n\n"
+            return msg
+
+        if len(self.results) == 1:
+            results_msg = msg_for_result(self.results[0])
+        else:
+            results_msg = "\n"
+            for i, result in enumerate(self.results):
+                msg = f"Partition #{i + 1}"
+                msg += (
+                    f" - {result.partition_description}:" if result.partition_description else ":"
+                )
+                msg += msg_for_result(result) or "\n\n"
+                results_msg += msg
+        message += results_msg
+        return message
+
+    def cacheable(self) -> bool:
+        """Is marked uncacheable to ensure that it always renders."""
+        return False
+
+
+@union
+class LintTargetsRequest(StyleRequest):
+    """AThe entry point for linters that need targets."""
+
+
+@union
+@dataclass(frozen=True)
+class LintFilesRequest(EngineAwareParameter):
+    """The entry point for linters that do not use targets."""
+
+    name: ClassVar[str]
+
+    file_paths: tuple[str, ...]
+
+    def debug_hint(self) -> str:
+        return self.name
+
+
+# If a user wants linter reports to show up in dist/ they must ensure that the reports
+# are written under this directory. E.g.,
+# ./pants --flake8-args="--output-file=reports/report.txt" lint <target>
+REPORT_DIR = "reports"
+
+
+class LintSubsystem(GoalSubsystem):
+    name = "lint"
+    help = "Run all linters and/or formatters in check mode."
+
+    @classmethod
+    def activated(cls, union_membership: UnionMembership) -> bool:
+        return LintTargetsRequest in union_membership or LintFilesRequest in union_membership
+
+    only = StrListOption(
+        "--only",
+        help=only_option_help("lint", "linter", "flake8", "shellcheck"),
+    )
+    batch_size = IntOption(
+        "--batch-size",
+        advanced=True,
+        default=128,
+        help=style_batch_size_help(uppercase="Linter", lowercase="linter"),
+    )
+
+
+class Lint(Goal):
+    subsystem_cls = LintSubsystem
+
+
+@goal_rule
+async def lint(
+    console: Console,
+    workspace: Workspace,
+    targets: Targets,
+    specs_snapshot: SpecsSnapshot,
+    lint_subsystem: LintSubsystem,
+    union_membership: UnionMembership,
+    dist_dir: DistDir,
+) -> Lint:
+    target_request_types = cast(
+        "Iterable[type[LintTargetsRequest]]", union_membership[LintTargetsRequest]
+    )
+    file_request_types = union_membership[LintFilesRequest]
+    specified_names = determine_specified_tool_names(
+        "lint",
+        lint_subsystem.only,
+        target_request_types,
+        extra_valid_names={request.name for request in file_request_types},
+    )
+    target_requests = tuple(
+        request_type(
+            request_type.field_set_type.create(target)
+            for target in targets
+            if (
+                request_type.name in specified_names
+                and request_type.field_set_type.is_applicable(target)
+            )
+        )
+        for request_type in target_request_types
+    )
+    file_requests = (
+        tuple(
+            request_type(specs_snapshot.snapshot.files)
+            for request_type in file_request_types
+            if request_type.name in specified_names
+        )
+        if specs_snapshot.snapshot.files
+        else ()
+    )
+
+    def address_str(fs: FieldSet) -> str:
+        return fs.address.spec
+
+    all_requests = [
+        *(
+            Get(LintResults, LintTargetsRequest, request.__class__(field_set_batch))
+            for request in target_requests
+            if request.field_sets
+            for field_set_batch in partition_sequentially(
+                request.field_sets,
+                key=address_str,
+                size_target=lint_subsystem.batch_size,
+                size_max=4 * lint_subsystem.batch_size,
+            )
+        ),
+        *(Get(LintResults, LintFilesRequest, request) for request in file_requests),
+    ]
+    all_batch_results = cast(
+        "tuple[LintResults, ...]",
+        await MultiGet(all_requests),  # type: ignore[arg-type]
+    )
+
+    def key_fn(results: LintResults):
+        return results.linter_name
+
+    # NB: We must pre-sort the data for itertools.groupby() to work properly.
+    sorted_all_batch_results = sorted(all_batch_results, key=key_fn)
+    # We consolidate all results for each linter into a single `LintResults`.
+    all_results = tuple(
+        sorted(
+            (
+                LintResults(
+                    itertools.chain.from_iterable(
+                        batch_results.results for batch_results in all_linter_results
+                    ),
+                    linter_name=linter_name,
+                )
+                for linter_name, all_linter_results in itertools.groupby(
+                    sorted_all_batch_results, key=key_fn
+                )
+            ),
+            key=key_fn,
+        )
+    )
+
+    def get_name(res: LintResults) -> str:
+        return res.linter_name
+
+    write_reports(
+        all_results,
+        workspace,
+        dist_dir,
+        goal_name=LintSubsystem.name,
+        get_name=get_name,
+    )
+
+    exit_code = 0
+    if all_results:
+        console.print_stderr("")
+    for results in all_results:
+        if results.skipped:
+            continue
+        elif results.exit_code == 0:
+            sigil = console.sigil_succeeded()
+            status = "succeeded"
+        else:
+            sigil = console.sigil_failed()
+            status = "failed"
+            exit_code = results.exit_code
+        console.print_stderr(f"{sigil} {results.linter_name} {status}.")
+
+    return Lint(exit_code)
+
+
+def rules():
+    return collect_rules()
