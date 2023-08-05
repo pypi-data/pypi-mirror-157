13b747c161a76614e32fd0a990826a72bd8d6cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:19 +0000

    Add package RCML/1.1.2/RCML-1.1.2.tar.gz

---
+++ packages/RCML/RCML-1.1.2.tar.gz/RCML-1.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+	name="RCML",
+	version="1.1.2",
+	package_dir={'RCML':'src'},
+	packages=['RCML'],
+	install_requires=['pandas','numpy','scikit-learn','matplotlib','cvxpy'],
+	license=open('LICENSE').read(),
+	description="Minimal implementations of classical ML algorithms",
+	long_description=open("README.md").read(),
+	long_description_content_type='text/markdown',
+	author="Piyush Singh",
+	url="https://github.com/piyush01123/Revisiting-Classical-ML"
+)
