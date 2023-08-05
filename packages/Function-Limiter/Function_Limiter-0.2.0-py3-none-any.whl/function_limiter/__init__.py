914df66d7017c3fbe622fc5bf5a74c5008b6a0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:22:25 +0000

    Add package Function-Limiter/0.2.0/Function_Limiter-0.2.0-py3-none-any.whl

---
+++ packages/Function-Limiter/Function_Limiter-0.2.0-py3-none-any.whl/function_limiter/__init__.py
@@ -0,0 +1,10 @@
+"""Function-Limiter Extension for limiting callable functions."""
+
+__author__ = 'm.ghorbani2357@gmail.com'
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
+
+from .limiter import *
