b72e45752b754fd6e86890924a38c3c84a54b0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:21 +0000

    Add package dassana/0.6.13/dassana-0.6.13.tar.gz

---
+++ packages/dassana/dassana-0.6.13.tar.gz/dassana-0.6.13/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="dassana",
+    version="0.6.13",
+    description="Dassana common data ingestion utilities",
+    url="https://github.com/dassana-io/dassana-python",
+    author="Dassana",
+    author_email="support@dassana.io",
+    license="MIT",
+    packages=["dassana"],
+    install_requires=["certifi", "requests", "urllib3"],
+    zip_safe=False,
+)
