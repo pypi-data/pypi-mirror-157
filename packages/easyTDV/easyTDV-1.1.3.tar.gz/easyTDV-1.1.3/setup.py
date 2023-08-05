fa743d5e5700c78b8b79c8fa7438d6a865316238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package easyTDV/1.1.3/easyTDV-1.1.3.tar.gz

---
+++ packages/easyTDV/easyTDV-1.1.3.tar.gz/easyTDV-1.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages, glob
+
+
+VERSION = '1.1.3'
+DESCRIPTION = "easyTDV: Train, Deploy and Vizualisation"
+LONG_DESCRIPTION = "Framwork pour l'entrainement, deploiement et la visualisation à temps réel des modèles ML sur AWS"
+
+# Setting up
+setup(
+    name="easyTDV",
+    version=VERSION,
+    author="HADJALI Lounas",
+    author_email="<hadjalilounas@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['boto3', 'pandas'],
+    keywords=['python', 'ML', 'Deployment', 'Train', 'AWS'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_data={'': ['clf_deployment_stack.json', 'clf_train_stack.json', 'lambda_deployment.zip', 'lambda_train.zip']},
+    include_package_data=True
+)
