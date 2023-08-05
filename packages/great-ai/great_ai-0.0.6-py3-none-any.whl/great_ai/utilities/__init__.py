2502af6b276a17a2dbfac225c3150dbeabb7b0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:19 +0000

    Add package great-ai/0.0.6/great_ai-0.0.6-py3-none-any.whl

---
+++ packages/great-ai/great_ai-0.0.6-py3-none-any.whl/great_ai/utilities/__init__.py
@@ -0,0 +1,10 @@
+from .chunk import chunk
+from .clean import clean
+from .config_file import ConfigFile, ParseError
+from .evaluate_ranking import evaluate_ranking
+from .get_sentences import get_sentences
+from .language import english_name_of_language, is_english, predict_language
+from .logger import get_logger
+from .match_names import match_names
+from .parallel_map import parallel_map
+from .unique import unique
