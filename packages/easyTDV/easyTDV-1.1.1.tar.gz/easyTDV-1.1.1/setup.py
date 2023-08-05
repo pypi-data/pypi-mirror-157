25839fd3e8b2bb7ebb45befe165e6349cab2d24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:42 +0000

    Add package easyTDV/1.1.1/easyTDV-1.1.1.tar.gz

---
+++ packages/easyTDV/easyTDV-1.1.1.tar.gz/easyTDV-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages, glob
+
+
+VERSION = '1.1.1'
+DESCRIPTION = "easyTDV pour Train, Deploy ad Vizualisation"
+LONG_DESCRIPTION = "Framwork pour l'entrainement, deploiement et la visualisation à temps réel des modèles ML sur AWS"
+
+# Setting up
+setup(
+    name="easyTDV",
+    version=VERSION,
+    author="HADJALI Lounas",
+    author_email="<hadjalilounas@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['boto3', 'pandas'],
+    keywords=['python', 'ML', 'Deployment', 'Train', 'AWS'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    data_files=glob.glob('resources/**')
+)
