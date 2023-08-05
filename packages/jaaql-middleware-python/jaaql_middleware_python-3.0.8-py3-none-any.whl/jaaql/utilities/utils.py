8b413c1296e2620eda6197dda0f23b0f52da61a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:18 +0000

    Add package jaaql-middleware-python/3.0.8/jaaql_middleware_python-3.0.8-py3-none-any.whl

---
+++ packages/jaaql-middleware-python/jaaql_middleware_python-3.0.8-py3-none-any.whl/jaaql/utilities/utils.py
@@ -0,0 +1,70 @@
+import requests
+from os.path import join, exists, dirname
+import os
+import glob
+from jaaql.constants import DIR__config, FILE__config, CONFIG_KEY__server, CONFIG_KEY_SERVER__port, ENVIRON__install_path
+from jaaql.db.db_interface import DBInterface
+from jaaql.db.db_utils import create_interface
+from jaaql.constants import VAULT_KEY__jaaql_lookup_connection
+import configparser
+import time
+import urllib.parse
+from jaaql.utilities.utils_no_project_imports import time_delta_ms  # Do not delete, relied on by others
+
+PATH__migrations = "migrations"
+
+
+def get_jaaql_root() -> str:
+    return dirname(dirname(__file__))
+
+
+def load_config(is_gunicorn):
+    config_root = get_jaaql_root()
+    if is_gunicorn:
+        config_root = "/JAAQL-middleware-python/jaaql"
+
+    config = configparser.ConfigParser()
+    config.sections()
+    config_path = join(config_root, DIR__config, FILE__config)
+    if not exists(config_path):
+        raise Exception("Could not find config. Please check working directory has access to '" + config_path + "'")
+    config.read(config_path)
+    return {s: dict(config.items(s)) for s in config.sections()}
+
+
+def loc(data: [dict], key: str, val: str):
+    return [row for row in data if row[key] == val]
+
+
+def load_email_templates():
+    templates_locs = glob.glob("templates/*.html")
+    templates = {}
+    for template in templates_locs:
+        template = template.replace("\\", "/")
+        with open(template, "r") as f:
+            templates[".".join(template.split("templates/")[1].split(".")[:-1])] = f.read()
+    return templates
+
+
+def get_base_url(config, is_gunicorn: bool):
+    if is_gunicorn:
+        return "http+unix://" + urllib.parse.quote(os.environ.get(ENVIRON__install_path) + "/jaaql.sock", safe='')
+    else:
+        return "http://127.0.0.1:" + str(int(config[CONFIG_KEY__server][CONFIG_KEY_SERVER__port]))
+
+
+def await_jaaql_installation(config, is_gunicorn: bool):
+    base_url = get_base_url(config, is_gunicorn)
+    while True:
+        try:
+            if requests.get(base_url + "/internal/is_installed").status_code == 200:
+                break
+        except:
+            pass
+        time.sleep(5)
+
+
+def get_jaaql_connection(config, vault):
+    jaaql_uri = vault.get_obj(VAULT_KEY__jaaql_lookup_connection)
+    address, port, db, username, password = DBInterface.fracture_uri(jaaql_uri)
+    return create_interface(config, address, port, db, username, password, is_jaaql_user=True)
