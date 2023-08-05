3bd876a6cfb3474be5b810ada3ed5f415e36aef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:45 +0000

    Add package blackboxopt/4.4.0/blackboxopt-4.4.0.tar.gz

---
+++ packages/blackboxopt/blackboxopt-4.4.0.tar.gz/blackboxopt-4.4.0/blackboxopt/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "1.0.0"
+
+from parameterspace.base import SearchSpace
+
+from .base import (
+    ConstraintsError,
+    ContextError,
+    EvaluationsError,
+    Objective,
+    ObjectivesError,
+    OptimizationComplete,
+    Optimizer,
+    OptimizerNotReady,
+)
+from .evaluation import Evaluation, EvaluationSpecification
