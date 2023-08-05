39ce0e499f5b5c5b423ca677c3244fca1fa5eaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package logbox/1.0.3/logbox-1.0.3-cp39-cp39-win_amd64.whl

---
+++ packages/logbox/logbox-1.0.3-cp39-cp39-win_amd64.whl/logbox/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2021 Matthias Nadig
+
+from ._api import info, warn
+from ._api import print_time, print_time_since_start
+from ._api import register
+from ._api import log_range, log_iter, log_enumerate, log_zip
+
+__version__ = '1.0.3'
+__author__ = 'Matthias Nadig'
