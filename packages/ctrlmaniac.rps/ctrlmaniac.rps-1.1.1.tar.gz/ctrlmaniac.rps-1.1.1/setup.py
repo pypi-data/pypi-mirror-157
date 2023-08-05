191b5666b424bf40003d83b9f8012d9421fbb8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:35 +0000

    Add package ctrlmaniac.rps/1.1.1/ctrlmaniac.rps-1.1.1.tar.gz

---
+++ packages/ctrlmaniac.rps/ctrlmaniac.rps-1.1.1.tar.gz/ctrlmaniac.rps-1.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ctrlmaniac', 'ctrlmaniac.rps']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['ctrlmaniac.rps = ctrlmaniac.rps.main:play']}
+
+setup_kwargs = {
+    'name': 'ctrlmaniac.rps',
+    'version': '1.1.1',
+    'description': 'The famous Rock Paper Scissors game written in python',
+    'long_description': None,
+    'author': 'Davide Di Criscito',
+    'author_email': 'davide.dicriscito@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
