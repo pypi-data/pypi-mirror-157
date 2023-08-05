70e2a68d4eca6890aab498ae55d91615bf50d259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package djeu/0.1.27/djeu-0.1.27-py2.py3-none-any.whl

---
+++ packages/djeu/djeu-0.1.27-py2.py3-none-any.whl/djeu/forms/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+import re
+
+from django import forms
+
+from django import VERSION
+if VERSION >= (2, 0):
+    from django.utils.translation import gettext_lazy as _
+else:
+    from django.utils.translation import ugettext_lazy as _
+
+from ..conf import settings
+# from ..models import Subject
+
+from django.core.exceptions import ImproperlyConfigured
+
+# try:
+#    import bleach
+# except ImportError:
+#    raise ImproperlyConfigured('django-crispy-contact-form application requires bleach package')
+
+from .julian_date_field import *
