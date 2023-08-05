aa702145e30cc4baba2815a1365cd97292af94cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:53 +0000

    Add package nob/0.8.0/nob-0.8.0-py2.py3-none-any.whl

---
+++ packages/nob/nob-0.8.0-py2.py3-none-any.whl/nob/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.8.0"
+
+from .nob import Nob, NobView  # noqa
+from .nob import Tree, TreeView  # noqa Deprecated
+from .path import Path  # noqa
+from .tools import allclose, diff  # noqa
