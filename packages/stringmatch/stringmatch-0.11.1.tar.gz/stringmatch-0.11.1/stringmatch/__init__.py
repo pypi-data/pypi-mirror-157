f4cba67e88c6146120e358d16fc205caaee484e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:26 +0000

    Add package stringmatch/0.11.1/stringmatch-0.11.1.tar.gz

---
+++ packages/stringmatch/stringmatch-0.11.1.tar.gz/stringmatch-0.11.1/stringmatch/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from .args import *
+from .distance import *
+from .match import *
+from .ratio import *
+from .scorer import *
+from .strings import *
+
+__title__ = "stringmatch"
+__version__ = "0.11.1"
