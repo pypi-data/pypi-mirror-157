08cdd82f6899a9dd299bdc4a3e72e9d00d32445c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:09 +0000

    Add package edc-reportable/0.3.18/edc_reportable-0.3.18-py3-none-any.whl

---
+++ packages/edc-reportable/edc_reportable-0.3.18-py3-none-any.whl/edc_reportable/calculators/egfr/percent_change.py
@@ -0,0 +1,5 @@
+def egfr_percent_change(egfr_value: float, baseline_egfr_value: float) -> float:
+    """Returns the percent change from baseline"""
+    if egfr_value and baseline_egfr_value:
+        return (float(baseline_egfr_value) - float(egfr_value)) / float(baseline_egfr_value)
+    return 0.0
