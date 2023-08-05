36f3785c21c013d4b9c7eae3a7e9c590d9947c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package bertdotbill/2.3.1/bertdotbill-2.3.1.tar.gz

---
+++ packages/bertdotbill/bertdotbill-2.3.1.tar.gz/bertdotbill-2.3.1/bertdotbill/topics.py
@@ -0,0 +1,21 @@
+import base64
+from bertdotbill.logger import Logger
+
+logger = Logger().init_logger(__name__)
+
+class Topics():
+
+  def __init__(self, **kwargs):
+    self.settings = kwargs['settings']
+    self.args = kwargs['args']
+
+  def get(self):
+
+    logger.info('Retrieving available topics')
+    topics = self.settings.get('topics', [])
+    if isinstance(topics, dict):
+      logger.info('Successfully retrieved available topics')
+      return {'topics': topics}
+    else:
+      logger.error("Improperly structured 'topics' config block, seek --help")
+      return {'topics': {}}
