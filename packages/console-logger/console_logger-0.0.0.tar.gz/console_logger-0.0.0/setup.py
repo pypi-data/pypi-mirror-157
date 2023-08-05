fabb82bebd3e184d5d75a113d5a32e9080e29b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:13 +0000

    Add package console-logger/0.0.0/console_logger-0.0.0.tar.gz

---
+++ packages/console-logger/console_logger-0.0.0.tar.gz/console_logger-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='console_logger',
+    version='0.0.0',
+    packages=['console_logger'],
+    url='',
+    license='MIT License',
+    author='Mikhail Buchnev',
+    author_email='me@buchnev.page',
+    description='Yet another logging package for Python'
+)
