cb5bf50c1187d7fa4b76f71f33345aff342818a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package webviz-subsurface-components/0.4.14a0/webviz_subsurface_components-0.4.14a0.tar.gz

---
+++ packages/webviz-subsurface-components/webviz_subsurface_components-0.4.14a0.tar.gz/webviz_subsurface_components-0.4.14a0/react/src/lib/components/DeckGLMap/layers/layersDefaultProps.ts
@@ -0,0 +1,159 @@
+export const layersDefaultProps: Record<string, unknown> = {
+    ColormapLayer: {
+        "@@type": "ColormapLayer",
+        name: "Property map",
+        id: "colormap-layer",
+        pickable: true,
+        visible: true,
+        valueRange: { type: "array", value: [0, 1] },
+        colorMapRange: { type: "array" },
+        valueDecoder: {
+            rgbScaler: [1, 1, 1],
+            // By default, scale the [0, 256*256*256-1] decoded values to [0, 1]
+            floatScaler: 1.0 / (256.0 * 256.0 * 256.0 - 1.0),
+            offset: 0,
+            step: 0,
+        },
+        rotDeg: 0,
+        colorMapName: "Rainbow",
+    },
+    Hillshading2DLayer: {
+        "@@type": "Hillshading2DLayer",
+        name: "Hill shading",
+        id: "hillshading-layer",
+        opacity: 1.0,
+        pickable: true,
+        visible: true,
+        rotDeg: 0,
+        valueRange: { type: "array", value: [0, 1] },
+        lightDirection: [1, 1, 1],
+        ambientLightIntensity: 0.5,
+        diffuseLightIntensity: 0.5,
+        valueDecoder: {
+            rgbScaler: [1, 1, 1],
+            // By default, scale the [0, 256*256*256-1] decoded values to [0, 1]
+            floatScaler: 1.0 / (256.0 * 256.0 * 256.0 - 1.0),
+            offset: 0,
+            step: 0,
+        },
+    },
+    MapLayer: {
+        "@@type": "MapLayer",
+        name: "Map",
+        id: "map3d-layer-float32",
+        pickable: true,
+        visible: true,
+        // Url to png image for height field.
+        mesh: "",
+        // Mesh error in meters. The output mesh is in higher resolution (more vertices) if the error is smaller.
+        meshMaxError: { type: "number", value: 5 },
+        // Url to png image for map properties. (ex, poro or perm values as a texture)
+        propertyTexture: "",
+        // Bounding box of the terrain image, [minX, minY, maxX, maxY] in world coordinates
+        bounds: { type: "array", value: null, false: true, compare: true },
+        colorMapRange: { type: "array" },
+        rotDeg: 0,
+        contours: [-1.0, -1.0],
+        // readout is default property value but if set to true it will be depth/z-value.
+        isReadoutDepth: true,
+        enableSmoothShading: true,
+    },
+    Map3DLayer: {
+        "@@type": "Map3DLayer",
+        name: "Map 3D",
+        id: "map3d-layer",
+        pickable: true,
+        visible: true,
+        // Url to png image for height field.
+        mesh: "",
+        meshValueRange: { type: "array", value: [0, 1] },
+        // Mesh error in meters. The output mesh is in higher resolution (more vertices) if the error is smaller.
+        meshMaxError: { type: "number", value: 5 },
+        // Url to png image for map properties. (ex, poro or perm values as a texture)
+        propertyTexture: "",
+        // Bounding box of the terrain image, [minX, minY, maxX, maxY] in world coordinates
+        bounds: { type: "array", value: null, false: true, compare: true },
+        propertyValueRange: { type: "array", value: [0, 1] },
+        rotDeg: 0,
+        contours: [-1.0, -1.0],
+        // If contour lines should follow depth or properties.
+        isContoursDepth: true,
+        // readout is default property value but if set to true it will be depth/z-value.
+        isReadoutDepth: false,
+        enableSmoothShading: true,
+        material: true,
+    },
+    GridLayer: {
+        "@@type": "GridLayer",
+        id: "grid-layer",
+        name: "Grid",
+        pickable: true,
+        valueRange: { type: "array", value: [0, 1] },
+        colorMapRange: { type: "array", value: [0, 1] },
+        material: true,
+    },
+    WellsLayer: {
+        "@@type": "WellsLayer",
+        name: "Wells",
+        id: "wells-layer",
+        autoHighlight: true,
+        opacity: 1,
+        lineWidthScale: 1,
+        pointRadiusScale: 8,
+        style: { dash: false },
+        outline: true,
+        logRadius: 10,
+        logCurves: true,
+        refine: true,
+        visible: true,
+        wellNameVisible: false,
+        wellNameAtTop: false,
+        wellNameSize: 14,
+        wellNameColor: [0, 0, 0, 255],
+        selectedWell: "@@#editedData.selectedWells", // used to get data from deckgl layer
+    },
+    FaultPolygonsLayer: {
+        "@@type": "FaultPolygonsLayer",
+        name: "Fault polygons",
+        id: "fault-polygons-layer",
+        pickable: true,
+        visible: true,
+        filled: true,
+        lineWidthMinPixels: 2,
+    },
+    PieChartLayer: {
+        "@@type": "PieChartLayer",
+        name: "Pie chart",
+        id: "pie-layer",
+        pickable: true,
+        visible: true,
+        selectedPie: "@@editedData.selectedPie", // used to get data from deckgl layer
+    },
+    AxesLayer: {
+        "@@type": "AxesLayer",
+        name: "Axes",
+        id: "axes-layer",
+        visible: true,
+    },
+    NorthArrow3DLayer: {
+        "@@type": "NorthArrow3DLayer",
+        name: "NorthArrow3D",
+        id: "north-arrow-layer",
+        visible: true,
+    },
+    DrawingLayer: {
+        "@@type": "DrawingLayer",
+        name: "Drawing",
+        id: "drawing-layer",
+        pickable: true,
+        visible: true,
+        mode: "drawLineString",
+
+        // Props used to get/set data in the drawing layer.
+        selectedFeatureIndexes: [] as number[],
+        data: {
+            type: "FeatureCollection",
+            features: [],
+        },
+    },
+};
---
+++ packages/webviz-subsurface-components/webviz_subsurface_components-0.4.14a0.tar.gz/webviz_subsurface_components-0.4.14a0/react/src/lib/components/DeckGLMap/storybook/components/colorLegends/ContinuousLegend.stories.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ContinuousLegend } from "@emerson-eps/color-tables";
+export default {
+    component: ContinuousLegend,
+    title: "DeckGLMap/Components/ColorLegends/ContinuousLegend",
+};
+import colorTables from "@emerson-eps/color-tables/dist/component/color-tables.json";
+
+const min = 0;
+const max = 0.35;
+const dataObjectName = "Wells / PORO";
+const position = [16, 10];
+const name = "PORO";
+const horizontal = false;
+const colorName = "Rainbow";
+
+const Template = (args) => {
+    return <ContinuousLegend {...args} />;
+};
+
+export const ContinuousTemplate = Template.bind({});
+ContinuousTemplate.args = {
+    min,
+    max,
+    dataObjectName,
+    position,
+    name,
+    colorName,
+    colorTables,
+    horizontal,
+};
