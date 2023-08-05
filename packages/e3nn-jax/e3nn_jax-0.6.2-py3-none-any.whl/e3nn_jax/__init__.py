352a0c61bb1a892f705cd1bf940a226608396ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:19 +0000

    Add package e3nn-jax/0.6.2/e3nn_jax-0.6.2-py3-none-any.whl

---
+++ packages/e3nn-jax/e3nn_jax-0.6.2-py3-none-any.whl/e3nn_jax/__init__.py
@@ -0,0 +1,118 @@
+__version__ = "0.6.2"
+
+from ._graph_util import index_add, radius_graph
+from ._rotation import (
+    rand_matrix,
+    identity_angles,
+    rand_angles,
+    compose_angles,
+    inverse_angles,
+    identity_quaternion,
+    rand_quaternion,
+    compose_quaternion,
+    inverse_quaternion,
+    rand_axis_angle,
+    compose_axis_angle,
+    matrix_x,
+    matrix_y,
+    matrix_z,
+    angles_to_matrix,
+    matrix_to_angles,
+    angles_to_quaternion,
+    matrix_to_quaternion,
+    axis_angle_to_quaternion,
+    quaternion_to_axis_angle,
+    matrix_to_axis_angle,
+    angles_to_axis_angle,
+    axis_angle_to_matrix,
+    quaternion_to_matrix,
+    quaternion_to_angles,
+    axis_angle_to_angles,
+    angles_to_xyz,
+    xyz_to_angles,
+)
+from ._su2 import su2_clebsch_gordan, su2_generators
+from ._so3 import clebsch_gordan, wigner_D, generators
+from ._instruction import Instruction
+from ._irreps import Irrep, MulIrrep, Irreps, IrrepsData
+from ._spherical_harmonics import spherical_harmonics, sh, set_default_spherical_harmonics_algorithm, legendre
+from ._soft_one_hot_linspace import sus, soft_one_hot_linspace
+from ._linear import FunctionalLinear, Linear
+from ._core_tensor_product import FunctionalTensorProduct
+from ._tensor_products import (
+    FunctionalFullyConnectedTensorProduct,
+    FullyConnectedTensorProduct,
+    full_tensor_product,
+    elementwise_tensor_product,
+    FunctionalTensorSquare,
+    TensorSquare,
+)
+from ._activation import scalar_activation, normalize_function
+from ._gate import gate
+from ._batchnorm import BatchNorm
+from ._dropout import Dropout
+from ._nn import MultiLayerPerceptron
+
+__all__ = [
+    "index_add",
+    "radius_graph",
+    "rand_matrix",
+    "identity_angles",
+    "rand_angles",
+    "compose_angles",
+    "inverse_angles",
+    "Instruction",
+    "identity_quaternion",
+    "rand_quaternion",
+    "compose_quaternion",
+    "inverse_quaternion",
+    "rand_axis_angle",
+    "compose_axis_angle",
+    "matrix_x",
+    "matrix_y",
+    "matrix_z",
+    "angles_to_matrix",
+    "matrix_to_angles",
+    "angles_to_quaternion",
+    "matrix_to_quaternion",
+    "axis_angle_to_quaternion",
+    "quaternion_to_axis_angle",
+    "matrix_to_axis_angle",
+    "angles_to_axis_angle",
+    "axis_angle_to_matrix",
+    "quaternion_to_matrix",
+    "quaternion_to_angles",
+    "axis_angle_to_angles",
+    "angles_to_xyz",
+    "xyz_to_angles",
+    "su2_clebsch_gordan",
+    "su2_generators",
+    "clebsch_gordan",
+    "wigner_D",
+    "generators",
+    "Irrep",
+    "MulIrrep",
+    "Irreps",
+    "IrrepsData",
+    "spherical_harmonics",
+    "sh",
+    "set_default_spherical_harmonics_algorithm",
+    "legendre",
+    "sus",
+    "soft_one_hot_linspace",
+    "FunctionalLinear",
+    "Linear",
+    "FunctionalTensorProduct",
+    "FunctionalFullyConnectedTensorProduct",
+    "FullyConnectedTensorProduct",
+    "full_tensor_product",
+    "elementwise_tensor_product",
+    "FunctionalTensorSquare",
+    "TensorSquare",
+    "scalar_activation",
+    "normalize_function",
+    "gate",
+    "BatchNorm",
+    "Dropout",
+    "MultiLayerPerceptron",
+]
---
+++ packages/e3nn-jax/e3nn_jax-0.6.2-py3-none-any.whl/e3nn_jax/_activation.py
@@ -0,0 +1,115 @@
+from typing import Callable, List, Optional
+
+import jax
+import jax.numpy as jnp
+
+from e3nn_jax import Irreps, IrrepsData
+from e3nn_jax.util.decorators import overload_for_irreps_without_data
+
+
+def normalize_function(phi):
+    with jax.ensure_compile_time_eval():
+        k = jax.random.PRNGKey(0)
+        x = jax.random.normal(k, (1_000_000,))
+        c = jnp.mean(phi(x) ** 2) ** 0.5
+
+        if jnp.allclose(c, 1.0):
+            return phi
+        else:
+
+            def rho(x):
+                return phi(x) / c
+
+            return rho
+
+
+def parity_function(phi):
+    with jax.ensure_compile_time_eval():
+        x = jnp.linspace(0.0, 10.0, 256)
+
+        a1, a2 = phi(x), phi(-x)
+        if jnp.max(jnp.abs(a1 - a2)) < 1e-5:
+            return 1
+        elif jnp.max(jnp.abs(a1 + a2)) < 1e-5:
+            return -1
+        else:
+            return 0
+
+
+def is_zero_in_zero(phi):
+    with jax.ensure_compile_time_eval():
+        return jnp.allclose(phi(jnp.array(0.0)), 0.0)
+
+
+@overload_for_irreps_without_data(irrepsdata_argnums=[0])
+def scalar_activation(input: IrrepsData, acts: List[Optional[Callable[[float], float]]]) -> IrrepsData:
+    assert isinstance(input, IrrepsData)
+
+    assert len(input.irreps) == len(acts), (input.irreps, acts)
+
+    list = []
+
+    irreps_out = []
+    for (mul, (l_in, p_in)), x, act in zip(input.irreps, input.list, acts):
+        if act is not None:
+            if l_in != 0:
+                raise ValueError(
+                    f"Activation: cannot apply an activation function to a non-scalar input. {input.irreps} {acts}"
+                )
+
+            act = normalize_function(act)
+
+            p_out = parity_function(act) if p_in == -1 else p_in
+            if p_out == 0:
+                raise ValueError(
+                    "Activation: the parity is violated! The input scalar is odd but the activation is neither even nor odd."
+                )
+
+            irreps_out.append((mul, (0, p_out)))
+            if x is None:
+                if is_zero_in_zero(act):
+                    list.append(None)
+                else:
+                    list.append(act(jnp.ones(input.shape + (mul, 1))))
+            else:
+                list.append(act(x))
+        else:
+            irreps_out.append((mul, (l_in, p_in)))
+            list.append(x)
+
+    irreps_out = Irreps(irreps_out)
+
+    if acts and acts.count(acts[0]) == len(acts):
+        # for performance, if all the activation functions are the same, we can apply it to the contiguous array as well
+        contiguous = input.contiguous if acts[0] is None else normalize_function(acts[0])(input.contiguous)
+        return IrrepsData(irreps=irreps_out, contiguous=contiguous, list=list)
+
+    return IrrepsData.from_list(irreps_out, list, input.shape)
+
+
+# TODO remove this class and follow the same pattern as scalar_activation
+class KeyValueActivation:
+    irreps_key: Irreps
+    irreps_value: Irreps
+    irreps_out: Irreps
+
+    def __init__(self, irreps_key, irreps_value, phi):
+        self.irreps_key = Irreps(irreps_key)
+        self.irreps_value = Irreps(irreps_value)
+
+        # TODO compute irreps_out
+        # irreps_out =
+
+    def __call__(self, keys, values):
+        return [jax.vmap(key_value_activation)(self.phi, k, v) for k, v in zip(keys, values)]
+
+
+def key_value_activation(phi, key, value):
+    assert key.ndim == 1
+    assert value.ndim == 1
+
+    d = value.shape[0]
+    key = key / jnp.sqrt(1 / 16 + jnp.sum(key**2))  # 1/16 is arbitrary small... but not too small...
+    scalar = jnp.sum(key * value)
+    scalar = normalize_function(phi)(scalar)
+    return d**0.5 * scalar * key  # component normalized
