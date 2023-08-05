cf6d99781c902512be65e3ea96d63c7494410334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:37 +0000

    Add package climail/1.9.1/CLImail-1.9.1.tar.gz

---
+++ packages/climail/CLImail-1.9.1.tar.gz/CLImail-1.9.1/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='CLImail',
+    version='1.9.1',
+    license='MIT',
+    author="HRLO77",
+    author_email='shakebmohammad.10@gmail.com',
+    packages=['CLImail'],
+    url='https://github.com/HRLO77/CLImail',
+    description='A Command Line Interface email client written in python.',
+    long_description="A Command Line Interface email client written in python."
+)
