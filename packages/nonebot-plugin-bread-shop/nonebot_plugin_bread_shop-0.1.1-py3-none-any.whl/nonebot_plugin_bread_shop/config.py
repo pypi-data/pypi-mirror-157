e55448723ee5abd74fc92229911996ecfef04281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:09 +0000

    Add package nonebot-plugin-bread-shop/0.1.1/nonebot_plugin_bread_shop-0.1.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-bread-shop/nonebot_plugin_bread_shop-0.1.1-py3-none-any.whl/nonebot_plugin_bread_shop/config.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from enum import Enum
+
+# 被禁止的群聊
+BANNED_GROUPS = []
+
+
+class CD(Enum):
+    """操作冷却（单位：秒）"""
+    BUY = 4200
+    EAT = 4800
+    ROB = 7000
+    GIVE = 3000
+    BET = 5400
+
+
+class MAX(Enum):
+    """操作随机值上限"""
+    BUY = 10
+    EAT = 7
+    ROB = 7
+    GIVE = 10
+    BET = 10
+
+
+class MIN(Enum):
+    """操作随机值下限"""
+    BUY = 1
+    EAT = 1
+    ROB = 1
+    GIVE = 1
+    BET = 5
