50b5c219a2b0177481f9ada40b88ee2504111e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:24:13 +0000

    Add package bf-kion/1.0.7/bf_kion-1.0.7.tar.gz

---
+++ packages/bf-kion/bf_kion-1.0.7.tar.gz/bf_kion-1.0.7/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'bf_kion',        
+  packages = ['bf_kion'],
+  version = '1.0.7',      
+  license='MIT',        
+  description = 'template update',   
+  author = 'kion',                   
+  author_email = 'yufan2hao@sina.com',     
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.9',
+  ],
+)
