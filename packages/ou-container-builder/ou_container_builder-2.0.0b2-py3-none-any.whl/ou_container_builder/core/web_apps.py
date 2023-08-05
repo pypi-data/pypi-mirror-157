afc94de2ed59b6abf9972f94c4aca2f86c50ec50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:25 +0000

    Add package ou-container-builder/2.0.0b2/ou_container_builder-2.0.0b2-py3-none-any.whl

---
+++ packages/ou-container-builder/ou_container_builder-2.0.0b2-py3-none-any.whl/ou_container_builder/core/web_apps.py
@@ -0,0 +1,28 @@
+"""Pack to handle web_apps setups."""
+from jinja2 import Environment
+
+from ..utils import merge_settings
+
+
+def apply_core(context: str, env: Environment, settings: dict) -> dict:
+    """Apply the web_apps core.
+
+    This simply sets the server's default_path, if a web application is set as the default.
+
+    :param context: The context path within which the generation is running
+    :type context: str
+    :param env: The Jinja2 environment to use for loading and rendering templates
+    :type env: :class:`~jinja2.environment.Environment`
+    :param settings: The settings parsed from the configuration file
+    :type settings: dict
+    :return: The updated settings
+    :rtype: dict
+    """
+    for web_app in settings['web_apps']:
+        if 'default' in web_app:
+            settings = merge_settings(settings, {
+                'server': {
+                    'default_path': web_app['path']
+                }
+            })
+    return settings
