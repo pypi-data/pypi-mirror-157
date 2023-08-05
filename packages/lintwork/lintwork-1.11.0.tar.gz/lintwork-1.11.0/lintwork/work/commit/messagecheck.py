bacbd1b1601ea7f2104efd90448fc80f622428c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package lintwork/1.11.0/lintwork-1.11.0.tar.gz

---
+++ packages/lintwork/lintwork-1.11.0.tar.gz/lintwork-1.11.0/lintwork/work/commit/messagecheck.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+from lintwork.work.abstract import WorkAbstract
+from lintwork.proto.proto import File, Format
+
+LINT_LEN_MIN = 4
+LINT_SEP = ":"
+
+
+class MessagecheckException(Exception):
+    def __init__(self, info):
+        super().__init__(self)
+        self._info = info
+
+    def __str__(self):
+        return self._info
+
+
+class Messagecheck(WorkAbstract):
+    def __init__(self, config):
+        if config is None:
+            config = []
+        super().__init__(config)
+
+    def _execution(self, project):
+        return self._lint(project)
+
+    def _parse(self, data):
+        buf = []
+        for item in data.splitlines():
+            b = item.strip().split(LINT_SEP)
+            if len(b) < LINT_LEN_MIN:
+                continue
+            buf.append(
+                {
+                    Format.FILE: b[0].strip(),
+                    Format.LINE: int(b[1].strip()),
+                    Format.TYPE: b[2],
+                    Format.DETAILS: " ".join(b[3:]).strip(),
+                }
+            )
+        return buf
+
+    def _popen(self, cmd, stdin=None):
+        return subprocess.Popen(
+            cmd, stdin=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+
+    def _lint(self, project):
+        cmd = ["messagecheck"]
+        cmd.extend(self._config)
+        cmd.extend(["-m", os.path.join(project, File.MESSAGE)])
+        with self._popen(cmd) as proc:
+            out, err = proc.communicate()
+            if proc.returncode != 0:
+                return []
+        return self._parse(
+            out.strip().decode("utf-8").replace(project + os.path.sep, "")
+        )
