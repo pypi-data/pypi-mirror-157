284702728bc5ffee1ebfd881b838ba17d9c1a5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:59 +0000

    Add package fmrib-unpack/3.3.1/fmrib-unpack-3.3.1.tar.gz

---
+++ packages/fmrib-unpack/fmrib-unpack-3.3.1.tar.gz/fmrib-unpack-3.3.1/funpack/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# __init__.py - funpack package
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+
+__version__ = '3.3.1'
+"""The ``funpack`` versioning scheme roughly follows Semantic Versioning
+conventions.
+"""
+
+
+from .util      import (findConfigDir,  # noqa
+                        findConfigDirs)
+from .custom    import (loader,         # noqa
+                        sniffer,
+                        formatter,
+                        exporter,
+                        processor,
+                        metaproc,
+                        cleaner)
+from .datatable import (DataTable,      # noqa
+                        Column)
