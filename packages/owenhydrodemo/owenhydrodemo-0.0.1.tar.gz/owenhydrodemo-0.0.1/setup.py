94da49876c59c8a76c6f2ffc1405f7d7e5a733d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:34 +0000

    Add package owenhydrodemo/0.0.1/owenhydrodemo-0.0.1.tar.gz

---
+++ packages/owenhydrodemo/owenhydrodemo-0.0.1.tar.gz/owenhydrodemo-0.0.1/setup.py
@@ -0,0 +1,32 @@
+"""
+Author: Wenyu Ouyang
+Date: 2022-06-28 21:29:43
+LastEditTime: 2022-06-29 14:01:38
+LastEditors: Wenyu Ouyang
+Description: setup file
+FilePath: \hydrodemo\setup.py
+Copyright (c) 2021-2022 Wenyu Ouyang. All rights reserved.
+"""
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "owenhydrodemo", # 输入项目名称
+    version = "0.0.1", # 输入版本号
+    keywords = ["test"], # 输入关键词
+    description = "hydro test", # 输入概述
+    long_description = "a test repo for pypi packaging", # 输入描述
+
+    url = "https://github.com/OuyangWenyu/hydro-test", # 输入项目Github仓库的链接
+    author = "Wenyu Ouyang", # 输入作者名字
+    author_email = "wenyuouyang@outlook.com", # 输入作者邮箱
+    license= "MIT_license", # 此为声明文件，一般填写 MIT_license
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy"], # 输入项目所用的包
+    python_requires='>=3.9', # Python版本要求
+)
