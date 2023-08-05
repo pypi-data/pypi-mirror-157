3d7a173a96aa86123900f2bc0ca6896ab52c5f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:25 +0000

    Add package PyStorCLI2/0.5.0/PyStorCLI2-0.5.0.tar.gz

---
+++ packages/PyStorCLI2/PyStorCLI2-0.5.0.tar.gz/PyStorCLI2-0.5.0/pystorcli/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018, Martin Dojcak <martin@dojcak.sk>
+# Copyright (c) 2022, Rafael Leira & Naudit HPCN S.L. <rafael.leira@naudit.es>
+# See LICENSE for details.
+
+'''StorCLI python module
+'''
+
+from .version import __version__
+from .storcli import StorCLI
+from .controller import Controller, Controllers
