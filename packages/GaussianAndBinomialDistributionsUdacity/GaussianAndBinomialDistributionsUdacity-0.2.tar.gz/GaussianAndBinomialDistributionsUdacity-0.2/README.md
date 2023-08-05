9ae35ccab0c2588899256fe87c4286896dd607f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:02 +0000

    Add package GaussianAndBinomialDistributionsUdacity/0.2/GaussianAndBinomialDistributionsUdacity-0.2.tar.gz

---
+++ packages/GaussianAndBinomialDistributionsUdacity/GaussianAndBinomialDistributionsUdacity-0.2.tar.gz/GaussianAndBinomialDistributionsUdacity-0.2/README.md
@@ -0,0 +1,11 @@
+# Distributions package
+
+This package contains various functions for Gaussian and Binomial distributions along with a generic Distributions class.
+
+# Files
+
+Description of the files
+
+# Installation 
+
+How to install
---
+++ packages/GaussianAndBinomialDistributionsUdacity/GaussianAndBinomialDistributionsUdacity-0.2.tar.gz/GaussianAndBinomialDistributionsUdacity-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GaussianAndBinomialDistributionsUdacity',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['GaussianAndBinomialDistributionsUdacity'],
+      zip_safe=False)
