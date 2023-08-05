9c45cc97db8e5f870150c7e99d673c5530c62ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:23 +0000

    Add package busylight-for-humans/0.20.3/busylight_for_humans-0.20.3-py3-none-any.whl

---
+++ packages/busylight-for-humans/busylight_for_humans-0.20.3-py3-none-any.whl/busylight/effects/gradient.py
@@ -0,0 +1,43 @@
+"""Color Gradient Effect
+"""
+
+from itertools import cycle
+
+from ..color import ColorList, ColorTuple
+
+from .effect import BaseEffect
+
+
+class Gradient(BaseEffect):
+    def __init__(
+        self,
+        color: ColorTuple,
+        duty_cycle: float,
+        step: int = 1,
+    ) -> None:
+        self.color = color
+        self.duty_cycle = duty_cycle
+        # XXX need to choose steps that make sense for scaled colors
+        #     where the max(color) << 255
+        self.step = max(0, min(step, 255))
+
+    @property
+    def colors(self) -> ColorList:
+        try:
+            return self._colors
+        except AttributeError:
+            pass
+
+        red, green, blue = self.color
+
+        colors = []
+        for i in range(1, 256, self.step):
+            scale = i / 255
+            r = round(scale * red)
+            g = round(scale * green)
+            b = round(scale * blue)
+            colors.append((r, g, b))
+
+        self._colors = colors + list(reversed(colors[:-1]))
+
+        return self._colors
