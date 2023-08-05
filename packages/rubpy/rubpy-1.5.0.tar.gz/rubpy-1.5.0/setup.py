0fe456d6ca000a1d5819891a069b2280543d192f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:03 +0000

    Add package rubpy/1.5.0/rubpy-1.5.0.tar.gz

---
+++ packages/rubpy/rubpy-1.5.0.tar.gz/rubpy-1.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pycryptodome","pillow",'mutagen']
+
+setup(
+    name="rubpy",
+    version="1.5.0",
+    author="Shayan Heidari",
+    author_email="snipe4kill.tg@gmail.com",
+    description="This is an unofficial library for deploying robots on Rubika accounts.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/snipe4kill/rubika/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
