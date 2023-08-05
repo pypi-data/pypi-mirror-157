a4d3d44398668a3714ffd939208a7a9304ebf252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:15 +0000

    Add package databutton-web/0.15.7/databutton_web-0.15.7.tar.gz

---
+++ packages/databutton-web/databutton_web-0.15.7.tar.gz/databutton_web-0.15.7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['databutton_web']
+
+package_data = \
+{'': ['*'], 'databutton_web': ['local/*', 'local/assets/*']}
+
+setup_kwargs = {
+    'name': 'databutton-web',
+    'version': '0.15.7',
+    'description': '',
+    'long_description': None,
+    'author': 'Databutton',
+    'author_email': 'hi@databutton.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
