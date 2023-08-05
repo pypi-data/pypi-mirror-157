e34acbd24f411fe11da2e6bcc4de6545950e6153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package ddt-kion/1.5.1/ddt_kion-1.5.1.tar.gz

---
+++ packages/ddt-kion/ddt_kion-1.5.1.tar.gz/ddt_kion-1.5.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'ddt_kion',         
+  packages = ['ddt_kion'], 
+  version = '1.5.1',      
+  license='MIT',        
+  description = 'ddt desc',   
+  author = 'kion',                   
+  author_email = 'yufan2hao@sina.com',     
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.9',
+  ],
+)
