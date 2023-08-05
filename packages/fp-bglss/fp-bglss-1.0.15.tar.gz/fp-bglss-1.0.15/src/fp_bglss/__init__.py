58a2c152174f3a216bf4e65644588cc683283883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:34 +0000

    Add package fp-bglss/1.0.15/fp-bglss-1.0.15.tar.gz

---
+++ packages/fp-bglss/fp-bglss-1.0.15.tar.gz/fp-bglss-1.0.15/src/fp_bglss/__init__.py
@@ -0,0 +1,90 @@
+"""
+(C) Copyright Bitglass Inc. 2021. All Rights Reserved.
+Author: eng@bitglass.com
+"""
+__author__ = 'Bitglass'
+
+
+class Logger(object):
+    """ For redirecting logging output to proprietory platform APIs (such as QRadar).
+        Default ctor is equivalent to logging not defined as the case is when on some platforms
+        one needs to know the data path from the config first before can initialize logging.
+        This allows for using app.logger.xyz() across the app in a portable way across platforms.
+        By default the standard python 'logging' module is used whenever possible.
+    """
+
+    def debug(self, msg):   # pylint: disable=E0202
+        self.log(msg, level='DEBUG')
+
+    def info(self, msg):    # pylint: disable=E0202
+        self.log(msg, level='INFO')
+
+    def warning(self, msg):     # pylint: disable=E0202
+        self.log(msg, level='WARNING')
+
+    def error(self, msg):   # pylint: disable=E0202
+        self.log(msg, level='ERROR')
+
+    def nop(self, msg):     # pylint: disable=E0202
+        pass
+
+    def __bool__(self):
+        return bool(self.conf)
+
+    # TODO Remove Python 2 crutch
+    def __nonzero__(self):
+        return self.__bool__()
+
+    def __init__(self, conf=None, log=None, set_log_level=None, nop=None):
+        self.conf = conf
+        self.log = log
+        if nop:
+            self.nop = nop  # type: ignore[assignment]
+        if conf and log and set_log_level:
+            if 'error' in conf.logging_level.lower():
+                set_log_level('ERROR')
+                self.debug = self.nop   # type: ignore[assignment]
+                self.info = self.nop    # type: ignore[assignment]
+                self.warning = self.nop     # type: ignore[assignment]
+            elif 'warn' in conf.logging_level.lower():
+                set_log_level('WARNING')
+                self.debug = self.nop   # type: ignore[assignment]
+                self.info = self.nop    # type: ignore[assignment]
+            elif 'info' in conf.logging_level.lower():
+                set_log_level('INFO')
+                self.info = self.nop    # type: ignore[assignment]
+        else:
+            self.debug = self.nop   # type: ignore[assignment]
+            self.info = self.nop    # type: ignore[assignment]
+            self.warning = self.nop     # type: ignore[assignment]
+            self.error = self.nop   # type: ignore[assignment]
+
+
+# Can't initialize here b/c it's data path dependent (different for different platforms)
+logger = Logger()
+
+
+# Uncomment for the merged module support
+# class App:
+#    Logger = Logger
+#
+#     def __init__(self, logger):
+#         self.logger = demisto
+#
+#
+# app = App(logger)
+
+
+# Skip this part for the merged module support
+try:
+    # Export for gunicorn (and QRadar, required by the upgrade scenario to be app:app)
+    from app.flaskinit import application as app   # noqa
+
+    # UI initialized successfully
+    pass
+
+# Not ImportError, to support arbitrary failing of cli integrations from the flat package, just in case
+except Exception as e:  # nosec # noqa
+
+    # UI error
+    pass    # nosec
---
+++ packages/fp-bglss/fp-bglss-1.0.15.tar.gz/fp-bglss-1.0.15/src/fp_bglss/secret.py
@@ -0,0 +1,106 @@
+"""
+(C) Copyright Bitglass Inc. 2021. All Rights Reserved.
+Author: eng@bitglass.com
+"""
+
+
+# Only mask chars (and sep chars if multi-password - below) are sent back to the client, never the actual password
+PSWD_MASK_CHAR = '*'
+
+# All passwords should be validated to not contain this control char, in case it's used
+# to concatenate multiple passwords for bulk encryption as in the 'proxies' setting
+MULTI_PSWD_SEP_CHAR = '\t'
+
+
+# All special proxy formatting chars are still valid (the re used makes sure of that), except the whitespace
+# The re parsing already guarantees whitespace to be excluded so no additional checking is necessary
+# INVALID_PSWD_CHARS = '/:@. \t'
+INVALID_PSWD_CHARS = ' \t'
+
+
+class Secret(object):
+    """ For the sake of 'extra' security echo a dummy string back from the server.
+        The dummy value of the same length will pass the form validation so there is no need to retype.
+        On the way back to the server, detect it and don't override the value keeping it the same.
+        Furthermore, explicitly clear the value when the session goes out.
+    """
+
+    def __get__(self, instance, _):     # _ owner
+        return '' if instance.pswd is None or instance.pswd == '' else ''.join(
+            MULTI_PSWD_SEP_CHAR if c == MULTI_PSWD_SEP_CHAR else PSWD_MASK_CHAR for c in instance.pswd)
+
+    def __set__(self, instance, value):
+        pswd = value if any(a() for a in [lambda: instance.pswd is None,
+                                          lambda: instance.pswd == '',
+                                          lambda: len(value) != len(instance.pswd),
+                                          lambda: not set(value).issubset(
+                                              set(PSWD_MASK_CHAR + MULTI_PSWD_SEP_CHAR)
+                                          )]) else instance.pswd
+        if pswd != instance.pswd:
+            instance.pswd = pswd
+            if instance.pswd != '':
+                instance.save(instance.conf)
+
+
+class Password(object):
+    secret = Secret()
+
+    def __init__(self, name, session=None):
+        self.pswd = None
+        self.name = name
+        self.session = session
+
+    def getUser(self):
+        try:
+            user = self.session['logged_in']
+        except Exception:
+            user = 'secret'
+        return user
+
+    def simpleHash(self, s):
+        import ctypes
+        v = ord(s[0]) << 7
+        for c in s:
+            v = ctypes.c_int32((int(1000003) * v) & 0xffffffff).value ^ ord(c)
+        v = v ^ len(s)
+        if v == -1:
+            v = -2
+        return int(v)
+
+    def clear(self):
+        self.pswd = None
+
+    def load(self, conf):
+        """ Load from the secure storage
+        """
+        self.conf = conf
+        # Must load from scratch as the secret is per-user and should not leak
+        self.clear()
+        try:
+            if not conf._isEnabled('qradar'):
+                raise ImportError('Skip qpylib for LSS debug')
+
+            from qpylib.encdec import Encryption, EncryptionError
+            try:
+                self.pswd = Encryption({'name': self.name, 'user': self.simpleHash(self.getUser())}).decrypt()
+            except EncryptionError:
+                pass
+        except ImportError:
+            # TODO Implement for LSS app
+            pass
+
+    def save(self, conf):
+        """ Save to the secure storage
+        """
+        try:
+            if not conf._isEnabled('qradar'):
+                raise ImportError('Skip qpylib for LSS debug')
+
+            from qpylib.encdec import Encryption, EncryptionError
+            try:
+                Encryption({'name': self.name, 'user': self.simpleHash(self.getUser())}).encrypt(self.pswd)
+            except EncryptionError:
+                pass
+        except ImportError:
+            # TODO Implement for LSS app
+            pass
