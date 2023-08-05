e7fce86a8693c43ea61ebaf71ca4d8e8e09ef52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:23 +0000

    Add package signed-pickle/1.0.0/signed-pickle-1.0.0.tar.gz

---
+++ packages/signed-pickle/signed-pickle-1.0.0.tar.gz/signed-pickle-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['signed_pickle']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{'lz4': ['lz4']}
+
+setup_kwargs = {
+    'name': 'signed-pickle',
+    'version': '1.0.0',
+    'description': 'Description',
+    'long_description': '# Signed Pickle\n',
+    'author': 'Daniel Sullivan',
+    'author_email': 'mumblepins@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/mumblepins/signed-pickle/',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
