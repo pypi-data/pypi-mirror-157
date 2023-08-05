044c9178f69be26f1e640379e92df5a9cc427d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:37 +0000

    Add package codeforlife-portal/6.10.0/codeforlife-portal-6.10.0.tar.gz

---
+++ packages/codeforlife-portal/codeforlife-portal-6.10.0.tar.gz/codeforlife-portal-6.10.0/portal/tests/test_invite_teacher.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.test import TestCase, Client
+
+
+class TestInviteTeacher(TestCase):
+    def test_invite_teacher_successful(self):
+        url = reverse("invite_teacher")
+        client = Client()
+        data = {"email": "valid_email@example.com"}
+        response = client.post(url, data)
+        assert response.status_code == 200
+        self.assertTemplateUsed(response, "portal/email_invitation_sent.html")
+
+    def test_invite_teacher_fail(self):
+        url = reverse("invite_teacher")
+        client = Client()
+        data = {"email": "invalid_email"}
+        response = client.post(url, data)
+        self.assertTemplateNotUsed(response, "portal/email_invitation_sent.html")
