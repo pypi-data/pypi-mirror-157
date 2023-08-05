496243f4981c896be4dd95c7e0fd040d34b4dc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:52 +0000

    Add package beobench/0.5.1/beobench-0.5.1.tar.gz

---
+++ packages/beobench/beobench-0.5.1.tar.gz/beobench-0.5.1/beobench/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for beobench."""
+
+__author__ = """Beobench authors"""
+__email__ = "-"
+__version__ = "0.5.1"
+
+from beobench.utils import restart
+from beobench.experiment.scheduler import run
---
+++ packages/beobench/beobench-0.5.1.tar.gz/beobench-0.5.1/tests/conftest.py
@@ -0,0 +1,12 @@
+"""Test configuration with fixtures."""
+
+import pytest
+
+import beobench.experiment.config_parser
+
+collect_ignore_glob = ["performance"]
+
+
+@pytest.fixture
+def run_config():
+    return beobench.experiment.config_parser.get_standard_config("test_energym")
