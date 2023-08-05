77539569df46fb4c523ecf5fdbe7d23a91f64b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:13 +0000

    Add package GooseHDF5/0.17.1/GooseHDF5-0.17.1.tar.gz

---
+++ packages/GooseHDF5/GooseHDF5-0.17.1.tar.gz/GooseHDF5-0.17.1/docs/conf.py
@@ -0,0 +1,15 @@
+# Project information
+
+project = "GooseHDF5"
+copyright = "2017-2022, Tom de Geus"
+author = "Tom de Geus"
+
+# General configuration
+
+extensions = ["sphinx.ext.imgmath", "sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinxarg.ext"]
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Options for HTML output
+
+html_theme = "sphinx_rtd_theme"
