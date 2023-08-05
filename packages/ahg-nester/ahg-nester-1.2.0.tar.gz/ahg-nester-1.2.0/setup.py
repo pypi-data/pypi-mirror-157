3d066f1866864591a15a8a3c478c8e3376e0dfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:07 +0000

    Add package ahg-nester/1.2.0/ahg-nester-1.2.0.tar.gz

---
+++ packages/ahg-nester/ahg-nester-1.2.0.tar.gz/ahg-nester-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'ahg-nester',
+    version = '1.2.0',
+    py_modules = ['ahg-nester'],
+    author = 'ahg',
+    author_email = 'ahg@gmail.com',
+    description = 'A simple printer of nested lists',
+)
