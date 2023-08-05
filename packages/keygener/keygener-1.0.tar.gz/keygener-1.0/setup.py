cf9ed25cbcd45a74e064ddb5952990338b30c61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:26 +0000

    Add package keygener/1.0/keygener-1.0.tar.gz

---
+++ packages/keygener/keygener-1.0.tar.gz/keygener-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='keygener',
+    version='1.0',
+    description='Password and key generating tool',
+    long_description=long_description,
+    author='Saikrishna T Bijil',
+    url='',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['key generate', 'password generate', 'random number picker'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['keygener'],
+    package_dir={'':'src'},
+    install_requires=[]
+)
