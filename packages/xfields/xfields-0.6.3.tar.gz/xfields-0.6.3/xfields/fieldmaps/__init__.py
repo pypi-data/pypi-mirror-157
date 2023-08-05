52189b2b176bc3ab8e394e00571455c39f157762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:55 +0000

    Add package xfields/0.6.3/xfields-0.6.3.tar.gz

---
+++ packages/xfields/xfields-0.6.3.tar.gz/xfields-0.6.3/xfields/fieldmaps/__init__.py
@@ -0,0 +1,11 @@
+# copyright ################################# #
+# This file is part of the Xfields Package.   #
+# Copyright (c) CERN, 2021.                   #
+# ########################################### #
+
+from .interpolated import TriLinearInterpolatedFieldMap
+from .interpolated import TriLinearInterpolatedFieldMapData
+from .tricubicinterpolated import TriCubicInterpolatedFieldMap
+from .tricubicinterpolated import TriCubicInterpolatedFieldMapData
+from .bigaussian import BiGaussianFieldMap, mean_and_std
+from .bigaussian import BiGaussianFieldMapData
