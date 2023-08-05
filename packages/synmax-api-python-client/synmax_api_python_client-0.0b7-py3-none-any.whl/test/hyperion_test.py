66cff910033b6e8951e518b8869271ac2cecd8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:28 +0000

    Add package synmax-api-python-client/0.0b7/synmax_api_python_client-0.0b7-py3-none-any.whl

---
+++ packages/synmax-api-python-client/synmax_api_python_client-0.0b7-py3-none-any.whl/test/hyperion_test.py
@@ -0,0 +1,28 @@
+import logging
+
+from synmax.hyperion import HyperionApiClient, ApiPayload
+
+logging.basicConfig(level=logging.INFO)
+
+client = HyperionApiClient()
+
+
+def fetch_region():
+    regions = client.fetch_regions()
+    print(regions)
+
+
+def well_completion():
+    payload = ApiPayload(start_date='2021-01-1', end_date='2022-06-25', state_code='TX')
+
+    result_df = client.wells()
+    print(result_df.count())
+
+
+def main():
+    # fetch_region()
+    well_completion()
+
+
+if __name__ == '__main__':
+    main()
