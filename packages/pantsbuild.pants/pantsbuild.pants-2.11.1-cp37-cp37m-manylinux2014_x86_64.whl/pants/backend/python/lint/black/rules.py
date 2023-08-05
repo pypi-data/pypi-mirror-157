8043cc03bc8c404fe6352a53f573578869aed74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:35 +0000

    Add package pantsbuild.pants/2.11.1/pantsbuild.pants-2.11.1-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.11.1-cp37-cp37m-manylinux2014_x86_64.whl/pants/backend/python/lint/black/rules.py
@@ -0,0 +1,164 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from dataclasses import dataclass
+from typing import Tuple
+
+from pants.backend.python.lint.black.skip_field import SkipBlackField
+from pants.backend.python.lint.black.subsystem import Black
+from pants.backend.python.subsystems.setup import PythonSetup
+from pants.backend.python.target_types import InterpreterConstraintsField, PythonSourceField
+from pants.backend.python.util_rules import pex
+from pants.backend.python.util_rules.interpreter_constraints import InterpreterConstraints
+from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
+from pants.core.goals.fmt import FmtRequest, FmtResult
+from pants.core.goals.lint import LintResult, LintResults, LintTargetsRequest
+from pants.core.util_rules.config_files import ConfigFiles, ConfigFilesRequest
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.fs import Digest, MergeDigests
+from pants.engine.process import FallibleProcessResult, Process, ProcessResult
+from pants.engine.rules import Get, MultiGet, collect_rules, rule
+from pants.engine.target import FieldSet, Target
+from pants.engine.unions import UnionRule
+from pants.util.logging import LogLevel
+from pants.util.strutil import pluralize
+
+
+@dataclass(frozen=True)
+class BlackFieldSet(FieldSet):
+    required_fields = (PythonSourceField,)
+
+    source: PythonSourceField
+    interpreter_constraints: InterpreterConstraintsField
+
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return tgt.get(SkipBlackField).value
+
+
+class BlackRequest(FmtRequest, LintTargetsRequest):
+    field_set_type = BlackFieldSet
+    name = Black.options_scope
+
+
+@dataclass(frozen=True)
+class SetupRequest:
+    request: BlackRequest
+    check_only: bool
+
+
+@dataclass(frozen=True)
+class Setup:
+    process: Process
+    original_digest: Digest
+
+
+def generate_argv(source_files: SourceFiles, black: Black, *, check_only: bool) -> Tuple[str, ...]:
+    args = []
+    if check_only:
+        args.append("--check")
+    if black.config:
+        args.extend(["--config", black.config])
+    args.extend(["-W", "{pants_concurrency}"])
+    args.extend(black.args)
+    args.extend(source_files.files)
+    return tuple(args)
+
+
+@rule(level=LogLevel.DEBUG)
+async def setup_black(
+    setup_request: SetupRequest, black: Black, python_setup: PythonSetup
+) -> Setup:
+    # Black requires 3.6+ but uses the typed-ast library to work with 2.7, 3.4, 3.5, 3.6, and 3.7.
+    # However, typed-ast does not understand 3.8+, so instead we must run Black with Python 3.8+
+    # when relevant. We only do this if if <3.8 can't be used, as we don't want a loose requirement
+    # like `>=3.6` to result in requiring Python 3.8, which would error if 3.8 is not installed on
+    # the machine.
+    all_interpreter_constraints = InterpreterConstraints.create_from_compatibility_fields(
+        (field_set.interpreter_constraints for field_set in setup_request.request.field_sets),
+        python_setup,
+    )
+    tool_interpreter_constraints = (
+        all_interpreter_constraints
+        if (
+            black.options.is_default("interpreter_constraints")
+            and all_interpreter_constraints.requires_python38_or_newer(
+                python_setup.interpreter_universe
+            )
+        )
+        else black.interpreter_constraints
+    )
+
+    black_pex_get = Get(
+        VenvPex,
+        PexRequest,
+        black.to_pex_request(interpreter_constraints=tool_interpreter_constraints),
+    )
+
+    source_files_get = Get(
+        SourceFiles,
+        SourceFilesRequest(field_set.source for field_set in setup_request.request.field_sets),
+    )
+
+    source_files, black_pex = await MultiGet(source_files_get, black_pex_get)
+    source_files_snapshot = (
+        source_files.snapshot
+        if setup_request.request.prior_formatter_result is None
+        else setup_request.request.prior_formatter_result
+    )
+
+    config_files = await Get(
+        ConfigFiles, ConfigFilesRequest, black.config_request(source_files_snapshot.dirs)
+    )
+    input_digest = await Get(
+        Digest, MergeDigests((source_files_snapshot.digest, config_files.snapshot.digest))
+    )
+
+    process = await Get(
+        Process,
+        VenvPexProcess(
+            black_pex,
+            argv=generate_argv(source_files, black, check_only=setup_request.check_only),
+            input_digest=input_digest,
+            output_files=source_files_snapshot.files,
+            concurrency_available=len(setup_request.request.field_sets),
+            description=f"Run Black on {pluralize(len(setup_request.request.field_sets), 'file')}.",
+            level=LogLevel.DEBUG,
+        ),
+    )
+    return Setup(process, original_digest=source_files_snapshot.digest)
+
+
+@rule(desc="Format with Black", level=LogLevel.DEBUG)
+async def black_fmt(request: BlackRequest, black: Black) -> FmtResult:
+    if black.skip:
+        return FmtResult.skip(formatter_name=request.name)
+    setup = await Get(Setup, SetupRequest(request, check_only=False))
+    result = await Get(ProcessResult, Process, setup.process)
+    return FmtResult.from_process_result(
+        result,
+        original_digest=setup.original_digest,
+        formatter_name=request.name,
+        strip_chroot_path=True,
+    )
+
+
+@rule(desc="Lint with Black", level=LogLevel.DEBUG)
+async def black_lint(request: BlackRequest, black: Black) -> LintResults:
+    if black.skip:
+        return LintResults([], linter_name=request.name)
+    setup = await Get(Setup, SetupRequest(request, check_only=True))
+    result = await Get(FallibleProcessResult, Process, setup.process)
+    return LintResults(
+        [LintResult.from_fallible_process_result(result, strip_chroot_path=True)],
+        linter_name=request.name,
+    )
+
+
+def rules():
+    return [
+        *collect_rules(),
+        UnionRule(FmtRequest, BlackRequest),
+        UnionRule(LintTargetsRequest, BlackRequest),
+        *pex.rules(),
+    ]
---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.11.1-cp37-cp37m-manylinux2014_x86_64.whl/pants/backend/python/lint/isort/rules.py
@@ -0,0 +1,158 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from dataclasses import dataclass
+from typing import Tuple
+
+from pants.backend.python.lint.isort.skip_field import SkipIsortField
+from pants.backend.python.lint.isort.subsystem import Isort
+from pants.backend.python.target_types import PythonSourceField
+from pants.backend.python.util_rules import pex
+from pants.backend.python.util_rules.pex import PexRequest, PexResolveInfo, VenvPex, VenvPexProcess
+from pants.core.goals.fmt import FmtRequest, FmtResult
+from pants.core.goals.lint import LintResult, LintResults, LintTargetsRequest
+from pants.core.util_rules.config_files import ConfigFiles, ConfigFilesRequest
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.fs import Digest, MergeDigests
+from pants.engine.process import FallibleProcessResult, Process, ProcessResult
+from pants.engine.rules import Get, MultiGet, collect_rules, rule
+from pants.engine.target import FieldSet, Target
+from pants.engine.unions import UnionRule
+from pants.util.logging import LogLevel
+from pants.util.strutil import pluralize
+
+
+@dataclass(frozen=True)
+class IsortFieldSet(FieldSet):
+    required_fields = (PythonSourceField,)
+
+    source: PythonSourceField
+
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return tgt.get(SkipIsortField).value
+
+
+class IsortRequest(FmtRequest, LintTargetsRequest):
+    field_set_type = IsortFieldSet
+    name = Isort.options_scope
+
+
+@dataclass(frozen=True)
+class SetupRequest:
+    request: IsortRequest
+    check_only: bool
+
+
+@dataclass(frozen=True)
+class Setup:
+    process: Process
+    original_digest: Digest
+
+
+def generate_argv(
+    source_files: SourceFiles, isort: Isort, *, is_isort5: bool, check_only: bool
+) -> Tuple[str, ...]:
+    args = [*isort.args]
+    if check_only:
+        args.append("--check-only")
+    if is_isort5 and len(isort.config) == 1:
+        explicitly_configured_config_args = [
+            arg
+            for arg in isort.args
+            if (
+                arg.startswith("--sp")
+                or arg.startswith("--settings-path")
+                or arg.startswith("--settings-file")
+                or arg.startswith("--settings")
+            )
+        ]
+        # TODO: Deprecate manually setting this option, but wait until we deprecate
+        #  `[isort].config` to be a string rather than list[str] option.
+        if not explicitly_configured_config_args:
+            args.append(f"--settings={isort.config[0]}")
+    args.extend(source_files.files)
+    return tuple(args)
+
+
+@rule(level=LogLevel.DEBUG)
+async def setup_isort(setup_request: SetupRequest, isort: Isort) -> Setup:
+    isort_pex_get = Get(VenvPex, PexRequest, isort.to_pex_request())
+    source_files_get = Get(
+        SourceFiles,
+        SourceFilesRequest(field_set.source for field_set in setup_request.request.field_sets),
+    )
+    source_files, isort_pex = await MultiGet(source_files_get, isort_pex_get)
+
+    source_files_snapshot = (
+        source_files.snapshot
+        if setup_request.request.prior_formatter_result is None
+        else setup_request.request.prior_formatter_result
+    )
+
+    config_files = await Get(
+        ConfigFiles, ConfigFilesRequest, isort.config_request(source_files_snapshot.dirs)
+    )
+
+    # Isort 5+ changes how config files are handled. Determine which semantics we should use.
+    is_isort5 = False
+    if isort.config:
+        isort_info = await Get(PexResolveInfo, VenvPex, isort_pex)
+        is_isort5 = any(
+            dist_info.project_name == "isort" and dist_info.version.major >= 5
+            for dist_info in isort_info
+        )
+
+    input_digest = await Get(
+        Digest, MergeDigests((source_files_snapshot.digest, config_files.snapshot.digest))
+    )
+
+    process = await Get(
+        Process,
+        VenvPexProcess(
+            isort_pex,
+            argv=generate_argv(
+                source_files, isort, is_isort5=is_isort5, check_only=setup_request.check_only
+            ),
+            input_digest=input_digest,
+            output_files=source_files_snapshot.files,
+            description=f"Run isort on {pluralize(len(setup_request.request.field_sets), 'file')}.",
+            level=LogLevel.DEBUG,
+        ),
+    )
+    return Setup(process, original_digest=source_files_snapshot.digest)
+
+
+@rule(desc="Format with isort", level=LogLevel.DEBUG)
+async def isort_fmt(request: IsortRequest, isort: Isort) -> FmtResult:
+    if isort.skip:
+        return FmtResult.skip(formatter_name=request.name)
+    setup = await Get(Setup, SetupRequest(request, check_only=False))
+    result = await Get(ProcessResult, Process, setup.process)
+    return FmtResult.from_process_result(
+        result,
+        original_digest=setup.original_digest,
+        formatter_name=request.name,
+        strip_chroot_path=True,
+    )
+
+
+@rule(desc="Lint with isort", level=LogLevel.DEBUG)
+async def isort_lint(request: IsortRequest, isort: Isort) -> LintResults:
+    if isort.skip:
+        return LintResults([], linter_name=request.name)
+    setup = await Get(Setup, SetupRequest(request, check_only=True))
+    result = await Get(FallibleProcessResult, Process, setup.process)
+    return LintResults(
+        [LintResult.from_fallible_process_result(result, strip_chroot_path=True)],
+        linter_name=request.name,
+    )
+
+
+def rules():
+    return [
+        *collect_rules(),
+        UnionRule(FmtRequest, IsortRequest),
+        UnionRule(LintTargetsRequest, IsortRequest),
+        *pex.rules(),
+    ]
