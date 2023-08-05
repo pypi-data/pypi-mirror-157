1455f0f9f5afa9f00fb3de4417e7571a57c65b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:19 +0000

    Add package jarowinkler/1.0.5/jarowinkler-1.0.5.tar.gz

---
+++ packages/jarowinkler/jarowinkler-1.0.5.tar.gz/jarowinkler-1.0.5/setup.py
@@ -0,0 +1,35 @@
+from skbuild import setup
+import rapidfuzz_capi
+
+with open('README.md', 'rt', encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="jarowinkler",
+    version="1.0.5",
+    url="https://github.com/maxbachmann/JaroWinkler",
+    author="Max Bachmann",
+    author_email="pypi@maxbachmann.de",
+    description="library for fast approximate string matching using Jaro and Jaro-Winkler similarity",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License"
+    ],
+
+    packages=["jarowinkler"],
+    package_data={"jarowinkler": [
+        "*.pyi",
+        "py.typed"
+    ]},
+    python_requires=">=3.6",
+    cmake_args=[f'-DRF_CAPI_PATH:STRING={rapidfuzz_capi.get_include()}']
+)
