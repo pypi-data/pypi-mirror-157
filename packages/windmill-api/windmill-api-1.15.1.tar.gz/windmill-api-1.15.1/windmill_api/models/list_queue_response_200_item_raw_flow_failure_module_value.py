5e500895a6de7dc6f09f28d9e49fe8f2141d274d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package windmill-api/1.15.1/windmill-api-1.15.1.tar.gz

---
+++ packages/windmill-api/windmill-api-1.15.1.tar.gz/windmill-api-1.15.1/windmill_api/models/list_queue_response_200_item_raw_flow_failure_module_value.py
@@ -0,0 +1,64 @@
+from typing import Any, Dict, List, Type, TypeVar
+
+import attr
+
+from ..models.list_queue_response_200_item_raw_flow_failure_module_value_type import (
+    ListQueueResponse200ItemRawFlowFailureModuleValueType,
+)
+
+T = TypeVar("T", bound="ListQueueResponse200ItemRawFlowFailureModuleValue")
+
+
+@attr.s(auto_attribs=True)
+class ListQueueResponse200ItemRawFlowFailureModuleValue:
+    """ """
+
+    path: str
+    type: ListQueueResponse200ItemRawFlowFailureModuleValueType
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        path = self.path
+        type = self.type.value
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "path": path,
+                "type": type,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        path = d.pop("path")
+
+        type = ListQueueResponse200ItemRawFlowFailureModuleValueType(d.pop("type"))
+
+        list_queue_response_200_item_raw_flow_failure_module_value = cls(
+            path=path,
+            type=type,
+        )
+
+        list_queue_response_200_item_raw_flow_failure_module_value.additional_properties = d
+        return list_queue_response_200_item_raw_flow_failure_module_value
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
