11d3e17185614c137f45fb6633184d6185dc697c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:27 +0000

    Add package latest-chromedriver/2022.6.29/latest-chromedriver-2022.6.29.tar.gz

---
+++ packages/latest-chromedriver/latest-chromedriver-2022.6.29.tar.gz/latest-chromedriver-2022.6.29/latest_chromedriver/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['chrome_info', 'download_driver', 'enviroment']
+
+from latest_chromedriver.download_driver import download_only_if_needed
+from latest_chromedriver.enviroment import safely_set_chromedriver_path
