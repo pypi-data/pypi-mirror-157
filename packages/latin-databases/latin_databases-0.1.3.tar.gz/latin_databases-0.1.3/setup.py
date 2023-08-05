601ecfd6a54c1161b164eeb61d812f17ca0ebf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:30 +0000

    Add package latin-databases/0.1.3/latin_databases-0.1.3.tar.gz

---
+++ packages/latin-databases/latin_databases-0.1.3.tar.gz/latin_databases-0.1.3/setup.py
@@ -0,0 +1,32 @@
+
+
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = ( this_directory/ "README.md").read_text()
+
+setup(
+    name='latin_databases',
+    version='0.1.3',
+    description='none',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kylefoley76/latin_databases3',
+    author='Kyle Foley',
+    author_email='kylefoley202@gmail.com',
+    license='BSD',
+    zip_safe=False,
+    packages=['latin','latin/general_la'],
+    install_requires=['Levenshtein',
+                      'striprtf==0.0.12',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
+
