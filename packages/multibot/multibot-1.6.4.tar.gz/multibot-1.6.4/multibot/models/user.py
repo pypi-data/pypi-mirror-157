3632c8bd65d34467ca5fcaa9b09bb483a930617f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:49 +0000

    Add package multibot/1.6.4/multibot-1.6.4.tar.gz

---
+++ packages/multibot/multibot-1.6.4.tar.gz/multibot-1.6.4/multibot/models/user.py
@@ -0,0 +1,24 @@
+__all__ = ['User']
+
+from dataclasses import dataclass
+
+from multibot import constants
+from multibot.models.database import db
+from multibot.models.enums import Platform
+from multibot.models.event_component import EventComponent
+
+
+@dataclass(eq=False)
+class User(EventComponent):
+    collection = db.user
+    _unique_keys = ('platform', 'id')
+
+    platform: Platform = None
+    id: int = None
+    name: str = None
+    is_admin: bool = None
+    is_bot: bool = None
+    original_object: constants.ORIGINAL_USER = None
+
+    def __getstate__(self):
+        return self._mongo_repr()
