461023b52064f779e1a7a262184a2a4d78d06d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:38 +0000

    Add package vk-sdk/1.7.2/vk_sdk-1.7.2-py3-none-any.whl

---
+++ packages/vk-sdk/vk_sdk-1.7.2-py3-none-any.whl/vk_sdk/enums.py
@@ -0,0 +1,12 @@
+"""Kotlin-Like Enums"""
+
+import inspect
+from .dataClass import DataClass
+
+
+
+class Enum(object):
+    def __init__(self, customDataClass=None):
+        inspectedArgs = inspect.getfullargspec(self.__init__)
+        self.EnumValue = DataClass(
+            *inspectedArgs.args[1:]) if customDataClass is None else customDataClass(*inspectedArgs.args[1:])
