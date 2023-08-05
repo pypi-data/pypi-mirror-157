cba85fe71cb9d39110d74924136a3e4c4db5d8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:04 +0000

    Add package telefonia/0.1.0/telefonia-0.1.0.tar.gz

---
+++ packages/telefonia/telefonia-0.1.0.tar.gz/telefonia-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['telefonia']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.23.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'telefonia',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Bamatailo',
+    'author_email': '103551990+Bamatailo@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
