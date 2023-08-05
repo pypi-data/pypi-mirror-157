d802e96ed37b2ea70c4f5ebc6664b94632da45ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:34 +0000

    Add package perico/1.0.0/perico-1.0.0.tar.gz

---
+++ packages/perico/perico-1.0.0.tar.gz/perico-1.0.0/perico.py
@@ -0,0 +1,7 @@
+
+
+
+
+def print_whatever():
+    whatever = input("What do you want to say?:   ")
+    print (whatever)
---
+++ packages/perico/perico-1.0.0.tar.gz/perico-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "perico",
+    version = "1.0.0",
+    py_modules = ["perico"],
+    author = "Aventuras_con_Alberto",
+    author_email = "lalalamepicaunhuevo@hahaha.com",
+    url = "http://www.headfirstlabs.com",
+    description = "it will print the name of someone",
+    )
+    
