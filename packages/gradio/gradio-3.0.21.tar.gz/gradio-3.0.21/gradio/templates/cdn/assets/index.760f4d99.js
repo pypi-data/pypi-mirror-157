a351dc3390d56bec0e8adbceceb3e5d29d8e872d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:26 +0000

    Add package gradio/3.0.21/gradio-3.0.21.tar.gz

---
+++ packages/gradio/gradio-3.0.21.tar.gz/gradio-3.0.21/gradio/templates/cdn/assets/index.760f4d99.js
@@ -0,0 +1,13 @@
+import { ad as identity } from './index.6532f4a3.js';
+
+function fade(node, { delay = 0, duration = 400, easing = identity } = {}) {
+    const o = +getComputedStyle(node).opacity;
+    return {
+        delay,
+        duration,
+        easing,
+        css: t => `opacity: ${t * o}`
+    };
+}
+
+export { fade as f };
---
+++ packages/gradio/gradio-3.0.21.tar.gz/gradio-3.0.21/gradio/templates/frontend/assets/index.7282bd57.js
@@ -0,0 +1,13 @@
+import { ad as identity } from './index.2fcf676d.js';
+
+function fade(node, { delay = 0, duration = 400, easing = identity } = {}) {
+    const o = +getComputedStyle(node).opacity;
+    return {
+        delay,
+        duration,
+        easing,
+        css: t => `opacity: ${t * o}`
+    };
+}
+
+export { fade as f };
