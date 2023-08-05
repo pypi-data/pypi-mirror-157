e4a855942ada64530adeaec5d9e0ec887b50a8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:11 +0000

    Add package fake-bge-module-latest/20220629/fake_bge_module_latest-20220629-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20220629-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import add_mesh_torus
+from . import sequencer
+from . import console
+from . import file
+from . import assets
+from . import vertexpaint_dirt
+from . import uvcalc_follow_active
+from . import spreadsheet
+from . import mesh
+from . import object
+from . import freestyle
+from . import screen_play_rendered_anim
+from . import presets
+from . import object_quick_effects
+from . import wm
+from . import object_align
+from . import clip
+from . import anim
+from . import image
+from . import rigidbody
+from . import object_randomize_transform
+from . import bmesh
+from . import node
+from . import geometry_nodes
+from . import constraint
+from . import view3d
+from . import userpref
+from . import uvcalc_lightmap
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20220629-py3-none-any.whl/bpy/ops/sculpt_curves.py
@@ -0,0 +1,28 @@
+import sys
+import typing
+import bpy.types
+
+
+def brush_stroke(stroke: typing.Union[
+        typing.Dict[str, 'bpy.types.OperatorStrokeElement'], typing.
+        List['bpy.types.OperatorStrokeElement'], 'bpy_prop_collection'] = None,
+                 mode: typing.Union[str, int] = 'NORMAL'):
+    ''' Sculpt curves using a brush
+
+    :param stroke: Stroke
+    :type stroke: typing.Union[typing.Dict[str, 'bpy.types.OperatorStrokeElement'], typing.List['bpy.types.OperatorStrokeElement'], 'bpy_prop_collection']
+    :param mode: Stroke Mode, Action taken when a paint stroke is made * NORMAL Regular -- Apply brush normally. * INVERT Invert -- Invert action of brush for duration of stroke. * SMOOTH Smooth -- Switch brush to smooth mode for duration of stroke.
+    :type mode: typing.Union[str, int]
+    '''
+
+    pass
+
+
+def select_all(action: typing.Union[str, int] = 'TOGGLE'):
+    ''' (De)select all control points
+
+    :param action: Action, Selection action to execute * TOGGLE Toggle -- Toggle selection for all elements. * SELECT Select -- Select all elements. * DESELECT Deselect -- Deselect all elements. * INVERT Invert -- Invert selection of all elements.
+    :type action: typing.Union[str, int]
+    '''
+
+    pass
