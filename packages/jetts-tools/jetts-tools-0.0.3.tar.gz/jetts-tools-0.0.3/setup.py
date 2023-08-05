1f7e3878248bc72a43b885646e6241e47ec9a918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:05 +0000

    Add package jetts-tools/0.0.3/jetts-tools-0.0.3.tar.gz

---
+++ packages/jetts-tools/jetts-tools-0.0.3.tar.gz/jetts-tools-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='jetts-tools',
+    version='0.0.3',
+    author='Jett Crowson',
+    author_email='jettcrowson@gmail.com',
+    url='https://github.com/jettdc/jetts-tools',
+    description='Various useful python utility functions.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(exclude='test*'),
+    keywords='python tools utils',
+    python_requires='>=3.5'
+)
