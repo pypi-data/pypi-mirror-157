b9cd4d80edf75f2a365677974513873680826b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:19 +0000

    Add package ranx/0.2.8/ranx-0.2.8.tar.gz

---
+++ packages/ranx/ranx-0.2.8.tar.gz/ranx-0.2.8/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ranx",
+    version="0.2.8",
+    author="Elias Bassani",
+    author_email="elias.bssn@gmail.com",
+    description="ranx: A Blazing Fast Python Library for Ranking Evaluation and Comparison",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AmenRa/ranx",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "numba>=0.54.1",
+        "pandas",
+        "tabulate",
+        "tqdm",
+        "scipy",
+        "statsmodels",
+        "ir_datasets",
+        "rich",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: General",
+    ],
+    keywords=[
+        "trec_eval",
+        "information retrieval",
+        "evaluation",
+        "ranking",
+        "numba",
+    ],
+    python_requires=">=3.7",
+)
