a541c17b1860bfc6e2ae674a8990d4e0fce2ddb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:30 +0000

    Add package clang-tools/0.1.0/clang_tools-0.1.0-py3-none-any.whl

---
+++ packages/clang-tools/clang_tools-0.1.0-py3-none-any.whl/clang_tools/main.py
@@ -0,0 +1,31 @@
+import argparse
+from clang_tools.install import install_clang_tools
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(prog='clang-tools')
+
+    parser.add_argument(
+        "-i",
+        "--install",
+        default="12",
+        help="Install clang-tools with specific version. default is 12.",
+    )
+
+    # parser.add_argument(
+    #     "-d",
+    #     "--directory",
+    #     default="/usr/bin/",
+    #     help="The directory where is the clang-tools install.",
+    # )
+    args = parser.parse_args()
+
+    install_version = args.install
+    # install_dir = args.directory
+
+    if install_version:
+        install_clang_tools(install_version)
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
