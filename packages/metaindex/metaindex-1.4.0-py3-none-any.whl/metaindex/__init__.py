4083971fff300b83ae0edc639f25fbf2ab2caed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:12 +0000

    Add package metaindex/1.4.0/metaindex-1.4.0-py3-none-any.whl

---
+++ packages/metaindex/metaindex-1.4.0-py3-none-any.whl/metaindex/__init__.py
@@ -0,0 +1,6 @@
+from .cache import MemoryCache
+from .shared import CacheEntry
+from .query import Query
+from .configuration import Configuration
+from .indexer import IndexerBase, index_files, IndexerResult
+from .humanizer import humanize, register_humanizer
