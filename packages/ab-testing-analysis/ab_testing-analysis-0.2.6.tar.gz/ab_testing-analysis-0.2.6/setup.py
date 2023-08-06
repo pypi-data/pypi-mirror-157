b3ff2ca08b0c521df9a094a41ad6030c43753678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:50 +0000

    Add package ab-testing-analysis/0.2.6/ab_testing-analysis-0.2.6.tar.gz

---
+++ packages/ab-testing-analysis/ab_testing-analysis-0.2.6.tar.gz/ab_testing-analysis-0.2.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup 
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.2.6'
+DESCRIPTION = 'A python library dedicated for A/B testing analysis for experiment testing'
+
+# Setting up
+setup(
+    name="ab_testing-analysis",
+    version=VERSION,
+    author="Mihir Deo",
+    author_email="<mihirdeo16@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/mihir-workspace/ab-testing',
+    packages= find_packages(),
+    install_requires=["pandas","statsmodels"],
+    keywords=['python', 'a/b testing', 'abtest','abtesting', 'response analysis','conversion rate analysis','statistical test'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
