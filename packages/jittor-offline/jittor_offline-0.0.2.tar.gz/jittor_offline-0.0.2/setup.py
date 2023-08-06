a14f9f8c87c7ce96c0a3c3e33eec3d35db6fcf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:05:24 +0000

    Add package jittor-offline/0.0.2/jittor_offline-0.0.2.tar.gz

---
+++ packages/jittor-offline/jittor_offline-0.0.2.tar.gz/jittor_offline-0.0.2/setup.py
@@ -0,0 +1,28 @@
+
+import setuptools
+
+
+setuptools.setup(
+    name="jittor_offline",
+    version="0.0.2",
+    author="jittor",
+    author_email="jittor@qq.com",
+    description="jittor project",
+    long_description="jittor_offline",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jittor/jittor",
+    project_urls={
+        "Bug Tracker": "https://github.com/jittor/jittor/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=[],
+    package_dir={"": "python"},
+    package_data={'': ['*', '*.gz', '*/*', '*/*/*','*/*/*/*','*/*/*/*/*','*/*/*/*/*/*']},
+    python_requires=">=3.7",
+    install_requires=[
+        "jittor>=1.3.4.10",
+    ],
+)
