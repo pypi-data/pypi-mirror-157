34403f7f84554fc0bb70810ad020e28d5dad30e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:16 +0000

    Add package mm-sdk/0.1.1/mm-sdk-0.1.1.tar.gz

---
+++ packages/mm-sdk/mm-sdk-0.1.1.tar.gz/mm-sdk-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['sdk', 'sdk.mis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx==0.13.3', 'pre-commit==2.9.2', 'pydantic==1.7.3']
+
+setup_kwargs = {
+    'name': 'mm-sdk',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'dyus',
+    'author_email': 'dyuuus@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
