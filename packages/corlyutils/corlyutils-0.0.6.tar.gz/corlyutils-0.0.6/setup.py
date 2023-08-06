47b93c5d4a62e8e0496d0dc7695b5b0318db1eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:48 +0000

    Add package corlyutils/0.0.6/corlyutils-0.0.6.tar.gz

---
+++ packages/corlyutils/corlyutils-0.0.6.tar.gz/corlyutils-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='corlyutils',
+    version='0.0.6',
+    py_modules=['corlyutils.mysql_helper', 'corlyutils.mongo_helper', 'corlyutils.logging_helper'],
+    author='test',
+    author_email='xiaoxiangmuyu@gmail.com',
+    url='https://corly.cc/',
+    description='hello util',
+)
