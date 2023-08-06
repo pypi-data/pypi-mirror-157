1cb7ee105c2ad23485903280b9d1125e3fbc6195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:44:01 +0000

    Add package gpyocr/1.6/gpyocr-1.6.tar.gz

---
+++ packages/gpyocr/gpyocr-1.6.tar.gz/gpyocr-1.6/gpyocr/__init__.py
@@ -0,0 +1,15 @@
+"""
+gpyocr - Python wrapper to Tesseract OCR and Google Vision OCR
+"""
+
+
+__version__ = "1.6"
+
+
+from ._gpyocr import (
+    SUPPORTED_FORMATS,
+    get_google_vision_version,
+    get_tesseract_version,
+    google_vision_ocr,
+    tesseract_ocr,
+)
