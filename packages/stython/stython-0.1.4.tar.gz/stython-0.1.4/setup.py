ac80b16f296a8d7d3f097b3591420e64d76857e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:05 +0000

    Add package stython/0.1.4/stython-0.1.4.tar.gz

---
+++ packages/stython/stython-0.1.4.tar.gz/stython-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.txt') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='stython',
+    version='0.1.4',
+    description='Stython package',
+    long_description=readme,
+    author='Arun Kapila',
+    author_email='starshootercity@gmail.com',
+    url='https://github.com/cometcake575/stython',
+    license=license,
+    packages=find_packages()
+)
