222f86f442dfd54aab93cdf819e0775ed477aba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:49 +0000

    Add package projectVizuaMatix/0.1/projectVizuaMatix-0.1.tar.gz

---
+++ packages/projectVizuaMatix/projectVizuaMatix-0.1.tar.gz/projectVizuaMatix-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+name='projectVizuaMatix',
+version='0.1',
+author="Ravinath Wanni Arachchi",
+author_email="ravinathwanni@gmail.com",
+description="key for the company's project",
+packages=setuptools.find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+)
