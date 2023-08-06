8b36ee8effe5b3ad057344e682bc682fd693f63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:22 +0000

    Add package example-lagrange-workflow/0.1.0/example_lagrange_workflow-0.1.0.tar.gz

---
+++ packages/example-lagrange-workflow/example_lagrange_workflow-0.1.0.tar.gz/example_lagrange_workflow-0.1.0/tests/test_read_file.py
@@ -0,0 +1,13 @@
+import unittest
+
+from lagrange_workflow.read_file import ReadFile
+
+
+class TestSimple(unittest.TestCase):
+
+    def test_add(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
