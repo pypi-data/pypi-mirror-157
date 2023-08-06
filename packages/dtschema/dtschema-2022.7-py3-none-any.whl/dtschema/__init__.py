eb5e7befa3b90bdf9fa629f8ac090a696000de23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:38 +0000

    Add package dtschema/2022.7/dtschema-2022.7-py3-none-any.whl

---
+++ packages/dtschema/dtschema-2022.7-py3-none-any.whl/dtschema/__init__.py
@@ -0,0 +1,17 @@
+from dtschema.lib import (
+    add_schema_path,
+    load_schema,
+    load,
+    process_schemas,
+    set_schema,
+    DTValidator,
+    format_error,
+    extract_compatibles,
+    extract_types,
+    get_prop_types,
+    sized_int,
+)
+
+from dtschema.version import (
+    __version__
+)
