11b337fcafb5ca32e2b8ab5a3ee3bf968a729f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:41 +0000

    Add package tahmimshovon-nester/0.0.4/tahmimshovon_nester-0.0.4.tar.gz

---
+++ packages/tahmimshovon-nester/tahmimshovon_nester-0.0.4.tar.gz/tahmimshovon_nester-0.0.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name        = 'tahmimshovon_nester',
+    version     = '0.0.4',
+    author      = 'Tahmim_SHovon',
+    author_email= 'tahmim14@gmail.com',
+    description = 'This is a simple function that prints out nested lists',
+    packages    = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+
+    ],
+)
