632f0bf60e4a86ad217221068128ebe471ed1ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:58 +0000

    Add package guiscrcpy/2022.6.1/guiscrcpy-2022.6.1-py3-none-any.whl

---
+++ packages/guiscrcpy/guiscrcpy-2022.6.1-py3-none-any.whl/guiscrcpy/ux/__init__.py
@@ -0,0 +1,29 @@
+"""
+GUISCRCPY by srevinsaju
+Get it on : https://github.com/srevinsaju/guiscrcpy
+Licensed under GNU Public License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+
+# flake8: noqa
+
+from guiscrcpy.ui.pyside2.main import Ui_MainWindow  # noqa: F401
+from guiscrcpy.ui.pyside2.network import Ui_NetworkUI  # noqa: F401
+from guiscrcpy.ui.pyside2.panel import Ui_HorizontalPanel  # noqa: F401
+from guiscrcpy.ui.pyside2.settings import Ui_MainWindow as Ui_SettingsWindow
+from guiscrcpy.ui.pyside2.downloader import Ui_Initializer  # noqa: F401
+from guiscrcpy.ui.pyside2.toolkit import Ui_ToolbarPanel  # noqa: F401
