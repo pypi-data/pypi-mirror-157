6d7f5c0cb82c9329779cb9aa8df301f3ef2cfb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:42 +0000

    Add package rstats-logreader/3.1.2/rstats-logreader-3.1.2.tar.gz

---
+++ packages/rstats-logreader/rstats-logreader-3.1.2.tar.gz/rstats-logreader-3.1.2/src/rstats_logreader/__init__.py
@@ -0,0 +1,6 @@
+################################################################################
+#                               rstats-logreader                               #
+#   Parse RStats logfiles, display bandwidth usage, convert to other formats   #
+#                    (C) 2016, 2019-2020, 2022 Jeremy Brown                    #
+#                Released under Prosperity Public License 3.0.0                #
+################################################################################
