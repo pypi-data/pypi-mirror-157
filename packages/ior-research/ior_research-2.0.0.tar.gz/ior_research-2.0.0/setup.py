902153431da7dfaed376b6a615c1925a5c3851a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:17 +0000

    Add package ior-research/2.0.0/ior_research-2.0.0.tar.gz

---
+++ packages/ior-research/ior_research-2.0.0.tar.gz/ior_research-2.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='ior_research',
+     version='v2.0.0',
+     author="Mayank Shinde",
+     author_email="mayank31313@gmail.com",
+     description="A platform to control robots and electronic device over Internet",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/mayank31313/ior-python",
+     packages=setuptools.find_packages(),
+     # packages=['ior_research'],
+     keywords=['ior','iot','network_robos', 'control_net'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+         'Intended Audience :: Developers',
+     ]
+ )
