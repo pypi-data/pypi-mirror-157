1daa818281c5401fd405ab8b2081b3c6c00db1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:09 +0000

    Add package DataQC/0.0.102/DataQC-0.0.102.tar.gz

---
+++ packages/DataQC/DataQC-0.0.102.tar.gz/DataQC-0.0.102/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='DataQC',
+    version='0.0.102',
+    description='Data quality checker.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Md Ismail Hossain',
+    author_email='mdismail.hossain@student.nmt.edu',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Data QC',
+    packages=find_packages(),
+    install_requires=['python-docx == 0.8.5']
+)
