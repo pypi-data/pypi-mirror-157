13d6a79cfece46f42d8098696c67e765ed306b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:25:21 +0000

    Add package vufr/1.5.0/vufr-1.5.0.tar.gz

---
+++ packages/vufr/vufr-1.5.0.tar.gz/vufr-1.5.0/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import find_packages, setup
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='vufr',
+    version='1.5.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A simple api wrapper for (shorten-url) vu.fr.',
+    keywords='shorten-url shorten vu.fr',
+    url='https://github.com/jiroawesome/vufr',
+    author='JiroDeveloper',
+    author_email='contact@jiroawesome.tech',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ]
+)
