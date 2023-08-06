0053b2792255944a11c068c708843e2533c02456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:50 +0000

    Add package arbitragepy/2.0.0/arbitragepy-2.0.0-py3-none-any.whl

---
+++ packages/arbitragepy/arbitragepy-2.0.0-py3-none-any.whl/arbitragepy/__init__.py
@@ -0,0 +1,40 @@
+from arbitragepy.arbitrage import arbitrage
+from arbitragepy.exceptions import (
+    ImcompabileQuantityIncrementsError,
+    NotionalLessThanMinNotionalError,
+    QuantityLessThanMinQuantityError,
+)
+from arbitragepy.fee import minus_fee, plus_fee
+from arbitragepy.models import (
+    ArbitragePayload,
+    ArbitrageResult,
+    SymbolInfo,
+    OrderInfo,
+    OrderPayload,
+)
+from arbitragepy.quantity_increment import (
+    is_compatible_quantity_increments,
+    to_compatible_quantity_increment,
+    validate_quantity_increments,
+)
+from arbitragepy.spread import get_spread
+
+
+__all__ = [
+    "arbitrage",
+    "minus_fee",
+    "plus_fee",
+    "ArbitragePayload",
+    "ArbitrageResult",
+    "SymbolInfo",
+    "OrderInfo",
+    "OrderPayload",
+    "ImcompabileQuantityIncrementsError",
+    "NotionalLessThanMinNotionalError",
+    "QuantityLessThanMinQuantityError",
+    "is_compatible_quantity_increments",
+    "to_compatible_quantity_increment",
+    "validate_quantity_increments",
+    "get_spread",
+]
+__version__ = "2.0.0"
