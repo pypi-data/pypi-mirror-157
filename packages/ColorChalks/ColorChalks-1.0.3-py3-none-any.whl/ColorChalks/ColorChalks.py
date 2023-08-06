44dbe01aad550b3ef74df298ea392490e510c0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:08 +0000

    Add package ColorChalks/1.0.3/ColorChalks-1.0.3-py3-none-any.whl

---
+++ packages/ColorChalks/ColorChalks-1.0.3-py3-none-any.whl/ColorChalks/ColorChalks.py
@@ -0,0 +1,43 @@
+class FCOLORS: 
+    Black = '\u001b[30m'
+    Red = '\u001b[31m'
+    Green = '\u001b[32m'
+    Yellow = '\u001b[33m'
+    Blue = '\u001b[34m'
+    Magenta = '\u001b[35m'
+    Cyan = '\u001b[36m'
+    White = '\u001b[37m'
+    BrightBlack = '\u001b[30;1m'
+    BrightRed = '\u001b[31;1m'
+    BrightGreen = '\u001b[32;1m'
+    BrightYellow = '\u001b[33;1m'
+    BrightBlue = '\u001b[34;1m'
+    BrightMagenta = '\u001b[35;1m'
+    BrightCyan = '\u001b[36;1m'
+    BrightWhite = '\u001b[37;1m'
+
+class COLORS: 
+    Reset = '\u001b[0m'
+    
+
+class BCOLORS:
+    Black = '\u001b[40m'
+    Red = '\u001b[41m'
+    Green = '\u001b[42m'
+    Yellow = '\u001b[43m'
+    Blue = '\u001b[44m'
+    Magenta = '\u001b[45m'
+    Cyan = '\u001b[46m'
+    White = '\u001b[47m'
+    BrightBlack = '\u001b[40;1m'
+    BrightRed = '\u001b[41;1m'
+    BrightGreen = '\u001b[42;1m'
+    BrightYellow = '\u001b[43;1m'
+    BrightBlue = '\u001b[44;1m'
+    BrightMagenta = '\u001b[45;1m'
+    BrightCyan = '\u001b[46;1m'
+    BrightWhite = '\u001b[47;1m'
+
+
+
+
