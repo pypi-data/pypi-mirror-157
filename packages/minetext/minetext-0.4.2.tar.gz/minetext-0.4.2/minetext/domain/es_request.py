c5141768e7ee2e78a470b48c6b57f0edee92406d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:27:49 +0000

    Add package minetext/0.4.2/minetext-0.4.2.tar.gz

---
+++ packages/minetext/minetext-0.4.2.tar.gz/minetext-0.4.2/minetext/domain/es_request.py
@@ -0,0 +1,53 @@
+from typing import List
+
+
+class EsRequest:
+    _search_term: str
+    _filters: List[str]
+    _aggregation: bool
+    _page: int
+    _size: int
+    _analytics: bool
+
+    def __init__(self, search_term: str, filters: List[str] = None,
+                 aggregation: bool = False, page: int = 1, size: int = 10, analytics: bool = False):
+        """
+        A class used to wrap all necessary input for the search endpoint.
+
+        :param search_term: a query which follows Lucene syntax
+        :param filters: filters applied on the query
+        :param aggregation: indicate if the result should be aggregated or not
+        :param page: number of page
+        :param size: number of item per page
+        :param analytics: indicate if the result should have analytics or not
+        """
+        self._search_term = search_term
+        self._filters = filters
+        self._aggregation = aggregation
+        self._page = page
+        self._size = size
+        self._analytics = analytics
+
+    @property
+    def search_term(self):
+        return self._search_term
+
+    @property
+    def filters(self):
+        return self._filters
+
+    @property
+    def aggregation(self):
+        return self._aggregation
+
+    @property
+    def page(self):
+        return self._page
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def analytics(self):
+        return self._analytics
---
+++ packages/minetext/minetext-0.4.2.tar.gz/minetext-0.4.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="minetext",
+    version="0.4.2",
+    author="Triet Doan",
+    author_email="triet.doan@gwdg.de",
+    description="Python client for MINE system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.gwdg.de/mine/mine-python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'elasticsearch-dsl',
+        'elasticsearch',
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Development Status :: 4 - Beta"
+    ],
+    python_requires='>=3.6',
+)
