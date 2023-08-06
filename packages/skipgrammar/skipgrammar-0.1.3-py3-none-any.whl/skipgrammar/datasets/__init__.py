f051024e7496e344164b9f29e1db403ca6645fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package skipgrammar/0.1.3/skipgrammar-0.1.3-py3-none-any.whl

---
+++ packages/skipgrammar/skipgrammar-0.1.3-py3-none-any.whl/skipgrammar/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .common import (UserItemIterableDataset, UserItemMapDataset,  # noqa
+                     get_file)
+from .lastfm import USER_PROFILE as LASTFM_USER_PROFILE  # noqa
+from .lastfm import VARIANTS as LASTFM_VARIANTS  # noqa
+from .lastfm import LastFM, LastFMUserItemDataset  # noqa
+from .movielens import VARIANTS as MOVIELENS_VARIANTS  # noqa
+from .movielens import MovieLens, MovieLensUserItemDataset  # noqa
