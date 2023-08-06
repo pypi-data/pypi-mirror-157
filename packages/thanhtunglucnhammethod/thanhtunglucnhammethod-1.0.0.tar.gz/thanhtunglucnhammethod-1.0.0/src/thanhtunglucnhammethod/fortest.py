42553a39fda77dcbf1ee88ebfb346a3beb2eabab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:56:35 +0000

    Add package thanhtunglucnhammethod/1.0.0/thanhtunglucnhammethod-1.0.0.tar.gz

---
+++ packages/thanhtunglucnhammethod/thanhtunglucnhammethod-1.0.0.tar.gz/thanhtunglucnhammethod-1.0.0/src/thanhtunglucnhammethod/fortest.py
@@ -0,0 +1,8 @@
+import fortest as f
+# Các chức năng cần dùng để lên quẻ sẽ được liệt kê ví dụ ở đây
+#print(f.AnCanChi('ẤT', 'TUẤT'))
+#print(f.AnNienMenh('MÃO', 1, 0))
+# print(f.AnTrachMo('MÃO'))
+#print(f.ThienBanTuKhoa('Tiểu tuyết', 'TỊ', 'TÂN', 'MÃO'))
+#print(f.ThienTuongQuyNhan('TỊ', 'TÂN', 'MÃO', 'Tiểu tuyết'))
+#print(f.GiaoKhacMaoTinhBietTrach('ĐINH', 'MÃO', 'Lập xuân', 'MÙI'))
