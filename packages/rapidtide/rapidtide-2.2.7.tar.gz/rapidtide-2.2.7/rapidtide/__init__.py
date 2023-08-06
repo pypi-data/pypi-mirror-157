ca55b1e77b297821f9dc8f99ce56863c3329eb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:31 +0000

    Add package rapidtide/2.2.7/rapidtide-2.2.7.tar.gz

---
+++ packages/rapidtide/rapidtide-2.2.7.tar.gz/rapidtide-2.2.7/rapidtide/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2021, Blaise Frederick
+
+# -*- coding: latin-1 -*-
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
