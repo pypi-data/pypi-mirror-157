22190e05d43adaf3eda5bdd932dfca4cd5aff03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:10 +0000

    Add package pybiolib/1.1.62/pybiolib-1.1.62.tar.gz

---
+++ packages/pybiolib/pybiolib-1.1.62.tar.gz/pybiolib-1.1.62/biolib/biolib_singularity_client/__init__.py
@@ -0,0 +1,26 @@
+from spython.main import Client # type: ignore
+from biolib.biolib_logging import logger
+
+
+class BiolibSingularityClient:
+    singularity_client = None
+
+    @staticmethod
+    def get_singularity_client():
+        if BiolibSingularityClient.singularity_client is None:
+            # version() returns empty string when singularity is not installed
+            if Client.version() == '':
+                raise Exception('Could not get singularity version')
+
+            BiolibSingularityClient.singularity_client = Client
+            logger.debug(f"Running Singularity: {BiolibSingularityClient.singularity_client.version()}")
+
+        return BiolibSingularityClient.singularity_client
+
+    @staticmethod
+    def is_singularity_running():
+        try:
+            BiolibSingularityClient.get_singularity_client()
+            return True
+        except Exception:  # pylint: disable=broad-except
+            return False
