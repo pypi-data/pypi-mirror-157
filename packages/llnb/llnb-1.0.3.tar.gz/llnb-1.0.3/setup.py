e554e4cd551796682a01518f6d25448874e07a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:52 +0000

    Add package llnb/1.0.3/llnb-1.0.3.tar.gz

---
+++ packages/llnb/llnb-1.0.3.tar.gz/llnb-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='llnb',  # 包名
+      version='1.0.3',  # 版本号
+      description='llnb',
+      author='kevin',
+      author_email='me@example.com',
+      url='',
+      install_requires=[],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
