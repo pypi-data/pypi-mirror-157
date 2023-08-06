d8036b6fefce4b6b0cf15d9e28baef42323ff1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:10 +0000

    Add package pytoolconfig/1.2.1/pytoolconfig-1.2.1.tar.gz

---
+++ packages/pytoolconfig/pytoolconfig-1.2.1.tar.gz/pytoolconfig-1.2.1/tests/test_config.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import sys
+
+from pytoolconfig.sources import IniConfig, PyProject, SetupConfig
+
+
+def test_base_pyproject(cwd):
+    pyproject = PyProject(cwd, "pytoolconfig", [])
+    assert pyproject.parse()["formatter"] == "black"
+    universal = pyproject.universalconfig()
+    assert universal.min_py_version == (3, 7)
+    assert universal.max_py_version == (sys.version_info.major, sys.version_info.minor)
+    assert universal.formatter == "black"
+    assert "sphinx" in [dep.name for dep in universal.optional_dependencies["doc"]]
+
+
+def test_base_ini(cwd):
+    config = IniConfig(cwd, "test_config.ini", "pytoolconfig").parse()
+    assert config["formatter"] == "yapf"
+
+
+def test_setup_cfg(cwd):
+    setup_cfg = SetupConfig(cwd, "pytoolconfig")
+    assert setup_cfg.parse()
