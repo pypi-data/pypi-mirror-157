8f75ea8db15cba5626e27d996f1d961e15d92b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:03 +0000

    Add package scale-lidar-io-debug/0.1.60/scale_lidar_io_debug-0.1.60-py3-none-any.whl

---
+++ packages/scale-lidar-io-debug/scale_lidar_io_debug-0.1.60-py3-none-any.whl/scale_lidar_io_debug/__init__.py
@@ -0,0 +1,7 @@
+from scale_lidar_io import *
+from .JSONBinaryEncoder import JSONBinaryEncoder as JSONBinaryEncoder
+from .scene import DebugLidarScene as LidarScene
+
+
+def import_test():
+    print('Import OK')
