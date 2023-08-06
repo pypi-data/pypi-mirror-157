fbeac12b1ea0e7afadf6d748aff014e5d9ed7506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:56 +0000

    Add package streamlit-graphic-slider/0.2.0/streamlit-graphic-slider-0.2.0.tar.gz

---
+++ packages/streamlit-graphic-slider/streamlit-graphic-slider-0.2.0.tar.gz/streamlit-graphic-slider-0.2.0/streamlit_graphic_slider/frontend/build/precache-manifest.165b9d4e73fd2dbe42e4a7bdf06bbb2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d2f48e0a05e71735045027fec93725",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70cc828aef0f7429db50",
+    "url": "./static/js/2.1dbb1af8.chunk.js"
+  },
+  {
+    "revision": "064e9f956fb3abf44a50f235d62ce726",
+    "url": "./static/js/2.1dbb1af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1a70953b5daaa12341",
+    "url": "./static/js/main.50ae3759.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
