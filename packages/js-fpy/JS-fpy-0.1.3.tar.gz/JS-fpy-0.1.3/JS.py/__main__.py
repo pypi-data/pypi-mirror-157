09c1f3f339ec233319d65f001abb0ec432999309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:31 +0000

    Add package js-fpy/0.1.3/JS-fpy-0.1.3.tar.gz

---
+++ packages/js-fpy/JS-fpy-0.1.3.tar.gz/JS-fpy-0.1.3/JS.py/__main__.py
@@ -0,0 +1,46 @@
+import os
+import platform
+import sys
+from Build.Buildtools import finalize, build
+import shutil
+pl = platform.system()
+args = sys.argv
+if args[1] == 'build':
+    if os.path.splitext(__file__)[1] != '.bin' and pl == 'Linux':
+        print('\033[0;91mWarning: Use ./JS.py/m.bin for faster times.')
+
+    if args[2].startswith('./'):
+        t = os.getcwd()+args[2].replace('./','/')
+    else:
+        t = args[2]
+    if not t.endswith('.pyjs'):
+        print('Warning: Chosen file does not end with .pyjs')
+    print('\033[0;37m')
+    print('\rPrepraring for build...',flush=True, end="")
+    p = os.path.dirname(t)+'/'+os.path.basename(t)+'.build/'
+    os.mkdir(p)
+    os.mkdir(os.path.dirname(t)+'/'+os.path.basename(t)+'.build/cache')
+    with open(p+'parse.log', 'w'):
+        pass
+    with open(p+'build.log', 'w'):
+        pass
+    print('\rPrepraring for build... Done')
+    print('\rBuilding:')
+    build(t)
+    print('\rFinalizing...',flush=True, end="")
+    finalize(p, sys.argv, os.path.basename(t))
+    print('\rFinalizing... Done')
+    print('\rBuild complete')
+    if '-b' in args[3:]:
+        shutil.rmtree(p)
+    else:
+        print('Build dir kept.')
+    print('JS built')
+elif args[1] == "help":
+    help = """
+    * build <path> <any: -b -o>
+    \tpath: the file path to the file you want to build.
+    \tb: don't keep the build files.
+    \to: build the debug file outside the build files.
+    """
+    print(help)
