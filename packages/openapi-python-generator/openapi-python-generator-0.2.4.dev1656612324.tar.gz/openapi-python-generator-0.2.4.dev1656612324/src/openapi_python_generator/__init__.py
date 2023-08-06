ef3727941e2a8f4d038caa287005d5450b475d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:18 +0000

    Add package openapi-python-generator/0.2.4.dev1656612324/openapi-python-generator-0.2.4.dev1656612324.tar.gz

---
+++ packages/openapi-python-generator/openapi-python-generator-0.2.4.dev1656612324.tar.gz/openapi-python-generator-0.2.4.dev1656612324/src/openapi_python_generator/__init__.py
@@ -0,0 +1,11 @@
+"""Python client from an OPENAPI 3.0 specification in seconds."""
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
