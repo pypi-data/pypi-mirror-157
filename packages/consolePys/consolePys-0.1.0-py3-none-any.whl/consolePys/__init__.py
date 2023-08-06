c66967345b917a0c35c2e0412992e3bd9a4dec87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:39 +0000

    Add package consolePys/0.1.0/consolePys-0.1.0-py3-none-any.whl

---
+++ packages/consolePys/consolePys-0.1.0-py3-none-any.whl/consolePys/__init__.py
@@ -0,0 +1,32 @@
+from .PyConsole import Console
+from .PyConsole import activeColor
+
+__all__ = ["activeColor","Console"]
+
+"""
+# useg
+```py
+from consolePy import Console
+from consolePy import activeColor
+activeColor() # make sure to call this when using cmd/powershell/etc
+
+Console.error  ("Hello, World!")
+Console.fail   ("Hello, World!")
+Console.logc   ("Hello, World!")
+Console.log    ("Hello, World!")
+Console.info   ("Hello, World!")
+Console.header ("Hello, World!")
+Console.warn   ("Hello, World!")
+```
+"""
+__author__ = 'Alawi Hussein Adnan Al Sayegh'
+__description__ = 'Console class to print info and errors, warns easily'
+__license__ = """
+Copyright 2022 "Alawi Hussein Adnan Al Sayegh"
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+and the names are copyrighted
+"""
+name = "ConsolePy"
+
