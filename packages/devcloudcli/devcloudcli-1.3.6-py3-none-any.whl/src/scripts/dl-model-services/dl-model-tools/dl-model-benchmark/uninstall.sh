5e6a406547939db96b3be8dd0eb23510b2fa9229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:51 +0000

    Add package devcloudcli/1.3.6/devcloudcli-1.3.6-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.3.6-py3-none-any.whl/src/scripts/dl-model-services/dl-model-tools/dl-model-benchmark/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+unlink workload/tools
+echo -e "\e[1;32mDl-model-benchmark Symbolic link Unlinked\e[0m"
