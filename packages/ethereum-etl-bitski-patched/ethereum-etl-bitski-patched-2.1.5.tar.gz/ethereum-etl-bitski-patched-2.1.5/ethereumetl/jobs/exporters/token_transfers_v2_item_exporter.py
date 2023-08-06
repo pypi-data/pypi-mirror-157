eb2c326d4e6b9728d1ae3d761a000c619238d22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:35 +0000

    Add package ethereum-etl-bitski-patched/2.1.5/ethereum-etl-bitski-patched-2.1.5.tar.gz

---
+++ packages/ethereum-etl-bitski-patched/ethereum-etl-bitski-patched-2.1.5.tar.gz/ethereum-etl-bitski-patched-2.1.5/ethereumetl/jobs/exporters/token_transfers_v2_item_exporter.py
@@ -0,0 +1,27 @@
+
+from blockchainetl.jobs.exporters.composite_item_exporter import CompositeItemExporter
+
+FIELDS_TO_EXPORT = [
+    'contract_address',
+    'from_address',
+    'to_address',
+    'amount',
+    'token_type',
+    'token_id',
+    'transaction_hash',
+    'log_index',
+    'block_number',
+    'chain_id'
+]
+
+
+def token_transfers_v2_item_exporter(token_transfer_output, converters=()):
+    return CompositeItemExporter(
+        filename_mapping={
+            'token_transfer_v2': token_transfer_output
+        },
+        field_mapping={
+            'token_transfer_v2': FIELDS_TO_EXPORT
+        },
+        converters=converters
+    )
