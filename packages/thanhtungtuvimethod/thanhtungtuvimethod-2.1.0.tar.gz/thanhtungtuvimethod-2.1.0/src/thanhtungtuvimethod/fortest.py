ddd30196644bec7987d0e91170139da6179daad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:01 +0000

    Add package thanhtungtuvimethod/2.1.0/thanhtungtuvimethod-2.1.0.tar.gz

---
+++ packages/thanhtungtuvimethod/thanhtungtuvimethod-2.1.0.tar.gz/thanhtungtuvimethod-2.1.0/src/thanhtungtuvimethod/fortest.py
@@ -0,0 +1,45 @@
+import method as m
+x = 'CANH TUẤT'
+y = 5
+z = 'SỬU'
+t = 0
+
+#print(f"Nạp âm của ĐINH MÃO là {m.NguHanhNapAm('ĐINH', 'MÃO')}")
+#print(f"Thập thần tứ trụ: {x} lấy {y} làm {m.ThapThanTuTru(x, y)} ")
+#print(f"Âm nam dương nữ với can năm {x} và namNu = {y}, (nam 1, nữ 0) là {m.AmNamDuongNu(x, y)}")
+print(f"Số đại vận tứ trụ y = {y} là {m.ListSoDaiVanTuTru(y)}")
+print(
+    f"List can chi đại vận tứ trụ từ {x} đi {t} là {m.ListCanChiDaiVanTuTru(x,y)}")
+#print(f"Số tt cung an mệnh từ index tháng {x}, index giờ {y} là {m.SoCungAnMenh(x,y)}")
+#print(f"List cung mệnh thân, với chi tháng {x}, giờ {y} là {m.ListCungMenhThan(x,y)}")
+#print(f"Sao chủ mệnh thân năm {x} là {m.SaoChuMenhThan(x)} ")
+#print(f"Cục số can năm {x}, tháng {y}, giờ {z} là {m.CucSo(x, y, z)}")
+#print(f"Đại vận can năm {x}, tháng {y}, giờ {z} đi {t} là {m.DaiVan(x,y,z,t)}")
+# print(
+#    f"Vòng Tử vi Thiên phủ ngày {x} cục {y} là {m.VongTuViThienPhu(x,y)[0:12]} ")
+# print(
+#    f"Vòng Tử vi Thiên phủ ngày {x} cục {y} là {m.VongTuViThienPhu(x,y)[12:]} ")
+#print(f"Vòng bác sĩ {m.VongBacSi_Ten(x, t)}")
+#print(f"Vòng tướng tinh {m.VongTuongTinh(x)}")
+#print(f"Vòng thái tuế {m.VongThaiTue(x)}")
+#print(f"Vòng trường sinh {m.VongTrangSinh(y,1)}")
+# print(m.TaHuuHinhDieuThienDiaGiai(x))
+
+
+chiThang = 'TUẤT'
+chiGio = 'SỬU'
+canNam = 'ĐINH'
+chiNam = 'MÃO'
+thuanNghich = 0
+soNgay = 14
+soCuc = 5
+soChiGio = 1
+soChiThang = 10
+canNamLuuNien = 'NHÂM'
+chiNamLuuNien = 'DẦN'
+
+#ketQua = m.An12Cung(canNam, chiNam, chiThang, chiGio, thuanNghich,soNgay, soCuc, soChiThang, soChiGio, chiNamLuuNien, canNamLuuNien)
+
+#print('Cung Tý trái: ', ketQua[0][0])
+#print('Cung Tý phải: ', ketQua[0][1])
+#print('Cung Tý phải: ', ketQua[0][0][12])
