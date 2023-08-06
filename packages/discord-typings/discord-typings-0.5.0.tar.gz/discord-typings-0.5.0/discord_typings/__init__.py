f58545105f47c2ea0aedb8741623c97f98d17e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:49:59 +0000

    Add package discord-typings/0.5.0/discord-typings-0.5.0.tar.gz

---
+++ packages/discord-typings/discord-typings-0.5.0.tar.gz/discord-typings-0.5.0/discord_typings/__init__.py
@@ -0,0 +1,8 @@
+"""Maintained typings of all objects Discord sends as `TypedDict`s."""
+
+__version__ = "1.0.0"
+
+from .gateway import *
+from .interactions import *
+from .reference import *
+from .resources import *
---
+++ packages/discord-typings/discord-typings-0.5.0.tar.gz/discord-typings-0.5.0/discord_typings/resources/application.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Optional
+
+from typing_extensions import NotRequired, TypedDict, final
+
+if TYPE_CHECKING:
+    from ..reference import Snowflake
+    from .user import UserData
+
+__all__ = ('ApplicationData', 'TeamData', 'TeamMemberData')
+
+
+# https://discord.com/developers/docs/resources/application#application-object-application-structure
+
+
+@final
+class ApplicationData(TypedDict):
+    id: Snowflake
+    name: str
+    icon: Optional[str]
+    description: str
+    rpc_origins: List[str]
+    bot_public: bool
+    bot_require_code_grant: bool
+    terms_of_service_url: NotRequired[str]
+    privacy_policy_url: NotRequired[str]
+    owner: NotRequired[UserData]
+    summary: str
+    verify_key: str
+    team: Optional[TeamData]
+    guild_id: NotRequired[Snowflake]
+    primary_sku_id: NotRequired[Snowflake]
+    slug: NotRequired[str]
+    cover_image: NotRequired[str]
+    flags: NotRequired[int]
+
+
+# https://discord.com/developers/docs/topics/teams#data-models-team-object
+
+
+@final
+class TeamData(TypedDict):
+    icon: Optional[str]
+    id: Snowflake
+    members: List[TeamMemberData]
+    name: str
+    owner_user_id: Snowflake
+
+
+# https://discord.com/developers/docs/topics/teams#data-models-team-member-object
+
+
+@final
+class TeamMemberData(TypedDict):
+    membership_state: int
+    permissions: List[str]
+    team_id: Snowflake
+    user: UserData
