6cb098e9d540c2db925af14812591d5b706882ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:51 +0000

    Add package ha-geo-detector/2/ha_geo_detector-2.tar.gz

---
+++ packages/ha-geo-detector/ha_geo_detector-2.tar.gz/ha_geo_detector-2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+import setuptools
+
+
+setuptools.setup(
+    name='ha_geo_detector',
+    version='2',
+    description='A demo for python packaging.',
+    long_description='A demo for python packaging.',
+    author='korotkov',  # Замените именем учетной записи на официальном сайте Pypi
+    author_email='korotkov@hypeauditor.com ',  # Замените адресом электронной почты, привязанным к имени вашей учетной записи Pypi
+    packages=setuptools.find_packages(),
+    url='https://github.com/liahimratman/ha_geo_detector',
+    include_package_data=True,
+    license='MIT')
