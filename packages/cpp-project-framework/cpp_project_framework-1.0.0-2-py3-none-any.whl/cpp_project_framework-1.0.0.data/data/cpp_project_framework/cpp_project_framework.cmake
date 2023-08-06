7f8555b18d928e465d82c573fbff562fa0772beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:03 +0000

    Add package cpp-project-framework/1.0.0/cpp_project_framework-1.0.0-2-py3-none-any.whl

---
+++ packages/cpp-project-framework/cpp_project_framework-1.0.0-2-py3-none-any.whl/cpp_project_framework-1.0.0.data/data/cpp_project_framework/cpp_project_framework.cmake
@@ -0,0 +1,13 @@
+# @file     cpp_project_framework.cmake
+# @author   Curtis Lo
+
+include(${CMAKE_SOURCE_DIR}/cpp_project_framework_callables.cmake)
+
+cpf_detect_build_type()
+cpf_detect_virtual_environment()
+cpf_install_conan_dependencies()
+cpf_inject_conan_info()
+cpf_install_cpp_project_framework_files()
+
+# enable testing support
+enable_testing()
