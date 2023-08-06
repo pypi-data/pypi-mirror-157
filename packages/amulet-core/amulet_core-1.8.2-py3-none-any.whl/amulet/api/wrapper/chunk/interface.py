f19556a9ad5efbaacf4154c8c5d0e8e93553c54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:21 +0000

    Add package amulet-core/1.8.2/amulet_core-1.8.2-py3-none-any.whl

---
+++ packages/amulet-core/amulet_core-1.8.2-py3-none-any.whl/amulet/api/wrapper/chunk/interface.py
@@ -0,0 +1,437 @@
+from __future__ import annotations
+
+import warnings
+from abc import ABC, abstractmethod
+from typing import (
+    Tuple,
+    Any,
+    Union,
+    TYPE_CHECKING,
+    Optional,
+    overload,
+    Type,
+    Iterable,
+    Sequence,
+    Callable,
+)
+from enum import Enum
+
+from amulet.api.block_entity import BlockEntity
+from amulet.api.entity import Entity
+from amulet.api.data_types import AnyNDArray, VersionNumberAny, VersionIdentifierType
+import amulet_nbt
+from amulet_nbt import TAG_List, TAG_Compound, AnyNBT, BaseValueType
+
+if TYPE_CHECKING:
+    from amulet.api.wrapper import Translator
+    from amulet.api.chunk import Chunk
+
+
+class EntityIDType(Enum):
+    int_id = 0
+    str_id = 1
+    namespace_str_id = 2
+    namespace_str_Id = 3
+    namespace_str_identifier = 4
+
+
+class EntityCoordType(Enum):
+    xyz_int = 0
+    Pos_list_float = 1
+    Pos_list_double = 2
+    Pos_list_int = 3
+    Pos_array_int = 4
+
+
+PosTypeMap = {
+    3: EntityCoordType.Pos_list_int,
+    5: EntityCoordType.Pos_list_float,
+    6: EntityCoordType.Pos_list_double,
+}
+
+
+class Interface(ABC):
+    @abstractmethod
+    def decode(self, *args, **kwargs) -> Tuple["Chunk", AnyNDArray]:
+        raise NotImplementedError
+
+    def _decode_entity(
+        self,
+        nbt: amulet_nbt.NBTFile,
+        id_type: EntityIDType,
+        coord_type: EntityCoordType,
+    ) -> Optional[Entity]:
+        entity = self._decode_base_entity(nbt, id_type, coord_type)
+        if entity is not None:
+            namespace, base_name, x, y, z, nbt = entity
+            return Entity(
+                namespace=namespace, base_name=base_name, x=x, y=y, z=z, nbt=nbt
+            )
+
+    def _decode_block_entity(
+        self,
+        nbt: amulet_nbt.NBTFile,
+        id_type: EntityIDType,
+        coord_type: EntityCoordType,
+    ) -> Optional[BlockEntity]:
+        entity = self._decode_base_entity(nbt, id_type, coord_type)
+        if entity is not None:
+            namespace, base_name, x, y, z, nbt = entity
+            return BlockEntity(
+                namespace=namespace, base_name=base_name, x=x, y=y, z=z, nbt=nbt
+            )
+
+    @staticmethod
+    def _decode_base_entity(
+        nbt: amulet_nbt.NBTFile, id_type: EntityIDType, coord_type: EntityCoordType
+    ) -> Optional[
+        Tuple[
+            str,
+            str,
+            Union[int, float],
+            Union[int, float],
+            Union[int, float],
+            amulet_nbt.NBTFile,
+        ]
+    ]:
+        if not (
+            isinstance(nbt, amulet_nbt.NBTFile)
+            and isinstance(nbt.value, amulet_nbt.TAG_Compound)
+        ):
+            return
+
+        if id_type == EntityIDType.namespace_str_id:
+            entity_id = nbt.pop("id", amulet_nbt.TAG_String(""))
+            if (
+                not isinstance(entity_id, amulet_nbt.TAG_String)
+                or entity_id.value == ""
+                or ":" not in entity_id.value
+            ):
+                return
+            namespace, base_name = entity_id.value.split(":", 1)
+
+        elif id_type == EntityIDType.namespace_str_Id:
+            entity_id = nbt.pop("Id", amulet_nbt.TAG_String(""))
+            if (
+                not isinstance(entity_id, amulet_nbt.TAG_String)
+                or entity_id.value == ""
+                or ":" not in entity_id.value
+            ):
+                return
+            namespace, base_name = entity_id.value.split(":", 1)
+
+        elif id_type == EntityIDType.str_id:
+            entity_id = nbt.pop("id", amulet_nbt.TAG_String(""))
+            if (
+                not isinstance(entity_id, amulet_nbt.TAG_String)
+                or entity_id.value == ""
+            ):
+                return
+            namespace = ""
+            base_name = entity_id.value
+
+        elif id_type in [EntityIDType.namespace_str_identifier, EntityIDType.int_id]:
+            if "identifier" in nbt:
+                entity_id = nbt.pop("identifier")
+                if (
+                    not isinstance(entity_id, amulet_nbt.TAG_String)
+                    or entity_id.value == ""
+                    or ":" not in entity_id.value
+                ):
+                    return
+                namespace, base_name = entity_id.value.split(":", 1)
+            elif "id" in nbt:
+                entity_id = nbt.pop("id")
+                if not isinstance(entity_id, amulet_nbt.TAG_Int):
+                    return
+                namespace = ""
+                base_name = str(entity_id.value)
+            else:
+                return
+        else:
+            raise NotImplementedError(f"Entity id type {id_type}")
+
+        if coord_type in [
+            EntityCoordType.Pos_list_double,
+            EntityCoordType.Pos_list_float,
+            EntityCoordType.Pos_list_int,
+        ]:
+            if "Pos" not in nbt:
+                return
+            pos = nbt.pop("Pos")
+            pos: amulet_nbt.TAG_List
+
+            if (
+                not isinstance(pos, amulet_nbt.TAG_List)
+                or len(pos) != 3
+                or PosTypeMap.get(pos.list_data_type) != coord_type
+            ):
+                return
+            x, y, z = [c.value for c in pos]
+        elif coord_type == EntityCoordType.Pos_array_int:
+            if "Pos" not in nbt:
+                return
+            pos = nbt.pop("Pos")
+            pos: amulet_nbt.TAG_Int_Array
+
+            if not isinstance(pos, amulet_nbt.TAG_Int_Array) or len(pos) != 3:
+                return
+            x, y, z = pos
+        elif coord_type == EntityCoordType.xyz_int:
+            if not all(
+                c in nbt and isinstance(nbt[c], amulet_nbt.TAG_Int)
+                for c in ("x", "y", "z")
+            ):
+                return
+            x, y, z = [nbt.pop(c).value for c in ("x", "y", "z")]
+        else:
+            raise NotImplementedError(f"Entity coord type {coord_type}")
+
+        return namespace, base_name, x, y, z, nbt
+
+    @abstractmethod
+    def encode(self, *args, **kwargs) -> Any:
+        """
+        Take a version-specific chunk and encode it to raw data for the format to store.
+        """
+        raise NotImplementedError
+
+    def _encode_entity(
+        self, entity: Entity, id_type: EntityIDType, coord_type: EntityCoordType
+    ) -> Optional[amulet_nbt.NBTFile]:
+        return self._encode_base_entity(entity, id_type, coord_type)
+
+    def _encode_block_entity(
+        self, entity: BlockEntity, id_type: EntityIDType, coord_type: EntityCoordType
+    ) -> Optional[amulet_nbt.NBTFile]:
+        return self._encode_base_entity(entity, id_type, coord_type)
+
+    @staticmethod
+    def _encode_base_entity(
+        entity: Union[Entity, BlockEntity],
+        id_type: EntityIDType,
+        coord_type: EntityCoordType,
+    ) -> Optional[amulet_nbt.NBTFile]:
+        if not isinstance(entity.nbt, amulet_nbt.NBTFile) and isinstance(
+            entity.nbt.value, amulet_nbt.TAG_Compound
+        ):
+            return
+        nbt = entity.nbt
+
+        if id_type == EntityIDType.namespace_str_id:
+            nbt["id"] = amulet_nbt.TAG_String(entity.namespaced_name)
+        elif id_type == EntityIDType.namespace_str_Id:
+            nbt["Id"] = amulet_nbt.TAG_String(entity.namespaced_name)
+        elif id_type == EntityIDType.namespace_str_identifier:
+            nbt["identifier"] = amulet_nbt.TAG_String(entity.namespaced_name)
+        elif id_type == EntityIDType.str_id:
+            nbt["id"] = amulet_nbt.TAG_String(entity.base_name)
+        elif id_type == EntityIDType.int_id:
+            if not entity.base_name.isnumeric():
+                return
+            nbt["id"] = amulet_nbt.TAG_Int(int(entity.base_name))
+        else:
+            raise NotImplementedError(f"Entity id type {id_type}")
+
+        if coord_type == EntityCoordType.Pos_list_double:
+            nbt["Pos"] = amulet_nbt.TAG_List(
+                [
+                    amulet_nbt.TAG_Double(float(entity.x)),
+                    amulet_nbt.TAG_Double(float(entity.y)),
+                    amulet_nbt.TAG_Double(float(entity.z)),
+                ]
+            )
+        elif coord_type == EntityCoordType.Pos_list_float:
+            nbt["Pos"] = amulet_nbt.TAG_List(
+                [
+                    amulet_nbt.TAG_Float(float(entity.x)),
+                    amulet_nbt.TAG_Float(float(entity.y)),
+                    amulet_nbt.TAG_Float(float(entity.z)),
+                ]
+            )
+        elif coord_type == EntityCoordType.Pos_list_int:
+            nbt["Pos"] = amulet_nbt.TAG_List(
+                [
+                    amulet_nbt.TAG_Int(int(entity.x)),
+                    amulet_nbt.TAG_Int(int(entity.y)),
+                    amulet_nbt.TAG_Int(int(entity.z)),
+                ]
+            )
+        elif coord_type == EntityCoordType.Pos_array_int:
+            nbt["Pos"] = amulet_nbt.TAG_Int_Array(
+                [int(entity.x), int(entity.y), int(entity.z)]
+            )
+        elif coord_type == EntityCoordType.xyz_int:
+            nbt["x"] = amulet_nbt.TAG_Int(int(entity.x))
+            nbt["y"] = amulet_nbt.TAG_Int(int(entity.y))
+            nbt["z"] = amulet_nbt.TAG_Int(int(entity.z))
+        else:
+            raise NotImplementedError(f"Entity coord type {coord_type}")
+
+        return nbt
+
+    @overload
+    @staticmethod
+    def check_type(obj: TAG_Compound, key: str, dtype: Type[AnyNBT]) -> bool:
+        ...
+
+    @overload
+    @staticmethod
+    def check_type(obj: TAG_List, key: int, dtype: Type[AnyNBT]) -> bool:
+        ...
+
+    @staticmethod
+    def check_type(
+        obj: Union[TAG_Compound, TAG_List], key: Union[str, int], dtype: Type[AnyNBT]
+    ) -> bool:
+        """Check the key exists and the type is correct."""
+        return key in obj and isinstance(obj[key], dtype)
+
+    @overload
+    def get_obj(
+        self,
+        obj: TAG_Compound,
+        key: str,
+        dtype: Type[AnyNBT],
+        default: Optional[AnyNBT] = None,
+    ) -> Optional[AnyNBT]:
+        ...
+
+    @overload
+    def get_obj(
+        self,
+        obj: TAG_List,
+        key: int,
+        dtype: Type[AnyNBT],
+        default: Optional[AnyNBT] = None,
+    ) -> Optional[AnyNBT]:
+        ...
+
+    def get_obj(
+        self, obj, key, dtype: Type[AnyNBT], default: Optional[AnyNBT] = None
+    ) -> Optional[AnyNBT]:
+        """Pop a key from a container object if it exists and the type is correct. Otherwise return default.
+        This works in much the same way as dict.get but uses default if the data type does not match.
+
+        :param obj: The TAG_Compound to read from.
+        :param key: The key to use.
+        :param dtype: The expected data type.
+        :param default: The default value to use if the existing is not valid. If None will use dtype()
+        :return: The final value in the key.
+        """
+        if key in obj:
+            if isinstance(obj[key], dtype):
+                # if it exists and is correct
+                return obj.pop(key)
+        if default is None:
+            return dtype()
+        return default
+
+    def get_nested_obj(
+        self,
+        obj: Union[TAG_Compound, TAG_List],
+        path: Sequence[  # The path to the object.
+            Tuple[Union[str, int], Type[BaseValueType]],
+        ],
+        default: Union[None, AnyNBT, Callable[[], Any]] = None,
+        *,
+        pop_last=False,
+    ):
+        """
+        Get an object from a nested NBT structure
+
+        :param obj: The root NBT object
+        :param path: The path to the desired object (key, dtype)
+        :param default: The default value to use if the existing is not valid. If default is callable then return the called result.
+        :param pop_last: If true the last key will be popped
+        :return:
+        """
+        try:
+            last_index = len(path) - 1
+            for i, (key, dtype) in enumerate(path):
+                if i == last_index and pop_last:
+                    obj = obj.pop(key)
+                else:
+                    obj = obj[key]
+                if dtype is not obj.__class__:
+                    raise TypeError
+        except (KeyError, IndexError, TypeError):
+            if default is None or isinstance(default, BaseValueType):
+                return default
+            elif callable(default):
+                return default()
+            else:
+                raise TypeError(
+                    "default must be None, an NBT instance or an NBT class."
+                )
+        else:
+            return obj
+
+    @staticmethod
+    def set_obj(
+        obj: TAG_Compound,
+        key: str,
+        dtype: Type[AnyNBT],
+        default: Union[None, AnyNBT, Callable[[], Any]] = None,
+        path: Sequence[str] = (),  # The path to the object.
+        *,
+        setdefault=False,
+    ) -> AnyNBT:
+        """
+        Works like setdefualt on a dictionary but works with an optional nested path.
+
+        :param obj: The compound tag to get the data from
+        :param key: The key to setdefault
+        :param dtype: The dtype that the key must be
+        :param default: The default value to set if it does not exist or the type is wrong
+        :param path: Optional path to the nested compound.
+        :param setdefault: If True will behave like setdefault. If False will replace existing data.
+        :return: The data at the path
+        """
+        for path_key in path:
+            obj_ = obj.get(path_key, None)
+            if not isinstance(obj_, TAG_Compound):
+                # if it does not exist or the type is wrong then create it
+                obj_ = obj[path_key] = TAG_Compound()
+            obj = obj_
+        obj_ = obj.get(key, None)
+        if not setdefault or not isinstance(obj_, dtype):
+            # if it does not exist or the type is wrong then create it
+            if default is None:
+                obj_ = dtype()
+            elif isinstance(default, BaseValueType):
+                obj_ = default
+            elif callable(default):
+                obj_ = default()
+            else:
+                raise TypeError(
+                    "default must be None, an NBT instance or an NBT class."
+                )
+            obj[key] = obj_
+        return obj_
+
+    @abstractmethod
+    def get_translator(
+        self,
+        max_world_version: VersionIdentifierType,
+        data: Any = None,
+    ) -> Tuple["Translator", VersionNumberAny]:
+        """
+        Get the Translator class for the requested version.
+        :param max_world_version: The game version the world was last opened in. Version number tuple or data version number.
+        :param data: Optional data to get translator based on chunk version rather than world version
+        :return: Tuple[Translator, version number for PyMCTranslate to use]
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def is_valid(key: Tuple) -> bool:
+        """
+        Returns whether this Interface is able to interface with the chunk type with a given identifier key,
+        generated by the format.
+
+        :param key: The key who's decodability needs to be checked.
+        :return: True if this interface can interface with the chunk version associated with the key, False otherwise.
+        """
+        raise NotImplementedError
---
+++ packages/amulet-core/amulet_core-1.8.2-py3-none-any.whl/amulet/level/interfaces/chunk/anvil/anvil_1444.py
@@ -0,0 +1,327 @@
+from __future__ import annotations
+
+from typing import Dict, Set, Tuple, Iterable, Optional, TYPE_CHECKING
+
+import numpy
+from amulet_nbt import (
+    TAG_Compound,
+    TAG_List,
+    TAG_String,
+    TAG_Long_Array,
+    TAG_Long,
+    TAG_Short,
+)
+
+import amulet
+from amulet.api.data_types import AnyNDArray, BlockCoordinates
+from amulet.api.block import Block
+from amulet.api.chunk import StatusFormats
+from .base_anvil_interface import ChunkDataType, ChunkPathType
+from .anvil_0 import Anvil0Interface as ParentInterface
+from amulet.utils.world_utils import (
+    decode_long_array,
+    encode_long_array,
+)
+
+if TYPE_CHECKING:
+    from amulet.api.chunk import Chunk
+
+
+class Anvil1444Interface(ParentInterface):
+    """
+    Moved TerrainPopulated and LightPopulated to Status
+    Made blocks paletted
+    Added more tick tags
+    Added structures tag
+    """
+
+    TerrainPopulated = None
+    LightPopulated = None
+    Status: ChunkPathType = (
+        "region",
+        [("Level", TAG_Compound), ("Status", TAG_String)],
+        TAG_String("full"),
+    )
+
+    ToBeTicked: ChunkPathType = (
+        "region",
+        [("Level", TAG_Compound), ("ToBeTicked", TAG_List)],
+        TAG_List,
+    )
+
+    LiquidTicks: ChunkPathType = (
+        "region",
+        [("Level", TAG_Compound), ("LiquidTicks", TAG_List)],
+        TAG_List,
+    )
+    LiquidsToBeTicked: ChunkPathType = (
+        "region",
+        [("Level", TAG_Compound), ("LiquidsToBeTicked", TAG_List)],
+        TAG_List,
+    )
+
+    PostProcessing: ChunkPathType = (
+        "region",
+        [("Level", TAG_Compound), ("PostProcessing", TAG_List)],
+        TAG_List,
+    )
+
+    Structures: ChunkPathType = (
+        "region",
+        [("Level", TAG_Compound), ("Structures", TAG_Compound)],
+        TAG_Compound,
+    )
+
+    LongArrayDense = True
+
+    def __init__(self):
+        super().__init__()
+        self._set_feature("status", StatusFormats.Java_13)
+
+        self._register_decoder(self._decode_fluid_ticks)
+        self._register_decoder(self._decode_post_processing)
+        self._register_decoder(self._decode_structures)
+
+        self._register_encoder(self._encode_fluid_ticks)
+        self._register_encoder(self._encode_post_processing)
+        self._register_encoder(self._encode_structures)
+
+    @staticmethod
+    def minor_is_valid(key: int):
+        return 1444 <= key < 1466
+
+    def _decode_status(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        chunk.status = self.get_layer_obj(data, self.Status, pop_last=True).value
+
+    def _decode_block_section(
+        self, section: TAG_Compound
+    ) -> Optional[Tuple[numpy.ndarray, list]]:
+        if "Palette" not in section:  # 1.14 makes block_palette/blocks optional.
+            return None
+        section_palette = self._decode_block_palette(section.pop("Palette"))
+        decoded = decode_long_array(
+            section.pop("BlockStates").value,
+            4096,
+            max(4, (len(section_palette) - 1).bit_length()),
+            dense=self.LongArrayDense,
+        ).astype(numpy.uint32)
+        arr = numpy.transpose(decoded.reshape((16, 16, 16)), (2, 0, 1))
+        return arr, section_palette
+
+    def _decode_blocks(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        blocks: Dict[int, numpy.ndarray] = {}
+        palette = [Block(namespace="minecraft", base_name="air")]
+
+        for cy, section in self._iter_sections(data):
+            data = self._decode_block_section(section)
+            if data is not None:
+                arr, section_palette = data
+                blocks[cy] = arr + len(palette)
+                palette += section_palette
+
+        np_palette, inverse = numpy.unique(palette, return_inverse=True)
+        np_palette: numpy.ndarray
+        inverse: numpy.ndarray
+        inverse = inverse.astype(numpy.uint32)
+        for cy in blocks:
+            blocks[cy] = inverse[blocks[cy]]
+        chunk.blocks = blocks
+        chunk.misc["block_palette"] = np_palette
+
+    @staticmethod
+    def _decode_block_palette(palette: TAG_List) -> list:
+        blockstates = []
+        for entry in palette:
+            namespace, base_name = entry["Name"].value.split(":", 1)
+            properties = entry.get("Properties", TAG_Compound({})).value
+            block = Block(
+                namespace=namespace, base_name=base_name, properties=properties
+            )
+            blockstates.append(block)
+        return blockstates
+
+    @staticmethod
+    def _decode_to_be_ticked(ticks: TAG_List, floor_cy: int) -> Set[BlockCoordinates]:
+        ticks_out = set()
+        for cy, section_ticks in enumerate(ticks):
+            block_cy = (floor_cy + cy) << 4
+            for pos in section_ticks:
+                # TODO: check if these are correct. The order may be wrong.
+                x = pos & 0xF
+                y = (pos >> 4) & 0xF
+                z = (pos >> 8) & 0xF
+                ticks_out.add((x, block_cy + y, z))
+        return ticks_out
+
+    def _decode_block_ticks(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        super()._decode_block_ticks(chunk, data, floor_cy, height_cy)
+        chunk.misc["to_be_ticked"] = self._decode_to_be_ticked(
+            self.get_layer_obj(data, self.ToBeTicked, pop_last=True), floor_cy
+        )
+
+    def _decode_fluid_ticks(self, chunk: Chunk, data: ChunkDataType, floor_cy: int, __):
+        chunk.misc.setdefault("fluid_ticks", {}).update(
+            self._decode_ticks(
+                self.get_layer_obj(data, self.LiquidTicks, pop_last=True)
+            )
+        )
+        chunk.misc["liquids_to_be_ticked"] = self._decode_to_be_ticked(
+            self.get_layer_obj(data, self.LiquidsToBeTicked, pop_last=True), floor_cy
+        )
+
+    def _decode_post_processing(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        chunk.misc["post_processing"] = self._decode_to_be_ticked(
+            self.get_layer_obj(data, self.PostProcessing, pop_last=True), floor_cy
+        )
+
+    def _decode_structures(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        chunk.misc["structures"] = self.get_layer_obj(
+            data, self.Structures, pop_last=True
+        )
+
+    def _encode_status(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        # Order the float value based on the order they would be run. Newer replacements for the same come just after
+        # to save back find the next lowest valid value.
+        status = chunk.status.as_type(self._features["status"])
+        self.set_layer_obj(data, self.Status, TAG_String(status))
+
+    def _encode_inhabited_time(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        self.set_layer_obj(
+            data, self.InhabitedTime, TAG_Long(chunk.misc.get("inhabited_time", 0))
+        )
+
+    def _encode_block_section(
+        self,
+        chunk: Chunk,
+        sections: Dict[int, TAG_Compound],
+        palette: AnyNDArray,
+        cy: int,
+    ) -> bool:
+        block_sub_array = numpy.transpose(
+            chunk.blocks.get_sub_chunk(cy), (1, 2, 0)
+        ).ravel()
+
+        sub_palette_, block_sub_array = numpy.unique(
+            block_sub_array, return_inverse=True
+        )
+        sub_palette = self._encode_block_palette(palette[sub_palette_])
+        if len(sub_palette) == 1 and sub_palette[0]["Name"].value == "minecraft:air":
+            return False
+
+        section = sections.setdefault(cy, TAG_Compound())
+        section["BlockStates"] = TAG_Long_Array(
+            encode_long_array(
+                block_sub_array, dense=self.LongArrayDense, min_bits_per_entry=4
+            )
+        )
+        section["Palette"] = sub_palette
+
+    @staticmethod
+    def _encode_block_palette(blockstates: Iterable[Block]) -> TAG_List:
+        palette = TAG_List()
+        for block in blockstates:
+            entry = TAG_Compound()
+            entry["Name"] = TAG_String(f"{block.namespace}:{block.base_name}")
+            if block.properties:
+                string_properties = {
+                    k: v
+                    for k, v in block.properties.items()
+                    if isinstance(v, TAG_String)
+                }
+                if string_properties:
+                    entry["Properties"] = TAG_Compound(string_properties)
+            palette.append(entry)
+        return palette
+
+    @staticmethod
+    def _encode_to_be_ticked(
+        ticks: Set[BlockCoordinates], floor_cy: int, height_cy: int
+    ) -> TAG_List:
+        ceil_cy = floor_cy + height_cy
+        ticks_out = TAG_List([TAG_List([], 2) for _ in range(floor_cy, ceil_cy)])
+        if isinstance(ticks, set):
+            for k in ticks:
+                try:
+                    (x, y, z) = k
+                    cy = y >> 4
+                    if floor_cy <= cy < ceil_cy:
+                        x = x & 15
+                        y = y & 15
+                        z = z & 15
+                        ticks_out[cy].append(TAG_Short((z << 8) + (y << 4) + x))
+                except Exception:
+                    amulet.log.error(f"Could not serialise tick data {k}")
+        return ticks_out
+
+    def _encode_block_ticks(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        super()._encode_block_ticks(chunk, data, floor_cy, height_cy)
+        self.set_layer_obj(
+            data,
+            self.ToBeTicked,
+            self._encode_to_be_ticked(
+                chunk.misc.get("to_be_ticked"), floor_cy, height_cy
+            ),
+        )
+
+    def _encode_fluid_ticks(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        self.set_layer_obj(
+            data,
+            self.LiquidTicks,
+            self._encode_ticks(chunk.misc.get("fluid_ticks", {})),
+        )
+        self.set_layer_obj(
+            data,
+            self.LiquidsToBeTicked,
+            self._encode_to_be_ticked(
+                chunk.misc.get("liquids_to_be_ticked"), floor_cy, height_cy
+            ),
+        )
+
+    def _encode_post_processing(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        self.set_layer_obj(
+            data,
+            self.PostProcessing,
+            self._encode_to_be_ticked(
+                chunk.misc.get("post_processing"), floor_cy, height_cy
+            ),
+        )
+
+    def _encode_structures(
+        self, chunk: Chunk, data: ChunkDataType, floor_cy: int, height_cy: int
+    ):
+        self.set_layer_obj(
+            data,
+            self.Structures,
+            chunk.misc.get(
+                "structures",
+                TAG_Compound(
+                    {
+                        "References": TAG_Compound(),
+                        "Starts": TAG_Compound(),
+                    }
+                ),
+            ),
+        )
+
+
+export = Anvil1444Interface
