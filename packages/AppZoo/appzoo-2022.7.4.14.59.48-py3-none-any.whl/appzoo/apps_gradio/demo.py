a32d07862f4798d2c69e675ba6dcca4121df7071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:14 +0000

    Add package AppZoo/2022.7.4.14.59.48/appzoo-2022.7.4.14.59.48-py3-none-any.whl

---
+++ packages/AppZoo/appzoo-2022.7.4.14.59.48-py3-none-any.whl/appzoo/apps_gradio/demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : demo
+# @Time         : 2021/9/6 上午9:51
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  :
+# /Users/yuanjie/Library/Python/3.7/lib/python/site-packages/pip
+
+# <module 'gradio' from '/Users/yuanjie/Library/Python/3.7/lib/python/site-packages/gradio/__init__.py'>
+
+import gradio
+
+
+print(gradio)
+
---
+++ packages/AppZoo/appzoo-2022.7.4.14.59.48-py3-none-any.whl/examples/plus/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : __init__.py
+# @Time         : 2022/5/30 下午3:19
+# @Author       : yuanjie
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+
