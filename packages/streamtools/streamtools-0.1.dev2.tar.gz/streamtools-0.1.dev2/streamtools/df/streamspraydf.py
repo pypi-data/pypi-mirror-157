c539ab4c43939ec81052734631b83f477b875711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:35 +0000

    Add package streamtools/0.1.dev2/streamtools-0.1.dev2.tar.gz

---
+++ packages/streamtools/streamtools-0.1.dev2.tar.gz/streamtools-0.1.dev2/streamtools/df/streamspraydf.py
@@ -0,0 +1,4 @@
+from galpy.df.df import df
+class streamspraydf(df):
+    def __init__(self,*args,**kwargs):
+        raise ImportError('streamtools.df.streamspraydf has been moved to galpy.df.streamspraydf... please install the latest version of galpy to use streamspraydf')
