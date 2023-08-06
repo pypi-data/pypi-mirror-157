9fdf8c14648cd54ac8afd99673693067d7a8374d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:01 +0000

    Add package systembridgebackend/3.3.0.dev5/systembridgebackend-3.3.0.dev5.tar.gz

---
+++ packages/systembridgebackend/systembridgebackend-3.3.0.dev5.tar.gz/systembridgebackend-3.3.0.dev5/systembridgebackend/server/notification.py
@@ -0,0 +1,76 @@
+"""System Bridge: Server Handler - Notification"""
+from __future__ import annotations
+
+import os
+import platform
+
+from plyer import notification
+from sanic.request import Request
+from sanic.response import HTTPResponse, json
+from systembridgeshared.settings import Settings
+
+
+def send_notification(
+    message: str,
+    title: str | None = None,
+    app_name: str | None = None,
+    app_icon: str | None = None,
+    timeout: int | None = 5,
+) -> None:
+    """Send a notification."""
+    if title is None:
+        title = "System Bridge"
+    if app_name is None:
+        app_name = "System Bridge"
+    if app_icon is None:
+        app_icon = (
+            os.path.join(os.path.dirname(__file__), "../icon.ico")
+            if "Windows" in platform.system()
+            else os.path.join(os.path.dirname(__file__), "../icon.png")
+        )
+    if timeout is None:
+        timeout = 5
+
+    notification.notify(
+        title=title,
+        message=message,
+        app_name=app_name,
+        app_icon=app_icon,
+        timeout=timeout,
+    )
+
+
+async def handler_notification(
+    request: Request,
+    _: Settings,
+) -> HTTPResponse:
+    """Send a notification."""
+    if request.json is None:
+        return json(
+            {
+                "mesage": "Missing JSON body",
+            },
+            status=400,
+        )
+
+    if "message" not in request.json:
+        return json(
+            {
+                "message": "No message provided",
+            },
+            status=400,
+        )
+
+    send_notification(
+        message=request.json["message"],
+        title=request.json.get("title"),
+        app_name=request.json.get("app_name"),
+        app_icon=request.json.get("app_icon"),
+        timeout=request.json.get("timeout"),
+    )
+
+    return json(
+        {
+            "message": "Notification sent",
+        }
+    )
