8048a0d9931bc9193a410d41df2d8f010bcc9d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:46 +0000

    Add package cppyy-cling/6.27.0/cppyy_cling-6.27.0-py2.py3-none-win32.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.27.0-py2.py3-none-win32.whl/cppyy_backend/include/RConfigOptions.h
@@ -0,0 +1,7 @@
+#ifndef ROOT_RConfigOptions
+#define ROOT_RConfigOptions
+
+#define R__CONFIGUREOPTIONS   "CLANG_EXECUTABLE_VERSION=9 CLING_INCLUDE_DIRS=C:/Users/wlav/Test/cppyy-backend/cling/src/interpreter/cling/include CMAKE_CACHE_MAJOR_VERSION=3 CMAKE_CACHE_MINOR_VERSION=11 CMAKE_CACHE_PATCH_VERSION=18040201 CMAKE_CXX_STANDARD_LIBRARIES=kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib CMAKE_C_STANDARD_LIBRARIES=kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib LIBCLANG_LIBRARY_VERSION=9 LLVM_INCLUDE_BENCHMARKS=OFF LLVM_INCLUDE_EXAMPLES=OFF LLVM_INCLUDE_RUNTIMES=ON LLVM_INCLUDE_TOOLS=ON LLVM_INCLUDE_UTILS=ON LLVM_VERSION_PRINTER_SHOW_HOST_TARGET_INFO=ON ZLIB_INCLUDE_DIR=C:/Users/wlav/Test/cppyy-backend/cling/src/builtins/zlib ZLIB_INCLUDE_DIRS=C:/Users/wlav/Test/cppyy-backend/cling/src/builtins/zlib ZLIB_LIBRARIES=ZLIB::ZLIB ZLIB_LIBRARY=$<TARGET_FILE:ZLIB> ZLIB_VERSION=1.2.8 ZLIB_VERSION_STRING=1.2.8 "
+#define R__CONFIGUREFEATURES  "cxx17  builtin_clang builtin_llvm builtin_zlib shared"
+
+#endif
