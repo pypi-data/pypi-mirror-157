71babe800a330967a3ebb55e242b1610661d4bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:00 +0000

    Add package ha-detect-geo-text-my1/1/ha_detect_geo_text_my1-1.tar.gz

---
+++ packages/ha-detect-geo-text-my1/ha_detect_geo_text_my1-1.tar.gz/ha_detect_geo_text_my1-1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+import setuptools
+
+
+setuptools.setup(
+    name='ha_detect_geo_text_my1',
+    version='1',
+    # packages=['ha_detect_geo_text'],
+    description='A demo for python packaging.',
+    long_description='A demo for python packaging.',
+    author='korotkov',  # Замените именем учетной записи на официальном сайте Pypi
+    author_email='korotkov@hypeauditor.com ',  # Замените адресом электронной почты, привязанным к имени вашей учетной записи Pypi
+    packages=setuptools.find_packages(),
+    url='https://github.com/liahimratman/ha_detect_geo_text_my1',
+    license='MIT')
