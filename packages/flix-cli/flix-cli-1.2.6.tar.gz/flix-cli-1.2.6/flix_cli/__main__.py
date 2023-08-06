aff85c39f43369ddfff4076fb9c600128668624f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:12 +0000

    Add package flix-cli/1.2.6/flix-cli-1.2.6.tar.gz

---
+++ packages/flix-cli/flix-cli-1.2.6.tar.gz/flix-cli-1.2.6/flix_cli/__main__.py
@@ -0,0 +1,9 @@
+from .core import __flix_cli__
+from .core.utils import player
+from .core.utils import download
+
+def __flixcli__():
+    __flix_cli__.show_info
+
+if __name__ == "__main__":
+    __flixcli__()
