23bb4221c074bced03a1bc3ccc8d45198e54501e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:49 +0000

    Add package alpa/0.1.3/alpa-0.1.3-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/alpa/alpa-0.1.3-cp38-cp38-manylinux2014_x86_64.whl/alpa/model/model_util.py
@@ -0,0 +1,395 @@
+# flake8: noqa
+from collections import OrderedDict
+from dataclasses import fields
+from typing import Any, Callable, Optional, Tuple, Optional, Union
+
+import flax
+from flax.training import train_state
+import numpy as np
+import jax
+import jax.numpy as jnp
+import jaxlib.xla_extension as jax_xla
+import optax
+
+
+def is_tensor(x):
+    """
+    Tests if ``x`` is a :obj:`torch.Tensor`, :obj:`tf.Tensor`, obj:`jaxlib.xla_extension.DeviceArray` or
+    :obj:`np.ndarray`.
+    """
+    #if is_torch_fx_proxy(x):
+    #    return True
+    #if is_torch_available():
+    #    import torch
+
+    #    if isinstance(x, torch.Tensor):
+    #        return True
+    #if is_tf_available():
+    #    import tensorflow as tf
+
+    #    if isinstance(x, tf.Tensor):
+    #        return True
+
+    #if is_flax_available():
+    if True:
+        import jaxlib.xla_extension as jax_xla
+        from jax.core import Tracer
+
+        if isinstance(x, (jax_xla.DeviceArray, Tracer)):
+            return True
+
+    return isinstance(x, np.ndarray)
+
+
+class ModelOutput(OrderedDict):
+    """
+    Base class for all model outputs as dataclass. Has a ``__getitem__`` that allows indexing by integer or slice (like
+    a tuple) or strings (like a dictionary) that will ignore the ``None`` attributes. Otherwise behaves like a regular
+    python dictionary.
+    .. warning::
+        You can't unpack a :obj:`ModelOutput` directly. Use the :meth:`~transformers.file_utils.ModelOutput.to_tuple`
+        method to convert it to a tuple before.
+    """
+
+    def __post_init__(self):
+        class_fields = fields(self)
+
+        # Safety and consistency checks
+        assert len(class_fields), f"{self.__class__.__name__} has no fields."
+        assert all(
+            field.default is None for field in class_fields[1:]
+        ), f"{self.__class__.__name__} should not have more than one required field."
+
+        first_field = getattr(self, class_fields[0].name)
+        other_fields_are_none = all(
+            getattr(self, field.name) is None for field in class_fields[1:])
+
+        if other_fields_are_none and not is_tensor(first_field):
+            try:
+                iterator = iter(first_field)
+                first_field_iterator = True
+            except TypeError:
+                first_field_iterator = False
+
+            # if we provided an iterator as first field and the iterator is a (key, value) iterator
+            # set the associated fields
+            if first_field_iterator:
+                for element in iterator:
+                    if (not isinstance(element, (list, tuple)) or
+                            not len(element) == 2 or
+                            not isinstance(element[0], str)):
+                        break
+                    setattr(self, element[0], element[1])
+                    if element[1] is not None:
+                        self[element[0]] = element[1]
+            elif first_field is not None:
+                self[class_fields[0].name] = first_field
+        else:
+            for field in class_fields:
+                v = getattr(self, field.name)
+                if v is not None:
+                    self[field.name] = v
+
+    def __delitem__(self, *args, **kwargs):
+        raise Exception(
+            f"You cannot use ``__delitem__`` on a {self.__class__.__name__} instance."
+        )
+
+    def setdefault(self, *args, **kwargs):
+        raise Exception(
+            f"You cannot use ``setdefault`` on a {self.__class__.__name__} instance."
+        )
+
+    def pop(self, *args, **kwargs):
+        raise Exception(
+            f"You cannot use ``pop`` on a {self.__class__.__name__} instance.")
+
+    def update(self, *args, **kwargs):
+        raise Exception(
+            f"You cannot use ``update`` on a {self.__class__.__name__} instance."
+        )
+
+    def __getitem__(self, k):
+        if isinstance(k, str):
+            inner_dict = {k: v for (k, v) in self.items()}
+            return inner_dict[k]
+        else:
+            return self.to_tuple()[k]
+
+    def __setattr__(self, name, value):
+        if name in self.keys() and value is not None:
+            # Don't call self.__setitem__ to avoid recursion errors
+            super().__setitem__(name, value)
+        super().__setattr__(name, value)
+
+    def __setitem__(self, key, value):
+        # Will raise a KeyException if needed
+        super().__setitem__(key, value)
+        # Don't call self.__setattr__ to avoid recursion errors
+        super().__setattr__(key, value)
+
+    def to_tuple(self) -> Tuple[Any]:
+        """
+        Convert self to a tuple containing all the attributes/keys that are not ``None``.
+        """
+        return tuple(self[k] for k in self.keys())
+
+
+@flax.struct.dataclass
+class FlaxBaseModelOutput(ModelOutput):
+    """
+    Base class for model's outputs, with potential hidden states and attentions.
+    Args:
+        last_hidden_state (:obj:`jax_xla.DeviceArray` of shape :obj:`(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the model.
+        hidden_states (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for the output of the embeddings + one for the output of each
+            layer) of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for each layer) of shape :obj:`(batch_size, num_heads,
+            sequence_length, sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    last_hidden_state: jax_xla.DeviceArray = None
+    hidden_states: Optional[Tuple[jax_xla.DeviceArray]] = None
+    attentions: Optional[Tuple[jax_xla.DeviceArray]] = None
+
+
+@flax.struct.dataclass
+class FlaxBaseModelOutputWithPooling(ModelOutput):
+    """
+    Base class for model's outputs that also contains a pooling of the last hidden states.
+    Args:
+        last_hidden_state (:obj:`jax_xla.DeviceArray` of shape :obj:`(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the model.
+        pooler_output (:obj:`jax_xla.DeviceArray` of shape :obj:`(batch_size, hidden_size)`):
+            Last layer hidden-state of the first token of the sequence (classification token) further processed by a
+            Linear layer and a Tanh activation function. The Linear layer weights are trained from the next sentence
+            prediction (classification) objective during pretraining.
+        hidden_states (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for the output of the embeddings + one for the output of each
+            layer) of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for each layer) of shape :obj:`(batch_size, num_heads,
+            sequence_length, sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    last_hidden_state: jax_xla.DeviceArray = None
+    pooler_output: jax_xla.DeviceArray = None
+    hidden_states: Optional[Tuple[jax_xla.DeviceArray]] = None
+    attentions: Optional[Tuple[jax_xla.DeviceArray]] = None
+
+
+@flax.struct.dataclass
+class FlaxBertForPreTrainingOutput(ModelOutput):
+    """
+    Output type of :class:`~transformers.BertForPreTraining`.
+    Args:
+        prediction_logits (:obj:`jax_xla.DeviceArray` of shape :obj:`(batch_size, sequence_length, config.vocab_size)`):
+            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+        seq_relationship_logits (:obj:`jax_xla.DeviceArray` of shape :obj:`(batch_size, 2)`):
+            Prediction scores of the next sequence prediction (classification) head (scores of True/False continuation
+            before SoftMax).
+        hidden_states (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for the output of the embeddings + one for the output of each
+            layer) of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for each layer) of shape :obj:`(batch_size, num_heads,
+            sequence_length, sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    prediction_logits: jax_xla.DeviceArray = None
+    seq_relationship_logits: jax_xla.DeviceArray = None
+    hidden_states: Optional[Tuple[jax_xla.DeviceArray]] = None
+    attentions: Optional[Tuple[jax_xla.DeviceArray]] = None
+
+
+@flax.struct.dataclass
+class FlaxMaskedLMOutput(ModelOutput):
+    """
+    Base class for masked language models outputs.
+    Args:
+        logits (:obj:`jax_xla.DeviceArray` of shape :obj:`(batch_size, sequence_length, config.vocab_size)`):
+            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+        hidden_states (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for the output of the embeddings + one for the output of each
+            layer) of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(jax_xla.DeviceArray)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`jax_xla.DeviceArray` (one for each layer) of shape :obj:`(batch_size, num_heads,
+            sequence_length, sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    logits: jax_xla.DeviceArray = None
+    hidden_states: Optional[Tuple[jax_xla.DeviceArray]] = None
+    attentions: Optional[Tuple[jax_xla.DeviceArray]] = None
+
+
+def softmax_cross_entropy(logits, labels):
+    return -jnp.sum(labels * jax.nn.log_softmax(logits, axis=-1), axis=-1)
+
+
+def optax_adafactor(
+    learning_rate: Optional[Any] = None,
+    min_dim_size_to_factor: int = 128,
+    decay_rate: float = 0.8,
+    decay_offset: int = 0,
+    multiply_by_parameter_scale: float = True,
+    clipping_threshold: Optional[float] = 1.0,
+    momentum: Optional[float] = None,
+    dtype_momentum: Any = jnp.float32,
+    weight_decay_rate: Optional[float] = None,
+    eps: float = 1e-30,
+    factored: bool = True,
+    weight_decay_mask: Optional[Union[Any, Callable[[Any], Any]]] = None,
+):
+    """
+    The same as optax.adafactor but adds the mask for weight decay.
+    """
+    from optax._src.alias import (combine, clipping, factorized, transform,
+                                  _scale_by_learning_rate)
+
+    # The core of the algorithm is a procedure for rescaling gradients
+    # by a factored estimate of the root mean squared gradients.
+    # This reduces memory compared to algorithms such as Adam or RmsProp,
+    # by not having to hold a separate estimate for each weight.
+    tx = [
+        factorized.scale_by_factored_rms(factored, decay_rate, decay_offset,
+                                         min_dim_size_to_factor, eps)
+    ]
+    # This basic rescaling is typically combined with one or more of the following
+    # transformation (all can be disabled via adafactor's constructor args).
+    if clipping_threshold is not None:
+        tx.append(clipping.clip_by_block_rms(clipping_threshold))
+    if learning_rate is not None:
+        tx.append(_scale_by_learning_rate(learning_rate, flip_sign=False))
+    if multiply_by_parameter_scale:
+        tx.append(transform.scale_by_param_block_rms())
+    if momentum is not None:
+        tx.append(
+            transform.ema(momentum,
+                          debias=False,
+                          accumulator_dtype=dtype_momentum))
+    if weight_decay_rate is not None:
+        tx.append(
+            transform.add_decayed_weights(weight_decay_rate,
+                                          mask=weight_decay_mask))
+    # In gradient "descent" we follow the negative gradient.
+    tx.append(transform.scale(-1))
+    return combine.chain(*tx)
+
+
+class TrainState(train_state.TrainState):
+    """This is an improved version of the original flax train state that wraps
+    the logic for mixed precision training.
+    """
+    master_copy: flax.core.FrozenDict[str, Any]
+    dynamic_scale: flax.optim.DynamicScale
+
+    def apply_gradients(self, *, grads, **kwargs):
+        """Updates `step`, `params`, `opt_state` and `**kwargs` in return value.
+        Note that internally this function calls `.tx.update()` followed by a call
+        to `optax.apply_updates()` to update `params` and `opt_state`.
+        Args:
+          grads: Gradients that have the same pytree structure as `.params`.
+          **kwargs: Additional dataclass attributes that should be `.replace()`-ed.
+        Returns:
+          An updated instance of `self` with `step` incremented by one, `params`
+          and `opt_state` updated by applying `grads`, and additional attributes
+          replaced as specified by `kwargs`.
+        """
+        if self.master_copy is None:
+            master_params = self.params
+        else:
+            master_params = self.master_copy
+
+        updates, new_opt_state = self.tx.update(grads, self.opt_state,
+                                                master_params)
+        new_master_params = optax.apply_updates(master_params, updates)
+
+        if self.master_copy is None:
+            new_master_copy = None
+            new_params = new_master_params
+        else:
+            new_master_copy = new_master_params
+            new_params = jax.tree_util.tree_map(
+                lambda x: jnp.asarray(x, dtype=jnp.float16), new_master_params)
+
+            # A hack to make the donation works perfectly in gradient accumulation:
+            # We need the accumulate_grad to take the old params as input.
+            new_params_flat, tree = jax.tree_util.tree_flatten(new_params)
+            old_params_flat, _ = jax.tree_util.tree_flatten(self.params)
+            new_params_flat = [
+                x + 0.0 * y for x, y in zip(new_params_flat, old_params_flat)
+            ]
+            new_params = jax.tree_util.tree_unflatten(tree, new_params_flat)
+
+        return self.replace(
+            step=self.step + 1,
+            params=new_params,
+            master_copy=new_master_copy,
+            opt_state=new_opt_state,
+            **kwargs,
+        )
+
+    @classmethod
+    def create(cls, *, apply_fn, params, tx, mixed_precision=False, **kwargs):
+        """Creates a new instance with `step=0` and initialized `opt_state`."""
+        opt_state = tx.init(params)
+
+        if mixed_precision:
+            master_copy = params
+            params = jax.tree_util.tree_map(
+                lambda x: jnp.asarray(x, dtype=jnp.float16), params)
+        else:
+            master_copy = None
+
+        return cls(
+            step=np.array(0, dtype=np.int32),
+            apply_fn=apply_fn,
+            params=params,
+            master_copy=master_copy,
+            tx=tx,
+            opt_state=opt_state,
+            **kwargs,
+        )
+
+    @classmethod
+    def create_aval(cls,
+                    *,
+                    apply_fn,
+                    params,
+                    tx,
+                    mixed_precision=False,
+                    **kwargs):
+        """Creates a new instance with `step=0` and initialized `opt_state`."""
+        opt_state = jax.eval_shape(tx.init, params)
+
+        if mixed_precision:
+            master_copy = params
+            params = jax.eval_shape(
+                lambda p: jax.tree_util.tree_map(
+                    lambda x: jnp.asarray(x, dtype=jnp.float16), p), params)
+        else:
+            master_copy = None
+
+        return cls(
+            step=np.array(0, dtype=np.int32),
+            apply_fn=apply_fn,
+            params=params,
+            master_copy=master_copy,
+            tx=tx,
+            opt_state=opt_state,
+            **kwargs,
+        )
