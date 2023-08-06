73f184fe8072a428affcfbfa17c3015cce362263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package tomoscan/1.0.0a1/tomoscan-1.0.0a1.tar.gz

---
+++ packages/tomoscan/tomoscan-1.0.0a1.tar.gz/tomoscan-1.0.0a1/tomoscan/nexus/paths/nxsource.py
@@ -0,0 +1,15 @@
+class NEXUS_SOURCE_PATH:
+    NAME = "name"
+    TYPE = "type"
+
+
+class NEXUS_SOURCE_PATH_V_1_0(NEXUS_SOURCE_PATH):
+    pass
+
+
+class NEXUS_SOURCE_PATH_V_1_1(NEXUS_SOURCE_PATH_V_1_0):
+    pass
+
+
+class NEXUS_SOURCE_PATH_V_1_2(NEXUS_SOURCE_PATH_V_1_1):
+    pass
