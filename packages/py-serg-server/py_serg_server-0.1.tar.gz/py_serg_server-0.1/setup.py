b3d0620c11a2d4646b3b619c96fb94c144fb4acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:25:12 +0000

    Add package py-serg-server/0.1/py_serg_server-0.1.tar.gz

---
+++ packages/py-serg-server/py_serg_server-0.1.tar.gz/py_serg_server-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="py_serg_server",
+      version="0.1",
+      description="Serg Client",
+      author="Sergei Sergeev",
+      author_email="isergantnik@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      # scripts=['server/server_run']
+      )
