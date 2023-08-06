1a53544ef2565e59c0ecb27a9420b52d8f9aff31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:18 +0000

    Add package sphinx-table-gen/0.0.1/sphinx_table_gen-0.0.1.tar.gz

---
+++ packages/sphinx-table-gen/sphinx_table_gen-0.0.1.tar.gz/sphinx_table_gen-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="sphinx_table_gen",
+    version="0.0.1",
+    description="reStructuredText sample table generator for sphinx.",
+    author="kuing",
+    author_email="samleeforme@gmail.com",
+    license="BSD 2-Clause License",
+    url="https://github.com/0x00t0x7f/sphinx_table_gen",
+    platforms=["any"],
+    packages=find_packages(),
+    keywords=["Python sphinx", "sphinx", "sphinx table generator", "reStructuredText table generator"]
+)
