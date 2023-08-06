bffe282da43b86ec19c049679c24204c2679636f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:52 +0000

    Add package pytest-mproc/5.1.3/pytest_mproc-5.1.3-py3.8.egg

---
+++ packages/pytest-mproc/pytest_mproc-5.1.3-py3.8.egg/pytest_mproc/remote/bundle.py
@@ -0,0 +1,255 @@
+import asyncio
+import os
+import platform
+from dataclasses import dataclass
+from glob import glob
+
+import sys
+import tempfile
+import zipfile
+from contextlib import suppress
+from pathlib import Path
+from threading import RLock
+from typing import (
+    Any,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+)
+
+from pytest_mproc.coordinator import Coordinator
+
+
+from pytest_mproc import user_output
+from pytest_mproc.ptmproc_data import ProjectConfig
+from pytest_mproc.remote.ssh import SSHClient, CommandExecutionFailure
+from pytest_mproc.user_output import debug_print, always_print
+
+_root = os.path.dirname(__file__)
+FIFTEEN_MINUTES = 15*60
+artifacts_root = os.environ.get('PTMPROC_HOST_ARTIFACTS_DIR', './artifacts')
+lock = RLock()
+
+
+@dataclass
+class CoordinatorProcDescription:
+    index: int
+    ssh_client: SSHClient
+    remote_artifacts_dir: Path
+    cwd: Path
+    env: Dict[str, str]
+    timeout: Optional[float]
+
+
+class Bundle:
+
+    CACHE_DIR = Path(os.path.expanduser('~')).absolute() / ".ptmproc"
+
+    def __init__(self, root_dir: Path,
+                 project_name: str,
+                 project_root: Path,
+                 artifacts_path: Path,
+                 test_files: List[Path],
+                 system_executable: str,
+                 prepare_script: Optional[Path] = None,
+                 finalize_script: Optional[Path] = None,
+                 env: Optional[Dict[str, str]] = None,
+                 relative_run_dir: Optional[Path] = None):
+        """
+        create a bundle for remote host deployment
+        :param root_dir: a delicious carbonated beverage (and also the root where bundle files are created)
+        :param test_files: location of test files, resources files, requirements files to upload to run tests
+        :param system_executable: location of python executable on remote host, or None for default
+        :param prepare_script: Optional path to script to run pre-test-execution
+        :param finalize_script:: Optional path to run post-test-execution
+        :param relative_run_dir: working directory, relative to root, where pytest should be executed; root dir if
+           not specified
+        """
+        self._output_task = None
+        self._project_name = project_name
+        self._artifacts_path = artifacts_path
+        if not root_dir.exists():
+            raise FileNotFoundError(f"'{root_dir}' does not exist")
+        if relative_run_dir is not None and relative_run_dir.is_absolute():
+            raise ValueError("Relative run dir was provided as an absolute path when creating bundle")
+        absolutes = [p for p in test_files if p.is_absolute()]
+        if absolutes:
+            raise ValueError(f"Files or wildcard patterns {absolutes} in project config file must be relative paths "
+                             "to the project config location")
+        self._zip_path = root_dir / "bundle.zip"
+        if os.path.exists(self._zip_path):
+            raise FileExistsError(f"File {self._zip_path} already exists")
+        self._root_dir = Path(root_dir)
+        self._prepare_script = Path(prepare_script) if prepare_script else None
+        self._finalize_script = Path(finalize_script) if finalize_script else None
+        self._remote_executable = system_executable or Path(sys.executable).name
+        self._resources: List[Tuple[Path, Path]] = []
+        self._requirements_path = None
+        self._test_files = []
+        self._relative_run_path = relative_run_dir or Path(".")
+        for path in test_files:
+            matched = glob(str(project_root / path), recursive=True)
+            if not matched:
+                raise ValueError(
+                    f"files or wildcard pattern '{str(project_root.absolute() / path)}' did not match any files")
+            self._test_files.extend([Path(p).relative_to(project_root) for p in matched])
+        if any([not (project_root / p).exists() for p in self._test_files]):
+            raise FileNotFoundError(f"One or more files in '{test_files}' do not exist in {project_root}")
+        if Path('requirements.txt') in self._test_files:
+            self._requirements_path = 'requirements.txt'
+        self._system = platform.system()
+        self._machine = platform.machine()
+        self._env = env or {}
+        self._coordinator: Dict[str, Coordinator] = {}
+        self._coordinator_descriptions: List[CoordinatorProcDescription] = []
+
+    @staticmethod
+    def remote_run_dir(remote_root: Path) -> Path:
+        """
+        :return: Path on remote host where test code and resources are located for the run, given the root dir
+           of installation of a bundle
+        """
+        return remote_root / "run"
+
+    @property
+    def prepare_script(self):
+        return self._prepare_script
+
+    @property
+    def finalize_script(self):
+        return self._finalize_script
+
+    def __enter__(self) -> "Bundle":
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        pass
+
+    @property
+    def zip_path(self):
+        return self._zip_path
+
+    @property
+    def tests_files(self):
+        return self._test_files
+
+    @classmethod
+    def create(cls,
+               root_dir: Path,
+               project_config: ProjectConfig,
+               system_executable: Optional[str] = None,
+               ) -> "Bundle":
+        """
+        Create a bundle
+        :param root_dir: where artifacts are to be created
+        :param project_config: project configuration defining contents of bundle
+        :param system_executable: path ON REMOTE HOST of system python executable to use
+        :return: created Bundle
+        """
+        try:
+            relative_run_dir = Path(os.getcwd()).relative_to(project_config.project_root.absolute())
+        except ValueError:
+            raise ValueError("You must execute pytest in the directory containing the project config, "
+                             f"{str(project_config.project_root)} or a subdirectory thereof")
+        bundle = Bundle(root_dir=root_dir,
+                        project_name=project_config.project_name,
+                        artifacts_path=project_config.artifcats_path,
+                        project_root=project_config.project_root,
+                        test_files=project_config.test_files,
+                        prepare_script=project_config.prepare_script,
+                        finalize_script=project_config.finalize_script,
+                        relative_run_dir=relative_run_dir,
+                        system_executable=system_executable)
+        os.write(sys.stderr.fileno(), f"\n\n>>> Building bundle...\n\n".encode('utf-8'))
+        always_print(">>> Zipping contents for remote worker...")
+        with zipfile.ZipFile(bundle.zip_path, mode='w') as zip_file:
+            for path in bundle._test_files:
+                zip_file.write(project_config.project_root / path, f"run{os.sep}{str(path)}")
+            if bundle.prepare_script:
+                zip_file.write(project_config.project_root / bundle.prepare_script, 'prepare')
+            if bundle.finalize_script:
+                zip_file.write(project_config.project_root / bundle.finalize_script, 'finalize')
+            with tempfile.NamedTemporaryFile(mode='a+') as out:
+                out.write(bundle._system)
+                out.write(bundle._machine)
+                out.flush()
+                zip_file.write(out.name, "host_ptmproc_platform")
+        return bundle
+
+    async def setup_remote_venv(self, ssh_client: SSHClient, remote_venv: Path) -> Path:
+        """
+        setup the remote base virtual environment (Python + pytest + pytest_mproc)
+        """
+        stdout = sys.stdout if user_output.is_verbose else asyncio.subprocess.DEVNULL
+        proc = await ssh_client.execute_remote_cmd(
+            'ls', '-d', str(remote_venv / 'bin' / 'python3'), stdout=asyncio.subprocess.DEVNULL,
+            stderr=asyncio.subprocess.DEVNULL
+        )
+
+        if proc.returncode != 0:
+            always_print(f"Installing python virtual environment to {remote_venv}")
+            proc = await ssh_client.execute_remote_cmd(
+                self._remote_executable, '-m', 'venv', str(remote_venv),
+                stdout=stdout,
+                stderr=asyncio.subprocess.PIPE
+            )
+            if proc.returncode != 0:
+                data = await proc.stderr.read()
+                raise CommandExecutionFailure(
+                    f"{self._remote_executable} -m venv {str(remote_venv)} failed:\n\n  {data}",
+                    proc.returncode)
+            await ssh_client.install_packages(
+                'pytest', 'pytest_mproc',
+                venv=remote_venv,
+                cwd=remote_venv.parent,
+                stdout=stdout,
+                stderr=sys.stderr)
+
+        return remote_venv
+
+    async def deploy(self, ssh_client: SSHClient,
+                     remote_venv: Path, remote_root: Path, timeout: Optional[float] = None) -> None:
+        """
+        Deploy this bundle to a remote ssh client
+        :param remote_venv: location of Python virtual environment on remote host
+        :param ssh_client: where to deploy
+        :param timeout: raise TimeoutError if deployment takes too long
+        :param remote_root: remote path to where bundle is deployed
+        """
+        always_print("Deploying bundle to remote worker %s...", ssh_client.host)
+        stdout = sys.stdout if user_output.is_verbose else asyncio.subprocess.DEVNULL
+        try:
+            await ssh_client.mkdir(self.remote_run_dir(remote_root), exists_ok=True)
+            debug_print(f">>> Pushing contents to remote worker {ssh_client.host}...")
+            await ssh_client.push(self._zip_path, remote_root / self._zip_path.name)
+            always_print(f">>> Unpacking tests on remote worker {ssh_client.host}...")
+            # noinspection PyProtectedMember
+            proc = await ssh_client._remote_execute(
+                f"cd {str(remote_root)} && unzip -o {str(self._zip_path.name)}",
+                stdout=stdout,
+                stderr=sys.stderr
+            )
+            await asyncio.wait_for(proc.wait(), timeout=timeout)
+            if proc.returncode != 0:
+                text = ('\n' + (await proc.stdout.read()).decode('utf-8')) if proc.stdout else ""
+                raise CommandExecutionFailure(f"Failed to unzip tests on remote client {text}", proc.returncode)
+            with suppress(Exception):
+                await ssh_client.remove(remote_root / self._zip_path.name)
+            if proc.returncode != 0:
+                text = ('\n' + (await proc.stdout.read()).decode('utf-8')) if proc.stdout else ""
+                raise CommandExecutionFailure(f"Failed to unzip requirements on remote client {text}", proc.returncode)
+            if self._requirements_path:
+                always_print(f"Installing requirements on remote worker {ssh_client.host} to {remote_venv}...")
+                await ssh_client.install(
+                    venv=remote_venv,
+                    remote_root=remote_root,
+                    requirements_path=f'run{os.sep}requirements.txt',
+                    cwd=remote_root,
+                    stdout=stdout,
+                    stderr=sys.stderr
+                )
+            await ssh_client.mkdir(remote_root / "run" / self._relative_run_path, exists_ok=True)
+        except Exception as e:
+            always_print(f"!!! Failed to deploy to {ssh_client.host}: {e}")
+            raise
---
+++ packages/pytest-mproc/pytest_mproc-5.1.3-py3.8.egg/pytest_mproc/remote_sessions.py
@@ -0,0 +1,197 @@
+import asyncio
+import multiprocessing
+import os
+import signal
+import subprocess
+import sys
+import tempfile
+from contextlib import suppress
+from pathlib import Path
+from typing import Optional, Dict, List, Tuple
+
+from pytest_mproc.constants import ARTIFACTS_ZIP
+from pytest_mproc.coordinator import Coordinator
+
+from pytest_mproc import get_auth_key, Settings, AsyncMPQueue
+from pytest_mproc.data import ClientDied
+from pytest_mproc.ptmproc_data import ProjectConfig, RemoteWorkerConfig
+from pytest_mproc.remote.bundle import Bundle
+from pytest_mproc.remote.ssh import SSHClient
+from pytest_mproc.user_output import always_print, debug_print
+
+
+POLLING_INTERVAL_HOST_VALIDATION = 5
+
+
+class RemoteSessionManager:
+    """
+    class to conduct a remote session with multiple remote worker nodes
+    """
+
+    def __init__(self,
+                 project_config: ProjectConfig,
+                 remote_sys_executable: Optional[str],
+                 ):
+        """
+        :param project_config: user defined project parameters needed for creating the bundle to send
+        :param remote_sys_executable: optional string path to remote executable for Python to use (must version-match)
+        """
+        super().__init__()
+        self._project_config = project_config
+        self._remote_sys_executable = remote_sys_executable
+        self._exc_manager = multiprocessing.managers.SyncManager(authkey=get_auth_key())
+        self._exc_manager.register("Queue", multiprocessing.Queue, exposed=["get", "put", "empty"])
+        self._validate_task = None
+        self._session_task: Optional[asyncio.Task] = None
+        self._pids: Dict[str, List[int]] = {}
+        self._project_name = project_config.project_name
+        self._tmp_path = tempfile.TemporaryDirectory()
+        self._bundle = Bundle.create(root_dir=Path(self._tmp_path.name),
+                                     project_config=self._project_config,
+                                     system_executable=self._remote_sys_executable)
+        self._deployment_tasks: Dict[str, asyncio.Task] = {}
+        self._worker_tasks: List[asyncio.Task] = []
+        self._worker_pids: Dict[str, List[int]] = {}
+        self._tmp_path.__enter__()
+        self._bundle.__enter__()
+        self._remote_roots: Dict[str, Path] = {}
+        self._coordinators: Dict[str, Coordinator.ClientProxy] = {}
+        self._ssh_clients: Dict[str, SSHClient] = {}
+
+    def shutdown(self):
+        with suppress(Exception):
+            self._validate_task.cancel()
+        for task in list(self._deployment_tasks.values()) + self._worker_tasks:
+            with suppress(Exception):
+                task.cancel()
+        for host, remote_pids in self._worker_pids.items():
+            always_print(f"Ensuring termination of remote worker pids {remote_pids} on {host}...")
+            self._ssh_clients[host].signal_sync(remote_pids, signo=signal.SIGKILL)
+        for host, remote_root in self._remote_roots.items():
+            with suppress(Exception):
+                ssh_client = self._ssh_clients[host]
+                artifacts_zip = remote_root / ARTIFACTS_ZIP
+                always_print(f"\n>> Pulling {artifacts_zip} from {host}\n")
+                ssh_client.pull_sync(artifacts_zip, Path('.') / ARTIFACTS_ZIP)
+                always_print(f"\n>> Unzipping {ARTIFACTS_ZIP}...\n")
+                proc = subprocess.run(
+                    f"unzip -o ./{ARTIFACTS_ZIP}",
+                    shell=True,
+                    stdout=sys.stdout,
+                    stderr=sys.stderr
+                )
+                if proc.returncode == 0:
+                    os.remove(ARTIFACTS_ZIP)
+                else:
+                    always_print(f"\n!! Failed to unzip {Path('.') / ARTIFACTS_ZIP}\n")
+        self._worker_pids = {}
+        self._deployment_tasks = {}
+        self._worker_tasks = []
+        self._remote_roots = {}
+        with suppress(Exception):
+            self._tmp_path.__exit__(None, None, None)
+        with suppress(Exception):
+            self._bundle.__exit__(None, None, None)
+
+    async def setup(self, worker_config: RemoteWorkerConfig,
+                    remote_root: Path,
+                    remote_venv_root: Path,
+                    deploy_timeout: Optional[float] = None) -> None:
+        ssh_client = SSHClient(username=worker_config.ssh_username or Settings.ssh_username,
+                               password=Settings.ssh_password,
+                               host=worker_config.remote_host)
+        remote_venv = remote_venv_root / 'venv'
+        futures, _ = await asyncio.wait(
+            [self._bundle.setup_remote_venv(ssh_client=ssh_client, remote_venv=remote_venv),
+             self._deploy(worker_config, timeout=deploy_timeout, remote_root=remote_root, remote_venv=remote_venv)],
+            timeout=deploy_timeout,
+            return_when=asyncio.ALL_COMPLETED
+        )
+        results = [f.result() for f in futures]
+        for r in results:
+            if isinstance(r, Exception):
+                raise r
+
+    async def _deploy(self, worker_config: RemoteWorkerConfig,
+                      remote_root: Path, remote_venv: Path, timeout: Optional[float] = None,):
+        host = worker_config.remote_host
+        ssh_client = self._ssh_clients[worker_config.remote_host]
+        await self._bundle.deploy(
+            ssh_client=ssh_client,
+            remote_root=remote_root,
+            remote_venv=remote_venv,
+            timeout=timeout
+        )
+        self._remote_roots[host] = remote_root
+
+    async def start_worker(
+            self,
+            worker_config: RemoteWorkerConfig,
+            coordinator: Coordinator.ClientProxy,
+            args: List[str],
+            results_q: AsyncMPQueue,
+            remote_root: Path,
+            remote_venv_root: Path,
+            deploy_timeout: Optional[float] = None,
+            env: Dict[str, str] = None,
+    ) -> Tuple[Path, Path]:
+        self._ssh_clients[worker_config.remote_host] = SSHClient(
+            username=worker_config.ssh_username or Settings.ssh_username,
+            password=Settings.ssh_password,
+            host=worker_config.remote_host)
+        if worker_config.remote_host not in self._deployment_tasks:
+            self._deployment_tasks[worker_config.remote_host] = asyncio.create_task(
+                self.setup(worker_config,
+                           remote_root=remote_root,
+                           remote_venv_root=remote_venv_root,
+                           deploy_timeout=deploy_timeout)
+            )
+
+        async def start(index: int):
+            try:
+                await self._deployment_tasks[worker_config.remote_host]
+                self._coordinators[worker_config.remote_host] = coordinator
+                worker_pid = coordinator.start_worker(index=index, args=args, addl_env=env)
+                self._worker_pids.setdefault(worker_config.remote_host, []).append(worker_pid)
+                if not self._validate_task:
+                    self._validate_task = asyncio.create_task(self.validate_clients(results_q=results_q))
+            except Exception as e:
+                import traceback
+                always_print(traceback.format_exc(), as_error=True)
+                always_print(f"EXCEPTION in starting worker: {e}", as_error=True)
+                raise
+
+        self._worker_tasks.append(asyncio.create_task(start(len(self._worker_tasks) + 1)))
+        return remote_root, remote_venv_root
+
+    async def validate_clients(self, results_q: AsyncMPQueue,) -> None:
+        """
+        Continually ping workers to ensure they are alive, and drop worker if not, rescheduling any pending tests to
+        that worker
+
+        :param results_q: Used to signal a client died
+        """
+        while self._coordinators:
+            for host, coordinator in self._coordinators.copy().items():
+                # test if worker is active
+                ssh_client = self._ssh_clients[host]
+                try:
+                    await ssh_client.execute_remote_cmd("echo", "PONG",
+                                                        stdout=asyncio.subprocess.DEVNULL,
+                                                        stderr=asyncio.subprocess.DEVNULL,
+                                                        timeout=5)
+                    debug_print(f"Host {host} is alive...")
+                except TimeoutError:
+                    always_print(f"Host {host} unreachable!", as_error=True)
+                    worker_pids = self._worker_pids.get(host)
+                    for worker_pid in worker_pids:
+                        await ssh_client.signal(worker_pid, signal.SIGKILL)
+                        # this will attempt to reschedule test
+                        result = ClientDied(worker_pid, host, True)
+                        await results_q.put(result)
+                        self._worker_pids[host].remove(worker_pid)
+                        if not self._worker_pids[host]:
+                            del self._worker_pids[host]
+                else:
+                    debug_print(f"{host} is alive")
+            await asyncio.sleep(POLLING_INTERVAL_HOST_VALIDATION)
