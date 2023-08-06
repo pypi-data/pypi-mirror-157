554a5efdbcd71da0b1745d886e0fd3d3c02b4145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:14 +0000

    Add package fishy/0.5.13/fishy-0.5.13-py3-none-any.whl

---
+++ packages/fishy/fishy-0.5.13-py3-none-any.whl/fishy/constants.py
@@ -0,0 +1,11 @@
+apiversion = 2
+
+# removed since 0.5.3
+chalutier = ("Chalutier", "https://www.esoui.com/downloads/dl2934/Chalutier_1.1.4.zip", 114)
+
+# addons used
+lam2 = ("LibAddonMenu-2.0", "https://www.esoui.com/downloads/dl7/LibAddonMenu-2.0r32.zip", 32)
+fishyqr = ("FishyQR", "https://github.com/fishyboteso/FishyQR/releases/download/v1.2.0/FishyQR.zip", 120)
+libgps = ("LibGPS", "https://cdn.esoui.com/downloads/file601/LibGPS_3_0_3.zip", 30)
+
+version = "0.5.13"
