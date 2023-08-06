204b9b837ff1824ce0f846e0b9921be838f24511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:29 +0000

    Add package labelstore/0.0.1/labelstore-0.0.1.tar.gz

---
+++ packages/labelstore/labelstore-0.0.1.tar.gz/labelstore-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="labelstore",
+    version="0.0.1",
+    description="labelstore",
+    packages=["labelstore"],
+    author="Activeloop",
+    author_email="support@activeloop.ai",
+    url="https://activeloop.ai",
+)
