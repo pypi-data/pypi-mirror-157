2c8fb124102f03ca1deadaae22d13905ff09913a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:05:33 +0000

    Add package getgfs/1.1.0/getgfs-1.1.0.tar.gz

---
+++ packages/getgfs/getgfs-1.1.0.tar.gz/getgfs-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="getgfs",
+    # Version number (initial):
+    version="1.1.0",
+    # Application author details:
+    author="Jago Strong-Wright",
+    author_email="jagoosw@protonmail.com",
+    # Packages
+    packages=["getgfs"],
+    # Details
+    url="https://getgfs.readthedocs.io/",
+    #
+    license="LICENSE.txt",
+    description="getgfs extracts weather forecast variables from the NOAA GFS forecast",
+    # Dependent packages (distributions)
+    install_requires=[
+        "scipy",
+        "requests",
+        "fuzzywuzzy",
+        "numpy",
+        "python_dateutil",
+        "regex",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup :: Markdown",
+    ],
+)
