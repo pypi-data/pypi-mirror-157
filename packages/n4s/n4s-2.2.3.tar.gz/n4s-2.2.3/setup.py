0a7855ba1f45c15e5d4725f365bf5283e4771be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:34 +0000

    Add package n4s/2.2.3/n4s-2.2.3.tar.gz

---
+++ packages/n4s/n4s-2.2.3.tar.gz/n4s-2.2.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='n4s',
+    version='2.2.3',
+    author='Mike Afshari',
+    author_email='theneed4swede@gmail.com',
+    description='Collection of useful methods by Need4Swede',
+    url='https://github.com/n4s/n4s',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
