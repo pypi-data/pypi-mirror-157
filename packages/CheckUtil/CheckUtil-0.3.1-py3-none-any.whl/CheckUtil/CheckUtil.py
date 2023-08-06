1f7656c4d36d0babe2cee973b3eef65b8473e530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:16 +0000

    Add package CheckUtil/0.3.1/CheckUtil-0.3.1-py3-none-any.whl

---
+++ packages/CheckUtil/CheckUtil-0.3.1-py3-none-any.whl/CheckUtil/CheckUtil.py
@@ -0,0 +1,12 @@
+import psutil
+import os
+
+class CheckUtil:
+    
+    def cpu_usage():
+        process = psutil.Process(os.getpid())
+        return process.cpu_percent()
+
+    def ram_usage():
+        process = psutil.Process(os.getpid())
+        return process.memory_percent()
