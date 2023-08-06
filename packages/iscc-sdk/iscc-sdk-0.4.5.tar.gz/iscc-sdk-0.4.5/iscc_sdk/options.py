e2042ae6eb425329e3e1e5985f2973fd60c4098c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:43 +0000

    Add package iscc-sdk/0.4.5/iscc-sdk-0.4.5.tar.gz

---
+++ packages/iscc-sdk/iscc-sdk-0.4.5.tar.gz/iscc-sdk-0.4.5/iscc_sdk/options.py
@@ -0,0 +1,51 @@
+"""*SDK configuration and options*."""
+from typing import Optional
+
+from iscc_core.options import CoreOptions
+from pydantic import Field
+
+
+__all__ = [
+    "SdkOptions",
+    "sdk_opts",
+]
+
+
+class SdkOptions(CoreOptions):
+
+    granular: bool = Field(
+        False,
+        description="Generate additional granular fingerprints for ISCC-CODES",
+    )
+
+    image_exif_transpose: bool = Field(
+        True,
+        description="Transpose image according to EXIF Orientation tag",
+    )
+
+    image_fill_transparency: bool = Field(
+        True, description="Add white background to image if it has alpha transparency"
+    )
+
+    image_trim_border: bool = Field(True, description="Crop empty borders of images")
+
+    image_thumbnail_size: int = Field(
+        128, description="Size of larger side of thumbnail in number of pixels"
+    )
+
+    image_thumbnail_quality: int = Field(
+        60, description="Thumbnail image compression setting (0-100)"
+    )
+
+    text_avg_chunk_size: int = Field(
+        1024,
+        description="Avg number of characters per text chunk for granular fingerprints",
+    )
+
+    video_fps: int = Field(
+        5,
+        description="Frames per second to process for video hash (ignored when 0).",
+    )
+
+
+sdk_opts = SdkOptions()
