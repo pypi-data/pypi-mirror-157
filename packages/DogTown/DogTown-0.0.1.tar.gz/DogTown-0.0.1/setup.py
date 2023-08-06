587eb2e135210589bcbbf6e4c885aba01ad62293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:00 +0000

    Add package DogTown/0.0.1/DogTown-0.0.1.tar.gz

---
+++ packages/DogTown/DogTown-0.0.1.tar.gz/DogTown-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__project__ = "DogTown"
+__version__ = "0.0.1"
+__description__ = "A Dog Program That is cool"
+__packages__ = ["dogtown"]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+)
