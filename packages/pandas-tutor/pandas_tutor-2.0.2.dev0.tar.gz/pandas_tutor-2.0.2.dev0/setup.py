a0d50933d029f8daa1476d0ecc70339599c8e5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package pandas-tutor/2.0.2.dev0/pandas_tutor-2.0.2.dev0.tar.gz

---
+++ packages/pandas-tutor/pandas_tutor-2.0.2.dev0.tar.gz/pandas_tutor-2.0.2.dev0/setup.py
@@ -0,0 +1,35 @@
+"""
+put an empty __init__.py inside of every directory you want to include in the
+wheel
+
+to build a wheel, run this command, (important: make sure to rm -rf the build
+cache):
+
+rm -rf build/; python setup.py bdist_wheel
+"""
+# type: ignore
+
+from setuptools import setup, find_packages
+
+version = "2.0.2dev0"
+
+# These packages are installed during pip install.
+# Only packages that AREN'T bundled inline need to be listed here.
+install_requires = [
+    "pandas>=1.3,<2.0",
+    "mypy-extensions==0.4.3",
+]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pandas_tutor",
+    version=version,
+    packages=find_packages(),
+    install_requires=install_requires,
+    package_data={
+        "": ["*.golden"]
+    },  # add all test .golden files into package along with .py files
+    include_package_data=True,
+)
