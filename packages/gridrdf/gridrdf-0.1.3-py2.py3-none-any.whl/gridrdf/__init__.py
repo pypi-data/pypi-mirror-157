aa73a2cde14e11faeb5a9035f997f51e6f00799e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:23:28 +0000

    Add package gridrdf/0.1.3/gridrdf-0.1.3-py2.py3-none-any.whl

---
+++ packages/gridrdf/gridrdf-0.1.3-py2.py3-none-any.whl/gridrdf/__init__.py
@@ -0,0 +1,31 @@
+""" Grouped Representations of Interatomic Distances (GRID)
+
+Package to compute, analyse and train ML models using the GRID descriptor
+for crystal structures.
+
+
+"""
+
+__version__ = "0.1.3"
+
+
+__all__ = ['composition',
+           'data_explore',
+           'data_io',
+           'data_prepare',
+           'earth_mover_distance',
+           'extendRDF',
+           'otherRDFs',
+           'train',
+           'visualization',
+           ]
+
+#import gridrdf.composition
+#import gridrdf.data_explore
+#import gridrdf.data_io
+#import gridrdf.data_prepare
+#import gridrdf.earth_mover_distance
+#import gridrdf.extendRDF
+#import gridrdf.otherRDFs
+#import gridrdf.train
+#import gridrdf.visualization
