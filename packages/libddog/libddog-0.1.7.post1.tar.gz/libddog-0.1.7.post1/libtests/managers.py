99f4c4e1cb6e2836db8a43ad372d477fd26a3eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:01 +0000

    Add package libddog/0.1.7.post1/libddog-0.1.7.post1.tar.gz

---
+++ packages/libddog/libddog-0.1.7.post1.tar.gz/libddog-0.1.7.post1/libtests/managers.py
@@ -0,0 +1,44 @@
+import re
+from datetime import datetime
+from pathlib import Path
+
+from libddog.crud.dashboards import DashboardManager
+from libddog.dashboards import Dashboard
+
+
+class QADashboardManager:
+    # detect occurrences of:  %(variable)s
+    rx_string_template = re.compile("%\\([a-zA-Z0-9_]+\\)s")
+
+    def __init__(self) -> None:
+        proj_root = Path(__file__).parent.parent
+        testdata_dir = proj_root.joinpath("testdata").absolute()
+
+        self.manager = DashboardManager(proj_path=str(testdata_dir))
+
+    def load_definition_by_title(self, title: str) -> Dashboard:
+        dashboards = self.manager.load_definitions()
+        for dashboard in dashboards:
+            if dashboard.title == title:
+                return dashboard
+
+        raise RuntimeError("Failed to get dashboard with title: %s" % title)
+
+    def assign_id_to_dashboard(self, dashboard: Dashboard) -> str:
+        all_dashboards = self.manager.list_dashboards()
+
+        existing_id: str = ""
+        for dash in all_dashboards:
+            if dash["title"] == dashboard.title:
+                existing_id = dash["id"]
+                return existing_id
+
+        return self.manager.create_dashboard(dashboard=dashboard)
+
+    def update_live_dashboard(self, dashboard: Dashboard, id: str) -> None:
+        if self.rx_string_template.search(dashboard.desc):
+            raise RuntimeError(
+                "Dashboard desc contains unpopulated template: %s" % dashboard.desc
+            )
+
+        self.manager.update_dashboard(dashboard, id)
