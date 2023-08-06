6d9ff58b1af313f59b054860daf11d2e14961064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package measured/0.7.1/measured-0.7.1.tar.gz

---
+++ packages/measured/measured-0.7.1.tar.gz/measured-0.7.1/tests/quantities/test_conversions.py
@@ -0,0 +1,117 @@
+import pytest
+
+from measured import Area, Length, Mass, Number, Numeric, One, Unit, conversions
+from measured.conversions import ConversionNotFound
+from measured.si import Meter, Second
+from measured.us import Acre, Foot, Inch
+
+
+def test_disallow_self_conversion() -> None:
+    with pytest.raises(ValueError, match="unit and itself"):
+        Meter.equals(1 * Meter)
+
+    with pytest.raises(ValueError, match="unit and itself"):
+        conversions.equate(1 * Meter, 1 * Meter)
+
+    with pytest.raises(ValueError, match="unit and itself"):
+        conversions.equate(1 * Meter, 2 * Meter)
+
+
+def test_disallow_self_zeroing() -> None:
+    with pytest.raises(ValueError, match="unit and itself"):
+        Meter.zero(1 * Meter)
+
+    with pytest.raises(ValueError, match="unit and itself"):
+        conversions.translate(Meter, 1 * Meter)
+
+
+def test_approximating_requires_units_with_conversion() -> None:
+    Bogus = Length.unit(name="bogus", symbol="bog")
+
+    one = 1 * Meter
+    other = 1 * Bogus
+
+    assert not one.approximates(other)
+
+
+def test_unit_conversion_must_be_in_same_dimension() -> None:
+    with pytest.raises(ConversionNotFound):
+        (1 * Meter).in_unit(Second)
+
+
+def test_unit_always_converts_to_itself() -> None:
+    converted = (10 * Meter).in_unit(Meter)
+    assert converted.magnitude == 10
+    assert converted.unit == Meter
+
+
+def test_conversion_with_exponents() -> None:
+    assert (1 * Foot) ** 2 == (12 * Inch) ** 2
+    assert 1 * Foot**2 == 144 * Inch**2
+
+    assert (1 * Foot) ** 3 == (12 * Inch) ** 3
+    assert 1 * Foot**3 == 1728 * Inch**3
+
+
+@pytest.mark.parametrize(
+    "start, end, magnitude",
+    [
+        (Foot, Inch, 12.0),
+        (Foot**2, Meter**2, 0.09290304),
+        (Meter**2, Foot**2, 10.76391041670972),
+        (Acre, Foot**2, 43560.0),
+        (Foot**2, Inch**2, 144.0),
+        (Acre, Inch**2, 43560.0 * 144.0),
+        (Inch**2, Foot**2, 0.006944444444444444),
+        (Foot**2, Acre, 0.00002295684113865932),
+        (Inch**2, Acre, 0.006944444444444444 * 0.00002295684113865932),
+    ],
+)
+def test_conversion_can_navigate_exponents(
+    start: Unit, end: Unit, magnitude: Numeric
+) -> None:
+    converted = conversions.convert(1 * start, end)
+    assert converted == magnitude * end
+    assert converted.unit == end
+    assert converted.magnitude == magnitude
+
+
+def test_backtracking_conversions_with_no_path() -> None:
+    # Set up some arbitrary units that don't convert to one another
+    Hiya = Length.unit("hello", "hello")
+    Mundo = Area.unit("world", "world")
+
+    with pytest.raises(ConversionNotFound):
+        conversions.convert(1 * Hiya**2, Mundo)
+
+    with pytest.raises(ConversionNotFound):
+        conversions.convert(1 * Mundo, Hiya**2)
+
+
+def test_failing_to_convert_numerator() -> None:
+    Gud = Length.unit("goodbye", "goodbye")
+    Mun = Length.unit("moon", "moon")
+
+    with pytest.raises(ConversionNotFound):
+        (1 * Gud).in_unit(Mun)
+
+
+def test_failing_to_convert_denominator() -> None:
+    Flib = Mass.unit("flibbidy", "flibbidy")
+    Flob = Mass.unit("flobbidy", "flobbidy")
+
+    assert (Meter / Flib).dimension is (Meter / Flob).dimension
+
+    with pytest.raises(ConversionNotFound):
+        (1 * Meter / Flib).in_unit(Meter / Flob)
+
+
+def test_failing_to_collapse_dimensions() -> None:
+    Jib = Mass.unit("jibbity", "jibbity")
+    Job = Mass.unit("jobbity", "jobbity")
+
+    assert (Jib / Job).dimension is Number
+    assert (Job / Jib).dimension is Number
+
+    with pytest.raises(ConversionNotFound):
+        ((1 * Jib) / (1 * Job)).in_unit(One)
---
+++ packages/measured/measured-0.7.1.tar.gz/measured-0.7.1/tests/test_parsing.py
@@ -0,0 +1,183 @@
+import pytest
+
+from measured import Numeric, Quantity, Unit, systems  # noqa: F401
+from measured.iec import Byte, Kibi
+from measured.parsing import ParseError
+from measured.si import Ampere, Hertz, Kilo, Meter, Micro, Ohm, Second
+
+ALL_UNITS = sorted(Unit.base(), key=lambda u: u.name or "")
+
+
+def test_unit_symbols_should_not_have_spaces() -> None:
+    with pytest.raises(ValueError, match="spaces"):
+        Meter.alias(symbol="the metre")
+
+
+@pytest.mark.parametrize("unit", ALL_UNITS, ids=[u.name for u in ALL_UNITS])
+def test_each_unit_parsable(unit: Unit) -> None:
+    assert Unit.parse(str(unit)) is unit
+
+
+@pytest.mark.parametrize("magnitude", [-1e-30 - 1.1, -1, 0, 1, 1.1, 1e30])
+@pytest.mark.parametrize("unit", ALL_UNITS, ids=[u.name for u in ALL_UNITS])
+def test_int_quantity_in_each_unit_parsable(magnitude: Numeric, unit: Unit) -> None:
+    assert Quantity.parse(str(magnitude * unit)) == magnitude * unit
+
+
+@pytest.mark.parametrize(
+    "string",
+    [
+        "fleebles",
+        "queeble²",
+        ",,,",
+        "",
+        " ",
+    ],
+)
+def test_unit_that_should_not_parse(string: str) -> None:
+    with pytest.raises((KeyError, ParseError)):
+        Unit.parse(string)
+
+
+@pytest.mark.parametrize(
+    "string, unit",
+    [
+        ("m", Meter),
+        ("meter", Meter),
+        ("Hz", Hertz),
+        ("hertz", Hertz),
+        ("Ω", Ohm),
+        ("ohm", Ohm),
+    ],
+)
+def test_single_unit(string: str, unit: Unit) -> None:
+    assert Unit.parse(string) is unit
+
+
+@pytest.mark.parametrize(
+    "string, unit",
+    [
+        ("m²", Meter**2),
+        ("meter²", Meter**2),
+        ("m²²²", Meter**222),
+        ("m⁻¹", Meter**-1),
+        ("m^2", Meter**2),
+        ("m^222", Meter**222),
+        ("m^-1", Meter**-1),
+    ],
+)
+def test_unit_with_exponent(string: str, unit: Unit) -> None:
+    assert Unit.parse(string) is unit
+
+
+@pytest.mark.parametrize(
+    "string, unit",
+    [
+        ("m²A³", Meter**2 * Ampere**3),
+        ("m²⋅A³", Meter**2 * Ampere**3),
+        ("m²*A³", Meter**2 * Ampere**3),
+        ("m²A³s⁻³", Meter**2 * Ampere**3 * Second**-3),
+        ("m²⋅A³⋅s^-3", Meter**2 * Ampere**3 * Second**-3),
+        ("m²*A³*s^-3", Meter**2 * Ampere**3 * Second**-3),
+    ],
+)
+def test_unit_series(string: str, unit: Unit) -> None:
+    assert Unit.parse(string) is unit
+
+
+@pytest.mark.parametrize(
+    "string, unit",
+    [
+        ("m/s", Meter / Second),
+        ("m²A³/s^3", (Meter**2 * Ampere**3) / Second**3),
+        ("m²⋅A³/s^3", (Meter**2 * Ampere**3) / Second**3),
+        ("m²*A³/s^3", (Meter**2 * Ampere**3) / Second**3),
+    ],
+)
+def test_unit_ratio(string: str, unit: Unit) -> None:
+    assert Unit.parse(string) == unit
+
+
+@pytest.mark.parametrize(
+    "string",
+    [
+        "m",
+        "5 zeebles",
+    ],
+)
+def test_quantities_that_should_not_parse(string: str) -> None:
+    with pytest.raises((KeyError, ParseError)):
+        Quantity.parse(string)
+
+
+@pytest.mark.parametrize(
+    "string, quantity",
+    [
+        ("5m", 5 * Meter),
+        ("5 m", 5 * Meter),
+        ("5.1 m", 5.1 * Meter),
+        ("5 Hz", 5 * Hertz),
+        ("5.1 Hz", 5.1 * Hertz),
+        ("5 Ω", 5 * Ohm),
+        ("5.1 Ω", 5.1 * Ohm),
+    ],
+)
+def test_quantity_with_single_unit(string: str, quantity: Quantity) -> None:
+    assert Quantity.parse(string) == quantity
+
+
+@pytest.mark.parametrize(
+    "string, quantity",
+    [
+        ("5 m²", 5 * Meter**2),
+        ("5.1 m²", 5.1 * Meter**2),
+        ("5 m²²²", 5 * Meter**222),
+        ("5 m⁻¹", 5 * Meter**-1),
+        ("5 m^2", 5 * Meter**2),
+        ("5.1 m^2", 5.1 * Meter**2),
+        ("5 m^222", 5 * Meter**222),
+        ("5 m^-1", 5 * Meter**-1),
+    ],
+)
+def test_quantity_with_unit_exponent(string: str, quantity: Quantity) -> None:
+    assert Quantity.parse(string) == quantity
+
+
+@pytest.mark.parametrize(
+    "string, quantity",
+    [
+        ("5 m²A³", 5 * Meter**2 * Ampere**3),
+        ("5 m²⋅A³", 5 * Meter**2 * Ampere**3),
+        ("5 m²*A³", 5 * Meter**2 * Ampere**3),
+        ("5 m²A³s⁻³", 5 * Meter**2 * Ampere**3 * Second**-3),
+        ("5 m²⋅A³⋅s^-3", 5 * Meter**2 * Ampere**3 * Second**-3),
+        ("5 m²*A³*s^-3", 5 * Meter**2 * Ampere**3 * Second**-3),
+    ],
+)
+def test_quantity_with_unit_series(string: str, quantity: Quantity) -> None:
+    assert Quantity.parse(string) == quantity
+
+
+@pytest.mark.parametrize(
+    "string, quantity",
+    [
+        ("5 m/s", 5 * Meter / Second),
+        ("5 m²A³/s^3", 5 * (Meter**2 * Ampere**3) / Second**3),
+        ("5 m²⋅A³/s^3", 5 * (Meter**2 * Ampere**3) / Second**3),
+        ("5 m²*A³/s^3", 5 * (Meter**2 * Ampere**3) / Second**3),
+    ],
+)
+def test_quantity_with_unit_ratio(string: str, quantity: Quantity) -> None:
+    assert Quantity.parse(string) == quantity
+
+
+@pytest.mark.parametrize(
+    "string, quantity",
+    [
+        ("5 km", 5 * Kilo * Meter),
+        ("5 KiB", 5 * Kibi * Byte),
+        ("5 μm", 5 * Micro * Meter),
+    ],
+)
+def test_quantity_with_unit_prefixes(string: str, quantity: Quantity) -> None:
+    assert Quantity.parse(string) == quantity
