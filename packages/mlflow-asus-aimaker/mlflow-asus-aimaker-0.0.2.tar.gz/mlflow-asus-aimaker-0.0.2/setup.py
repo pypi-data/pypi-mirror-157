5bc9c719cdc1f09ff6a5827ec0b08bb3ed629673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:28 +0000

    Add package mlflow-asus-aimaker/0.0.2/mlflow-asus-aimaker-0.0.2.tar.gz

---
+++ packages/mlflow-asus-aimaker/mlflow-asus-aimaker-0.0.2.tar.gz/mlflow-asus-aimaker-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mlflow-asus-aimaker",
+    version="0.0.2",
+    description="MLflow plugin for ASUS AI-Maker",
+    packages=find_packages(),
+    install_requires=["mlflow"],
+    entry_points={
+        "mlflow.tracking_store": "file-plugin=mlflow_plugin.plugin_rest_store:PluginRestStore",
+        "mlflow.model_registry_store": "file-plugin=mlflow_plugin.plugin_model_rest_store:PluginRegistryRestStore"
+    },
+)
