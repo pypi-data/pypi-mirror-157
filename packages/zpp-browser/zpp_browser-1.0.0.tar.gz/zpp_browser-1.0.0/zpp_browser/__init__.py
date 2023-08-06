04a4bfa0df0ad0770149854dd52e88478c66ec7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:35 +0000

    Add package zpp-browser/1.0.0/zpp_browser-1.0.0.tar.gz

---
+++ packages/zpp-browser/zpp_browser-1.0.0.tar.gz/zpp_browser-1.0.0/zpp_browser/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.0'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = 'Browser (Explorateur) de fichier en cli'
+__lastupdate__ = '27/06/2022'
+
+from .browser import *
