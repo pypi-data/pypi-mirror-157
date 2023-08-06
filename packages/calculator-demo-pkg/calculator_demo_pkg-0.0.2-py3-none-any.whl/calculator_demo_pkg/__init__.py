4dff1230c080a5f08cdee89f3f65096b7a3568fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:11 +0000

    Add package calculator-demo-pkg/0.0.2/calculator_demo_pkg-0.0.2-py3-none-any.whl

---
+++ packages/calculator-demo-pkg/calculator_demo_pkg-0.0.2-py3-none-any.whl/calculator_demo_pkg/__init__.py
@@ -0,0 +1,6 @@
+from .scientific.cos import cos
+from .scientific.sin import sin
+from .standard.add import add
+from .standard.divide import divide
+from .standard.multiply import multiply
+from .standard.subtract import subtract
---
+++ packages/calculator-demo-pkg/calculator_demo_pkg-0.0.2-py3-none-any.whl/calculator_demo_pkg/standard/multiply.py
@@ -0,0 +1,2 @@
+def multiply(x, y):
+    print("Performing calculation...Will get back to you")
