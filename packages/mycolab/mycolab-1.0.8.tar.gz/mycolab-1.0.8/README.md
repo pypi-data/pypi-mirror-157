12781b2666d686fb1d9e55828c99ae6cb3c4944d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:30 +0000

    Add package mycolab/1.0.8/mycolab-1.0.8.tar.gz

---
+++ packages/mycolab/mycolab-1.0.8.tar.gz/mycolab-1.0.8/README.md
@@ -0,0 +1,22 @@
+<a href="https://mycolab.org"><img src="https://raw.githubusercontent.com/mycolab/mycolab/master/images/mycolab-logo.svg" width="480"></a>
+
+Suite of mycology applications and tools for automating common lab workflows.
+
+- Website: https://mycolab.org
+- Email: <info@mycolab.org>
+- Github: https://github.com/mycolab
+- Discord: https://discord.gg/STW6wXaNBC
+
+## Features
+- Add specimens
+- Search GenBank
+- Curate [FASTA](https://en.wikipedia.org/wiki/FASTA_format) files
+- Align sequences
+- Generate phylogenetic trees
+- Publish findings to various sites: 
+  - [Genbank](https://blast.ncbi.nlm.nih.gov)
+  - [iNaturalist](https://inaturalist.org)
+  - [MushroomObserver](https://mushroomobserver.org)
+
+## Architecture
+![architecture](images/architecture.png)
---
+++ packages/mycolab/mycolab-1.0.8.tar.gz/mycolab-1.0.8/setup.py
@@ -0,0 +1,47 @@
+import pathlib
+from io import open
+from os import path
+
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# automatically captured required modules for install_requires in requirements.txt
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
+    not x.startswith('#')) and (not x.startswith('-'))]
+
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
+
+setup(
+    name='mycolab',
+    description='Command line tools for MycoLab',
+    version='1.0.8',
+    packages=find_packages(),
+    install_requires=install_requires,
+    python_requires='>=3.0',
+    entry_points='''
+          [console_scripts]
+          mycolab=mycolab.__main__:main
+    ''',
+    author="Joseph Harrison",
+    keyword="mycology, lab, phylogenetic, tree, DNA, RNA",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='Apache Software',
+    url='https://github.com/mycolab/mycolab',
+    download_url='https://github.com/mycolab/mycolab/archive/1.0.7.tar.gz',
+    dependency_links=dependency_links,
+    author_email='joe@mycolab.org',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
