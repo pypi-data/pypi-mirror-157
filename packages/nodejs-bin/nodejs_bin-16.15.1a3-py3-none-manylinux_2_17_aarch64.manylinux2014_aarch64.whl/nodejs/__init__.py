f7b5372ee2d9eecea970ae9309cc7e7a4bec2251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:19 +0000

    Add package nodejs-bin/16.15.1a3/nodejs_bin-16.15.1a3-py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/nodejs-bin/nodejs_bin-16.15.1a3-py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/nodejs/__init__.py
@@ -0,0 +1,8 @@
+from .node import path, main, call, run, Popen
+from . import npm
+from . import corepack
+from . import node
+from . import npx
+
+__version__ = "16.15.1a3"
+node_version = "16.15.1"
