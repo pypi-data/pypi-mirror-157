605171f9244f0b569c422e4bbea291e1ee43c442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:26 +0000

    Add package repeated-test/2.2.0a3/repeated_test-2.2.0a3-py2.py3-none-any.whl

---
+++ packages/repeated-test/repeated_test-2.2.0a3-py2.py3-none-any.whl/repeated_test/utils.py
@@ -0,0 +1,50 @@
+# repeated_test -- A framework for repeating a test over many values
+# Copyright (C) 2011-2022 by Yann Kaiser and contributors. See AUTHORS and
+# COPYING for details.
+
+from functools import partial
+
+def _tup(args, func):
+    return (func,) + args
+
+def tup(*args):
+    return partial(_tup, args)
+
+class options:
+    def __init__(self, **kwargs) -> None:
+        self.kwargs = kwargs
+
+    @classmethod
+    def split_into_args_kwargs(cls, args_and_options):
+        args = []
+        kwargs = {}
+        for arg in args_and_options:
+            if isinstance(arg, cls):
+                kwargs.update(arg.kwargs)
+            else:
+                args.append(arg)
+        return args, kwargs
+
+class _SkipOption:
+    def __repr__(self):
+        return "<skip option>"
+
+skip_option = _SkipOption()
+
+def with_options(**kwargs):
+    return with_options_matrix(**{
+        key: [value]
+        for key, value in kwargs.items()
+    })
+
+def with_options_matrix(**kwargs):
+    def wrap_class(cls):
+        return cls.update(options_matrix=kwargs)
+    return wrap_class
+
+def filter_skips(d):
+    return {
+        k: v
+        for k, v in d.items()
+        if v is not skip_option
+    }
