b7e491c64b00f3b68135c86578a144ee204689fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:37 +0000

    Add package browserist/0.15.0/browserist-0.15.0.tar.gz

---
+++ packages/browserist/browserist-0.15.0.tar.gz/browserist-0.15.0/src/browserist/browser/scroll/into_view_if_not_visible.py
@@ -0,0 +1,10 @@
+from ...constant import timeout
+from ...model.type.xpath import XPath
+from ..check_if.is_displayed import check_if_is_displayed
+from .into_view import scroll_into_view
+
+
+def scroll_into_view_if_not_visible(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> None:
+    xpath = XPath(xpath)
+    if not check_if_is_displayed(driver, xpath):
+        scroll_into_view(driver, xpath, timeout)
