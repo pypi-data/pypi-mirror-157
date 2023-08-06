3f1c665136f283c41031cae2fb327600e579c3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:09 +0000

    Add package fastapi-amis-admin-cli/0.1.0/fastapi_amis_admin_cli-0.1.0-py3-none-any.whl

---
+++ packages/fastapi-amis-admin-cli/fastapi_amis_admin_cli-0.1.0-py3-none-any.whl/fastapi_amis_admin_cli/App/{{cookiecutter.slug}}/admin.py
@@ -0,0 +1,24 @@
+from fastapi_amis_admin import amis,admin
+from fastapi_amis_admin.admin import AdminApp
+
+from core.adminsite import site
+# from .models import Category
+
+
+@site.register_admin
+class {{cookiecutter.slug|capitalize}}App(admin.AdminApp):
+    page_schema = amis.PageSchema(label='{{cookiecutter.name|capitalize}}', icon='fa fa-bolt')
+    router_prefix = '/{{cookiecutter.slug}}'
+
+    def __init__(self, app: "AdminApp"):
+        super().__init__(app)
+        # self.register_admin(CategoryAdmin)
+
+
+# Register your models here.
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     group_schema = None
+#     page_schema = amis.PageSchema(label='Category', icon='fa fa-folder')
+#     model = Category
+#     search_fields = [Category.name]
---
+++ packages/fastapi-amis-admin-cli/fastapi_amis_admin_cli-0.1.0-py3-none-any.whl/fastapi_amis_admin_cli/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0"
+
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent
