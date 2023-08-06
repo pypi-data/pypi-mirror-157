a25a8e9998e6edb38c9082a85802433f6243b0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:50 +0000

    Add package tecton/0.5.0b20/tecton-0.5.0b20-py3-none-any.whl

---
+++ packages/tecton/tecton-0.5.0b20-py3-none-any.whl/tecton/_stamp.py
@@ -0,0 +1,1 @@
+BUILD_STATUS = {'BUILD_ID': 'uncommitted-buildkite-agent-1657070251', 'BUILD_TIMESTAMP': '1657070251', 'GIT_COMMIT': 'ec0b4bddaef6eb71c4c6299ba7f8ac34238e280a', 'BUILD_EMBED_LABEL': '', 'BUILD_HOST': 'buildkite-agent-medium-75b4c457d9-22dpr', 'BUILD_USER': 'buildkite-agent'}
