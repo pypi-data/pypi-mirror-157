8c409ad5df061696467539fc8d885f9fae71bc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:53 +0000

    Add package cfxdb/22.6.1/cfxdb-22.6.1-py2.py3-none-any.whl

---
+++ packages/cfxdb/cfxdb-22.6.1-py2.py3-none-any.whl/cfxdb/_exception.py
@@ -0,0 +1,12 @@
+##############################################################################
+#
+#                        Crossbar.io Database
+#     Copyright (c) Crossbar.io Technologies GmbH. Licensed under MIT.
+#
+##############################################################################
+
+__all__ = ('InvalidConfigException', )
+
+
+class InvalidConfigException(Exception):
+    pass
