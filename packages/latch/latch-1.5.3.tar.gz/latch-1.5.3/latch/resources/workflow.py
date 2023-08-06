19305e5475a43b47e0b9a9fccd3479b2cc9d72a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:39 +0000

    Add package latch/1.5.3/latch-1.5.3.tar.gz

---
+++ packages/latch/latch-1.5.3.tar.gz/latch-1.5.3/latch/resources/workflow.py
@@ -0,0 +1,24 @@
+from textwrap import dedent
+from typing import Callable, Union
+
+from flytekit import workflow as _workflow
+
+from latch.types.metadata import LatchMetadata
+
+
+# this weird Union thing is to ensure backwards compatibility,
+# so that when users call @workflow without any arguments or
+# parentheses, the workflow still serializes as expected
+def workflow(metadata: Union[LatchMetadata, Callable]):
+    if isinstance(metadata, Callable):
+        return _workflow(metadata)
+    else:
+
+        def decorator(f: Callable):
+            if f.__doc__ is not None:
+                f.__doc__ = f"{dedent(f.__doc__)}\n\n" + str(metadata)
+            else:
+                f.__doc__ = "\n\n" + str(metadata)
+            return _workflow(f)
+
+        return decorator
