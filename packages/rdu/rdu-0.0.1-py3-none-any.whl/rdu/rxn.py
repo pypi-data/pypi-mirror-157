7c7458f15ef150fcaf9e008ed6c9b738be91b1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package rdu/0.0.1/rdu-0.0.1-py3-none-any.whl

---
+++ packages/rdu/rdu-0.0.1-py3-none-any.whl/rdu/rxn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/4 15:03
+# @Author  : zbc@mail.ustc.edu.cn
+# @File    : rxn.py
+# @Software: PyCharm
+
+
+if __name__ == "__main__":
+    pass
