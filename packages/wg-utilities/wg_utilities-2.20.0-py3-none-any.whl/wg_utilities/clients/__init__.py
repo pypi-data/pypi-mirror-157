29ab30d0121564a55c4845c2b0c70621b933a289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:54:34 +0000

    Add package wg-utilities/2.20.0/wg_utilities-2.20.0-py3-none-any.whl

---
+++ packages/wg-utilities/wg_utilities-2.20.0-py3-none-any.whl/wg_utilities/clients/__init__.py
@@ -0,0 +1,9 @@
+"""Useful clients for commonly accessed APIs/services"""
+
+from .google_calendar import GoogleCalendarClient
+from .google_drive import GoogleDriveClient
+from .google_fit import GoogleFitClient
+from .google_photos import GooglePhotosClient
+from .monzo import MonzoClient
+from .spotify import SpotifyClient
+from .truelayer import TrueLayerClient
