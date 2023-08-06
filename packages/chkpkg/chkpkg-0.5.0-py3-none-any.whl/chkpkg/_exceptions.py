24b33b110a52dae07971e762f4c5a8d268a8e077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:53 +0000

    Add package chkpkg/0.5.0/chkpkg-0.5.0-py3-none-any.whl

---
+++ packages/chkpkg/chkpkg-0.5.0-py3-none-any.whl/chkpkg/_exceptions.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
+# SPDX-License-Identifier: MIT
+
+from subprocess import CompletedProcess
+from typing import Optional
+
+
+class ChkpkgException(Exception):
+    def __init__(self,
+                 message: Optional[str] = None,
+                 inner: Optional[BaseException] = None):
+        super().__init__(message)
+        self.inner = inner
+
+    def __str__(self):
+        return "\n".join([
+            str(super()),
+            f"inner: {self.inner}"
+        ])
+
+
+class CompletedProcessError(ChkpkgException):
+    def __init__(self,
+                 message: Optional[str] = None,
+                 inner: Optional[BaseException] = None,
+                 process: Optional[CompletedProcess] = None):
+        super().__init__(message=message, inner=inner)
+        self.process = process
+
+    def __str__(self):
+        return "\n".join([
+            str(super()),
+            f"process: {self.process}"
+        ])
+
+
+class TwineCheckFailed(CompletedProcessError):
+    pass
+
+
+class FailedToInstallPackage(ChkpkgException):
+    pass
+
+
+class CannotInitializeEnvironment(ChkpkgException):
+    pass
+
+
+class CodeExecutionFailed(CompletedProcessError):
+    pass
+
+
+class PytypedNotFound(ChkpkgException):
+    pass
