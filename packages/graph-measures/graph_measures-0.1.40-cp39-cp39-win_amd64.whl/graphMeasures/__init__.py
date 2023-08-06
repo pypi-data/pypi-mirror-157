4d27d51df90d20d6405fddc2a520ae2e9a4ce689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:59 +0000

    Add package graph-measures/0.1.40/graph_measures-0.1.40-cp39-cp39-win_amd64.whl

---
+++ packages/graph-measures/graph_measures-0.1.40-cp39-cp39-win_amd64.whl/graphMeasures/__init__.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+# for path_name in [os.path.join(os.path.dirname(__file__)),
+#                   os.path.join(os.path.dirname(__file__), 'features_algorithms'),
+#                   os.path.join(os.path.dirname(__file__), 'features_algorithms', 'accelerated_graph_features'),
+#                   os.path.join(os.path.dirname(__file__), 'features_algorithms', 'vertices'),
+#                   os.path.join(os.path.dirname(__file__), 'features_infra'),
+#                   os.path.join(os.path.dirname(__file__), 'graph_infra'),
+#                   os.path.join(os.path.dirname(__file__), 'features_meta')
+#                   ]:
+#     sys.path.append(path_name)
+
+from .features_for_any_graph import FeatureCalculator
