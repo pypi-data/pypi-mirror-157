84e62fd3860b609e30627eaca18591dadf801345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:01 +0000

    Add package ankr-sdk/0.1.0/ankr-sdk-0.1.0.tar.gz

---
+++ packages/ankr-sdk/ankr-sdk-0.1.0.tar.gz/ankr-sdk-0.1.0/ankr/exceptions.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+from web3.types import RPCError
+
+
+class AdvancedAPIException(Exception):
+    def __init__(self, error: RPCError | str) -> None:
+        super().__init__(f"failed to handle request, {error}")
