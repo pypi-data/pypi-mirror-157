afeb9152291a1fb0c93ba2bd455621407e6ddd67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:30 +0000

    Add package dokos-cli/2.4.8/dokos-cli-2.4.8.tar.gz

---
+++ packages/dokos-cli/dokos-cli-2.4.8.tar.gz/dokos-cli-2.4.8/bench/config/__init__.py
@@ -0,0 +1,5 @@
+"""Module for setting up system and respective bench configurations"""
+
+def env():
+	from jinja2 import Environment, PackageLoader
+	return Environment(loader=PackageLoader('bench.config'))
