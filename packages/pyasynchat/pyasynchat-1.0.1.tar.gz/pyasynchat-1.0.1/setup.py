58c51e4d4c802b48173e24ae8583ee2956f4599a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:22:22 +0000

    Add package pyasynchat/1.0.1/pyasynchat-1.0.1.tar.gz

---
+++ packages/pyasynchat/pyasynchat-1.0.1.tar.gz/pyasynchat-1.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import sys
+
+# TODO: test by `import asynchat` then removing/renaming the file at the location shown from `print(asynchat)`
+#  make sure to temporarily the version check below when testing; sys.modules shows currently loaded modules if needed
+
+# note: this check is not technically required as imports default to standard libraries where available, but it
+# ensures that we cannot cause any adverse effects where existing code is somehow reliant on behaviours of the
+# standard library asynchat module that are different to the one provided here
+packages = []
+if sys.version_info[0] == 3 and sys.version_info[1] >= 12:
+    packages.append('asynchat')
+
+python_classifiers = ['Programming Language :: Python :: %s' % version for version in ['3.12']]
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='pyasynchat',
+    version='1.0.1',
+    description='Make asynchat available for Python 3.12 onwards',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Simon Robinson',
+    author_email='simon@robinson.ac',
+    url='https://github.com/simonrob/pyasynchat',
+
+    platforms='any',
+    install_requires=['pyasyncore'],
+    packages=packages,
+
+    license='Python Software Foundation License Version 2',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: Python Software Foundation License'] + python_classifiers,
+)
