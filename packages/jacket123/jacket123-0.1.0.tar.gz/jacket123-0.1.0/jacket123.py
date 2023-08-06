b1d441558786bf1807c40a3d00a2a40906ac48a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:09 +0000

    Add package jacket123/0.1.0/jacket123-0.1.0.tar.gz

---
+++ packages/jacket123/jacket123-0.1.0.tar.gz/jacket123-0.1.0/jacket123.py
@@ -0,0 +1,6 @@
+def minus(a, b):
+    return a - b
+
+
+def plus(x, y):
+    return x + y
---
+++ packages/jacket123/jacket123-0.1.0.tar.gz/jacket123-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="jacket123",
+    version="0.1.0",
+    description="This is a module which can create operations like plus and minus",
+    author="Someone",
+    author_email="someone@someone.com",
+    py_modules=['jacket123'],
+)
