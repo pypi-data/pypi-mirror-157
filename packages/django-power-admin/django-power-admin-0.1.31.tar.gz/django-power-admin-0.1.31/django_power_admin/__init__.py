261de9e815c608cbb2ce60e6acf15b434229e7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:39 +0000

    Add package django-power-admin/0.1.31/django-power-admin-0.1.31.tar.gz

---
+++ packages/django-power-admin/django-power-admin-0.1.31.tar.gz/django-power-admin-0.1.31/django_power_admin/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
+default_app_config = "django_power_admin.apps.DjangoPowerAdminConfig"
+
+app_requires = [
+    "mptt",
+    "django_static_fontawesome",
+    "django_static_jquery_ui",
+    "django_middleware_global_request",
+]
+
+app_middleware_requires = [
+    "django_middleware_global_request.middleware.GlobalRequestMiddleware",
+]
