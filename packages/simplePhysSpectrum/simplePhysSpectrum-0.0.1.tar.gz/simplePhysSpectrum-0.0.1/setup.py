5bab9334a15150088db780f1d71f225d23aa5333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:39 +0000

    Add package simplePhysSpectrum/0.0.1/simplePhysSpectrum-0.0.1.tar.gz

---
+++ packages/simplePhysSpectrum/simplePhysSpectrum-0.0.1.tar.gz/simplePhysSpectrum-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirement_path):
+        with open(requirement_path) as f:
+                    install_requires = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simplePhysSpectrum",
+    version="0.0.1",
+    author="Chris Davis",
+    author_email="ChrisSWDavis@gmail.com",
+    license="MIT",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChrisSWDavis/simplePhysSpectrum/",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
