070550372cdfa5b97bf84b1e40d9544c91909cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:56 +0000

    Add package dbdicom/0.0.9/dbdicom-0.0.9.tar.gz

---
+++ packages/dbdicom/dbdicom-0.0.9.tar.gz/dbdicom-0.0.9/dbdicom/__init__.py
@@ -0,0 +1,8 @@
+# do not show in documentation
+__pdoc__ = {}
+__pdoc__["utilities"] = False 
+__pdoc__["external"] = False 
+__pdoc__["dicm"] = False 
+
+from .folder import *
+from .functions import *
