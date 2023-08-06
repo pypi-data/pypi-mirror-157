b1e7da91358b095e953ae59833f6c568dad42c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:41 +0000

    Add package thanhtungthaiatmethod/3.1.0/thanhtungthaiatmethod-3.1.0-py3-none-any.whl

---
+++ packages/thanhtungthaiatmethod/thanhtungthaiatmethod-3.1.0-py3-none-any.whl/thanhtungthaiatmethod/fortest.py
@@ -0,0 +1,14 @@
+import methods as m
+import math
+
+#print(f'Can Chi năm Thái ất {m.CanChiNamThaiAt(10155917)}')
+#print(f"Bát môn trực sự: {m.BatMonTrucSu('KHAI',50,1)}")
+#print(f"Cửu tinh quý thần: {m.CuuTinhQuyThan('10153917')}")
+#print(f"Ngũ phúc và các sao: {m.NguPhucDaiDuTamCoTuThanDaiTieumuc(10155937)}")
+#print(f"Kết quả an sao:{m.AnSao('DẦN', 10153919,1,'Kể năm')}")
+#print(f"Cửu tinh thái ất trực phù: {m.CuuTinhLacThuQuyThan(10155557)}")
+#print(f"Bát môn trực sự: {m.BatMonTrucSu(10155917,1,'năm')}")
+#print(f"Sort Sao chính {m.MainStar(104, 1, 'THÌN')}")
+#print(f"Sort Left {m.LeftStar('THÌN', 10155917, 0, 'Kể giờ')}")
+print(
+    f"Sort MidAndRight Star: {m.MidAndRightStar(10155917, 'CANH', 'THÌN', 0, 'Kể giờ')}")
