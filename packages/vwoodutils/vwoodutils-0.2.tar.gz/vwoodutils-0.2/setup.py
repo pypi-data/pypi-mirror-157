d7f21481b2c4e5779e7ade2985452ceb2409cc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:17 +0000

    Add package vwoodutils/0.2/vwoodutils-0.2.tar.gz

---
+++ packages/vwoodutils/vwoodutils-0.2.tar.gz/vwoodutils-0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='vwoodutils',
+      version='0.2',
+      description='My python utilities',
+      long_description='My personal python utilities - now on pypi',
+      author='V Wood',
+      author_email='spam@vwood.org',
+      url='',
+      packages=['myutils'],
+    )
+
