0e0f353d7aad694a0d24e48ce69d27715848baa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:12 +0000

    Add package files-kedro/0.1.1/files-kedro-0.1.1.tar.gz

---
+++ packages/files-kedro/files-kedro-0.1.1.tar.gz/files-kedro-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="files-kedro",
+    version="0.1.1",
+    description="Meltano project files for kedro",
+    packages=find_packages(),
+    package_data={
+        "bundle": [
+            "utilities/kedro/README.md",
+            "utilities/kedro/run.py",
+            "utilities/kedro/setup.cfg",
+            "utilities/kedro/pyproject.toml",
+            "utilities/kedro/.gitignore",
+            "utilities/kedro/__init__.py",
+            "utilities/kedro/conf/**",
+            "utilities/kedro/conf/base/*.yml",
+            "utilities/kedro/conf/local/*.yml",
+            "utilities/kedro/data/01_raw/iris.csv",          
+            "utilities/kedro/data/**/.gitkeep",                      
+            "utilities/kedro/docs/source/conf.py",          
+            "utilities/kedro/docs/source/index.rst",                      
+            "utilities/kedro/logs/.gitkeep",          
+            "utilities/kedro/notebooks/.gitkeep",          
+            "utilities/kedro/src/requirements.txt",
+            "utilities/kedro/src/*.py",
+            "utilities/kedro/src/tests/*.py",
+            "utilities/kedro/src/meltano_kedro/*.py",            
+        ]
+    },
+)
