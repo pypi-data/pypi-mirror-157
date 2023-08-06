a962efc152e1e8f65751b60f1ac28cf24f2d1b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:15 +0000

    Add package PyEasier/0.0.5.1/PyEasier-0.0.5.1.tar.gz

---
+++ packages/PyEasier/PyEasier-0.0.5.1.tar.gz/PyEasier-0.0.5.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5.1'
+DESCRIPTION = 'This package makes you python experience better it includes nice shortcut functions to help solve problems in less lines of code.'
+
+# Setting up
+setup(
+    name="PyEasier",
+    version=VERSION,
+    author="Yellow Yams(Vedic Mukherjee)",
+    author_email="<yellowyams5@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['py2app', 'pyinstaller'],
+    keywords=['python', 'PyEasier', 'quick code', 'fast development', 'easy to use', 'build'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
