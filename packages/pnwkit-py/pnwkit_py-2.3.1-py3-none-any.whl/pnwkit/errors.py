e22cf45ce3dea6c792d4417a0d0db06654fedc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:15 +0000

    Add package pnwkit-py/2.3.1/pnwkit_py-2.3.1-py3-none-any.whl

---
+++ packages/pnwkit-py/pnwkit_py-2.3.1-py3-none-any.whl/pnwkit/errors.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+__all__ = (
+    "PnWKitException",
+    "GraphQLError",
+    "MaxTriesExceededError",
+    "MissingVariablesError",
+    "SocketError",
+    "NoReconnect",
+    "Unauthorized",
+    "SubscriptionDidNotSucceed",
+    "PersistedQueryNotFound",
+    "SubscribeError",
+)
+
+
+class PnWKitException(Exception):
+    ...
+
+
+class GraphQLError(PnWKitException):
+    ...
+
+
+class MaxTriesExceededError(PnWKitException):
+    ...
+
+
+class MissingVariablesError(PnWKitException):
+    ...
+
+
+class SocketError(PnWKitException):
+    ...
+
+
+class NoReconnect(SocketError):
+    ...
+
+
+class Unauthorized(SocketError):
+    ...
+
+
+class SubscriptionDidNotSucceed(SocketError):
+    ...
+
+
+class PersistedQueryNotFound(PnWKitException):
+    ...
+
+
+class SubscribeError(PnWKitException):
+    ...
