a8759474d234c2c588057f0869db65c091121de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package rdu/0.0.1/rdu-0.0.1.tar.gz

---
+++ packages/rdu/rdu-0.0.1.tar.gz/rdu-0.0.1/rdu/mol.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/4 15:03
+# @Author  : zbc@mail.ustc.edu.cn
+# @File    : mol.py
+# @Software: PyCharm
+
+
+class Mol:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
