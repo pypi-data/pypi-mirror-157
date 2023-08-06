438124d5e0aaff2c4fa4e97257d8be8a6e2f3e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:07 +0000

    Add package behave-to-cucumber/0.1.1/behave_to_cucumber-0.1.1.tar.gz

---
+++ packages/behave-to-cucumber/behave_to_cucumber-0.1.1.tar.gz/behave_to_cucumber-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['behave_to_cucumber']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'behave-to-cucumber',
+    'version': '0.1.1',
+    'description': '该项目主要用于将behave框架生成的behave.json转换为cucumber.json',
+    'long_description': None,
+    'author': 'chineseluo',
+    'author_email': '848257135@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/chineseluo/behave_to_cucumber',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
