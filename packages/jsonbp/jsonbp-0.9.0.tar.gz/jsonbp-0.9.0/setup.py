09d2d116b9bb0e387a00dbb44deb1664d1a673a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:35 +0000

    Add package jsonbp/0.9.0/jsonbp-0.9.0.tar.gz

---
+++ packages/jsonbp/jsonbp-0.9.0.tar.gz/jsonbp-0.9.0/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fd:
+	long_description = fd.read()
+
+setup(
+	name='jsonbp',
+	description='JSON Python Deserializer',
+	version='0.9.0',
+	license='MIT',
+	python_requires='>=3.6',
+	platforms='any',
+	author="Gustavo Venturini",
+	author_email='gustavo.c.venturini@gmail.com',
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	url='https://github.com/vottini/jsonbp',
+	keywords=['json', 'deserialization'],
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3 :: Only",
+	],
+)
