0f958d5919bdf1d3d8444e808e0023ce600ed33d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:09 +0000

    Add package lnrun/0.1.4/lnrun-0.1.4-py3-none-any.whl

---
+++ packages/lnrun/lnrun-0.1.4-py3-none-any.whl/lnrun/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from .send_message import (send_message, main as _send_message_cli)
+from .config import load_config, write_config, get_token, is_token_setted
+from .run import main as _run_main_cli
+from .get_configs import main as _get_configs_cli
+from .set_config import main as _set_config_cli
+
---
+++ packages/lnrun/lnrun-0.1.4-py3-none-any.whl/lnrun/run.py
@@ -0,0 +1,51 @@
+import subprocess
+
+import tempfile
+import sys
+
+from lnrun.send_message import send_message
+from lnrun.config import get_config, is_token_setted
+
+
+def main(_: bool = False):
+    if not is_token_setted():
+        print('script path is not setted, run "lnrun set_config script_path https://your.path.here" to set your path')
+        exit()
+
+    cmds = sys.argv[1:]
+    if cmds[0] == 'run':
+        cmds = cmds[1:]
+
+    send_errors = get_config('send_errors')
+
+    if send_errors:
+        file = tempfile.NamedTemporaryFile()
+        process = subprocess.run(cmds, stderr=file)
+    else:
+        process = subprocess.run(cmds)
+    return_code = process.returncode
+    # popen = subprocess.Popen(cmds, stderr=file)
+    # return_code = popen.wait()
+
+    message = f'lnrun done running : {cmds}'
+    if send_errors:
+        if return_code:
+            file.seek(0)
+            lines = file.readlines()
+            try:
+                lines = [line.decode(errors='replace') for line in lines]
+                lines = ''.join(lines)
+            except Exception:
+                pass
+            file.close()
+            message += f'\nwith error:\n{lines}'
+        else:
+            message += ' without errors'
+    else:
+        message += ' with error' if return_code else ' without error'
+    print(message)
+    send_message(message)
+
+
+if __name__ == '__main__':
+    main()
