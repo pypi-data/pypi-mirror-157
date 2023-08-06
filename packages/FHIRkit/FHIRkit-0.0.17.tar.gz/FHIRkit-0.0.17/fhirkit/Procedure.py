70af62d29b70757e3137aaa10bafb6e243c1cdcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:54:29 +0000

    Add package FHIRkit/0.0.17/FHIRkit-0.0.17.tar.gz

---
+++ packages/FHIRkit/FHIRkit-0.0.17.tar.gz/FHIRkit-0.0.17/fhirkit/Procedure.py
@@ -0,0 +1,57 @@
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+from typing import Optional, Union
+from pydantic import BaseModel, Field, ValidationError, validator
+from fhirkit.Resource import DomainResource
+from fhirkit.elements import CodeableConcept, Identifier, Period, Reference
+from fhirkit.data_types import dateTime
+
+
+ProcedureStatus = Literal[
+    "preparation",
+    "in-progress",
+    "not-done",
+    "on-hold",
+    "stopped",
+    "completed",
+    "entered-in-error",
+    "unknown",
+]
+
+
+class ProcedurePerformedChoiceTypeMixin(BaseModel):
+    performedDateTime: Optional[dateTime] = Field(None, exclude=True)
+    performedPeriod: Optional[Period] = Field(None, exclude=True)
+    performedString: Optional[str] = Field(None, exclude=True)
+    performed: Union[dateTime, Period, str] = Field(None, repr=True)
+
+    @validator("performed", pre=True, always=True, allow_reuse=True)
+    def determine_performed(cls, v, values):
+        if v is not None:
+            return v
+
+        # if Procedure.performed[x] is not send through 'value' check other typed field names
+        non_null_values = list(
+            filter(
+                lambda t: t[0].startswith("performed") and t[1] is not None,
+                values.items(),
+            )
+        )
+        if len(non_null_values) == 0:
+            raise ValueError("Procedure.performed[x] can not be None.")
+        elif len(non_null_values) > 1:
+            raise ValueError("Procedure.performed[x] can only have one value.")
+        return non_null_values[0][1]
+
+
+class Procedure(DomainResource, ProcedurePerformedChoiceTypeMixin):
+    resourceType: Literal["Procedure"] = Field("Procedure", const=True)
+    identifier: Optional[Identifier] = Field(None, repr=True)
+    status: ProcedureStatus = Field("completed", repr=True)
+    statusReason: Optional[CodeableConcept] = Field(None, repr=True)
+    category: Optional[CodeableConcept] = Field(None, repr=True)
+    code: Optional[CodeableConcept] = Field(None, repr=True)
+    subject: Reference
+    encounter: Optional[Reference] = Field(None, repr=True)
