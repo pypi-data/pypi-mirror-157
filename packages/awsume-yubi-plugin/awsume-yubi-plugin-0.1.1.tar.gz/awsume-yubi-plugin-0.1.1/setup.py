c58a2b3b52c57857a33cd78b24e83ed4686dc10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:24:36 +0000

    Add package awsume-yubi-plugin/0.1.1/awsume-yubi-plugin-0.1.1.tar.gz

---
+++ packages/awsume-yubi-plugin/awsume-yubi-plugin-0.1.1.tar.gz/awsume-yubi-plugin-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awsume-yubi-plugin',
+    packages=find_packages(),
+    version='0.1.1',
+    entry_points={
+        'awsume': [
+            'yubi = yubi'
+        ]
+    },
+    author='Kyle Watson',
+    author_email='kyle.watson@woodwing.com',
+    py_modules=['yubi'],
+    description='Plugin to use a Yubikey with awsume',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/kylejwatson/awsume-yubi-plugin',
+    install_requires=[
+        'awsume',
+        'yubikey-manager',
+    ]
+)
