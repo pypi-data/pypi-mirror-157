c9a3460bfd7a8632de9c849472d2cb0753c91549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:04 +0000

    Add package Autils/0.3.12/Autils-0.3.12.tar.gz

---
+++ packages/Autils/Autils-0.3.12.tar.gz/Autils-0.3.12/autils/__init__.py
@@ -0,0 +1,8 @@
+from .logger import Logger
+from .ost import OST
+from .sign import Sign, CRCCCITT, CRCXMODEM
+from .string import String
+from .digits import Digits
+from .date import DateTool
+
+__version__ = "0.3.12"
