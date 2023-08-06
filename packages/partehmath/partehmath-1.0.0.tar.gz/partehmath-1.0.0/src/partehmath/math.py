9955297b557725304d8203e94da6d2a7983ab8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:39 +0000

    Add package partehmath/1.0.0/partehmath-1.0.0.tar.gz

---
+++ packages/partehmath/partehmath-1.0.0.tar.gz/partehmath-1.0.0/src/partehmath/math.py
@@ -0,0 +1,27 @@
+import random
+def rand(min,max):
+    return random.randrange(min,max)
+def add(addend1,addend2):
+    return addend1 + addend2
+def sub(x,y):
+    return x - y
+def mul(factor1,factor2):
+    return factor1 * factor2
+def div(x,y):
+    return x / y
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+def dot(v1,v2):
+    return sum(x*y for x, y in zip(v1, v2))
+def square_root(num):
+    if num < 0:
+        return None
+    else:
+        return num**0.5
+def Power(k, n):
+    return k^n
+def DoubleNum(n):
+    return n + n
