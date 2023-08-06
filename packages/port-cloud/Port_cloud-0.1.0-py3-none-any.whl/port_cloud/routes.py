d81a50dc3301f9c2608493a2749c6655c1ed6182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:27 +0000

    Add package port-cloud/0.1.0/Port_cloud-0.1.0-py3-none-any.whl

---
+++ packages/port-cloud/Port_cloud-0.1.0-py3-none-any.whl/port_cloud/routes.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+import argparse
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import pty
+import os
+import subprocess
+import select
+import termios
+import struct
+import fcntl
+import shlex
+import logging
+import sys
+from port_cloud import app
+from flask import render_template, redirect, url_for, flash, request
+from port_cloud.models import Item, User
+from port_cloud.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
+from port_cloud import db
+from flask_login import login_user, logout_user, login_required, current_user
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/cloud', methods=['GET', 'POST'])
+@login_required
+def market_page():
+    purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
+    if request.method == "POST":
+        #Purchase Item Logic
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price}$", category='success')
+            else:
+                flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}!", category='danger')
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+
+
+        return redirect(url_for('market_page'))
+
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, selling_form=selling_form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
+
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
+
+app.config["SECRET_KEY"] = "secret!"
+app.config["fd"] = None
+app.config["child_pid"] = None
+socketio = SocketIO(app)
+
+
+def set_winsize(fd, row, col, xpix=0, ypix=0):
+    logging.debug("setting window size with termios")
+    winsize = struct.pack("HHHH", row, col, xpix, ypix)
+    fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
+
+
+def read_and_forward_pty_output():
+    max_read_bytes = 1024 * 20
+    while True:
+        socketio.sleep(0.01)
+        if app.config["fd"]:
+            timeout_sec = 0
+            (data_ready, _, _) = select.select([app.config["fd"]], [], [], timeout_sec)
+            if data_ready:
+                output = os.read(app.config["fd"], max_read_bytes).decode()
+                socketio.emit("pty-output", {"output": output}, namespace="/pty")
+
+
+@app.route("/terminal")
+def terminal_page():
+    return render_template("terminal.html")
+
+
+@socketio.on("pty-input", namespace="/pty")
+def pty_input(data):
+    """write to the child pty. The pty sees this as if you are typing in a real
+    terminal.
+    """
+    if app.config["fd"]:
+        logging.debug("received input from browser: %s" % data["input"])
+        os.write(app.config["fd"], data["input"].encode())
+
+
+@socketio.on("resize", namespace="/pty")
+def resize(data):
+    if app.config["fd"]:
+        logging.debug(f"Resizing window to {data['rows']}x{data['cols']}")
+        set_winsize(app.config["fd"], data["rows"], data["cols"])
+
+
+@socketio.on("connect", namespace="/pty")
+def connect():
+    """new client connected"""
+    logging.info("new client connected")
+    if app.config["child_pid"]:
+        # already started child process, don't start another
+        return
+
+    # create child process attached to a pty we can read from and write to
+    (child_pid, fd) = pty.fork()
+    if child_pid == 0:
+        # this is the child process fork.
+        # anything printed here will show up in the pty, including the output
+        # of this subprocess
+        subprocess.run(app.config["cmd"])
+    else:
+        # this is the parent process fork.
+        # store child fd and pid
+        app.config["fd"] = fd
+        app.config["child_pid"] = child_pid
+        set_winsize(fd, 50, 50)
+        cmd = " ".join(shlex.quote(c) for c in app.config["cmd"])
+        # logging/print statements must go after this because... I have no idea why
+        # but if they come before the background task never starts
+        socketio.start_background_task(target=read_and_forward_pty_output)
+
+        logging.info("child pid is " + child_pid)
+        logging.info(
+            f"starting background task with command `{cmd}` to continously read "
+            "and forward pty output to client"
+        )
+        logging.info("task started")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=(
+            "A fully functional terminal in your browser. "
+            "https://github.com/cs01/pyxterm.js"
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-p", "--port", default=5000, help="port to run server on")
+    parser.add_argument(
+        "--host",
+        default="127.0.0.1",
+        help="host to run server on (use 0.0.0.0 to allow access from other hosts)",
+    )
+    parser.add_argument("--debug", action="store_true", help="debug the server")
+    parser.add_argument("--version", action="store_true", help="print version and exit")
+    parser.add_argument(
+        "--command", default="bash", help="Command to run in the terminal"
+    )
+    parser.add_argument(
+        "--cmd-args",
+        default="",
+        help="arguments to pass to command (i.e. --cmd-args='arg1 arg2 --flag')",
+    )
+    args = parser.parse_args()
+    if args.version:
+        print(__version__)
+        exit(0)
+    app.config["cmd"] = [args.command] + shlex.split(args.cmd_args)
+    green = "\033[92m"
+    end = "\033[0m"
+    log_format = green + "Terminal > " + end + "%(levelname)s (%(funcName)s:%(lineno)s) %(message)s"
+    logging.basicConfig(
+        format=log_format,
+        stream=sys.stdout,
+        level=logging.DEBUG if args.debug else logging.INFO,
+    )
+    logging.info(f"serving on http://{args.host}:{args.port}")
+    socketio.run(app, debug=args.debug, port=args.port, host=args.host)
+
