4fc0f5bf28e315d584cff2b12de634fa4d83f67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:53 +0000

    Add package pytest-chunks/0.2.0/pytest-chunks-0.2.0.tar.gz

---
+++ packages/pytest-chunks/pytest-chunks-0.2.0.tar.gz/pytest-chunks-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytest_chunks', 'pytest_chunks.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=6.0.0']
+
+entry_points = \
+{'pytest11': ['pytest_chunks = pytest_chunks']}
+
+setup_kwargs = {
+    'name': 'pytest-chunks',
+    'version': '0.2.0',
+    'description': 'Run only a chunk of your test suite',
+    'long_description': None,
+    'author': 'Mehdi Abaakouk',
+    'author_email': 'sileht@sileht.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
