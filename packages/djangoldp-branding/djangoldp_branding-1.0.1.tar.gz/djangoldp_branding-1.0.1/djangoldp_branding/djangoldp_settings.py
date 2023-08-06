cd65c3feb2a99f8ed14dcdcfdc8a5040f6e4601f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:58 +0000

    Add package djangoldp-branding/1.0.1/djangoldp_branding-1.0.1.tar.gz

---
+++ packages/djangoldp-branding/djangoldp_branding-1.0.1.tar.gz/djangoldp_branding-1.0.1/djangoldp_branding/djangoldp_settings.py
@@ -0,0 +1,28 @@
+INSTALLED_APPS = [
+    'sass_processor'
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+SASS_OUTPUT_STYLE = 'compressed'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'djangoldp_branding.processor.branding_context',
+            ],
+        },
+    },
+]
