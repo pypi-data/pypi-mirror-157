49c2363c0fbe1604fa19cbfa9be6cd48fa37db49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:32 +0000

    Add package anasim/0.0.6/anasim-0.0.6.tar.gz

---
+++ packages/anasim/anasim-0.0.6.tar.gz/anasim-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="anasim",
+    version="0.0.6",
+    author="david.sulc",
+    author_email="sulda.str@gmail.com",
+    py_modules=['text'],
+    package_dir={'':'anasim'}
+)
