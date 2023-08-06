09481efb6f4c24b07e6813ceb67324de66e65c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package PTEA/0.0.1/PTEA-0.0.1.tar.gz

---
+++ packages/PTEA/PTEA-0.0.1.tar.gz/PTEA-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PTEA",  ##注意和文件夹名一致
+    version="0.0.1",
+    description=(
+        "原始模型"
+    ),
+    long_description_content_type='text/markdown',
+    author="Fanstandy",
+    author_email="2806481560@qq.com",
+    packages=find_packages(),
+    url="https://github.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
