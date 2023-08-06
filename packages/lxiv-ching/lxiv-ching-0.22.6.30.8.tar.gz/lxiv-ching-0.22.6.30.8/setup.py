3e9f2bb23843ab3927729593efe4e0d394191535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:41 +0000

    Add package lxiv-ching/0.22.6.30.8/lxiv-ching-0.22.6.30.8.tar.gz

---
+++ packages/lxiv-ching/lxiv-ching-0.22.6.30.8.tar.gz/lxiv-ching-0.22.6.30.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='lxiv-ching',
+    version='0.22.6.30.8',
+    license='gpl-3.0',
+    author="Kosma Marcin Hyżorek",
+    author_email='kosmah@tutanota.com',
+    packages=find_packages('src', 'src/lxiv-ching'),
+    long_description_content_type = "text/markdown",
+    package_dir={'': 'src'},
+    url='https://github.com/kosma-hyzoe/lxivChing',
+    keywords='lxiv-ching',
+    python_requires='>=3.10',
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+    exclude_package_data = {'': ['.gitignore', 'GBCh.md', 'history.lxiv.txy']}
+)
