86f4ebd48f33320eb45fa724a688f1f733cc0f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:04 +0000

    Add package simpletyper/0.1.0/simpletyper-0.1.0.tar.gz

---
+++ packages/simpletyper/simpletyper-0.1.0.tar.gz/simpletyper-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['simpletyper']
+
+package_data = \
+{'': ['*'], 'simpletyper': ['words/*']}
+
+install_requires = \
+['pyfiglet>=0.8.post1,<0.9',
+ 'requests>=2.28.1,<3.0.0',
+ 'textual>=0.1.18,<0.2.0']
+
+setup_kwargs = {
+    'name': 'simpletyper',
+    'version': '0.1.0',
+    'description': 'Typing speed tester powered by Textual',
+    'long_description': None,
+    'author': 'Wilson Oh',
+    'author_email': 'oh.wilson123@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
