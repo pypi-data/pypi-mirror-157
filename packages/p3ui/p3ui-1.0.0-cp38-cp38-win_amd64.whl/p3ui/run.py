3be5dc3610af23edad664db7b5ad3bbed2214e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:57 +0000

    Add package p3ui/1.0.0/p3ui-1.0.0-cp38-cp38-win_amd64.whl

---
+++ packages/p3ui/p3ui-1.0.0-cp38-cp38-win_amd64.whl/p3ui/run.py
@@ -0,0 +1,16 @@
+import asyncio
+from .gui_event_loop import GuiEventLoop
+
+
+async def __entry_task(loop, entry_function):
+    await entry_function
+    loop.close()
+
+
+def run(entry_function):
+    loop = GuiEventLoop()
+    asyncio.set_event_loop(loop)
+    asyncio._set_running_loop(loop)
+    loop.create_task(__entry_task(loop, entry_function))
+    loop.run_forever()
+    loop.shutdown_default_executor()
