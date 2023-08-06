58b2d9ed1e47adc295e7f5cdf2feb11c149fc31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:59 +0000

    Add package statistics-probability-distributions/0.1/statistics-probability-distributions-0.1.tar.gz

---
+++ packages/statistics-probability-distributions/statistics-probability-distributions-0.1.tar.gz/statistics-probability-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statistics-probability-distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial',
+      packages=['statistics-probability-distributions'],
+      author='Muhammad Rizwan',
+      author_email='xrizwanr@gmail.com',
+      zip_safe=False)
