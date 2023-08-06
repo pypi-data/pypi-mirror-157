72fdefc3d40113926f409b82d35382a48c7dd758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:35 +0000

    Add package audl/0.0.4/audl-0.0.4-py3-none-any.whl

---
+++ packages/audl/audl-0.0.4-py3-none-any.whl/audl/stats/library/parameters.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env/python
+
+class Division:
+    altantic = 'Atlantic'
+    central = 'Central'
+    west = 'West'
+    canada = 'Canada Cup'
+
+
+class StatisticsDescription:
+    YR = 'Year'
+    G = 'Games Played'
+    PP = 'Points Played'
+    TM = 'Team'
+    AST = 'Assists'
+    GLS = 'Goals'
+    BLK = 'Blocks'
+    PLUS_MINUS = 'Plus Minus'
+    CMP = 'Completions'
+    CMP_PERC = 'Completion Percentage'
+    TY = 'Throwing Yards'
+    RY = 'Receiving Yards'
+    HA = 'Hockey Assists'
+    T = 'Throwaways'
+    S = 'Stall'
+    C = 'Callahan'
+    D = 'Drops'
+    OPP = 'Offensive Points Played'
+    DPP = 'Defensive Points Played'
+    P = 'Pulls'
+
+
+class StatisticAbbreviation:
+    stat_col_year = 'YR'
+    stat_col_games_played = 'G'
+    stat_col_points_played = 'PP'
+    stat_col_team = 'TM'
+    stat_col_assists = 'AST'
+    stat_col_goals = 'GLS'
+    stat_col_blocks = 'BLK'
+    stat_col_plus_minus = '+/-'
+    stat_col_completions = 'CMP'
+    stat_col_completion_perc = 'CMP%'
+    stat_col_throwing_yards = 'TY'
+    stat_col_receiving_yards = 'RY'
+    stat_col_hockey_assists = 'HA'
+    stat_col_throwaways = 'T'
+    stat_col_stalls = 'S'
+    stat_col_callahans = 'C'
+    stat_col_drops = 'D'
+    stat_col_offensive_points_played = 'OPP'
+    stat_col_defensive_points_played = 'DPP'
+    stat_col_pulls = 'P'
+
+
+class FileName:
+    alltimeplayer = 'AllTimePlayerStats'
+    seasonplayerstats = 'SeasonPlayerStats'
+    seasonschedule = 'SeasonSchedule'
+
+#######################################################################
+#                         TeamSeasonSchedule                          #
+#######################################################################
+
+team_season_schedule_index_away	 = 0
+team_season_schedule_index_home	 = 1
+team_season_schedule_index_time = 2
+team_season_schedule_index_stadium = 3
+team_season_schedule_index_game_id = 4
+team_season_schedule_index_home_score = 5
+team_season_schedule_index_away_score = 6
+
+game_schedule_columns_name = [
+    "Away",
+    "Home",
+    "Time",
+    "Stadium",
+    "Game ID",
+    "Away Score",
+    "Home Score"
+]
+
+#######################################################################
+#                           Season Schedule                           #
+#######################################################################
+
+NUM_WEEKS_IN_SEASON = 12
+
+
+#######################################################################
+#                             Box Scores                              #
+#######################################################################
+
+box_scores_columns_names = ["Team", "Q1", "Q2", "Q3", "Q4", "T"]
+
+# Absolute time in seconds for each quarter
+# Regular time: 12 min per quarter
+# 1rst OT: 5 minutes
+# 2nd OT: sudden death
+quarters_clock_dict = {
+    "Q1_start": 1,      # 0 min
+    "Q1_end": 720,      # 12 min
+    "Q2_start": 721,    # 12 min
+    "Q2_end": 1440,     # 24 min
+    "Q3_start": 1441,   # 24 min
+    "Q3_end": 2160,     # 36 min
+    "Q4_start": 2161,   # 36 min
+    "Q4_end": 2880,        # 48 min
+    "OT1_start": 2881,     # 48 min
+    "OT1_end": 3180,       # 53 min
+    "OT2_start": 3181,     # 53 min
+    #  "OT2_end": 0,       # ? min
+}
+
+#######################################################################
+#                          Games Parameters                           #
+#######################################################################
+
+# Types of throws and their id :\t on heroku server
+
+
+class GameEventAction:
+    # Types of events defined on heroku server
+    # Block
+    # Dish
+    # Dump
+    # Huck throwaway
+    # Pass
+    # Pull
+    # Score
+    # Swing
+    pass
+
+
+class TeamStatsName:
+    Completions = 'Completions'
+    Hucks = 'Hucks'
+    Offensive_holds = 'Offensive Holds'
+    Defensive_breaks = 'Defensive Breaks'
+    Red_zone_possessions = 'Red Zone Posessions'
+    Hucks = 'Hucks'
+    Turnovers = 'Turnovers'
+    Blocks = 'Blocks'
+    Turnovers = 'Turnovers'
+
+
+team_stats_row_names = [
+    TeamStatsName.Completions,
+    TeamStatsName.Hucks,
+    TeamStatsName.Offensive_holds,
+    TeamStatsName.Defensive_breaks,
+    TeamStatsName.Red_zone_possessions,
+    TeamStatsName.Blocks,
+    TeamStatsName.Turnovers,
+]
+
+team_stats_perc_columns_names = [
+    "",
+    "Successful",
+    "Opportunities",
+    "Percentage"
+]
+
+team_roster_columns_name = [
+    "identification_num",
+    "first_name",
+    "last_name",
+    "jersey_number",
+    "player_id"
+
+]
+
+# \t: number mapings
+# \r: receiver
+# \l: lineup
+# \x: disc absolute position in x
+# \y: disc absolut position in y
+# \s: ???
+
+
+class HerokuPlay:
+    OLineIndex = 1
+    DLineIndex = 2
+    Pull = 3
+    Block = 5
+    ThrowawayCaused = 8
+    Throwaway = 9
+    EndOfQ1 = 23
+    EndOfQ2 = 24
+    EndOfQ3 = 25
+    EndOfQ4 = 26
+    PassCompleted = 20
+    ScoredOn = 21
+    Goal = 22
+    TimeoutDefense = 40
+    TimeoutOffense = 41
+
+
+team_points_by_points_columns_names = [
+    "Current Score",
+    "Line",
+    "Clock",
+    "Point Duration",
+    "Quarter",
+    "Lineup"
+]
+
+play_by_play_columns_names = [
+    "Score",
+    "Offensive Team",
+    "Defensive Team",
+    "O-Line",
+    "D-Line",
+    "Quarter",
+    "Point Duration",
+    "Clock",
+    "Outcome"
+]
+
+
+#######################################################################
+#                            Miscellaneous                            #
+#######################################################################
+season_dict = {
+    2021: '1',
+    2019: '2',
+    2018: '3',
+    2017: '4',
+    2015: '5',
+    2016: '6',
+    2014: '7',
+    2013: '8',
+    2012: '9',
+}
