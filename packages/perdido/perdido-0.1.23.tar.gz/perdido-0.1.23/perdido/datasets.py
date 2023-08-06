d0e475ae31d9dc5f08bff64dc5ac67fb5d45375b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:35 +0000

    Add package perdido/0.1.23/perdido-0.1.23.tar.gz

---
+++ packages/perdido/perdido-0.1.23.tar.gz/perdido-0.1.23/perdido/datasets.py
@@ -0,0 +1,39 @@
+from typing import Dict
+
+import pkg_resources
+#import importlib.resources as pkg_resources
+import pandas as pd
+
+from perdido.perdido import PerdidoCollection
+
+
+def load_edda_artfl() -> Dict:
+    filepath = pkg_resources.resource_stream(__name__, 'data/edda_artfl/edda_artf_dataset.csv')
+    d = {}
+    d['data'] = pd.read_csv(filepath, sep='\t')
+    d['description'] = 'The description of the dataset will be available soon!'
+    d['feature_names'] = ['filename', 'volume', 'number', 'head', 'normClass', 'author', 'text']
+
+    return d
+
+
+def load_edda_perdido(dataset:str = 'nominatim') -> Dict:
+    d = {}
+    collection = PerdidoCollection()
+    
+    filepath = pkg_resources.resource_filename(__name__, 'data/edda_perdido/edda_perdido_vol7_' + dataset +'_dataset.pickle')
+    
+    collection.load(filepath)
+    
+    d['data'] = collection
+    d['description'] = 'The description of the dataset will be available soon!'
+    
+    return d
+
+
+def load_choucas_hikes() -> None:
+    pass
+
+
+
+
