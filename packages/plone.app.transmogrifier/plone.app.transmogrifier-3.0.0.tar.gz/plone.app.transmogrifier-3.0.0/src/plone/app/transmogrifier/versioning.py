a94b6546b7904cf4a8bb06ed6fafee5b992e377f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:08 +0000

    Add package plone.app.transmogrifier/3.0.0/plone.app.transmogrifier-3.0.0.tar.gz

---
+++ packages/plone.app.transmogrifier/plone.app.transmogrifier-3.0.0.tar.gz/plone.app.transmogrifier-3.0.0/src/plone/app/transmogrifier/versioning.py
@@ -0,0 +1,63 @@
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from Products.CMFCore.utils import getToolByName
+from zope.annotation.interfaces import IAnnotations
+from zope.interface import implementer
+from zope.interface import provider
+
+
+VERSIONABLE_KEY = "plone.app.transmogrifier.versioning:versionable"
+
+
+@implementer(ISection)
+class BaseVersioningSection:
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.repository = getToolByName(transmogrifier.context, "portal_repository")
+        self.anno = IAnnotations(transmogrifier)
+        self.save()
+
+    def save(self):
+        versionable = self.repository._versionable_content_types
+        self.anno[VERSIONABLE_KEY] = tuple(versionable)
+
+    def restore(self):
+        versionable = self.repository._versionable_content_types
+        versionable[:] = ()
+        versionable.extend(self.anno[VERSIONABLE_KEY])
+
+    def clear(self):
+        versionable = self.repository._versionable_content_types
+        versionable[:] = ()
+
+
+@provider(ISectionBlueprint)
+class DisableVersioningSection(BaseVersioningSection):
+    def __iter__(self):
+        for item in self.previous:
+            # http://stackoverflow.com/questions/2339358/workaround-for-python-2-4s-yield-not-allowed-in-try-block-with-finally-clause
+            # try:
+            #    self.save()
+            #    self.clear()
+            #    yield item
+            # finally:
+            #    self.restore()
+
+            # XXX I'm not entirely sure if the code below does the same thing
+            # as the code above. [aclark]
+            try:
+                self.save()
+                self.clear()
+                yield item
+            except (AttributeError, KeyError, ValueError):
+                self.restore()
+            self.restore()
+
+
+@provider(ISectionBlueprint)
+class EnableVersioningSection(BaseVersioningSection):
+    def __iter__(self):
+        for item in self.previous:
+            self.restore()
+            yield item
