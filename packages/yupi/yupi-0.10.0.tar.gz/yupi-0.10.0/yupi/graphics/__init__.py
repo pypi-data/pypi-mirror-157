d144d1151fb5dc8a2f53455cbc27b1f5c985751d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:46 +0000

    Add package yupi/0.10.0/yupi-0.10.0.tar.gz

---
+++ packages/yupi/yupi-0.10.0.tar.gz/yupi-0.10.0/yupi/graphics/__init__.py
@@ -0,0 +1,36 @@
+"""
+This module constains a set of functions dadicated to plot
+trajectories and extracted statistical data from ``yupi.stats``.
+
+All the resources of this module should be imported directly
+from ``yupi.graphics``.
+"""
+
+from yupi.graphics._stats_plots import (
+    plot_angles_hist,
+    plot_hist,
+    plot_hists,
+    plot_kurtosis,
+    plot_msd,
+    plot_psd,
+    plot_speed_hist,
+    plot_vacf,
+)
+from yupi.graphics._style import DOTTED, LINE, LINE_DOTTED
+from yupi.graphics._trajs_plots import plot_2D, plot_3D
+
+__all__ = [
+    "plot_angles_hist",
+    "plot_kurtosis",
+    "plot_msd",
+    "plot_vacf",
+    "plot_speed_hist",
+    "plot_2D",
+    "plot_3D",
+    "plot_psd",
+    "plot_hist",
+    "plot_hists",
+    "LINE",
+    "LINE_DOTTED",
+    "DOTTED",
+]
