e1eedf155eb441fc581f4b2b058bafd291f50150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:26:08 +0000

    Add package lianyhaii/0.0.142/lianyhaii-0.0.142-py3-none-any.whl

---
+++ packages/lianyhaii/lianyhaii-0.0.142-py3-none-any.whl/lianyhaii/__init__.py
@@ -0,0 +1,7 @@
+name = "lianyhaii"
+from lianyhaii import metrics
+from lianyhaii import eda_tool
+from lianyhaii import fe_selector
+from lianyhaii import eda_tool,feature_tools,TimeSeries_FE,reg_model,model,ts_model,tools
+
+__version__ = '0.0.142'
