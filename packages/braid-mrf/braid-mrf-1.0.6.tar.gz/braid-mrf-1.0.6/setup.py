d89099d5fe13b37e733bfc9e34bc26e21199bbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:12 +0000

    Add package braid-mrf/1.0.6/braid-mrf-1.0.6.tar.gz

---
+++ packages/braid-mrf/braid-mrf-1.0.6.tar.gz/braid-mrf-1.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(name='braid-mrf',
+      version='1.0.6',
+      description='Predicting protein complexes',
+      author='Wasinee Rungsarityotin',
+      author_email='wasinees@gmail.com',
+      url='https://github.com/wasineer-dev/braid.git',
+      packages=['braidmrf', 'braidmrf.meanfield', 'braidmrf.inputFile', 'braidmrf.observation'],
+     )
