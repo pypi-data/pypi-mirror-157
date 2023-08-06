ca3fb388ff0c65df69ba7b049b8173ac364b56aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:11 +0000

    Add package owlapy/0.0.9/owlapy-0.0.9-py3-none-any.whl

---
+++ packages/owlapy/owlapy-0.0.9-py3-none-any.whl/owlapy/__init__.py
@@ -0,0 +1,15 @@
+"""OWLAPY
+
+loosely based on OWL API
+
+many help texts copied from OWL API [1]
+OWLAPI licence: LGPL and Apache
+
+[1] https://github.com/owlcs/owlapi
+"""
+
+
+__version__ = '0.0.9'
+
+# the import order must be fixed otherwise there are circular import errors
+import owlapy.model  # noqa: F401
