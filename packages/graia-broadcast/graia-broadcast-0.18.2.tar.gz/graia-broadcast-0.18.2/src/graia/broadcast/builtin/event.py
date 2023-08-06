b682c86dde40b72f737fa814284e99c16a6ad48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:05 +0000

    Add package graia-broadcast/0.18.2/graia-broadcast-0.18.2.tar.gz

---
+++ packages/graia-broadcast/graia-broadcast-0.18.2.tar.gz/graia-broadcast-0.18.2/src/graia/broadcast/builtin/event.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from ..entities.dispatcher import BaseDispatcher
+from ..entities.event import Dispatchable
+
+if TYPE_CHECKING:
+    from graia.broadcast.interfaces.dispatcher import DispatcherInterface
+
+
+class ExceptionThrowed(Dispatchable):
+    exception: Exception
+    event: Dispatchable
+
+    def __init__(self, exception: Exception, event: Dispatchable) -> None:
+        self.exception = exception
+        self.event = event
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        async def catch(interface: "DispatcherInterface['ExceptionThrowed']"):
+            if interface.annotation is interface.event.exception.__class__:
+                return interface.event.exception
+            if interface.name == "event":
+                return interface.event.event
