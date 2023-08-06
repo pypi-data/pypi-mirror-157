7d60bd2b98d41417f3342a38d53b58e72b9d2e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:25 +0000

    Add package CrafterColor/0.2.0/CrafterColor-0.2.0.tar.gz

---
+++ packages/CrafterColor/CrafterColor-0.2.0.tar.gz/CrafterColor-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  "Intended Audience :: Developers",
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='CrafterColor',
+  version='0.2.0',
+  description='simplest and coolest color Library. print and menaged colors in Python!',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Alawi Hussein Adnan Al Sayegh',
+  author_email='programming.laboratorys@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='CrafterColor', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
