5c9702161da5ffb301476dbc6070d118c649a602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:44 +0000

    Add package Crypto20373215155/2022.6.30/Crypto20373215155-2022.6.30.tar.gz

---
+++ packages/Crypto20373215155/Crypto20373215155-2022.6.30.tar.gz/Crypto20373215155-2022.6.30/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Crypto20373215155',
+    version='2022.06.30',
+    keywords='Code',
+    description='a library for Crypto',
+    license='MIT License',
+    author='W20373215',
+    author_mail='20373215@buaa.edu.cn',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    requires=[]
+)
+
+'''
+https://code.aliyun.com/203732151552574880/Crypto.git'''
+'''from distutils.core import  setup
+
+packages = ['Crypto']# 唯一的包名，自己取名
+setup(name='Crypto',
+	version='1.0',
+	author='w20373215',
+    packages=packages,
+    package_dir={'requests': 'requests'},)'''
