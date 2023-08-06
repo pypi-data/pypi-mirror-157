06e4d5a2c1f9244b7b29386e357ee2113e3a0454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:28 +0000

    Add package installcmd/0.4.7/installcmd-0.4.7.tar.gz

---
+++ packages/installcmd/installcmd-0.4.7.tar.gz/installcmd-0.4.7/setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import setuptools
+
+setuptools.setup(
+    name="installcmd",
+    version="0.4.7",
+    author="Azat Akhmetov",
+    description="Print correct command for installing a package.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/metov/installcmd",
+    packages=setuptools.find_packages(),
+    package_data={"": ["commands.yaml"]},
+    entry_points={"console_scripts": ["installcmd = installcmd.cli:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "coloredlogs",
+        "docopt",
+        "metovlogs",
+        "python-dotenv",
+        "pyyaml",
+    ],
+)
