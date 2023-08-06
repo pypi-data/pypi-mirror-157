af8ccbeab164db75d091d7c0f6ce6226f0e529b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:25:11 +0000

    Add package nbproject/0.1.1/nbproject-0.1.1.tar.gz

---
+++ packages/nbproject/nbproject-0.1.1.tar.gz/nbproject-0.1.1/nbproject/dev/_jupyter_lab_commands.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+from ipylab import JupyterFrontEnd
+
+_app = None
+
+
+def _save_notebook():
+    global _app
+    if _app is None:
+        _app = JupyterFrontEnd()
+
+    _app.commands.execute("docmanager:save")
+    sleep(1)
+
+
+def _restart_notebook():
+    global _app
+    if _app is None:
+        _app = JupyterFrontEnd()
+
+    _app.commands.execute("docmanager:reload")  # reload notebook from disk
+    _app.commands.execute("notebook:restart-and-run-to-selected")
+    # It'd be nice to execute more cells here, but that's impossible
+    # as the kernel is dead at this point
+    #
+    # "notebook:restart-run-all" is also no alternative, as that will run
+    # the entire notebook, which might be undesirable
+    # also in combination with something like the below in `Header()` seems hacky
+    # as it prints a big "Keyboard Interrupt"
+    # if (
+    #     time_run - datetime.fromisoformat(nb.metadata["nbproject"]["time_init"])
+    # ) < 2:
+    # from ipylab import JupyterFrontEnd
+    # app = JupyterFrontEnd()
+    # app.commands.execute('notebook:interrupt-kernel')
