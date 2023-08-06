7ec4fd8a1dc25c402f86dcb96a1c9f7f2c21aa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:48 +0000

    Add package django-idom/1.1.0/django_idom-1.1.0-py2.py3-none-any.whl

---
+++ packages/django-idom/django_idom-1.1.0-py2.py3-none-any.whl/django_idom/config.py
@@ -0,0 +1,19 @@
+from typing import Dict
+
+from django.conf import settings
+from django.core.cache import DEFAULT_CACHE_ALIAS, caches
+from idom.core.types import ComponentConstructor
+
+
+IDOM_REGISTERED_COMPONENTS: Dict[str, ComponentConstructor] = {}
+
+IDOM_WEBSOCKET_URL = getattr(settings, "IDOM_WEBSOCKET_URL", "idom/")
+IDOM_WS_MAX_RECONNECT_TIMEOUT = getattr(
+    settings, "IDOM_WS_MAX_RECONNECT_TIMEOUT", 604800
+)
+
+# Determine if using Django caching or LRU cache
+if "idom" in getattr(settings, "CACHES", {}):
+    IDOM_CACHE = caches["idom"]
+else:
+    IDOM_CACHE = caches[DEFAULT_CACHE_ALIAS]
