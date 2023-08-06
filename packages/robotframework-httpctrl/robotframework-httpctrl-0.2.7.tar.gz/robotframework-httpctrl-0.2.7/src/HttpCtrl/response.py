9e0cb1abc6bc939bec1d7835bb6ee4e3bb497248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:47 +0000

    Add package robotframework-httpctrl/0.2.7/robotframework-httpctrl-0.2.7.tar.gz

---
+++ packages/robotframework-httpctrl/robotframework-httpctrl-0.2.7.tar.gz/robotframework-httpctrl-0.2.7/src/HttpCtrl/response.py
@@ -0,0 +1,42 @@
+"""
+
+HttpCtrl library provides HTTP/HTTPS client and server API to Robot Framework to make REST API testing easy.
+
+Authors: Andrei Novikov
+Date: 2018-2022
+Copyright: The 3-Clause BSD License
+
+"""
+
+
+from HttpCtrl.utils.logger import LoggerAssistant
+
+
+class Response:
+    def __init__(self, status, reason, body, headers):
+        self.__status = status
+        self.__reason = reason
+        self.__body = body
+        self.__headers = headers
+
+    def __str__(self):
+        if self.__body is None or len(self.__body) == 0:
+            return str(self.__status)
+
+        body_to_log = LoggerAssistant.get_body(self.__body)
+        return "%s\n%s" % (str(self.__status), body_to_log)
+
+    def __copy__(self):
+        return Response(self.__status, self.__reason, self.__body, self.__headers)
+
+    def get_status(self):
+        return self.__status
+
+    def get_reason(self):
+        return self.__reason
+
+    def get_body(self):
+        return self.__body
+
+    def get_headers(self):
+        return self.__headers
