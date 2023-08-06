8732ee72bbdd54e4208ecbe32c85a78330d8bf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:14 +0000

    Add package CheeseFramework/1.4.27/CheeseFramework-1.4.27-py3-none-any.whl

---
+++ packages/CheeseFramework/CheeseFramework-1.4.27-py3-none-any.whl/Cheese/repositoriesBuilder.py
@@ -0,0 +1,85 @@
+#cheese
+
+import os
+import re
+
+from Cheese.resourceManager import ResMan
+from Cheese.finder import Finder
+
+class RepositoriesBuilder:
+
+    @staticmethod
+    def build(parent):
+        repositories = []
+
+        for root, dirs, files in os.walk(ResMan.root()):
+            for file in files:
+                if (not file.endswith(".py")): continue
+
+                path = os.path.join(root, file)
+
+                if (Finder.isSomething(path, "repository")): 
+                    repositories.append(path)
+
+        parent.doJson(repositories, "REPOSITORIES", 
+            ["REPOSITORY", "DBSCHEME", "DBMODEL"],
+            [("QUERY", [("RETURN", "raw")]), ("COMMIT", [])])
+
+        for repo in parent.dictJson["REPOSITORIES"].keys():
+            scheme = parent.dictJson["REPOSITORIES"][repo]["DBSCHEME"]
+            pk = re.search("\((?P<pk>\w+),.*", scheme)
+            parent.dictJson["REPOSITORIES"][repo]["PRIMARY_KEY"] = pk["pk"]
+
+        RepositoriesBuilder.preQueries(parent)
+        RepositoriesBuilder.preCommits(parent)
+    
+
+    # prefabricated query methods
+    @staticmethod
+    def preQueries(parent):
+        for repoKey in parent.dictJson["REPOSITORIES"].keys():
+            repo = parent.dictJson["REPOSITORIES"][repoKey]
+
+            repo["METHODS"]["findAll"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']};",
+                "RETURN": "array"
+            }
+            repo["METHODS"]["find"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']} where {repo['PRIMARY_KEY']}=:primaryKey;",
+                "RETURN": "one"
+            }
+            repo["METHODS"]["findBy"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']} where :columnName=:value;",
+                "RETURN": "array"
+            }
+            repo["METHODS"]["findOneBy"] = {
+                "QUERY": f"select * from {repo['REPOSITORY']} where :columnName=:value;",
+                "RETURN": "one"
+            }
+            repo["METHODS"]["findNewId"] = {
+                    "QUERY": f"select max(id) from {repo['REPOSITORY']};",
+                    "RETURN": "num"
+                }
+
+    # prefabricated commit methods 
+    @staticmethod
+    def preCommits(parent):
+        for repoKey in parent.dictJson["REPOSITORIES"].keys():
+            repo = parent.dictJson["REPOSITORIES"][repoKey]
+            scheme = repo["DBSCHEME"]
+            name = repo["REPOSITORY"]
+
+            repo["METHODS"]["save"] = {
+                    "COMMIT": f"insert into {name} {scheme} values :obj;",
+                    "RETURN": ""
+                }
+            
+            repo["METHODS"]["update"] = {
+                    "COMMIT": f"update {name} set {scheme} = :obj where {repo['PRIMARY_KEY']}=:obj.{repo['PRIMARY_KEY']};",
+                    "RETURN": ""
+                }
+
+            repo["METHODS"]["delete"] = {
+                    "COMMIT": f"delete from {name} where {repo['PRIMARY_KEY']}=:obj.{repo['PRIMARY_KEY']};",
+                    "RETURN": ""
+                }
---
+++ packages/CheeseFramework/CheeseFramework-1.4.27-py3-none-any.whl/Cheese/security.py
@@ -0,0 +1,135 @@
+#cheese
+
+import re
+import requests
+import json
+
+from Cheese.cheeseController import CheeseController as cc
+from Cheese.database import Database
+from Cheese.metadata import Metadata
+from Cheese.appSettings import Settings
+from Cheese.httpClientErrors import *
+
+class Security:
+
+    @staticmethod
+    def authenticate(server, path):
+        if (not Metadata.authentication["enabled"]):
+            return True
+
+        pth = cc.getPath(path)
+        try:
+            acc = Metadata.access[pth]
+            if (acc["minRoleId"] < 0):
+                return True
+        except:
+            return True
+
+        role = None
+        dict = None
+        authHeader = server.headers.get("Authorization")
+
+        if (authHeader != None):
+            if (authHeader.startswith("Basic ")):
+                authHeader = authHeader.replace("Basic ", "")
+
+            auth = Metadata.decode64(authHeader)
+
+            for tp in Metadata.authentication["types"]:
+                dict = Security.fitPatern(auth, tp["patern"])
+                if (dict != None):
+
+                    encoders = []
+                    if ("encoders" in tp.keys()):
+                        encoders = tp["encoders"]
+
+                    valid = Security.validate(dict, tp["validation"], encoders, server)
+                    if (valid):
+                        role = Security.findRole(dict, tp["roleId"])
+
+                        if ("additional" in tp.keys()):
+                            additional = tp["additional"]
+                            for add in additional:
+                                if (not Security.validate(dict, add["validation"], encoders, server)):
+                                    if ("exceptions" in add.keys()):
+                                        Security.handleExceptions(add["exceptions"], dict, encoders, server)
+                                    if ("raise" in add.keys()):
+                                        raise Unauthorized(add["raise"])
+                    break
+        
+        if (pth in Metadata.access.keys()):
+            if (role == None):
+                raise Unauthorized("Wrong credentials")
+            
+            if (role["value"] > acc["minRoleId"]):
+                raise Unauthorized("You do not have access to this endpoint")           
+
+        return {
+            "role": role,
+            "login": dict
+        }
+
+    @staticmethod
+    def handleExceptions(excp, dict, encoders, server):
+        endpoint = excp["endpoint"]
+        method = excp["method"]
+        content = {}
+
+        for cont in excp["content"].keys():
+            content[cont] = Security.prepareString(dict, excp["content"][cont], encoders, server)
+        
+        if (method == "GET"):
+            requests.get(f"http://localhost:{Settings.port}{endpoint}")
+        elif (method == "POST"):
+            requests.post(f"http://localhost:{Settings.port}{endpoint}", json=content)
+
+    @staticmethod
+    def findRole(dict, roleIdSql):
+        for key in dict.keys():
+            roleIdSql = roleIdSql.replace(f"${key}$", f"'{dict[key]}'")
+
+        db = Database()
+        response = db.query(roleIdSql)
+        db.done()
+
+        roleId = "None"
+        if (response != []):
+            roleId = str(response[0][0])
+
+        if (roleId not in Metadata.roles):
+            return None
+        return Metadata.roles[roleId]
+
+    @staticmethod
+    def validate(dict, validation, encoders, server):   
+        validation = Security.prepareString(dict, validation, encoders, server)
+
+        db = Database()
+        trueNess = (1, 0)
+        # negace
+        if (validation.startswith("!")):
+            validation = validation[1:]
+            trueNess = (0, 1)
+
+        response = db.query(f"select case when exists ({validation}) then cast({trueNess[0]} as bit) else cast({trueNess[1]} as bit) end;")
+        db.done()
+        return bool(int(response[0][0]))
+
+    @staticmethod
+    def prepareString(dict, string, encoders, server):
+        for key in dict.keys():
+            value = dict[key]
+            if (key in encoders.keys()):
+                value = Metadata.encode(value, getattr(Settings, encoders[key]))
+            string = string.replace(f"${key}$", f"'{value}'")
+
+        string = string.replace("$client_ip$", f"'{cc.getClientAddress(server)}'")
+        string = string.replace("$headers$", json.dumps(cc.getHeadersDict(server)))
+
+        return string
+
+    @staticmethod
+    def fitPatern(auth, patern):
+        p = re.search(patern, auth)
+        if (p == None): return None
+        return p.groupdict()
