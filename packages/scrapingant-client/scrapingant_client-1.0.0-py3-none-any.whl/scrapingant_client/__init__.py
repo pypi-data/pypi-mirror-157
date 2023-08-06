dd7e6881fef10c07f9f8bfb2eec79e9a42c5939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:16 +0000

    Add package scrapingant-client/1.0.0/scrapingant_client-1.0.0-py3-none-any.whl

---
+++ packages/scrapingant-client/scrapingant_client-1.0.0-py3-none-any.whl/scrapingant_client/__init__.py
@@ -0,0 +1,27 @@
+__version__ = "1.0.0"
+
+from scrapingant_client.client import ScrapingAntClient
+from scrapingant_client.cookie import Cookie
+from scrapingant_client.errors import (
+    ScrapingantClientException,
+    ScrapingantInvalidTokenException,
+    ScrapingantInvalidInputException,
+    ScrapingantInternalException,
+    ScrapingantSiteNotReachableException,
+    ScrapingantDetectedException,
+)
+from scrapingant_client.proxy_type import ProxyType
+from scrapingant_client.response import Response
+
+__all__ = [
+    'ScrapingAntClient',
+    'Cookie',
+    'ProxyType',
+    'ScrapingantClientException',
+    'ScrapingantInvalidTokenException',
+    'ScrapingantInvalidInputException',
+    'ScrapingantInternalException',
+    'ScrapingantSiteNotReachableException',
+    'ScrapingantDetectedException',
+    'Response',
+]
