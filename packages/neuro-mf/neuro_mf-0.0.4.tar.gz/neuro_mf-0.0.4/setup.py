73b10a55cd47914aa9ba35a81e25efb2b91fd258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:47 +0000

    Add package neuro-mf/0.0.4/neuro_mf-0.0.4.tar.gz

---
+++ packages/neuro-mf/neuro_mf-0.0.4.tar.gz/neuro_mf-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup, find_packages
+from pathlib import Path
+from typing import List
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+PROJECT_NAME = "neuro_mf"
+VERSION = "0.0.4"
+AUTHOR = "Avnish Yadav"
+DESCRIPTION = """
+   iNeuron Model Factory helps us to generate model training and grid search code automatically based 
+    on configuration provided.
+    """
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    packages=find_packages(),
+    install_requires=["PyYAML","scikit-learn"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
