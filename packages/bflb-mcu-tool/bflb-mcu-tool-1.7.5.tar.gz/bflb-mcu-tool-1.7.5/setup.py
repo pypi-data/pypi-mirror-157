1fab179fed931b559ca778e20dcd846114918afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:04 +0000

    Add package bflb-mcu-tool/1.7.5/bflb-mcu-tool-1.7.5.tar.gz

---
+++ packages/bflb-mcu-tool/bflb-mcu-tool-1.7.5.tar.gz/bflb-mcu-tool-1.7.5/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+packages = [
+    'bflb_mcu_tool',
+    'bflb_mcu_tool.core',
+    'bflb_mcu_tool.libs',
+    'bflb_mcu_tool.libs.bl616',
+    'bflb_mcu_tool.libs.bl602',
+    'bflb_mcu_tool.libs.bl702',
+    'bflb_mcu_tool.libs.bl808',
+]
+
+entry_points = {'console_scripts': ['bflb-mcu-tool = bflb_mcu_tool.__main__:run_main']}
+
+setup(
+    name="bflb-mcu-tool",
+    version="1.7.5",
+    author="bouffalolab",
+    author_email="jxtan@bouffalolab.com",
+    description="Bouffalolab Mcu Tool",
+    license="MIT",
+    url="https://pypi.org/project/bflb-mcu-tool/",
+    packages=packages,  # 包的代码主目录
+    #package_data=package_data,
+    include_package_data=True,
+    entry_points=entry_points,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: Unix',
+        'Environment :: Console',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'ecdsa>=0.15',
+        'pycryptodome==3.9.8',
+        'bflb-crypto-plus==1.0',
+        'pycklink>=0.0.7',
+        'pyserial==3.5',
+        'pylink-square==0.5.0',
+        'portalocker==2.0.0'       
+    ],
+    python_requires='>3.0,<4.0',
+    zip_safe=False,
+)
