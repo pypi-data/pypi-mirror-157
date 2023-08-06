5fc22835221c18156a9cce7703e389a85301fec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:54 +0000

    Add package PaperCrawlerUtil/0.0.48/PaperCrawlerUtil-0.0.48-py3-none-any.whl

---
+++ packages/PaperCrawlerUtil/PaperCrawlerUtil-0.0.48-py3-none-any.whl/PaperCrawlerUtil/__init__.py
@@ -0,0 +1,6 @@
+
+from PaperCrawlerUtil.crawler_util import *
+from PaperCrawlerUtil.document_util import *
+from PaperCrawlerUtil.common_util import *
+
+basic_config(logs_style=LOG_STYLE_PRINT, require_proxy_pool=True)
