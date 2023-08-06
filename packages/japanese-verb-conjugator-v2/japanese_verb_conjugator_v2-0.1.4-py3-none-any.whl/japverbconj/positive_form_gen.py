514b44ea95f250673c064b6cd7ea6636b74950a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:23 +0000

    Add package japanese-verb-conjugator-v2/0.1.4/japanese_verb_conjugator_v2-0.1.4-py3-none-any.whl

---
+++ packages/japanese-verb-conjugator-v2/japanese_verb_conjugator_v2-0.1.4-py3-none-any.whl/japverbconj/positive_form_gen.py
@@ -0,0 +1,347 @@
+# Local modules
+from .constants.enumerated_types import Formality, Tense, VerbClass
+from .constants.particle_constants import *
+from .constants.verb_ending_constants import *
+from .utils import (
+    base_te_ta_form,
+    get_verb_stem,
+    handle_irregular_verb,
+    map_dictionary_to_a_ending,
+    map_dictionary_to_e_ending,
+    map_dictionary_to_i_ending,
+    map_dictionary_to_o_ending,
+)
+
+
+# ---------------------------------------------------------- #
+#                       Positive Verb Forms                  #
+# ---------------------------------------------------------- #
+class PositiveVerbForms:
+    @classmethod
+    def generate_plain_form(cls, verb, verb_class, tense):
+        """Generate the positive polite form of the verb depending
+        on the tense.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            tense (enum): Tense Enum representing the tense for the conjugated verb
+
+        Returns:
+            str: positive plain form of the verb based on the tense parameter
+        """
+        if tense == Tense.NONPAST:
+            return verb
+        return base_te_ta_form(verb, verb_class, TA_PARTICLE, DA_PARTICLE)
+
+    @classmethod
+    def generate_polite_form(cls, verb, verb_class, tense):
+        """Generate the positive polite form of the verb depending
+        on the tense.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            tense (enum): Tense Enum representing the tense for the conjugated verb
+
+        Returns:
+            str: positive polite form of the verb based on the tense parameter
+        """
+        if tense == Tense.NONPAST:
+            ending = MASU_POSITIVE_NONPAST
+        else:
+            ending = MASU_POSITIVE_PAST
+
+        if verb_class == VerbClass.IRREGULAR:
+            return handle_irregular_verb(verb, True, ending, ending, ending)
+        else:
+            if verb_class == VerbClass.GODAN:
+                verb_stem = map_dictionary_to_i_ending(verb)
+            else:
+                verb_stem = get_verb_stem(verb, verb_class)
+            return f"{verb_stem}{ending}"
+
+    @classmethod
+    def generate_te_form(cls, verb, verb_class):
+        """Utilize base_te_ta_form function to generate the -te form
+        of the verb
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+
+        Returns:
+            str: -te form of the verb
+        """
+        return base_te_ta_form(verb, verb_class, TE_PARTICLE, DE_PARTICLE)
+
+    @classmethod
+    def generate_conditional_form(cls, verb, verb_class, formality):
+        """Generate the positive conditional form of the verb depending
+        on the level of formality.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (enum): Formality Enum representing the formality class
+                for the conjugated verb
+
+        Returns:
+            str: positive conditional form of the verb based on the formality
+        parameter
+        """
+        if formality == Formality.PLAIN:
+            verb_stem = base_te_ta_form(verb, verb_class, TA_PARTICLE, DA_PARTICLE)
+        else:
+            verb_stem = cls.generate_polite_form(verb, verb_class, Tense.PAST)
+        return f"{verb_stem}{RA_PARTICLE}"
+
+    @classmethod
+    def generate_volitional_form(cls, verb, verb_class, formality):
+        """Generate the positive volitional form of the verb depending
+        on the level of formality.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (enum): Formality Enum representing the formality class
+                for the conjugated verb
+
+        Returns:
+            str: positive volitional form of the verb based on the formality
+            parameter
+        """
+        if verb_class == VerbClass.IRREGULAR:
+            if formality == Formality.PLAIN:
+                return handle_irregular_verb(
+                    verb,
+                    suru_ending=VOLITIONAL_SURU_ENDING,
+                    kuru_ending=VOLITIONAL_KURU_ENDING,
+                    kuru_kanji_ending=VOLITIONAL_KURU_KANJI_ENDING,
+                )
+            else:
+                return handle_irregular_verb(
+                    verb,
+                    suru_ending=VOLITIONAL_POLITE_SURU_ENDING,
+                    kuru_ending=VOLITIONAL_POLITE_KURU_ENDING,
+                    kuru_kanji_ending=VOLITIONAL_POLITE_KURU_KANJI_ENDING,
+                )
+
+        elif verb_class == VerbClass.GODAN:
+            if formality == Formality.PLAIN:
+                verb_stem = map_dictionary_to_o_ending(verb)
+                ending = U_PARTICLE
+            else:
+                verb_stem = map_dictionary_to_i_ending(verb)
+                ending = VOLITIONAL_POLITE_ENDING
+        else:
+            verb_stem = get_verb_stem(verb, verb_class)
+            if formality == Formality.PLAIN:
+                ending = VOLITIONAL_ICHIDAN_PLAIN_ENDING
+            else:
+                ending = VOLITIONAL_POLITE_ENDING
+        return f"{verb_stem}{ending}"
+
+    @classmethod
+    def generate_potential_form(cls, verb, verb_class, formality):
+        """Generate the positive potential form of the verb depending
+        on the level of formality.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (enum): Formality Enum representing the formality class
+                for the conjugated verb
+
+        Returns:
+            str: positive potential form of the verb based on the specified formality
+        parameter
+        """
+        if verb_class == VerbClass.IRREGULAR:
+            if formality == Formality.PLAIN:
+                suru_ending = POTENTIAL_SURU_PLAIN_POSITIVE_ENDING
+                kuru_ending = POTENTIAL_KURU_PLAIN_POSITIVE_ENDING
+                kuru_kanji_ending = POTENTIAL_KURU_KANJI_PLAIN_POSITIVE_ENDING
+            else:
+                suru_ending = POTENTIAL_SURU_POLITE_POSITIVE_ENDING
+                kuru_ending = POTENTIAL_KURU_POLITE_POSITIVE_ENDING
+                kuru_kanji_ending = POTENTIAL_KURU_KANJI_POLITE_POSITIVE_ENDING
+            return handle_irregular_verb(
+                verb, False, suru_ending, kuru_ending, kuru_kanji_ending
+            )
+        if verb_class == VerbClass.GODAN:
+            if formality == Formality.PLAIN:
+                verb_stem = map_dictionary_to_e_ending(verb)
+                ending = RU_PARTICLE
+            else:
+                verb_stem = map_dictionary_to_e_ending(verb)
+                ending = MASU_POSITIVE_NONPAST
+        else:
+            verb_stem = get_verb_stem(verb, verb_class)
+            if formality == Formality.PLAIN:
+                ending = POTENTIAL_ICHIDAN_ENDING
+            else:
+                ending = POTENTIAL_POLITE_ICHIDAN_ENDING
+        return f"{verb_stem}{ending}"
+
+    @classmethod
+    def generate_imperative_form(cls, verb, verb_class, formality):
+        """Generate the positive imperative form of the verb depending
+        on the level of formality.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (enum): Formality Enum representing the formality class
+                for the conjugated verb
+
+        Returns:
+            str: positive imperative form based on the specified formality
+        parameter
+        """
+        if verb_class == VerbClass.IRREGULAR:
+            if formality == Formality.PLAIN:
+                return handle_irregular_verb(
+                    verb,
+                    suru_ending=IMPERATIVE_SURU_PLAIN_POSITIVE_ENDING,
+                    kuru_ending=IMPERATIVE_KURU_PLAIN_POSITIVE_ENDING,
+                    kuru_kanji_ending=IMPERATIVE_KURU_KANJI_PLAIN_POSITIVE_ENDING,
+                )
+            else:
+                return f"{cls.generate_te_form(verb, verb_class)}{KUDASAI}"
+        if formality == Formality.PLAIN:
+            if verb_class == VerbClass.GODAN:
+                verb_stem = map_dictionary_to_e_ending(verb)
+                ending = ""
+            else:
+                verb_stem = get_verb_stem(verb, verb_class)
+                ending = RO_PARTICLE
+        else:
+            verb_stem = cls.generate_te_form(verb, verb_class)
+            ending = KUDASAI
+        return f"{verb_stem}{ending}"
+
+    @classmethod
+    def generate_provisional_form(cls, verb, verb_class, formality=None):
+        """Generate the positive provisional form of the verb depending
+        on the level of formality. No formality parameter required for
+        non-irregular verbs.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (:obj: enum, optional): Formality Enum representing the formality class
+                for the conjugated verb. Defaults to None.
+
+        Returns:
+            str: positive provisional form based on the specified formality
+        parameter
+        """
+        if verb_class == VerbClass.IRREGULAR:
+            if formality == Formality.PLAIN:
+                suru_ending = PROVISIONAL_SURU_PLAIN_POSITIVE_ENDING
+                kuru_ending = PROVISIONAL_KURU_PLAIN_POSITIVE_ENDING
+                kuru_kanji_ending = PROVISIONAL_KURU_KANJI_PLAIN_POSITIVE_ENDING
+            else:
+                suru_ending = PROVISIONAL_SURU_POLITE_POSITIVE_ENDING
+                kuru_ending = PROVISIONAL_KURU_POLITE_POSITIVE_ENDING
+                kuru_kanji_ending = PROVISIONAL_KURU_KANJI_POLITE_POSITIVE_ENDING
+            return handle_irregular_verb(
+                verb, False, suru_ending, kuru_ending, kuru_kanji_ending
+            )
+        # assuming godan verb
+        if verb_class == VerbClass.GODAN:
+            verb_stem = map_dictionary_to_e_ending(verb)
+            ending = BA_PARTICLE
+        else:
+            verb_stem = get_verb_stem(verb, verb_class)
+            ending = f"{RE_PARTICLE}{BA_PARTICLE}"
+        return f"{verb_stem}{ending}"
+
+    @classmethod
+    def generate_causative_form(cls, verb, verb_class, formality):
+        """Generate the positive causative form of the verb depending
+        on the level of formality.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (enum): Formality Enum representing the formality class
+                for the conjugated verb
+
+        Returns:
+            str: positive causative form based on the specified formality
+        parameter
+        """
+        if verb_class == VerbClass.IRREGULAR:
+            return handle_irregular_verb(
+                verb,
+                suru_ending=CAUSATIVE_PLAIN_SURU_ENDING,
+                kuru_ending=CAUSATIVE_PLAIN_KURU_ENDING,
+                kuru_kanji_ending=CAUSATIVE_PLAIN_KURU_KANJI_ENDING,
+            )
+
+        if verb_class == VerbClass.GODAN:
+            verb_stem = f"{map_dictionary_to_a_ending(verb)}{SE_PARTICLE}"
+        else:
+            verb_stem = f"{get_verb_stem(verb, verb_class)}{SA_PARTICLE}{SE_PARTICLE}"
+
+        if formality == Formality.PLAIN:
+            ending = RU_PARTICLE
+        else:
+            ending = MASU_POSITIVE_NONPAST
+        return f"{verb_stem}{ending}"
+
+    @classmethod
+    def generate_passive_form(cls, verb, verb_class, formality):
+        """Generate the positive passive form of the verb depending
+        on the level of formality.
+
+        Args:
+            verb (str): Japanese verb in kana, might contain kanji
+            verb_class (enum): VerbClass Enum representing the verb class
+                to which the verb belongs
+            formality (enum): Formality Enum representing the formality class
+                for the conjugated verb
+
+        Returns:
+            str: positive passive form based on the specified formality
+        parameter
+        """
+        if verb_class == VerbClass.IRREGULAR:
+            if formality == Formality.PLAIN:
+                return handle_irregular_verb(
+                    verb,
+                    suru_ending=PASSIVE_SURU_PLAIN_POSITIVE_ENDING,
+                    kuru_ending=PASSIVE_KURU_PLAIN_POSITIVE_ENDING,
+                    kuru_kanji_ending=PASSIVE_KURU_KANJI_PLAIN_POSITIVE_ENDING,
+                )
+            else:
+                return handle_irregular_verb(
+                    verb,
+                    suru_ending=PASSIVE_SURU_POLITE_POSITIVE_ENDING,
+                    kuru_ending=PASSIVE_KURU_POLITE_POSITIVE_ENDING,
+                    kuru_kanji_ending=PASSIVE_KURU_KANJI_POLITE_POSITIVE_ENDING,
+                )
+        if verb_class == VerbClass.GODAN:
+            verb_stem = map_dictionary_to_a_ending(verb)
+            if formality == Formality.PLAIN:
+                ending = f"{RE_PARTICLE}{RU_PARTICLE}"
+            else:
+                ending = f"{RE_PARTICLE}{MASU_POSITIVE_NONPAST}"
+        else:
+            verb_stem = get_verb_stem(verb, verb_class)
+            if formality == Formality.PLAIN:
+                ending = PASSIVE_ICHIDAN_PLAIN_POSITIVE_ENDING
+            else:
+                ending = PASSIVE_ICHIDAN_POLITE_POSITIVE_ENDING
+        return f"{verb_stem}{ending}"
---
+++ packages/japanese-verb-conjugator-v2/japanese_verb_conjugator_v2-0.1.4-py3-none-any.whl/japverbconj/utils.py
@@ -0,0 +1,241 @@
+from .constants.enumerated_types import VerbClass
+from .constants.particle_constants import *
+from .constants.verb_ending_constants import *
+from .exceptions import NonIrregularVerbError
+
+
+# ---------------------------------------------------------- #
+#                UTIL VERB GENERATOR FUNCTIONS               #
+# ---------------------------------------------------------- #
+def splice_verb(verb, verb_class):
+    """Split Japanese verb between stem and ending particle(s). The number of ending
+    particles returned depends on the verb class (i.e. godan / ichidan will return one
+    particle while irregular verbs will return two particles)
+
+    Args:
+        verb (str): Japanese verb in kanji and/or kana
+        verb_class (enum): VerbClass enum representing the Japanese verb class
+
+    Returns:
+        tuple: Verb stem and particle ending
+    """
+    num_ending_particles = 1
+    if verb_class == VerbClass.IRREGULAR:
+        num_ending_particles = 2
+    return verb[: -1 * num_ending_particles], verb[-1 * num_ending_particles :]
+
+
+def get_verb_stem(verb, verb_class):
+    """Split Japanese verb between stem and ending particle(s). The number of ending
+    particles returned depends on the verb class (i.e. godan / ichidan will return one
+    particle while irregular verbs will return two particles)
+
+    Args:
+        verb (str): Japanese verb in kanji and/or kana
+        verb_class (enum): VerbClass enum representing the Japanese verb class
+
+    Returns:
+        str: Verb stem
+    """
+    return splice_verb(verb, verb_class)[0]
+
+
+def get_ending_particle(verb, verb_class):
+    """Split Japanese verb between stem and ending particle(s). The number of ending
+    particles returned depends on the verb class (i.e. godan / ichidan will return one
+    particle while irregular verbs will return two particles)
+
+    Args:
+        verb (str): Japanese verb in kanji and/or kana
+        verb_class (enum): VerbClass enum representing the Japanese verb class
+
+    Returns:
+        tuple: Particle ending
+    """
+    return splice_verb(verb, verb_class)[1]
+
+
+def handle_irregular_verb(
+    verb,
+    append_stem_particle=False,
+    suru_ending=None,
+    kuru_ending=None,
+    kuru_kanji_ending=None,
+):
+    """Handles irregular verb conjugations depending on suru or kuru verb type.
+    Isolates logic of irregular verbs.
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+        append_stem_particle (bool): verb base particle depends on if the verb is a
+            suru or kuru verb. し particle is appended to suru verbs and き is appended
+            to kuru verbs. Not all conjugations require these particles to be appended
+            to the verb stem.
+        suru_ending (:obj: str, optional): suru verb ending based on the conjugation form.
+            Defaults to None.
+        kuru_ending (:obj: str, optional): kuru verb ending based on the conjugation form.
+            Defaults to None.
+        kuru_kanji_ending (:obj: str, optional): kuru as kanji verb ending based on the conjugation form.
+            Defaults to None.
+
+    Returns:
+        str: irregular verb with appropriate particles and ending attached depending
+            on verb conjugation
+    """
+    verb_stem, particle_ending = splice_verb(verb, VerbClass.IRREGULAR)
+    if particle_ending not in [SURU_ENDING, KURU_ENDING, KURU_KANJI_ENDING]:
+        raise NonIrregularVerbError("Non-Irregular Verb Ending Found", particle_ending)
+    stem_particle = ""
+    if particle_ending == SURU_ENDING:
+        if append_stem_particle:
+            stem_particle = SHI_PARTICLE
+        ending = suru_ending
+    elif particle_ending == KURU_ENDING:
+        if append_stem_particle:
+            stem_particle = KI_PARTICLE
+        ending = kuru_ending
+    else:
+        if append_stem_particle:
+            stem_particle = KURU_KANJI
+        ending = kuru_kanji_ending
+    return f"{verb_stem}{stem_particle}{ending}"
+
+
+def generate_nai_form(verb, verb_class, is_regular_nai):
+    """Generates the nai form of a Japanese verb
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+        verb_class (enum): VerbClass Enum representing the verb class
+            to which the verb belongs
+        is_regular_nai (bool): indicates whether to attach nai ending directly
+            to verb param or generate the explicit nai form of a verb. Some
+            verb conjugations require the nai ending without calculating the
+            nai form
+
+    Returns:
+        str: nai ending attached to verb param
+    """
+    if not is_regular_nai:
+        return f"{verb}{NAI_ENDING}"
+    if verb == ARU:
+        return NAI_ENDING
+    verb_stem, particle_ending = splice_verb(verb, verb_class)
+    stem_particle = ""
+    if verb_class == VerbClass.IRREGULAR:
+        if particle_ending == SURU_ENDING:
+            stem_particle = SHI_PARTICLE
+        elif particle_ending == KURU_ENDING:
+            stem_particle = KO_PARTICLE
+        else:
+            stem_particle = KURU_KANJI
+    else:
+        if verb_class == VerbClass.GODAN:
+            verb_stem = map_dictionary_to_a_ending(verb)
+    return f"{verb_stem}{stem_particle}{NAI_ENDING}"
+
+
+def base_te_ta_form(verb, verb_class, regular_ending, dakuten_ending):
+    """Handle the formation of the -te / -ta form for verbs belonging to
+    any verb class. Logic for both forms follows similar logic but differs
+    between (-te, -de) and (-ta, -da) based on the last particle of a Godan
+    verb.
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+        verb_class (enum): VerbClass Enum representing the verb class
+            to which the verb belongs
+        regular_ending (str): ending without dakuten
+        dakuten_ending (str): ending with dakuten
+
+        TODO... reformat this logic for endings
+
+    Returns:
+        str: The verb stem plus the -te / -ta particle depending on the
+        verb class.
+    """
+    if verb_class == VerbClass.IRREGULAR:
+        return handle_irregular_verb(
+            verb, True, regular_ending, regular_ending, regular_ending
+        )
+    else:
+        verb_stem, particle_ending = splice_verb(verb, verb_class)
+        if verb_class == VerbClass.ICHIDAN:
+            verb_ending = regular_ending
+        else:
+            if particle_ending in [RU_PARTICLE, TSU_PARTICLE, U_PARTICLE]:
+                verb_ending = f"{CHISAI_TSU_PARTICLE}{regular_ending}"
+            elif particle_ending in [BU_PARTICLE, MU_PARTICLE, NU_PARTICLE]:
+                verb_ending = f"{N_PARTICLE}{dakuten_ending}"
+            elif particle_ending in [KU_PARTICLE]:
+                verb_ending = f"{I_PARTICLE}{regular_ending}"
+            elif particle_ending in [GU_PARTICLE]:
+                verb_ending = f"{I_PARTICLE}{dakuten_ending}"
+            else:
+                verb_ending = f"{SHI_PARTICLE}{regular_ending}"
+        return f"{verb_stem}{verb_ending}"
+
+
+def map_dictionary_to_a_ending(verb):
+    """Generates Godan verb stem with corresponding -a particle attached
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+
+    Returns:
+        str: verb stem with the correct -a particle attached (Godan verbs only)
+    """
+    return map_dict_form_to_different_ending(verb, A_PARTICLE)
+
+
+def map_dictionary_to_e_ending(verb):
+    """Generates Godan verb stem with corresponding -e particle attached
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+
+    Returns:
+        str: verb stem with the correct -e particle attached (Godan verbs only)
+    """
+    return map_dict_form_to_different_ending(verb, E_PARTICLE)
+
+
+def map_dictionary_to_i_ending(verb):
+    """Generates Godan verb stem with corresponding -i particle attached
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+
+    Returns:
+        str: verb stem with the correct -i particle attached (Godan verbs only)
+    """
+    return map_dict_form_to_different_ending(verb, I_PARTICLE)
+
+
+def map_dictionary_to_o_ending(verb):
+    """Generates Godan verb stem with corresponding -o particle attached
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+
+    Returns:
+        str: verb stem with the correct -o particle attached (Godan verbs only)
+    """
+    return map_dict_form_to_different_ending(verb, O_PARTICLE)
+
+
+def map_dict_form_to_different_ending(verb, desired_ending):
+    """Generates Godan verb stem and computes the correct particle to attach based on the
+    verb's last kana
+
+    Args:
+        verb (str): Japanese verb in kana, might contain kanji
+        desired_ending (str): target base_particle
+
+    Returns:
+        str: verb stem with the correct particle attached depending on the last kana particle
+    of the Godan verb
+    """
+    verb_stem, particle_ending = splice_verb(verb, VerbClass.GODAN)
+
+    return f"{verb_stem}{ENDING_DICT[particle_ending][desired_ending]}"
