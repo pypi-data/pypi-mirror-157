9114e323315580ad3cdf6438a92ddb888967a7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:25 +0000

    Add package transform/1.0.27/transform-1.0.27.tar.gz

---
+++ packages/transform/transform-1.0.27.tar.gz/transform-1.0.27/transform/constants.py
@@ -0,0 +1,9 @@
+DEFAULT_QUERY_TIMEOUT = 180
+MAX_PAGE_SIZE = 1000  # arbitrary large number to return all results
+
+API_KEY_CONFIG_KEY = "api_key"
+BEARER_TOKEN_CONFIG_KEY = "bearer_token"
+BEARER_TOKEN_EXPIRES_AT_CONFIG_KEY = "bearer_token_expires_at"
+MQL_OVERRIDE_CONFIG_KEY = "mql_server_url_override"
+PINNED_MODEL_ID_CONFIG_KEY = "pinned_model_id"
+BACKEND_OVERRIDE_CONFIG_KEY = "backend_url_override"
