656914a48ed6d68f48b91632bec134fbaa3beed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:54 +0000

    Add package moviemaker/0.0.1/moviemaker-0.0.1-py3-none-any.whl

---
+++ packages/moviemaker/moviemaker-0.0.1-py3-none-any.whl/moviemaker/__main__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  3 22:10:33 2022
+
+@author: Antonio
+"""
+
+from moviemaker.moviemaker import main as _main
+
+if __name__ == "__main__":
+    _main()
