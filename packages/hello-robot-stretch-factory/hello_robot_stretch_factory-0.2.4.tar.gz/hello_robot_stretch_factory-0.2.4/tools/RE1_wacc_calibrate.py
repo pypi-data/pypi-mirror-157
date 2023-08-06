51113f11fad2c6ece5fe6ecc67459457c3438cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package hello-robot-stretch-factory/0.2.4/hello_robot_stretch_factory-0.2.4.tar.gz

---
+++ packages/hello-robot-stretch-factory/hello_robot_stretch_factory-0.2.4.tar.gz/hello_robot_stretch_factory-0.2.4/tools/RE1_wacc_calibrate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from future.builtins import input
+import os, sys
+import math
+import time
+import stretch_body.wacc as wacc
+
+print('Calibrating Wacc. Ensure arm is retracted and level to ground')
+input('Hit enter when ready')
+
+
+w=wacc.Wacc()
+w.params['config']['accel_gravity_scale']=1.0
+w.startup()
+
+# Calibrate accel
+cum=0.0
+for i in range(100):
+    w.pull_status()
+    z=math.sqrt(w.status['ax']**2+w.status['ay']**2+w.status['az']**2)
+    print('Itr',i,'Val',z)
+    cum=cum+z
+    time.sleep(0.05)
+w.stop()
+cum=cum/100.0
+print('Got a average value of',cum)
+s=9.80665/cum
+accel_gravity_scale_max= 1.1
+accel_gravity_scale_min= 0.9
+if s>accel_gravity_scale_min and s<accel_gravity_scale_max:
+    print('Gravity scalar of %f within bounds of %f to %f'%(s,accel_gravity_scale_min ,accel_gravity_scale_max ))
+    print('Writing yaml...')
+    w.write_configuration_param_to_YAML('wacc.config.accel_gravity_scale', s)
+else:
+    print('Gravity scalar of %f outside bounds of %f to %f' % (s, accel_gravity_scale_min,accel_gravity_scale_max))
+w.stop()
