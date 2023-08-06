596bc7dd56869800d119a232ca6265cb20348054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:44:19 +0000

    Add package mix-voice/0.1/mix_voice-0.1.tar.gz

---
+++ packages/mix-voice/mix_voice-0.1.tar.gz/mix_voice-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="mix_voice",
+version="0.1",
+description="This is a talk module. Check the long description for more info.",
+license="./license",
+packages=['mix_voice'],
+keywords=["talk", "talk module", "TALK MODULE", "Talk Module", "speaking", "speak", "module", "MODULE", "Module"],
+install_requires=['pyttsx3', 'speechRecognition'],
+author="Nishant Deb",
+author_email="debnishant830@gmail.com")
