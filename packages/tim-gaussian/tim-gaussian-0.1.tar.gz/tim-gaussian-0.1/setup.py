d67a14e00843abcb029458943bd0f6741b3f8402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:20 +0000

    Add package tim-gaussian/0.1/tim-gaussian-0.1.tar.gz

---
+++ packages/tim-gaussian/tim-gaussian-0.1.tar.gz/tim-gaussian-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tim-gaussian',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      author='Timothy Hobbs Kumah',
+      packages=['tim-gaussian'],
+      zip_safe=False)
