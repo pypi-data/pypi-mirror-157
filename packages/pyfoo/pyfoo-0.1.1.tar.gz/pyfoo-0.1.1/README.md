733db4b69a9d021e4ac1c6ac1f93aece5e57f81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:52:07 +0000

    Add package pyfoo/0.1.1/pyfoo-0.1.1.tar.gz

---
+++ packages/pyfoo/pyfoo-0.1.1.tar.gz/pyfoo-0.1.1/README.md
@@ -0,0 +1,1 @@
+This is a package for playing around with adding Julia dependencies to Python packages using JuliaPkg.
---
+++ packages/pyfoo/pyfoo-0.1.1.tar.gz/pyfoo-0.1.1/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup_args = {
+   'description': 'Testing...',
+   'author': 'Daniel Ingraham',
+   'author_email': 'd.j.ingraham@gmail.com',
+   'install_requires': ['juliapkg', 'juliacall'],
+   'url': 'https://github.com/dingraha/pyfoo',
+   'license': 'MIT',
+   'name': 'pyfoo',
+   'packages': ['pyfoo'],
+   'version': '0.1.1',
+   'include_package_data': True}
+
+setup(**setup_args)
