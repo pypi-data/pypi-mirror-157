c9bb9eb4dec1dc1c57923429924f9df6c6e07da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:22 +0000

    Add package labeldb/0.0.1/labeldb-0.0.1.tar.gz

---
+++ packages/labeldb/labeldb-0.0.1.tar.gz/labeldb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="labeldb",
+    version="0.0.1",
+    description="LabelDB",
+    packages=["labeldb"],
+    author="Activeloop",
+    author_email="support@activeloop.ai",
+    url="https://activeloop.ai",
+)
