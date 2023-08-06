9ce8ec00d711bca9c56a5800e0b461b3bed8819c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:32 +0000

    Add package pydflow/1.1.6/pydflow-1.1.6.tar.gz

---
+++ packages/pydflow/pydflow-1.1.6.tar.gz/pydflow-1.1.6/src/dflow/config.py
@@ -0,0 +1,9 @@
+config = {
+    "host": "https://127.0.0.1:2746",
+    "token": None,
+    "k8s_config_file": None,
+    "k8s_api_server": None,
+    "private_key_host_path": "/home/docker/.ssh",
+    "save_path_as_parameter": True,
+    "catalog_file_name": ".dflow"
+}
