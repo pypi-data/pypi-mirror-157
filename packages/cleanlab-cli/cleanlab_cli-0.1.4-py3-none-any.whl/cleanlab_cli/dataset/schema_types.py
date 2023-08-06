2c868268b86f00a1fd5938307aac14baad5efbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:29 +0000

    Add package cleanlab-cli/0.1.4/cleanlab_cli-0.1.4-py3-none-any.whl

---
+++ packages/cleanlab-cli/cleanlab_cli-0.1.4-py3-none-any.whl/cleanlab_cli/dataset/schema_types.py
@@ -0,0 +1,20 @@
+from decimal import Decimal
+
+from cleanlab_cli.version import __version__
+
+DATA_TYPES_TO_FEATURE_TYPES = {
+    "string": {"text", "categorical", "datetime", "identifier"},
+    "integer": {"categorical", "datetime", "identifier", "numeric"},
+    "float": {"datetime", "numeric"},
+    "boolean": {"boolean"},
+}
+
+PYTHON_TYPES_TO_READABLE_STRING = {
+    str: "string",
+    float: "float",
+    int: "integer",
+    bool: "boolean",
+    Decimal: "float",
+}
+
+SCHEMA_VERSION = __version__
