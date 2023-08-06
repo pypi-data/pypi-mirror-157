3bce74b95bd573a73780911d30764e626bcfb9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:19 +0000

    Add package min-dalle/0.2.1/min-dalle-0.2.1.tar.gz

---
+++ packages/min-dalle/min-dalle-0.2.1.tar.gz/min-dalle-0.2.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='min-dalle',
+    description = 'min(DALL·E)',
+    version='0.2.1',
+    author='Brett Kuprel',
+    author_email='brkuprel@gmail.com',
+    url='https://github.com/kuprel/min-dalle',
+    packages=[
+        'min_dalle', 
+        'min_dalle.models'
+    ],
+    license='MIT',
+    install_requires=[
+        'torch>=1.11.0',
+        'typing_extensions>=4.1.0'
+    ],
+    keywords = [
+        'artificial intelligence',
+        'deep learning',
+        'text-to-image',
+        'pytorch'
+    ]
+)
