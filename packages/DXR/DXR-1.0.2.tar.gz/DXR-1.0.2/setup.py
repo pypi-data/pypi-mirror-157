78b9fd934c3808fda6cd7aaeb845b3091ae546cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:07 +0000

    Add package DXR/1.0.2/DXR-1.0.2.tar.gz

---
+++ packages/DXR/DXR-1.0.2.tar.gz/DXR-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DXR',
+    version='1.0.2',
+    packages=['Dxr_mqtt', 'Dxr_log', 'Dxr_bytes', 'Dxr_utils', 'Dxr_video'],
+    install_requires=['paho-mqtt', 'pyyaml', 'grpcio', 'grpcio-tools', 'opencv-python'],
+    author='luzhipeng',
+    author_email='402087139@qq.com',
+    license='MIT',
+    url='http://pycn.me'
+)
