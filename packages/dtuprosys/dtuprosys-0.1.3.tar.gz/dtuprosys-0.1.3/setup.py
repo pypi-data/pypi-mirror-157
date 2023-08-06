01913b30b6d94c5d1ce289b1f1798bfc8d208f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:54 +0000

    Add package dtuprosys/0.1.3/dtuprosys-0.1.3.tar.gz

---
+++ packages/dtuprosys/dtuprosys-0.1.3.tar.gz/dtuprosys-0.1.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dtuprosys",
+    version_config=True,
+    setup_requires=["setuptools-git-versioning"],
+    author="Pau Cabaneros",
+    author_email="pau.cabaneros@gmail.com",
+    description="Package to teach chemometrics in the context of fermentation processes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paucablop/dtu.prosys",
+    project_urls={
+        "Bug Tracker": "https://github.com/paucablop/dtu.prosys/issues/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "dtuprosys"},
+    packages=setuptools.find_packages(where="dtuprosys"),
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy==1.23.0",
+        "pandas==1.4.3",
+        "matplotlib==3.5.2",
+        "mbpls==1.0.8b1",
+        "scipy==1.9.0rc1",
+        "scikit-learn==1.1.1",
+    ],
+)
