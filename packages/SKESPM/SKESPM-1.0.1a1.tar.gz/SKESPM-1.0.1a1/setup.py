f0d985bca63f35e4c8f4e1fc72e704448222d648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:50:00 +0000

    Add package SKESPM/1.0.1a1/SKESPM-1.0.1a1.tar.gz

---
+++ packages/SKESPM/SKESPM-1.0.1a1.tar.gz/SKESPM-1.0.1a1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name='SKESPM',
+
+    version='1.0.1a1',
+
+    packages=[
+        'SKESPM',
+    ],
+
+    url='https://github.com/SwiftKRAS-Enterprise-software/project-manager',
+
+    license='GNU General Public License v3.0',
+
+    author='dssk-esoftware',
+    author_email='dssk-esoftware@hotmail.com',
+
+    description='This framework help create and manage default project',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+
+    zip_safe=False,
+)
