ea651fcfad3d45700d905346c9d7a824ba2c4fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:35 +0000

    Add package ta-cn/0.2.5/ta_cn-0.2.5-py3-none-any.whl

---
+++ packages/ta-cn/ta_cn-0.2.5-py3-none-any.whl/ta_cn/talib/__init__.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author     :wukan
+# @License    :(C) Copyright 2022, wukan
+# @Date       :2022-06-16
+"""
+Examples
+--------
+只要替换导入即可直接支持二维矩阵
+>>> import talib as ta
+>>> import ta_cn.talib as ta
+
+"""
+from functools import wraps
+
+import numpy as np
+import talib as _talib
+from talib import abstract as _abstract
+
+from ta_cn.utils import pd_to_np, is_np_pd
+
+
+def tafunc_nditer_0(tafunc, args, kwargs, output_names):
+    """直接调用talib"""
+    return tafunc(*args, **kwargs)
+
+
+def tafunc_nditer_1(tafunc, args, kwargs, output_names):
+    """内部按列迭代函数，参数完全仿照talib
+
+    Parameters
+    ----------
+    tafunc
+        计算单列的函数
+    args
+        位置参数
+    kwargs
+        命名参数
+    output_names: list
+        tafunc输出参数名
+
+    Returns
+    -------
+    tuple
+        输出数组
+
+    """
+    # 分组
+    args_input = [pd_to_np(v) for v in args if is_np_pd(v)]  # 位置非数字参数
+    args_param = [pd_to_np(v) for v in args if not is_np_pd(v)]  # 位置数字参数
+    kwargs_input = {k: pd_to_np(v) for k, v in kwargs.items() if is_np_pd(v)}  # 命名非数字参数
+    kwargs_param = {k: pd_to_np(v) for k, v in kwargs.items() if not is_np_pd(v)}  # 命名数字参数
+
+    # 取一个非数字，得用于得到形状
+    if len(args_input) > 0:
+        real = args_input[0]
+    else:
+        real = list(kwargs_input.values())[0]
+
+    if real.ndim == 1:
+        # 一维，直接计算并返回
+        return tafunc(*args_input, *args_param, **kwargs_input, **kwargs_param)
+
+    # =====以下是二维======
+
+    # 输出缓存
+    outputs = [np.empty_like(real) for _ in output_names]
+    # 输入源
+    inputs = [*args_input, *kwargs_input.values()]
+
+    # 只有一行输入时需要特别处理
+    with np.nditer(inputs + outputs,
+                   flags=['external_loop'] if real.shape[0] > 1 else None,
+                   order='F',
+                   op_flags=[['readonly']] * len(inputs) + [['writeonly']] * len(outputs)) as it:
+
+        for i, in_out in enumerate(it):
+            if real.shape[0] == 1:
+                # 需要将0维array改成1维，否则talib报错
+                in_out = [v.reshape(1) for v in in_out]
+
+            _in = in_out[:len(inputs)]  # 分离输入
+            _out = in_out[len(inputs):]  # 分离输出
+            args_in = in_out[:len(args_input)]  # 分离位置输入
+            kwargs_in = {k: v for k, v in zip(kwargs_input.keys(), _in[len(args_input):])}  # 分离命名输入
+
+            # 计算并封装
+            ta_out = tafunc(*args_in, *args_param, **kwargs_in, **kwargs_param)
+            if not isinstance(ta_out, tuple):
+                ta_out = tuple([ta_out])
+
+            # 转存数据
+            for _i, _o in zip(_out, ta_out):
+                _i[...] = _o
+
+    # 输出
+    if len(outputs) == 1:
+        return outputs[0]
+    return outputs
+
+
+def tafunc_nditer_2(tafunc, args, kwargs, output_names):
+    """内部按列迭代函数，支持timeperiod等命名参数向量化
+
+    Parameters
+    ----------
+    tafunc
+        计算单列的函数
+    args
+        位置参数
+    kwargs
+        命名参数
+    output_names: list
+        tafunc输出参数名
+
+    Returns
+    -------
+    tuple
+        输出元组
+
+    """
+
+    def num_to_np(x, like):
+        """将单字数字转成矩阵"""
+        if hasattr(x, "__getitem__"):
+            # 长度不足时，用最后一个值填充之后的参数
+            y = np.full_like(like, fill_value=x[-1])
+            # 长度超出时，截断
+            y[:len(x)] = x[:len(y)]
+            return y
+        # 单一值，填充成唯一值
+        return np.full_like(like, fill_value=x)
+
+    real = args[0]
+
+    if real.ndim == 1:
+        # 一维，直接计算并返回
+        return tafunc(*args, **kwargs)
+
+    # =====以下是二维======
+
+    # 输出缓存
+    outputs = [np.empty_like(real) for _ in output_names]
+    kwargs = {k: num_to_np(v, real[0]) for k, v in kwargs.items()}
+
+    # 只有一行输入时需要特别处理
+    with np.nditer(list(args) + outputs,
+                   flags=['external_loop'] if real.shape[0] > 1 else None,
+                   order='F',
+                   op_flags=[['readonly']] * len(args) + [['writeonly']] * len(outputs)) as it:
+        for i, in_out in enumerate(it):
+            if real.shape[0] == 1:
+                # 需要将0维array改成1维，否则talib报错
+                in_out = [v.reshape(1) for v in in_out]
+
+            _in = in_out[:-len(outputs)]  # 分离输入
+            _out = in_out[-len(outputs):]  # 分离输出
+            # 切片得到每列的参数
+            _kw = {k: v[i] for k, v in kwargs.items()}
+
+            # 计算并封装
+            ta_out = tafunc(*_in, **_kw)
+            if not isinstance(ta_out, tuple):
+                ta_out = tuple([ta_out])
+
+            for _i, _o in zip(_out, ta_out):
+                _i[...] = _o
+
+    # 输出
+    if len(outputs) == 1:
+        return outputs[0]
+    return outputs
+
+
+def ta_decorator(func, mode, output_names):
+    # 设置对应处理函数
+    ff = {0: tafunc_nditer_0, 1: tafunc_nditer_1, 2: tafunc_nditer_2}.get(mode)
+
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        return ff(func, args, kwargs, output_names)
+
+    return decorated
+
+
+def init(mode=1):
+    """初始化环境
+
+    Parameters
+    ----------
+    mode:
+        0: 直接转向原版talib。只支持一维向量
+        1: 输入数据支持二维矩阵。用法与原talib完全一样
+        3. 输入参数支持一维向量。数据使用位置，周期等使用命名。否则报错
+
+    """
+    print(f'ta_cn 应用模式，当前为: {mode}')
+    if mode == 0:
+        print(f'\t0. 一维转发模式。直接转发的talib，因封装了一层，还失去了IDE智能提示，推荐直接使用原版talib')
+    if mode == 1:
+        print(f'\t1. 二维数据模式。既可以使用位置参数，也可以使用命名参数。与原talib完全一样。会根据类型自动区分是开高低收等数据，还是周期长度等参数')
+    if mode == 2:
+        print(f'\t2. 一维参数模式。在二维数据模式的基础上，周期参数由只支持标量升级为一维向量。命令参数用于传周期参数，位置参数用于传开高低收等数据，不可混淆。')
+
+    for i, func_name in enumerate(_talib.get_functions()):
+        """talib遍历"""
+        _ta_func = getattr(_talib, func_name)
+        output_names = _abstract.Function(func_name)._Function__info['output_names']
+
+        # 创建函数
+        globals()[func_name] = ta_decorator(_ta_func, mode, output_names)
+
+
+# 默认使用二维矩阵模式
+init(mode=1)
+
+# =============================================
+
+TA_COMPATIBILITY_DEFAULT = 0  # 使用MA做第一个值
+TA_COMPATIBILITY_METASTOCK = 1  # 使用Price做第一个值
+
+_COMPATIBILITY_ENABLE_ = False
+
+
+def set_compatibility_enable(enable):
+    """talib兼容性设置"""
+    global _COMPATIBILITY_ENABLE_
+    _COMPATIBILITY_ENABLE_ = enable
+
+
+def set_compatibility(compatibility):
+    """talib兼容性设置"""
+    global _COMPATIBILITY_ENABLE_
+    if _COMPATIBILITY_ENABLE_:
+        print('do talib.set_compatibility', compatibility)
+        _talib.set_compatibility(compatibility)
