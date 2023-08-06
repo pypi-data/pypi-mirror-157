a8b7318d173c76de5a9d2049450b2ae54cd239b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:26:13 +0000

    Add package jhu/1.0.1/jhu-1.0.1.tar.gz

---
+++ packages/jhu/jhu-1.0.1.tar.gz/jhu-1.0.1/jhu/webdriver/chrome.py
@@ -0,0 +1,10 @@
+"""
+作者:J.Hu
+日期:2022-06-24
+内容:
+- 谷歌Chrome自动化运行脚本
+"""
+
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import WebDriverException
