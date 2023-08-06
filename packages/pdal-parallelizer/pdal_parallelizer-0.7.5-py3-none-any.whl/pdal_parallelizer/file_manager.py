2be69d190f944468b708e942bf8f642135e6bcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:41 +0000

    Add package pdal-parallelizer/0.7.5/pdal_parallelizer-0.7.5-py3-none-any.whl

---
+++ packages/pdal-parallelizer/pdal_parallelizer-0.7.5-py3-none-any.whl/pdal_parallelizer/file_manager.py
@@ -0,0 +1,28 @@
+"""
+In the file_manager, we get all the files that we need for the executions. We can get all the files from the input directory, or get all the serialized pipelines of the temp directory
+"""
+
+import os.path
+import pickle
+from os import listdir
+from os.path import join
+
+
+def getFiles(input_directory, nFiles=None):
+    files = [join(input_directory, f) for f in listdir(input_directory)]
+    if not nFiles:
+        return files
+    else:
+        filesSize = [(join(input_directory, f), os.path.getsize(join(input_directory, f))) for f in files]
+        filesSize.sort(key=lambda tup: tup[1], reverse=True)
+        sortedFiles = [filesSize[i][0] for i in range(len(filesSize))]
+        return sortedFiles[0:nFiles]
+
+
+def getSerializedPipelines(temp_directory):
+    pipelines = []
+    for tmp in listdir(temp_directory):
+        with open(join(temp_directory, tmp), 'rb') as p:
+            pipelines.append(pickle.load(p))
+
+    return pipelines
