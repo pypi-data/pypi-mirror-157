f34a886b17b6210c07230a29ac5c5ba3e576d938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:54 +0000

    Add package vival/3.5.0/vival-3.5.0-py3-none-any.whl

---
+++ packages/vival/vival-3.5.0-py3-none-any.whl/tester/__main__.py
@@ -0,0 +1,165 @@
+from enum import Enum
+from tempfile import TemporaryDirectory
+
+from tester.testmanip import TestsParser, ParseFormat
+from tester.compiler import Compiler
+from tester.lang import Lang, detect_lang
+
+from tqdm import tqdm
+import click
+import os
+import shutil
+
+import pkg_resources
+
+__version__ = pkg_resources.require('vival')[0].version
+
+
+class Mode(Enum):
+    TEST = 'test'
+    FILL = 'fill'
+
+class Encoding(Enum):
+    ASCII = 'ascii'
+    UTF = 'utf-8'
+
+@click.command()
+@click.version_option(__version__, prog_name='VIVAL')
+@click.option('-ue', '--use-encoding',
+              default=Encoding.ASCII.value,
+              type=click.Choice([enc.value for enc in Encoding], case_sensitive=False),
+              help="Text file encoding to use. Select 'utf-8' on Windows.")
+@click.option('-t', '--tests', 'tests_file',
+              default='tests.txt',
+              type=click.File(),
+              help='Path to file with tests.')
+@click.option('-nt', '--ntests',
+              default=5, show_default=True,
+              type=click.INT,
+              help='Number of failed tests to display.')
+@click.option('-o', '--output', 'output_filename',
+              default=None,
+              type=click.Path(writable=True),
+              help='File to store all the extracted (and maybe filled) tests.')
+@click.option('-l', '--lang',
+              default=Lang.CPP.value, show_default=True,
+              type=click.Choice([lang.value for lang in Lang], case_sensitive=False),
+              help='Source language.')
+@click.option('-m', '--mode',
+              default=Mode.TEST.value,
+              type=click.Choice([mode.value for mode in Mode], case_sensitive=False),
+              help='In fill mode will fill in outputs of unfilled tests. '
+                   'In test mode will run executable on given tests.')
+@click.option('--old-format',
+              is_flag=True,
+              help='Flag for backward compatibility.')
+@click.option('--add-quotes',
+              is_flag=True,
+              default=False,
+              help='Add quotes to executable path. Us on Windows if you pass full absolute path to executable.')
+@click.argument("executable_path", type=click.Path(exists=True, resolve_path=True))
+@click.option('-vg', '--valgrind',
+              default=False,
+              is_flag=True,
+              help='Flag for valgrind memory checks.')
+@click.option('-bf', '--break-fail',
+              default=-1, show_default=False,
+              type=click.INT,
+              help='Stop testing when failed specified number of times.')
+def main(executable_path, tests_file, ntests, output_filename, lang, mode, use_encoding, old_format, valgrind, break_fail, add_quotes):
+    with TemporaryDirectory() as tempdir_name:
+        executable_path = os.path.abspath(executable_path)
+
+        if output_filename is not None:
+            output_filename = os.path.abspath(output_filename)
+
+        mode = Mode(mode)
+        parser = TestsParser(ParseFormat.OLD if old_format else ParseFormat.NEW, expect_filled_tests=(mode == Mode.TEST), encoding=use_encoding, exec_quotes=add_quotes)
+        tests = parser.parse(tests_file)
+
+        if parser.get_sanitizers() and valgrind:
+            print('Warning: valgrind is enabled, so sanitizers were deleted from flags')
+            parser.delete_sanitizers()
+
+        if tests is None:
+            print('Parse failed!')
+            print(parser.parse_details['error_message'])
+            return
+
+        if len(parser.parse_details['warning_messages']) > 0:
+            print('Warnings from parser:')
+            for warning in parser.parse_details['warning_messages']:
+                print(warning)
+
+        detected_language = detect_lang(executable_path)
+        if detected_language is None:
+            detected_language = lang
+
+        if detected_language == Lang.CPP or detected_language == Lang.C:
+            compiler = Compiler(lang=detected_language, temp_dir=tempdir_name, flags=parser.get_flags())
+
+            if parser.has_main():
+                executable_path = compiler.compile(executable_path, parser.get_main())
+            else:
+                executable_path = compiler.compile(executable_path)
+
+            if executable_path is None:
+                print('Compilation failed!')
+                print(compiler.compile_details['error_message'])
+                return
+
+        if parser.get_checker():
+            executable_path = ' '.join([os.path.abspath(parser.get_checker()), executable_path])
+
+        if valgrind:
+            valgrind_options = [shutil.which('valgrind'), '-q', '--leak-check=full']
+            executable_path = ' '.join(valgrind_options + [executable_path])
+
+        passed = 0
+        failed = 0
+        suitable = 0
+
+        timeout = parser.get_timeout()
+
+        mode2desc = {Mode.TEST: 'Testing', Mode.FILL: 'Filling'}
+        for test in tqdm(tests, desc=mode2desc[mode], leave=False):
+            run_succeeded = False
+            if (test.filled and mode == Mode.TEST) or (not test.filled and mode == Mode.FILL):
+                run_succeeded = test.run(executable_path, timeout)
+                suitable += 1
+
+            if run_succeeded:
+                passed += 1
+                if mode == Mode.FILL:
+                    test.fill()
+            else:
+                failed += 1
+                if break_fail > 0 and failed >= break_fail:
+                    break
+
+        print('\n' + str(parser) + '\n')
+
+        if passed < suitable:
+            print('Failed on these tests:\n')
+
+            printed = 0
+            for test in tests:
+                if printed >= ntests:
+                    break
+
+                if test.failed:
+                    test.print_last_run()
+                    printed += 1
+
+        if mode == Mode.TEST:
+            print('Passed tests: ' + str(passed) + '/' + str(suitable))
+
+        if mode == Mode.FILL:
+            print('Filled tests: ' + str(passed) + '/' + str(suitable))
+
+        if output_filename is not None:
+            parser.write_tests(tests, output_filename)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/vival/vival-3.5.0-py3-none-any.whl/tester/testmanip.py
@@ -0,0 +1,374 @@
+from subprocess import PIPE
+import subprocess
+
+from typing import Dict, Any, List, Iterable, TextIO
+
+import os
+
+from tester.features import Tag, Feature, construct_test_features, construct_file_features, FeatureContainer
+
+
+class ParseFormat:
+    OLD = 'old'
+    NEW = 'new'
+
+
+def scan(text: str, tags: Iterable[str]) -> Dict[str, List[int]]:
+    """Scans given text for every tag from tags. 
+    Returns map tag->[positions]"""
+    res = {}
+    for tag in tags:
+        res[tag] = []
+
+        tag_pos = text.find(tag)
+        while tag_pos != -1:
+            res[tag].append(tag_pos)
+
+            tag_pos += len(tag)
+            tag_pos = text.find(tag, tag_pos)
+
+    return res
+
+
+def alignable(example: str, target: str) -> bool:
+    return target.startswith(example)
+
+
+def align(possible: str, target: str, added_symbol: str) -> bool:
+    total = len(possible)
+    if total == 0 and len(target) == 0:
+        return True
+
+    for ind, example in enumerate(possible):
+        if total > 1:
+            example += added_symbol
+
+        if alignable(example, target):
+            aligned = align(possible[:ind] + possible[ind + 1:], target[len(example):], added_symbol)
+
+            if aligned:
+                return True
+
+    return False
+
+
+class Test(FeatureContainer):
+    """Single extracted test"""
+
+    ENCODING: str = 'ascii'
+    USE_QUOTES_FRAMED_PATH: bool = False
+
+    def __init__(self, title='Unnamed Test'):
+        super(Test, self).__init__()
+        for feature in construct_test_features():
+            self.add_feature(feature)
+
+        self.title = title
+        self.prog_output = None
+        self.failed = None
+        self.filled = False
+
+    def validate(self) -> bool:
+        """Checks if prog_output is correct"""
+        if 'mSHUFFLED' in self.get_feature(Tag.OUTPUT).mods:
+            possible = self.get_feature(Tag.OUTPUT).merged_contents()
+            return align(possible, self.prog_output, self.get_feature(Tag.OUTPUT).join_symbol)
+        else:
+            return self.get_feature(Tag.OUTPUT).merged_contents() == self.prog_output
+
+    def run(self, exec_path: os.PathLike, timeout: float = float(Feature.default_content(Tag.TIMEOUT))) -> bool:
+        """Runs executable on this test. Returns True if run succeeded"""
+        exec_path = os.path.abspath(exec_path)
+
+        cmd = self.get_feature(Tag.CMD).merged_contents()
+        stdin = self.get_feature(Tag.INPUT).merged_contents()
+        startup = self.get_feature(Tag.STARTUP).merged_contents()
+        cleanup = self.get_feature(Tag.CLEANUP).merged_contents()
+
+        all_args: str = None
+        if (Test.USE_QUOTES_FRAMED_PATH):
+            all_args = '\"' + str(exec_path) + '\" ' + cmd  # Useful for Windows users.
+        else:
+            all_args = str(exec_path) + ' ' + cmd
+
+        if startup is not None:
+            for args in startup.split('\n'):
+                if args != '':
+                    code = subprocess.call(args, shell=True)  # TODO test this
+                    if code != 0:
+                        self.prog_output = 'The program was not executed due to errors during environment preparation stage. ' \
+                                           'Failed to execute: ' + args
+                        self.failed = True
+                        return not self.failed
+        try:
+            prog_output = subprocess.run(all_args, stderr=subprocess.STDOUT, stdout=PIPE, input=stdin,
+                                         timeout=timeout, encoding=Test.ENCODING, shell=True).stdout
+
+        except subprocess.TimeoutExpired:
+            prog_output = 'Time limit exceeded'
+            self.failed = True
+
+        self.prog_output = prog_output
+
+        if cleanup is not None:
+            for args in cleanup.split('\n'):
+                if args != '':
+                    code = subprocess.call(args, shell=True)  # TODO test this
+                    if code != 0:
+                        self.prog_output = 'Cleanup stage failed. Failed to execute: ' + args
+                        self.failed = True
+                        return not self.failed
+
+        if self.filled:
+            self.failed = not self.validate()
+            return not self.failed
+        else:
+            self.failed = False
+            return not self.failed
+
+    def fill(self) -> None:
+        """Fills in test using last run"""
+        self.filled = True
+        self.replace_feature(Feature(Tag.OUTPUT, [self.prog_output]))
+
+    def print_last_run(self) -> None:
+        """Prints details on last run"""
+        print('-' * 30)
+
+        print(self.title + '\n')
+
+        for feature in sorted(self._tag2feature.values()):
+            if feature.info() is not None and not feature.is_empty():
+                if feature.info() != '':
+                    print(feature.info() + ':')
+                print(feature.merged_contents() + '\n')
+
+        print('PROGRAM OUTPUT:')
+        print(self.prog_output + '\n')
+
+        print('-' * 30)
+
+    def __str__(self):
+        str_repr = ''
+
+        if self.filled:
+            str_repr += self.title + '\n\n'
+        else:
+            str_repr += self.title + ' (unfilled)\n\n'
+
+        for feature in self._tag2feature.values():
+            if not feature.is_empty():
+                str_repr += str(feature)
+
+        return str_repr
+
+    def add_feature(self, feature):
+        super(Test, self).add_feature(feature)
+
+        if feature.tag == Tag.OUTPUT:
+            self.filled = True
+
+
+class TestsParser(FeatureContainer):
+    """Parses text file with tests"""
+
+    def __init__(self, parse_format: ParseFormat = ParseFormat.NEW, expect_filled_tests: bool = True, encoding: str = None, exec_quotes: bool = None):
+        if encoding is not None:
+            Test.ENCODING = encoding
+        if exec_quotes is not None:
+            Test.USE_QUOTES_FRAMED_PATH = exec_quotes
+        super(TestsParser, self).__init__()
+        for feature in construct_file_features():
+            self.add_feature(feature)
+
+        self.format = parse_format
+        self.expect_filled_tests = expect_filled_tests
+        self.parse_details: Dict[str, Any] = {
+            'ntests': 0,
+            'format': None,
+            'error_message': None,
+            'warning_messages': [],
+        }
+
+    def __str__(self):
+        desc = self.get_feature(Tag.DESCRIPTION).merged_contents()
+        if desc != '':
+            return desc
+        else:
+            return 'No description was provided'
+
+    def get_flags(self) -> str:
+        return self.get_feature(Tag.FLAGS).merged_contents()
+
+    def get_checker(self) -> str:
+        return self.get_feature(Tag.CHECKER).merged_contents()
+
+    def get_sanitizers(self) -> List[str]:
+        sanitizers = []
+        for f in self.get_feature(Tag.FLAGS).merged_contents().split(' '):
+            if '-fsanitize' in f:
+                sanitizers += f.split('=')[1].split(',')
+        return sanitizers 
+
+    def delete_sanitizers(self) -> None:
+        flags = self.get_feature(Tag.FLAGS).merged_contents().split(' ')
+        new_flags = [f for f in flags if '-fsanitize' not in f]
+        self.replace_feature(Feature(Tag.FLAGS, new_flags))
+
+    def has_main(self) -> bool:
+        return not self.get_feature(Tag.MAIN).is_empty()
+
+    def get_main(self) -> str:
+        return self.get_feature(Tag.MAIN).merged_contents()
+
+    def get_timeout(self) -> float:
+        return float(self.get_feature(Tag.TIMEOUT).merged_contents())
+
+    def parse(self, tests_file: TextIO):
+        """Parses tests_file and returns list of Test objects. Returns None in case of an error"""
+        self.parse_details['ntests'] = 0
+        tests = []
+
+        text = tests_file.read()
+
+        brackets = scan(text, ['/{', '}/'])
+
+        if len(brackets['/{']) == 0 or self.format == ParseFormat.OLD:
+            if self.format == ParseFormat.NEW:
+                self.parse_details['warning_messages'].append('Old format detected!\n')
+            return self.old_parse(text)
+
+        if len(brackets['/{']) != len(brackets['}/']):
+            self.parse_details['error_message'] = 'Wrong format! Unmatched number of /{ and }/ brackets.\n'
+            return None
+
+        curr_test = Test('Test ' + str(self.parse_details['ntests'] + 1))
+        filled_fields = set()
+        prev_tag = None
+
+        section_start = 0
+        for lbracket_ind, rbracket_ind in zip(brackets['/{'], brackets['}/']):
+            contents = text[lbracket_ind + len('/{'): rbracket_ind]
+
+            wild_space = text[section_start: lbracket_ind]
+
+            # search for tag in wild space
+            search_results = scan(wild_space, map(lambda t: t.value, Feature.tag_configs.keys()))
+
+            best_tag = None
+            max_pos = -1
+            for tag in search_results:
+                if len(search_results[tag]) != 0:
+                    curr_max = max(search_results[tag])
+                    if curr_max > max_pos:
+                        best_tag = tag
+
+                    max_pos = max(curr_max, max_pos)
+
+            # search for modifiers in wild space
+            search_results = scan(wild_space, Feature.all_mods)
+            mods = []
+            for mod in search_results:
+                if len(search_results[mod]) != 0:
+                    mods.append(mod)
+
+            if best_tag is None:
+                # no tag was found in wild space
+                feature = Feature(Tag(prev_tag), [contents])
+                feature.apply_mod(*mods)
+
+                if feature.is_file_type():
+                    self.add_feature(feature)
+
+                if feature.is_test_type():
+                    curr_test.add_feature(feature)
+            else:
+                feature = Feature(Tag(best_tag), [contents])
+                feature.apply_mod(*mods)
+
+                if best_tag in filled_fields:
+                    # new test has started
+                    tests.append(curr_test)
+
+                    self.parse_details['ntests'] += 1
+                    curr_test = Test('Test ' + str(self.parse_details['ntests'] + 1))
+                    filled_fields = {best_tag}
+
+                    curr_test.add_feature(feature)
+                else:
+                    # continue filling in current test
+                    if feature.is_file_type():
+                        self.add_feature(feature)
+
+                    if feature.is_test_type():
+                        filled_fields.add(best_tag)
+                        curr_test.add_feature(feature)
+
+                prev_tag = best_tag
+
+            section_start = rbracket_ind + len('}/')
+
+        tests.append(curr_test)
+        self.parse_details['ntests'] += 1
+
+        return tests
+
+    def old_parse(self, text: str):
+        tests = []
+        if not self.expect_filled_tests:
+            contents = list(text.split('[INPUT]\n'))[1:]
+            for content in contents:
+                curr_test = Test('Test ' + str(self.parse_details['ntests'] + 1))
+
+                if '{CMD}\n' in content:
+                    cmd, inp = content.split('{CMD}\n')
+
+                    curr_test.add_feature(Feature(Tag.CMD, [cmd]))
+                else:
+                    inp = content
+
+                curr_test.add_feature(Feature(Tag.INPUT, [inp]))
+
+                tests.append(curr_test)
+                self.parse_details['ntests'] += 1
+        else:
+            contents = list(text.split('[INPUT]\n'))[1:]
+            for content in contents:
+                curr_test = Test("Test " + str(self.parse_details['ntests'] + 1))
+
+                content, outp = content.split('[OUTPUT]\n')
+
+                if '{CMD}\n' in content:
+                    cmd, inp = content.split('{CMD}\n')
+
+                    curr_test.add_feature(Feature(Tag.CMD, [cmd]))
+                else:
+                    inp = content
+
+                curr_test.add_feature(Feature(Tag.INPUT, [inp]))
+                curr_test.add_feature(Feature(Tag.OUTPUT, [outp]))
+
+                tests.append(curr_test)
+                self.parse_details['ntests'] += 1
+
+        return tests
+
+    def write_tests(self, tests, output_filename):
+        """Writes contents of tests and parser to output_filename"""
+        with open(output_filename, 'w') as tests_file:
+            line_len = 70
+
+            tests_file.write('Contents of this file were automatically generated by VIVAL tool.\n\n')
+            tests_file.write('Install with pip: pip install vival\n')
+            tests_file.write('Visit GitHub for more info: https://github.com/ViktorooReps/vival\n\n')
+
+            for feature in self._tag2feature.values():
+                if not feature.is_empty():
+                    tests_file.write(str(feature))
+
+            dashes = (line_len - len('Tests')) // 2
+            tests_file.write(dashes * '-' + 'Tests' + dashes * '-' + '\n\n')
+
+            for test in tests:
+                tests_file.write(str(test))
+                tests_file.write("\n\n")
