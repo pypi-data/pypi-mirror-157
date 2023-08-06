516a48306c853514df6177d7475d8a85cd3304ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:57 +0000

    Add package dtuprosys/0.1.9/dtuprosys-0.1.9-py3-none-any.whl

---
+++ packages/dtuprosys/dtuprosys-0.1.9-py3-none-any.whl/dtuprosys/datasets/_base.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def load_train_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Loads the train data.
+    @return train_spectra: spectra in the training data.
+    @return train_hplc: hplc in the training data.
+    """
+    train_spectra = pd.read_csv(r"./dtuprosys/datasets/data/train_spectra.csv")
+    train_spectra.columns = train_spectra.columns.astype(float)
+    train_hplc = pd.read_csv(r"./dtuprosys/datasets/data/train_hplc.csv")
+
+    return train_spectra, train_hplc
+
+
+def load_fermentation_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Loads the fermentation data.
+    @return fermentation_spectra: spectra measured during the fermentation.
+    @return fermentation_hplc: hplc measured during the fermentation.
+    """
+    fermentation_spectra = pd.read_csv(
+        r"./dtuprosys/datasets/data/fermentation_spectra.csv"
+    )
+    fermentation_spectra.columns = fermentation_spectra.columns.astype(float)
+    fermentation_hplc = pd.read_csv(r"./dtuprosys/datasets/data/fermentation_hplc.csv")
+
+    return fermentation_spectra, fermentation_hplc
