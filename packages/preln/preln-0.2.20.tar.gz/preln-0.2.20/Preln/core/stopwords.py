8d4b17c787da984e94000d8e32b8ece11e1c6307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:38 +0000

    Add package preln/0.2.20/preln-0.2.20.tar.gz

---
+++ packages/preln/preln-0.2.20.tar.gz/preln-0.2.20/Preln/core/stopwords.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import logging
+
+_EXTERNAL_PATH_STP = 'Preln/_external/stopwords.csv'
+stopwords = pd.read_csv(_EXTERNAL_PATH_STP)['stopwords'].values.tolist()
+
+def stopwords_es(text, debug):
+    """
+    Method for eliminate text punctuation.
+    
+    :param text: The text to be preprocessed
+    :type: str
+    
+    :param debug: Apply debugging
+    :type: bool
+    """
+    
+    if debug: logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)8s | %(message)s')
+    
+    text = text.split()    
+    text = " ".join(word for word in text if word not in stopwords)
+    logging.debug('-- Stopwords cleanned!')
+    
+    return text
+
+
+if __name__ == '__main__':
+  stopwords_es()
