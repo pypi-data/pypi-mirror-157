02b96a807fec4596dec8993fdbc7b0ee7a372efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package rch/0.13/rch-0.13-py3-none-any.whl

---
+++ packages/rch/rch-0.13-py3-none-any.whl/rch/__init__.py
@@ -0,0 +1,11 @@
+import rch.arrays
+import rch.df
+import rch.gdf
+import rch.hydro
+import rch.sudoku
+import rch.web
+
+__all__ = ['arrays', 'df', 'gdf', 'hydro', 'sudoku', 'web']
+__version__ = '0.13'
+__author__ = 'Riley Hales'
+__license__ = 'BSD Clear'
