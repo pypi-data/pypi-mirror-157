4a02e53b9f57ea4c56a2aad40e15bd6881c0cdfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:25:32 +0000

    Add package graia-ariadne/0.6.17/graia_ariadne-0.6.17-py3-none-any.whl

---
+++ packages/graia-ariadne/graia_ariadne-0.6.17-py3-none-any.whl/graia/ariadne/dispatcher.py
@@ -0,0 +1,83 @@
+"""Ariadne 内置的 Dispatcher"""
+
+
+import contextlib
+
+from graia.broadcast.entities.dispatcher import BaseDispatcher as AbstractDispatcher
+from graia.broadcast.interfaces.dispatcher import DispatcherInterface
+
+from .message.chain import MessageChain
+from .message.element import Source
+from .typing import generic_isinstance, generic_issubclass
+
+
+class MessageChainDispatcher(AbstractDispatcher):
+    """从 MessageEvent 提取 MessageChain 的 Dispatcher"""
+
+    @staticmethod
+    async def catch(interface: DispatcherInterface):
+        from .event.message import ActiveMessage, MessageEvent
+
+        if isinstance(interface.event, (MessageEvent, ActiveMessage)):
+            if generic_issubclass(MessageChain, interface.annotation):
+                return interface.event.messageChain
+
+
+class ContextDispatcher(AbstractDispatcher):
+    """提取上下文的 Dispatcher"""
+
+    @staticmethod
+    async def catch(interface: DispatcherInterface):
+        from . import get_running
+
+        if generic_isinstance(interface.event, interface.annotation):
+            return interface.event
+
+        return get_running(interface.annotation, fail_err=False)
+
+
+class SourceDispatcher(AbstractDispatcher):
+    """提取 MessageEvent 消息链 Source 元素的 Dispatcher"""
+
+    @staticmethod
+    async def catch(interface: DispatcherInterface):
+        from .event.message import ActiveMessage, MessageEvent
+
+        if isinstance(interface.event, (MessageEvent, ActiveMessage)):
+            if generic_issubclass(Source, interface.annotation):
+                return interface.event.messageChain.getFirst(Source)
+
+
+class SenderDispatcher(AbstractDispatcher):
+    """
+    从 MessageEvent 提取 sender 的 Dispatcher.
+    """
+
+    @staticmethod
+    async def catch(interface: DispatcherInterface):
+        from .event.message import MessageEvent
+
+        if isinstance(interface.event, MessageEvent):
+            with contextlib.suppress(TypeError):
+                if generic_isinstance(interface.event.sender, interface.annotation):
+                    return interface.event.sender
+
+
+class SubjectDispatcher(AbstractDispatcher):
+    """从 ActiveMessage 提取 subject 的 Dispatcher"""
+
+    @staticmethod
+    async def catch(interface: DispatcherInterface):
+        from .event.message import ActiveMessage
+
+        if isinstance(interface.event, ActiveMessage):
+            if generic_issubclass(interface.annotation, interface.event.subject):
+                return interface.event.subject
+
+
+class BaseDispatcher(AbstractDispatcher):
+    """空 Dispatcher"""
+
+    @staticmethod
+    async def catch(*_):
+        pass
