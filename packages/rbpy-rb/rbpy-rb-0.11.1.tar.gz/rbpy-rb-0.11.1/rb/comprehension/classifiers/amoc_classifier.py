0a35209dc2329743e835f4cf567ed01397ad1f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:16 +0000

    Add package rbpy-rb/0.11.1/rbpy-rb-0.11.1.tar.gz

---
+++ packages/rbpy-rb/rbpy-rb-0.11.1.tar.gz/rbpy-rb-0.11.1/rb/comprehension/classifiers/amoc_classifier.py
@@ -0,0 +1,24 @@
+from sklearn.model_selection import train_test_split
+from rb.comprehension.classifiers.loader import load_file
+from sklearn.ensemble import RandomForestClassifier
+from statsmodels.stats.anova import AnovaRM
+
+
+def random_forest_classification(filepath):
+    X, y = load_file(filepath)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=True)
+    clf = RandomForestClassifier(n_estimators=10, n_jobs=2, random_state=0, verbose=2)
+    clf.fit(X_train, y_train)
+    acc = clf.score(X_test, y_test)
+    print(acc)
+    print(clf.feature_importances_)
+    return acc
+
+
+def anova_analysis(filepath):
+    df = load_file_for_anova(filepath)
+    columns = list(df.columns.values)
+    columns.remove('texts_ids')
+    columns.remove('clasa')
+    for c in columns:
+        print(f"{c}: {AnovaRM(data=df, depvar=c, subject='texts_ids', within=['clasa']).fit()}")
---
+++ packages/rbpy-rb/rbpy-rb-0.11.1.tar.gz/rbpy-rb-0.11.1/rb/comprehension/classifiers/loader.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def load_file(filename):
+    df = pd.read_csv(filename, header=0)
+
+    df = df.drop("name", axis=1)
+    df = df.drop("class", axis=1)
+    columns = df.columns
+    X = []
+    y = []
+    for index in range(df.shape[0]):
+        features = []
+        if index % 2 == 0:
+            this_row = df.iloc[index]
+            next_row = df.iloc[index + 1]
+            build_features(columns, features, next_row, this_row)
+            y.append(0)
+        else:
+            this_row = df.iloc[index]
+            prev_row = df.iloc[index - 1]
+            build_features(columns, features, prev_row, this_row)
+            y.append(1)
+        X.append(features)
+    return X, y
+
+
+def load_file_for_anova(filename):
+    df = pd.read_csv(filename, header=0)
+    df = df.drop("name", axis=1)
+    number_of_rows = len(df.index)
+    texts_ids = []
+    for i in range(int(number_of_rows / 2)):
+        texts_ids.append(i)
+        texts_ids.append(i)
+    print(texts_ids)
+    df['texts_ids'] = texts_ids
+    df = df.rename(columns={"class":"clasa"})
+    return df
+
+
+def build_features(columns, features, other_row, this_row):
+    for column in columns:
+        features.append(float(this_row[column]) / float(other_row[column]) * 100)
