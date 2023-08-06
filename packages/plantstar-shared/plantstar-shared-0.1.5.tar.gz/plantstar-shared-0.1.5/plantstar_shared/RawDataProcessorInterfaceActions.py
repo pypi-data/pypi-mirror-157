1afdf5f8135911c44e8eca26892c69cd8ae840b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:06 +0000

    Add package plantstar-shared/0.1.5/plantstar-shared-0.1.5.tar.gz

---
+++ packages/plantstar-shared/plantstar-shared-0.1.5.tar.gz/plantstar-shared-0.1.5/plantstar_shared/RawDataProcessorInterfaceActions.py
@@ -0,0 +1,8 @@
+class RawDataProcessorInterfaceActions:
+    GET_DRIVER_VERSION_STRING = "get_driver_version_string"
+    GET_INPUT_SIGNAL_DICTIONARIES = "get_input_signal_dictionaries"
+    GET_MOCK_RAW_DATA_PROCESSOR_STATUS = "get_mock_raw_data_processor_status"
+    GET_VERSION_STRING = "get_version_string"
+    UPDATE_ANALOG_INPUTS = "update_analog_inputs"
+    UPDATE_DIGITAL_INPUT_DEBOUNCE = "update_digital_input_debounce"
+    UPLOAD_NEW_DRIVER_SETTINGS = "upload_new_driver_settings"
