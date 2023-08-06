5df999e9372998967eaf76ffd43c60fc3925571c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:49 +0000

    Add package preln/0.2.3/preln-0.2.3.tar.gz

---
+++ packages/preln/preln-0.2.3.tar.gz/preln-0.2.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    long_description_content_type='text/markdown',
+    name='preln',
+    packages=find_packages(include=['Preln', 'Preln.core']),
+    version='0.2.3',
+    description='A preprocessing libray for text in spanish',
+    author='Adrián H.S & Raúl M.R',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    include_package_data=True
+)
