5282144cf92d29e592ba04b052cd8c4db77e3645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:51 +0000

    Add package meta-edc/0.1.85/meta-edc-0.1.85.tar.gz

---
+++ packages/meta-edc/meta-edc-0.1.85.tar.gz/meta-edc-0.1.85/meta_subject/admin/medication_adherence_admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from edc_adherence.model_admin_mixin import MedicationAdherenceAdminMixin
+
+from ..admin_site import meta_subject_admin
+from ..forms import MedicationAdherenceForm
+from ..models import MedicationAdherence
+from .modeladmin import CrfModelAdmin
+
+
+@admin.register(MedicationAdherence, site=meta_subject_admin)
+class MedicationAdherenceAdmin(MedicationAdherenceAdminMixin, CrfModelAdmin):
+
+    form = MedicationAdherenceForm
+
+    radio_fields = {
+        "pill_count_performed": admin.VERTICAL,
+        "last_missed_pill": admin.VERTICAL,
+    }
