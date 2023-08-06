513c3ea403f2def9e507f39c100a27f796255b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:32 +0000

    Add package egycli/0.0.1/egycli-0.0.1.tar.gz

---
+++ packages/egycli/egycli-0.0.1.tar.gz/egycli-0.0.1/README.md
@@ -0,0 +1,13 @@
+<img alt="tool pic" src="./egycli.png">
+# egy-cli
+## a tool to download movies and tv shows from egybest without ads
+
+
+# installation
+
+`pip3 install egy-cli`
+
+# usage
+just run `egybest` in the terminal lmao
+
+### note that this tool does not work with windows but you can use it with wsl
---
+++ packages/egycli/egycli-0.0.1.tar.gz/egycli-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import io, re
+
+with io.open('__version__.py', 'rt', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+DESCRIPTION = 'Downloading movies and tv shows thorugh egybest'
+LONG_DESCRIPTION = 'a cli tool that allows you to download movies and tv shows through egybest'
+setup (
+    name="egycli",
+    version=version,
+    author="Cytolytic (Aktham Mostafa)",
+    author_email="cytolyticdev@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    keywords=["egybest", "python", "movies", "download"],
+    packages=find_packages(),
+    install_requires=[
+        "rich",
+        "requests",
+        "beautifulsoup4",
+        "Js2Py",
+        "simple-term-menu"
+    ],
+    entry_points="""
+    [console_scripts]
+    egybest=egycli:main
+    """
+)
