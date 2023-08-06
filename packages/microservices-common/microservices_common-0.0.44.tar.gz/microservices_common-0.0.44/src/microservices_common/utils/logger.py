e3ab02de6b692a6f11c224c69c0156a2732f5fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:25:39 +0000

    Add package microservices-common/0.0.44/microservices_common-0.0.44.tar.gz

---
+++ packages/microservices-common/microservices_common-0.0.44.tar.gz/microservices_common-0.0.44/src/microservices_common/utils/logger.py
@@ -0,0 +1,39 @@
+import os
+import logging
+
+
+class Logger:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def format_message(cls, message, logging_info=None):
+        formatted_message = ""
+        if logging_info:
+            formatted_message += logging_info + " "
+        formatted_message += message
+        return formatted_message
+
+    @classmethod
+    def info(cls, message, logging_info=None):
+        message_to_print = cls.format_message(message, logging_info)
+        if os.getenv('IS_DEV'):
+            print(message_to_print)
+        else:
+            logging.info(message_to_print)
+
+    @classmethod
+    def error(cls, message, logging_info=None):
+        message_to_print = cls.format_message(message, logging_info)
+        if os.getenv('IS_DEV'):
+            print(message_to_print)
+        else:
+            logging.error(message_to_print)
+
+    @classmethod
+    def warning(cls, message, logging_info=None):
+        message_to_print = cls.format_message(message, logging_info)
+        if os.getenv('IS_DEV'):
+            print(message_to_print)
+        else:
+            logging.warning(message_to_print)
