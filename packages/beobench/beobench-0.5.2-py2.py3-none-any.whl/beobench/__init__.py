4bb9591db92f66eccd17e9640bddbcb27729780e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:57 +0000

    Add package beobench/0.5.2/beobench-0.5.2-py2.py3-none-any.whl

---
+++ packages/beobench/beobench-0.5.2-py2.py3-none-any.whl/beobench/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for beobench."""
+
+__author__ = """Beobench authors"""
+__email__ = "-"
+__version__ = "0.5.2"
+
+from beobench.utils import restart
+from beobench.experiment.scheduler import run
