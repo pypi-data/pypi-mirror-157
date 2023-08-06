e0fc1b4dd471ff786a5baabff3345c8234445909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:12 +0000

    Add package koda/1.0.0/koda-1.0.0-py3-none-any.whl

---
+++ packages/koda/koda-1.0.0-py3-none-any.whl/koda/__init__.py
@@ -0,0 +1,50 @@
+from koda.either import (
+    Either,
+    Either3,
+    Either4,
+    Either5,
+    Fifth,
+    First,
+    Fourth,
+    Second,
+    Third,
+)
+from koda.maybe import Just, Maybe, Nothing, nothing
+from koda.result import Err, Ok, Result
+from koda.utils import (
+    always,
+    compose,
+    identity,
+    load_once,
+    mapping_get,
+    safe_try,
+    to_maybe,
+    to_result,
+)
+
+__all__ = (
+    "always",
+    "Either",
+    "Either3",
+    "Either4",
+    "Either5",
+    "First",
+    "Second",
+    "Third",
+    "Fourth",
+    "Fifth",
+    "Maybe",
+    "Just",
+    "Nothing",
+    "nothing",
+    "Result",
+    "Ok",
+    "Err",
+    "compose",
+    "identity",
+    "mapping_get",
+    "load_once",
+    "safe_try",
+    "to_maybe",
+    "to_result",
+)
