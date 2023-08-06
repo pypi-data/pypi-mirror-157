f1979b51da7141f5a4da452ff4fe72f0288b1c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:20 +0000

    Add package ftr-classifier2/2.2/ftr_classifier2-2.2.tar.gz

---
+++ packages/ftr-classifier2/ftr_classifier2-2.2.tar.gz/ftr_classifier2-2.2/ftr_classifier/models.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 13:57:28 2019
+
+@author: cole
+"""
+#load spacy
+import spacy
+
+#import models
+#english
+try:    
+    en = spacy.load('en_core_web_sm')
+except OSError:
+    print('Downloading English language model for the spaCy\n'
+          "(don't worry, this will only happen once)")
+    spacy.cli.download('en')
+    en = spacy.load('en_core_web_sm')
+    
+#dutch
+try:    
+    nl = spacy.load('nl_core_news_sm')
+except OSError:
+    print('Downloading Dutch language model for the spaCy\n'
+          "(don't worry, this will only happen once)")
+    spacy.cli.download('nl')
+    nl = spacy.load('nl_core_news_sm')
+    
+try:    
+    de = spacy.load('de_core_news_sm')
+except OSError:
+    print('Downloading German language model for the spaCy\n'
+          "(don't worry, this will only happen once)")
+    spacy.cli.download('de')
+    de = spacy.load('de_core_news_sm')
+
+#make MODELS dictionary
+MODELS = {'english':en,
+          'dutch':nl,
+          'german':de}
