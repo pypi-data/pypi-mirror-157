bccd21ca09e8dc1876813133b6e779475c2b401e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:17 +0000

    Add package classicML/0.9/classicML-0.9-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/classicML/classicML-0.9-cp37-cp37m-macosx_10_15_x86_64.whl/classicML/api/models/ensemble/boosting.py
@@ -0,0 +1,194 @@
+from pickle import dumps, loads
+
+import numpy as np
+
+from classicML import _cml_precision
+from classicML import CLASSICML_LOGGER
+from classicML.api.models import BaseModel
+from classicML.api.models import TwoLevelDecisionTreeClassifier
+from classicML.backend import io
+
+EPSILON = 1e-36  # 常小数.
+
+
+class AdaBoostClassifier(BaseModel):
+    """AdaBoost分类器.
+
+    Attributes:
+        estimators: list of `BaseLearner`实例,
+            AdaBoost集成的基学习器列表.
+        alpha_list: list of float,
+            AdaBoost集成的基学习器对应的权重.
+        BaseLearner: `BaseLearner`对象, default=None
+            AdaBoost使用的基学习器.
+        is_trained: bool, default=False,
+            模型训练后将被标记为True.
+        is_loaded: bool, default=False,
+            如果模型加载了权重将被标记为True.
+    """
+    def __init__(self):
+        """初始化AdaBoost分类器.
+        """
+        super(AdaBoostClassifier, self).__init__()
+
+        self.estimators = []
+        self.alpha_list = []
+        self.BaseLearner = None
+
+        self.is_trained = False
+        self.is_loaded = False
+
+    def compile(self, base_algorithm=TwoLevelDecisionTreeClassifier):
+        """编译AdaBoost分类器.
+
+        Args:
+            base_algorithm: `BaseLearner`对象, default=TwoLevelDecisionTreeClassifier,
+                AdaBoost使用的基学习器算法;
+                目前只实现了`TwoLevelDecisionTreeClassifier`, 未来将接入更多算法.
+        """
+        self.BaseLearner = base_algorithm
+
+    def fit(self, x, y, max_estimators=50):
+        """训练AdaBoost分类器.
+
+        Args:
+            x: numpy.ndarray or array-like, 特征数据.
+            y: numpy.ndarray or array-like, 标签.
+            max_estimators: int, default=50,
+                基学习器集成的最大数量(训练的最大轮数).
+
+        Return:
+            AdaBoostClassifier实例.
+        """
+        x = np.asarray(x, dtype=_cml_precision.float)
+        y = np.asarray(y, dtype=_cml_precision.int)
+
+        # 样本分布初始化.
+        sample_distribution = np.ones(len(x), dtype=_cml_precision.float) / len(x)
+
+        for _ in range(max_estimators):
+            # 基于样本分布训练基学习器.
+            base_learner = self.BaseLearner().fit(x, y, sample_distribution=sample_distribution)
+            y_pred = base_learner.predict(x)
+            # 计算当前基学习器的误差(损失).
+            error = np.sum(sample_distribution[y_pred != y])
+            # 计算基学习器的权重, 保存权重和基学习器.
+            alpha = np.log((1 - error) / np.maximum(error, EPSILON)) / 2
+            alpha = alpha.astype(_cml_precision.float)  # 使用常小数避免除零.
+            self.alpha_list.append(alpha)
+            self.estimators.append(base_learner)
+            # 更新样本分布.
+            error_index = np.ones(len(x))
+            error_index[y_pred == y] = -1
+            sample_distribution *= np.exp(error_index * alpha)
+            # 进行规范化(和为1).
+            sample_distribution /= np.sum(sample_distribution)
+            # 退出条件, 与原始伪码略有不同.
+            # 1. 退出条件增加误差过小, 误差过小时不必集成更多学习器, 降低过拟合和运算时间.
+            # 2. 退出条件放在最后, 避免出现极端情况第一个基学习器误差大于0.5, AdaBoost为空.
+            if error > 0.5 or error < EPSILON:
+                break
+
+        self.alpha_list = np.asarray(self.alpha_list)
+
+        # 标记训练完成.
+        self.is_trained = True
+
+        return self
+
+    def predict(self, x, **kwargs):
+        """使用AdaBoost分类器进行预测.
+
+        Args:
+            x: numpy.ndarray or array-like,
+                特征数据.
+
+        Return:
+            AdaBoostClassifier预测的结果.
+
+        Raise:
+            ValueError: 模型没有训练的错误.
+        """
+        if self.is_trained is False and self.is_loaded is False:
+            CLASSICML_LOGGER.error('模型没有训练')
+            raise ValueError('你必须先进行训练')
+
+        y_pred = np.zeros(shape=x.shape[0], dtype=_cml_precision.float)
+
+        for i, (estimator, alpha) in enumerate(zip(self.estimators, self.alpha_list)):
+            y_pred += alpha * estimator.predict(x)
+
+        y_pred /= np.sum(self.alpha_list)  # 进行规范化.
+        y_pred[y_pred >= 0] = 1
+        y_pred[y_pred < 0] = -1
+
+        return y_pred.astype(_cml_precision.int)
+
+    def score(self, x, y):
+        """在预测模式下计算准确率.
+
+        Arguments:
+            x: array-like, 特征数据.
+            y: array-like, 标签.
+
+        Returns:
+            当前的准确率.
+        """
+        return super(AdaBoostClassifier, self).score(x, y)
+
+    def load_weights(self, filepath):
+        """加载模型参数.
+
+        Arguments:
+            filepath: str, 权重文件加载的路径.
+
+        Raises:
+            KeyError: 模型权重加载失败.
+        """
+        # 初始化权重文件.
+        parameters_gp = io.initialize_weights_file(filepath=filepath,
+                                                   mode='r',
+                                                   model_name='AdaBoostClassifier')
+        # 加载模型参数.
+        try:
+            compile_ds = parameters_gp['compile']
+            weights_ds = parameters_gp['weights']
+
+            self.BaseLearner = loads(compile_ds.attrs['base_algorithm'].tobytes())
+
+            self.estimators = loads(weights_ds.attrs['estimators'].tobytes())
+            self.alpha_list = weights_ds.attrs['alpha_list']
+            # 标记加载完成.
+            self.is_loaded = True
+        except KeyError:
+            CLASSICML_LOGGER.error('模型权重加载失败, 请检查文件是否损坏')
+            raise KeyError('模型权重加载失败')
+
+    def save_weights(self, filepath):
+        """将模型权重保存为一个HDF5文件.
+
+        Arguments:
+            filepath: str, 权重文件保存的路径.
+
+        Raises:
+            TypeError: 模型权重保存失败.
+
+        References:
+            - [如何存储原始的二进制数据](https://docs.h5py.org/en/2.3/strings.html)
+        """
+        # 初始化权重文件.
+        parameters_gp = io.initialize_weights_file(filepath=filepath,
+                                                   mode='w',
+                                                   model_name='AdaBoostClassifier')
+        # 保存模型参数.
+        try:
+            compile_ds = parameters_gp['compile']
+            weights_ds = parameters_gp['weights']
+
+            compile_ds.attrs['base_algorithm'] = np.void(dumps(self.BaseLearner))
+
+            weights_ds.attrs['estimators'] = np.void(dumps(self.estimators))
+            weights_ds.attrs['alpha_list'] = self.alpha_list
+        except TypeError:
+            CLASSICML_LOGGER.error('模型权重保存失败, 请检查文件是否损坏')
+            raise TypeError('模型权重保存失败')
---
+++ packages/classicML/classicML-0.9-cp37-cp37m-macosx_10_15_x86_64.whl/classicML/backend/__init__.py
@@ -0,0 +1,112 @@
+import os
+
+from classicML import CLASSICML_LOGGER
+
+# python和cc多后端模块
+if os.environ['CLASSICML_ENGINE'] == 'CC':
+    from classicML.backend.cc import activations
+    from classicML.backend.cc import callbacks
+    from classicML.backend.python import data
+    from classicML.backend.cc import initializers
+    from classicML.backend.cc import kernels
+    from classicML.backend.cc import losses
+    from classicML.backend.cc import metrics
+    from classicML.backend.python import optimizers
+    from classicML.backend.python import tree
+
+    from classicML.backend.cc.activations import __version__ as activations__version__
+    from classicML.backend.cc.callbacks import __version__ as callbacks__version__
+    from classicML.backend.cc.initializers import __version__ as initializers__version__
+    from classicML.backend.cc.kernels import __version__ as kernels__version__
+    from classicML.backend.cc.losses import __version__ as losses__version__
+    from classicML.backend.cc.metrics import __version__ as metrics__version__
+
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(activations__version__))
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(callbacks__version__))
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(initializers__version__))
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(kernels__version__))
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(losses__version__))
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(metrics__version__))
+else:
+    from classicML.backend.python import activations
+    from classicML.backend.python import callbacks
+    from classicML.backend.python import data
+    from classicML.backend.python import initializers
+    from classicML.backend.python import kernels
+    from classicML.backend.python import losses
+    from classicML.backend.python import metrics
+    from classicML.backend.python import optimizers
+    from classicML.backend.python import tree
+
+# ops模块
+if os.environ['CLASSICML_ENGINE'] == 'CC':
+    from classicML.backend.cc.ops import cc_bootstrap_sampling
+    # from classicML.backend.cc.ops import ConvexHull
+    from classicML.backend.cc.ops import cc_calculate_centroids
+    from classicML.backend.cc.ops import cc_calculate_error
+    from classicML.backend.cc.ops import cc_calculate_euclidean_distance
+    from classicML.backend.cc.ops import cc_clip_alpha
+    from classicML.backend.cc.ops import cc_compare_differences
+    from classicML.backend.cc.ops import cc_get_cluster
+    from classicML.backend.cc.ops import cc_get_conditional_probability
+    from classicML.backend.cc.ops import cc_get_dependent_prior_probability
+    from classicML.backend.cc.ops import cc_get_prior_probability
+    from classicML.backend.cc.ops import cc_get_probability_density
+    from classicML.backend.cc.ops import cc_get_w as cc_get_w_v1  # 正式版将移除.
+    from classicML.backend.cc.ops import cc_get_w_v2
+    from classicML.backend.cc.ops import cc_get_w_v2 as cc_get_w
+    from classicML.backend.cc.ops import cc_get_within_class_scatter_matrix
+    from classicML.backend.cc.ops import cc_init_centroids
+    from classicML.backend.cc.ops import cc_select_second_alpha
+    from classicML.backend.cc.ops import cc_type_of_target as cc_type_of_target_v1  # 正式版将移除.
+    from classicML.backend.cc.ops import cc_type_of_target_v2
+    from classicML.backend.cc.ops import cc_type_of_target_v2 as cc_type_of_target
+
+    from classicML.backend.cc.ops import cc_bootstrap_sampling as bootstrap_sampling
+    from classicML.backend.cc.ops import ConvexHull
+    from classicML.backend.cc.ops import cc_calculate_centroids as calculate_centroids
+    from classicML.backend.cc.ops import cc_calculate_error as calculate_error
+    from classicML.backend.cc.ops import cc_calculate_euclidean_distance as calculate_euclidean_distance
+    from classicML.backend.cc.ops import cc_clip_alpha as clip_alpha
+    from classicML.backend.cc.ops import cc_compare_differences as compare_differences
+    from classicML.backend.cc.ops import cc_get_cluster as get_cluster
+    from classicML.backend.cc.ops import cc_get_conditional_probability as get_conditional_probability
+    from classicML.backend.cc.ops import cc_get_dependent_prior_probability as get_dependent_prior_probability
+    from classicML.backend.cc.ops import cc_get_prior_probability as get_prior_probability
+    from classicML.backend.cc.ops import cc_get_probability_density as get_probability_density
+    from classicML.backend.cc.ops import cc_get_w_v2 as get_w
+    from classicML.backend.cc.ops import cc_get_within_class_scatter_matrix as get_within_class_scatter_matrix
+    from classicML.backend.cc.ops import cc_init_centroids as init_centroids
+    from classicML.backend.cc.ops import cc_select_second_alpha as select_second_alpha
+    from classicML.backend.cc.ops import cc_type_of_target_v2 as type_of_target
+
+    from classicML.backend.cc.ops import __version__ as ops__version__
+    CLASSICML_LOGGER.info('后端版本是: {}'.format(ops__version__))
+else:
+    from classicML.backend.python.ops import bootstrap_sampling
+    from classicML.backend.python.ops import ConvexHull
+    from classicML.backend.python.ops import calculate_centroids
+    from classicML.backend.python.ops import calculate_error
+    from classicML.backend.python.ops import calculate_euclidean_distance
+    from classicML.backend.python.ops import clip_alpha
+    from classicML.backend.python.ops import compare_differences
+    from classicML.backend.python.ops import get_cluster
+    from classicML.backend.python.ops import get_conditional_probability
+    from classicML.backend.python.ops import get_dependent_prior_probability
+    from classicML.backend.python.ops import get_prior_probability
+    from classicML.backend.python.ops import get_probability_density
+    from classicML.backend.python.ops import get_w as get_w_v1  # 正式版将移除.
+    from classicML.backend.python.ops import get_w_v2
+    from classicML.backend.python.ops import get_w_v2 as get_w
+    from classicML.backend.python.ops import get_within_class_scatter_matrix
+    from classicML.backend.python.ops import init_centroids
+    from classicML.backend.python.ops import select_second_alpha
+    from classicML.backend.python.ops import type_of_target
+
+from classicML.backend import io
+from classicML.backend.training import get_initializer
+from classicML.backend.training import get_kernel
+from classicML.backend.training import get_loss
+from classicML.backend.training import get_metric
+from classicML.backend.training import get_optimizer
+from classicML.backend.training import get_pruner
