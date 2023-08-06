a3bc0cd85d5eae049b05dec7e3425512a042bd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:57 +0000

    Add package sqtdlearn/1.3/sqtdlearn-1.3.tar.gz

---
+++ packages/sqtdlearn/sqtdlearn-1.3.tar.gz/sqtdlearn-1.3/sqtdlearn/demo1.py
@@ -0,0 +1,6 @@
+# encoding=utf-8
+print('this is demo1 -- version1.3')
+
+
+def demo():
+    print('demo1 test -- version1.3')
---
+++ packages/sqtdlearn/sqtdlearn-1.3.tar.gz/sqtdlearn-1.3/sqtdlearn/demo2.py
@@ -0,0 +1,6 @@
+# encoding=utf-8
+print('this is demo2 -- version1.3')
+
+
+def demo():
+    print('demo2 test -- version1.3')
