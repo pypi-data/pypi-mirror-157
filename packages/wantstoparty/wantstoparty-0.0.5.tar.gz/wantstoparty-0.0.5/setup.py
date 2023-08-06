83cf60ce8b4d64c99ca840b73adc0a05910d3bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:11 +0000

    Add package wantstoparty/0.0.5/wantstoparty-0.0.5.tar.gz

---
+++ packages/wantstoparty/wantstoparty-0.0.5.tar.gz/wantstoparty-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+VERSION = "0.0.5"
+DESCRIPTION = "An API wrapper for the wants-to.party API."
+
+long_desc = ""
+with open(".github/README.md", "r") as f:
+    long_desc = f.read()
+
+setup(
+    name="wantstoparty",
+    version=VERSION,
+    author="acatiadroid",
+    author_email="<acatia@mail.com>",
+    description=DESCRIPTION,
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["docs", "examples", "build", "dist", "*.egg-info"]),
+    install_requires=["aiohttp", "requests"],
+    keywords=["python", "api wrapper"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
