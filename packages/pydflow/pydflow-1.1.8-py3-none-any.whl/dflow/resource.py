b152fed763fdf7b82b80928f033750955381ed5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:33 +0000

    Add package pydflow/1.1.8/pydflow-1.1.8-py3-none-any.whl

---
+++ packages/pydflow/pydflow-1.1.8-py3-none-any.whl/dflow/resource.py
@@ -0,0 +1,17 @@
+class Resource(object):
+    """
+    Resource
+
+    Args:
+        action: action on the Kubernetes resource
+        success_condition: expression representing success
+        failure_condition: expression representing failure
+    """
+    action = None
+    success_condition = None
+    failure_condition = None
+    def get_manifest(self, template):
+        """
+        The method to get the manifest (str)
+        """
+        raise NotImplementedError()
