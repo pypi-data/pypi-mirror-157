9acece4a4dd4bd8b1dcb764444b059b3be8103bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:59 +0000

    Add package milla-tdf/0.0.17/milla_tdf-0.0.17-py3-none-any.whl

---
+++ packages/milla-tdf/milla_tdf-0.0.17-py3-none-any.whl/milla_tdf/data_model/abstract.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+@dataclass_json
+@dataclass
+class AbstractDataModel:
+    pass
+
+@dataclass_json
+@dataclass
+class AbstractMetaData:
+    pass
