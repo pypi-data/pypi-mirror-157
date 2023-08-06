d2bc3c1c57133549fb5717dcddc15a711c2c827d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:01 +0000

    Add package toolchest-client/0.9.15/toolchest_client-0.9.15-py3-none-any.whl

---
+++ packages/toolchest-client/toolchest_client-0.9.15-py3-none-any.whl/toolchest_client/tools/__init__.py
@@ -0,0 +1,15 @@
+from .tool import Tool
+from .alphafold import AlphaFold
+from .bowtie2 import Bowtie2
+from .cellranger import CellRangerCount
+from .clustalo import ClustalO
+from .demucs import Demucs
+from .diamond import DiamondBlastp, DiamondBlastx
+from .kraken2 import Kraken2
+from .megahit import Megahit
+from .rapsearch2 import Rapsearch2
+from .shi7 import Shi7
+from .shogun import ShogunAlign, ShogunFilter
+from .star import STARInstance
+from .test import Test
+from .unicycler import Unicycler
