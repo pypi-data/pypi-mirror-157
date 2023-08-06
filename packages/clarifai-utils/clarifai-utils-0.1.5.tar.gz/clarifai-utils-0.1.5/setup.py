b5531ffab384522b7e5a56453098bf606ebf395e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:17 +0000

    Add package clarifai-utils/0.1.5/clarifai-utils-0.1.5.tar.gz

---
+++ packages/clarifai-utils/clarifai-utils-0.1.5.tar.gz/clarifai-utils-0.1.5/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+packages = setuptools.find_packages(include=["clarifai_utils*"])
+
+package_data = {p: ["*.pyi", "*.css"] for p in packages}
+
+print(package_data)
+
+setuptools.setup(
+    name="clarifai-utils",
+    version="0.1.5",
+    author="Clarifai",
+    author_email="support@clarifai.com",
+    description="Clarifai Python Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Clarifai/clarifai-python-utils",
+    packages=packages,
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    license="Apache 2.0",
+    python_requires='>=3.6',
+    install_requires=[
+        "clarifai-grpc>=7.12.0rc1",
+    ],
+    package_data=package_data,
+    include_package_data=True)
