e3d8072bdf3a0dfa6e276e62b177587eaad54fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:25 +0000

    Add package featuretools/1.11.1/featuretools-1.11.1-py3-none-any.whl

---
+++ packages/featuretools/featuretools-1.11.1-py3-none-any.whl/featuretools/demo/api.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from .flight import load_flight
+from .mock_customer import load_mock_customer
+from .retail import load_retail
+from .weather import load_weather
---
+++ packages/featuretools/featuretools-1.11.1-py3-none-any.whl/featuretools/tests/primitive_tests/test_identity_features.py
@@ -0,0 +1,22 @@
+import featuretools as ft
+from featuretools.primitives.utils import PrimitivesDeserializer
+
+
+def test_relationship_path(es):
+    value = ft.IdentityFeature(es["log"].ww["value"])
+    assert len(value.relationship_path) == 0
+
+
+def test_serialization(es):
+    value = ft.IdentityFeature(es["log"].ww["value"])
+
+    dictionary = {
+        "name": "value",
+        "column_name": "value",
+        "dataframe_name": "log",
+    }
+
+    assert dictionary == value.get_arguments()
+    assert value == ft.IdentityFeature.from_dictionary(
+        dictionary, es, {}, PrimitivesDeserializer
+    )
