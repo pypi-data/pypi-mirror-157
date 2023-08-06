ae1cabf8a0f42e03724c8dd2b40bca1ace72a1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:23 +0000

    Add package bevy/1.3.10/Bevy-1.3.10.tar.gz

---
+++ packages/bevy/Bevy-1.3.10.tar.gz/Bevy-1.3.10/bevy/inject/context_injector.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from functools import cache
+from typing import overload, Type, TypeVar
+
+import bevy.context.abstract_context as bc
+import bevy.context as c
+
+
+T = TypeVar("T")
+
+
+class ContextInjector:
+    @overload
+    def __get__(self, instance: None, owner: Type[T]) -> None:
+        ...
+
+    @overload
+    def __get__(self, instance: T, owner: Type[T]) -> bc.AbstractContext:
+        ...
+
+    @cache
+    def __get__(self, instance: T | None, owner: Type[T]) -> bc.AbstractContext | None:
+        if not instance:
+            return
+
+        if instance.__bevy_context__:
+            return instance.__bevy_context__
+
+        return c.Context.factory()
