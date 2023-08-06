364e1f2b98ac878ecff734df3c21b17c6d624a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:33 +0000

    Add package my-linked-list/0.0.4/my_linked_list-0.0.4.tar.gz

---
+++ packages/my-linked-list/my_linked_list-0.0.4.tar.gz/my_linked_list-0.0.4/src/my_linked_list/linked_list.py
@@ -0,0 +1,31 @@
+import typing as t
+
+from base_linked_list import BaseLinkedList
+
+
+class LinkedList(BaseLinkedList):
+    def append(self, val):
+        """This method adds a value to the end of the list"""
+        self._append(val)
+
+    def prepend(self, val):
+        """This method adds a value to the beginning of the list"""
+        self._prepend(val)
+
+    def lookup(self, val) -> t.Optional[int]:
+        """This method finds the index of an element by value"""
+        return self._lookup(val)
+
+    def insert(self, idx: int, val):
+        """This method inserts an element at a specific index, shifting the elements to the right"""
+        self._insert(idx, val)
+
+    def delete(self, idx):
+        """This method removes an element by index"""
+        self._delete(idx)
+
+    def __str__(self) -> str:
+        return f'LinkedList({self._head})'
+
+    def __repr__(self) -> str:
+        return f'LinkedList({self._head})'
