4e19c45541505daab775a76c23e14bd73a0ce062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:23 +0000

    Add package pyvista-xarray/0.1.0/pyvista_xarray-0.1.0-py3-none-any.whl

---
+++ packages/pyvista-xarray/pyvista_xarray-0.1.0-py3-none-any.whl/pvxarray/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+from pvxarray._version import __version__
+from pvxarray.accessor import PyVistaAccessor
+from pvxarray.errors import DataCopyWarning
+from pvxarray.report import Report
