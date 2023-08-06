462bedef9cbdd26cfaabdb9780fc44f05f91d8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:30 +0000

    Add package raghavcalculator/0.0.23/raghavcalculator-0.0.23.tar.gz

---
+++ packages/raghavcalculator/raghavcalculator-0.0.23.tar.gz/raghavcalculator-0.0.23/README.md
@@ -0,0 +1,17 @@
+# Calculator
+
+This is a basic calculator which performs basic arithmetic operations
+```python
+pip install raghavcalculator
+```
+
+>[from raghavcalculator import Calculator as c]
+
+## Addition
+>[c.add_numbers(num1,num2)]
+
+![add](https://i.imgur.com/ojZIQm5_d.webp?maxwidth=760&fidelity=grand)
+
+### License
+
+This program is free (as in speech) software under the GPLv3. Please see the [license](LICENSE.txt) file for more.
---
+++ packages/raghavcalculator/raghavcalculator-0.0.23.tar.gz/raghavcalculator-0.0.23/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='raghavcalculator',
+  version='0.0.23',
+  description='A very basic calculator',
+  long_description_content_type="text/markdown",
+  # long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Chiransh Singh Mehra , Raghav Aggarwal',
+  author_email='singhchiransh4@gmail.com , raghavaggarwal200004@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['report','python','calculator'], 
+  packages=find_packages(),
+  install_requires=['bashplotlib', 'numpy','pandas','bokeh','ipywidgets','ipython','plotly','matplotlib','matplotlib-inline','seaborn','statistics'] 
+)
