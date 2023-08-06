1b0b74ecfb7cb0aa059c53ef61bbff093096cc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:44:49 +0000

    Add package rplot/1.0/rplot-1.0.tar.gz

---
+++ packages/rplot/rplot-1.0.tar.gz/rplot-1.0/README.md
@@ -0,0 +1,30 @@
+# NAME
+
+rplot - Stream-based realtime scientific data plotter.
+
+# SYNOPSIS
+
+rplot [-vca] [-F #] [-x n[,n...]] [file...]
+
+# DESCRIPTION
+
+TBD
+
+# OPTIONS
+
+TBD
+
+# INSTALLATION
+
+```sh
+$ pip3 install rplot
+```
+
+# AVAILABILITY
+
+The latest version of **rplot** is available at PyPI
+(https://pypi.org/project/rplot/) .
+
+# AUTHOR
+
+Hiroyuki Ohsaki <ohsaki[atmark]lsnl.jp>
---
+++ packages/rplot/rplot-1.0.tar.gz/rplot-1.0/rplot/__init__.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+#
+#
+# Copyright (c) 2022, Hiroyuki Ohsaki.
+# All rights reserved.
+#
+# $Id: __init__.py,v 1.9 2022/07/03 12:18:16 ohsaki Exp ohsaki $
+#
+
+import contextlib
+import time
+
+with contextlib.redirect_stdout(None):
+    import pygame
+    import pygame.gfxdraw
+import curses
+
+from perlcompat import die, warn, getopts
+import tbdump
+
+FONT_SIZE = 18
+
+BLACK = (0, 0, 0)
+
+DARK_GRAY = 100
+GRAY = 101
+
+# This code is taken from cellx/monitor/color.py.
+def hsv2rgb(h, s, v):
+    """Convert color in HSV (Hue, Saturation, Value) space to RGB.  Return (R,
+    G, B) as a list.  Transformation algorithm is taken from
+    http://psychology.wikia.com/wiki/HSV_color_space."""
+    hi = int(h / 60) % 6
+    f = h / 60 - hi
+    p = v * (1 - s)
+    q = v * (1 - f * s)
+    t = v * (1 - (1 - f) * s)
+    if hi == 0:
+        return v, t, p
+    elif hi == 1:
+        return q, v, p
+    elif hi == 2:
+        return p, v, t
+    elif hi == 3:
+        return p, q, v
+    elif hi == 4:
+        return t, p, v
+    elif hi == 5:
+        return v, p, q
+
+def color_rgb(n):
+    if n == DARK_GRAY:
+        return 32, 32, 32
+    if n == GRAY:
+        return 64, 64, 64
+    plist = [n / 10 for n in [6, 8, 1, 10, 0, 4, 9, 2, 5, 3, 7]]
+    i = n % len(plist)
+    r, g, b = hsv2rgb(255 * plist[i], .6, 1.)
+    return int(r * 255), int(g * 255), int(b * 255)
+
+class Series(list):
+    def __init__(self, id_=None, vmin=0, vmax=1, color=None, label=None):
+        self.id_ = id_
+        self.vmin = vmin
+        self.vmax = vmax
+        if color is None:
+            color = id_
+        self.color = color
+        if label is None:
+            label = f'#{id_}'
+        self.label = label
+
+    def __repr__(self):
+        return f'Series(id_={self.id_}, vmin={self.vmin}, vmax={self.vmax}, color={self.color}, label={self.label}, len={len(self)}'
+
+    def append(self, v):
+        if v < self.vmin:
+            self.vmin = v
+        if v > self.vmax:
+            self.vmax = v
+        super().append(v)
+
+    def at(self, ratio, window=None):
+        if window is None:
+            n = int(len(self) * ratio)
+            return self[n]
+        if len(self) < window:
+            return self.at(ratio, None)
+
+        offset = len(self) - window
+        n = int(offset + window * ratio)
+        return self[n]
+
+class Plot:
+    def __init__(self,
+                 curses=False,
+                 width=None,
+                 height=None,
+                 grid=None,
+                 subgrid=None):
+        self.curses = curses
+        if not curses and (width is None or height is None):
+            width, height = 800, 600
+        self.width = width
+        self.height = height
+        self.grid = grid
+        self.subgrid = subgrid
+        self._series = []
+        self.window = None
+
+    def __repr__(self):
+        return f'Plot(curses={self.curses}, width={self.width}, height={self.height}, grid={self.grid}, subgrid={self.subgrid}, _series={self._series}'
+
+    # draw primitives
+    def init_screen(self):
+        if not self.curses:
+            pygame.display.init()
+            self.screen = pygame.display.set_mode((self.width, self.height))
+            pygame.font.init()
+        else:
+            self.screen = curses.initscr()
+            curses.start_color()
+            self.height, self.width = self.screen.getmaxyx()
+            self.screen.erase()
+            curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+            curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+            curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+            curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+            curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+            curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
+            curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    def draw_line(self, x1, y1, x2, y2, color=0):
+        if not self.curses:
+            rgb = color_rgb(color)
+            pygame.gfxdraw.line(self.screen, x1, y1, x2, y2, rgb)
+            return
+
+        # Curses only.
+        points = ['●', '■', '◆', '▲', '▼', '◼', '○', '□', '◇', '△', '▽', '◻']
+        point = points[color % len(points)]
+
+        if color == GRAY:
+            point = '.'
+            color = 1
+        if color == DARK_GRAY:
+            point = '-'
+            color = 1
+
+        if abs(x1 - x2) >= abs(y1 - y2):
+            if x1 > x2:
+                x1, y1, x2, y2 = x2, y2, x1, y1
+            for x in range(x1, x2):
+                y = int(y1 + (y2 - y1) * (x - x1) / (x2 - x1))
+                self.draw_text(x, y, point, color)
+        else:
+            if y1 > y2:
+                x1, y1, x2, y2 = x2, y2, x1, y1
+            for y in range(y1, y2):
+                x = int(x1 + (x2 - x1) * (y - y1) / (y2 - y1))
+                self.draw_text(x, y, point, color)
+
+    def pos2xy(self, x, y, size=FONT_SIZE):
+        return x * size // 2, (y - 1) * size + size // 2
+
+    def draw_text(self, x, y, text, color=0, size=FONT_SIZE):
+        if not self.curses:
+            font = pygame.font.SysFont('Courier', size)
+            rgb = color_rgb(color)
+            # Enable antialiasing.
+            text = font.render(text, True, rgb)
+            x, y = self.pos2xy(x, y, size)
+            self.screen.blit(text, (x, y))
+        else:
+            attr = curses.color_pair(1 + color % 7)
+            try:
+                self.screen.addstr(y, x, text, attr)
+            except:
+                pass
+
+    def clear(self):
+        if not self.curses:
+            self.screen.fill(BLACK)
+        else:
+            self.screen.erase()
+
+    def update(self):
+        if not self.curses:
+            pygame.display.update()
+        else:
+            self.screen.refresh()
+
+    def wait(self):
+        if not self.curses:
+            while True:
+                event = pygame.event.wait()
+                if event.type == pygame.KEYDOWN:
+                    return
+        else:
+            pass
+
+    # series handling
+    def series(self, n):
+        while len(self._series) - 1 < n:
+            self._series.append(Series(1 + n))
+        return self._series[n]
+
+    def v2y(self, v, vmin, vmax):
+        return int(self.height - self.height * (v - vmin) / (vmax - vmin))
+
+    def draw_single_series(self, sr, vmin=None, vmax=None):
+        x0, y0 = None, None
+        for x in range(self.width):
+            ratio = x / self.width
+            v = sr.at(ratio, self.window)
+            y = self.v2y(v, vmin, vmax)
+            if x > 0:
+                self.draw_line(x0, y0, x, y, sr.color)
+            x0, y0 = x, y
+
+    def draw_series(self, vmin=None, vmax=None, excluded=None):
+        series = [sr for sr in self._series if sr.id_ not in excluded]
+        if vmin is None:
+            vmin = min([sr.vmin for sr in series])
+        if vmax is None:
+            vmax = max([sr.vmax for sr in series])
+        # Draw grid line.
+        if self.subgrid:
+            self.draw_grid(self.subgrid, vmin, vmax, DARK_GRAY)
+        if self.grid:
+            self.draw_grid(self.grid, vmin, vmax, GRAY)
+        # Draw all series.
+        for sr in series:
+            self.draw_single_series(sr, vmin, vmax)
+        # Draw legends.
+        # NOTE: legends are shown also for *excluded* serries
+        if not self.curses:
+            # Clear the backgound of legends.
+            x, y = self.pos2xy(1, 1)
+            w, h = self.pos2xy(16, len(self._series))
+            w += FONT_SIZE // 2
+            h += FONT_SIZE // 2
+            self.screen.fill((32, 32, 32), pygame.Rect(x, y, w, h))
+        self.draw_legends()
+
+    def draw_grid(self, grid, vmin, vmax, color):
+        ngrids = (vmax - vmin) / grid
+        n = int(ngrids)
+        if self.curses and n > 5:
+            return
+        frac = ngrids - n
+        v = vmin + grid * frac
+        for i in range(n):
+            y = self.v2y(v, vmin, vmax)
+            self.draw_line(0, y, self.width, y, color)
+            v += grid
+
+    def draw_legends(self):
+        for n, sr in enumerate(self._series):
+            x = 1
+            y = 1 + n
+            v = sr[-1]
+            self.draw_text(x, y, sr.label, sr.color)
+            self.draw_text(x + 6, y, f'{v:9.2f}', sr.color)
