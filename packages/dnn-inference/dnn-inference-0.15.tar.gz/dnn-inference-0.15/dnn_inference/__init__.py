b85372b42c0c13c0822a40c8bb245274f5efc187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:22:32 +0000

    Add package dnn-inference/0.15/dnn-inference-0.15.tar.gz

---
+++ packages/dnn-inference/dnn-inference-0.15.tar.gz/dnn-inference-0.15/dnn_inference/__init__.py
@@ -0,0 +1,11 @@
+from dnn_inference.sig_test import split_test, perm_test, Hperm_test
+import sys
+
+sys.path.append('..')
+
+__all__ = [
+    "sig_test",
+	"split_test", 
+	"perm_test",
+	"Hperm_test"
+	]
