0378229cd5692e8c1bf6dfebadbe646e96e88518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:52 +0000

    Add package awlongutils/0.2.31/awlongutils-0.2.31.tar.gz

---
+++ packages/awlongutils/awlongutils-0.2.31.tar.gz/awlongutils-0.2.31/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name = 'awlongutils',
+      version = '0.2.31',
+      author = 'Andy Long',
+      packages = ['awlong_utils',
+                 'awlong_utils',
+                 'awlong_utils'])
+
+# packages=setuptools.find_packages()
