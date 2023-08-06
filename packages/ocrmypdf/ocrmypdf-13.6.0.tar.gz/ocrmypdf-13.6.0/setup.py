736f430637f96c48ce8b3d63ef2b50364b6c9ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:48 +0000

    Add package ocrmypdf/13.6.0/ocrmypdf-13.6.0.tar.gz

---
+++ packages/ocrmypdf/ocrmypdf-13.6.0.tar.gz/ocrmypdf-13.6.0/setup.py
@@ -0,0 +1,18 @@
+# © 2021 James R. Barlow: github.com/jbarlow83
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""setup.py to support older setuptools and pip."""
+
+from setuptools import setup
+
+# Minimal setup to support older setuptools/setuptools_scm
+setup(
+    setup_requires=[  # can be removed whenever we can drop pip 9 support
+        'setuptools_scm',  # so that version will work
+        'setuptools_scm_git_archive',  # enable version from github tarballs
+    ],
+    use_scm_version={'version_scheme': 'post-release'},
+)
