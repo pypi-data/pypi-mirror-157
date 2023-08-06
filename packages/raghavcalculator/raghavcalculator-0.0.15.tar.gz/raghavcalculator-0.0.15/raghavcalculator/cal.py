3d9509bc5ae3483b6e4825a58f9634c40d1a2dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:01 +0000

    Add package raghavcalculator/0.0.15/raghavcalculator-0.0.15.tar.gz

---
+++ packages/raghavcalculator/raghavcalculator-0.0.15.tar.gz/raghavcalculator-0.0.15/raghavcalculator/cal.py
@@ -0,0 +1,33 @@
+import numpy as np
+from bashplotlib.histogram import plot_hist
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure, show, output_notebook
+import ipywidgets as widgets
+from IPython.display import display, clear_output
+from  plotly.offline import iplot
+import plotly as py 
+import plotly.tools as tls 
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
+import plotly.express as px
+from statistics import mean
+# output_notebook()
+
+class Calculator:
+    def add_numbers(num1, num2):
+        return num1 + num2
+
+    def subtract_numbers(num1, num2):
+        return num1 - num2
+
+    def multiply_numbers(num1, num2):
+        return num1 * num2
+
+    def divide_numbers(num1, num2):
+        return num1 / num2
+    
+    def p():
+        arr = np.random.normal(size=1000, loc=0, scale=1)
+        return plot_hist(arr, bincount=50)
