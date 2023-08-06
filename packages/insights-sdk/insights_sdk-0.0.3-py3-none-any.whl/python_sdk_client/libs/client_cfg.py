d2f6619a09ed2670943077870b71cc0733e4a875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package insights-sdk/0.0.3/insights_sdk-0.0.3-py3-none-any.whl

---
+++ packages/insights-sdk/insights_sdk-0.0.3-py3-none-any.whl/python_sdk_client/libs/client_cfg.py
@@ -0,0 +1,6 @@
+class InsightServiceCfg:
+    prod_base_url = "https://7w0h8fyvpg.execute-api.ap-southeast-1.amazonaws.com/dev"
+    qa_base_url = "https://7w0h8fyvpg.execute-api.ap-southeast-1.amazonaws.com/dev"
+    X_API_KEY = "X-Api-Key"
+
+
