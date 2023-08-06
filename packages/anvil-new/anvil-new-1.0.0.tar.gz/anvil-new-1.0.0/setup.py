855c0df50f66ae969a6cfb26c48511d6be1995c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:52 +0000

    Add package anvil-new/1.0.0/anvil-new-1.0.0.tar.gz

---
+++ packages/anvil-new/anvil-new-1.0.0.tar.gz/anvil-new-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='anvil-new',
+    version='1.0.0',
+    author='intergalactyc',
+    description='A Minecraft anvil file format parser, designed to work with minecraft versions 1.16+.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/intergalactyc/anvil-new',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'nbt',
+        'frozendict',
+    ],
+    include_package_data=True
+)
