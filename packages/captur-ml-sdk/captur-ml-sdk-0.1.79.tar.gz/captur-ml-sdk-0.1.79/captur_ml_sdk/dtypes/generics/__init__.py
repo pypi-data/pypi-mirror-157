640ed7c4113b8def1628813d646e1cb690941e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:54:51 +0000

    Add package captur-ml-sdk/0.1.79/captur-ml-sdk-0.1.79.tar.gz

---
+++ packages/captur-ml-sdk/captur-ml-sdk-0.1.79.tar.gz/captur-ml-sdk-0.1.79/captur_ml_sdk/dtypes/generics/__init__.py
@@ -0,0 +1,12 @@
+from .Image import Image
+from .Meta import PredictMeta, TrainingMeta, EvaluationMeta
+from .Model import Model, EvaluationModel
+
+__all__ = [
+    Image,
+    PredictMeta,
+    Model,
+    EvaluationModel,
+    TrainingMeta,
+    EvaluationMeta
+]
