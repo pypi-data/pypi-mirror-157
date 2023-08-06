122f5aa7e102e507f93316d06f5ee219d2e162b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:58 +0000

    Add package changedetector/0.0.3/changedetector-0.0.3.tar.gz

---
+++ packages/changedetector/changedetector-0.0.3.tar.gz/changedetector-0.0.3/changedetector/__init__.py
@@ -0,0 +1,19 @@
+"""
+CHANGE DETECTOR
+~~~~~~~~~~~~~~~
+A basic change detector for the Python, Ruby and C++.
+:copyright (c) 2022 LuxLuth
+:license: MIT, see LICENSE for more details.
+"""
+import os
+
+__title__ = 'changedetector'
+__author__ = 'LuxLuth'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 LuxLuth'
+__version__ = '0.0.3'
+
+
+from . import detectchange
+from . import _colors as colors
+from . import _purcent as purcent
