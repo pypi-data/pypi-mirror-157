1fc4732d0c462814b5312e7fd8ce280268e36024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:27:10 +0000

    Add package anubis-policy-api/0.4.0/anubis_policy_api-0.4.0-py3-none-any.whl

---
+++ packages/anubis-policy-api/anubis_policy_api-0.4.0-py3-none-any.whl/anubis/main.py
@@ -0,0 +1,94 @@
+from fastapi import Depends, FastAPI, Response
+from anubis.tenants import routers as t
+from anubis.tenants import models as t_models
+from anubis.policies import routers as p
+from anubis.policies import models as p_models
+from anubis.audit import routers as a
+from anubis.audit import models as a_models
+from anubis.version import ANUBIS_VERSION
+from fastapi.openapi.utils import get_openapi
+from anubis.database import engine
+
+from fastapi.middleware.cors import CORSMiddleware
+
+import os
+
+tags_metadata = [
+    {
+        "name": "services",
+        "description": "Manage Tenants and Service Paths",
+    },
+    {
+        "name": "policies",
+        "description": "Manage Policies for each Tenant and Service Path within a Tenant.",
+    },
+]
+
+app = FastAPI()
+
+allowed_origins = os.environ.get(
+    'CORS_ALLOWED_ORIGINS', "*").replace(" ", "").split(";")
+allowed_methods = os.environ.get(
+    'CORS_ALLOWED_METHODS', "*").replace(" ", "").split(";")
+allowed_headers = os.environ.get(
+    'CORS_ALLOWED_HEADERS', "*").replace(" ", "").split(";")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=allowed_methods,
+    allow_headers=allowed_headers,
+)
+
+# TODO auth
+# https://docs.authlib.org/en/latest/client/fastapi.html
+# https://developer.okta.com/blog/2020/12/17/build-and-secure-an-api-in-python-with-fastapi
+# TODO create "tenant" in keycloak
+
+app.include_router(t.router)
+app.include_router(p.router)
+app.include_router(a.router)
+
+
+@app.get("/v1/", summary="Return Anubis API endpoints")
+async def v1_root():
+    return {
+        "tenants_url": "/v1/tenants",
+        "policies_url": "/v1/policies",
+        "resources_url": "/v1/resources"}
+
+
+@app.get("/version/", summary="Return the version of the Anubis API")
+async def v1_version():
+    return {
+        "version": ANUBIS_VERSION}
+
+
+@app.on_event("startup")
+def on_startup():
+    p_models.init_db()
+    t_models.init_db()
+    a_models.init_db()
+
+
+@app.get("/ping", summary="Simple healthcheck endpoint")
+async def pong():
+    return {"ping": "pong!"}
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Anubis",
+        version=ANUBIS_VERSION,
+        description="Anubis is a flexible Policy Enforcement solution that makes easier to reuse security policies across different services, assuming the policies entail the same resource.",
+        routes=app.routes,
+    )
+    openapi_schema['tags'] = tags_metadata
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
---
+++ packages/anubis-policy-api/anubis_policy_api-0.4.0-py3-none-any.whl/anubis/wac.py
@@ -0,0 +1,97 @@
+from rdflib import Graph, URIRef, BNode, Literal
+from rdflib import Namespace
+from rdflib.namespace import FOAF, RDF
+from anubis.policies.models import Policy
+from sqlalchemy.orm import Session
+import yaml
+import os
+
+acl = Namespace("http://www.w3.org/ns/auth/acl#")
+oc_acl = Namespace("http://voc.orchestracities.io/oc-acl#")
+
+# follow keycloak scheme
+# user urls should be /{realm}/users/{id}
+# group urls should be /{realm}/groups/{id}
+
+
+def parse_rdf_graph(data):
+    g = Graph()
+    g.parse(data=data)
+    policies = {}
+    for subj, pred, obj in g:
+        policy = str(subj).split("/")[-1]
+        if not policies.get(policy):
+            policies[policy] = {}
+        if pred == acl.agentClass:
+            policies[policy]["agentClass"] = str(obj)
+        if pred == acl.mode:
+            policies[policy]["mode"] = str(obj)
+        if pred == acl.accessTo:
+            policies[policy]["accessTo"] = str(obj).split("://")[-1]
+        if pred == acl.default:
+            policies[policy]["accessTo"] = "default"
+        if pred == acl.accessToClass:
+            policies[policy]["accessToClass"] = str(obj).split("/")[-1]
+    return policies
+
+
+def serialize(
+        db: Session,
+        fiware_service: [str],
+        fiware_servicepath: [str],
+        policies: [Policy]):
+    with open(os.environ.get("DEFAULT_WAC_CONFIG_FILE", '../config/opa-service/default_wac_config.yml'), 'r') as file:
+        default_wac = yaml.load(file, Loader=yaml.FullLoader)["wac"]
+
+    g = Graph()
+    g.bind("foaf", FOAF)
+    g.bind("acl", acl)
+    g.bind("oc_acl", oc_acl)
+
+    if default_wac.get(fiware_service):
+        n = Namespace(default_wac[fiware_service]["baseUrl"])
+        g.bind(default_wac[fiware_service]["prefix"], n)
+    else:
+        n = Namespace(default_wac["default"]["baseUrl"])
+        g.bind(default_wac["default"]["prefix"], n)
+        fiware_service = "default"
+
+    for policy in policies:
+        policy_node = URIRef(policy.id, n.policy)
+        g.add((policy_node, RDF.type, acl.Authorization))
+
+        mode_property = acl.mode
+        for index, mode in enumerate(policy.mode):
+            mode = URIRef(mode.iri, acl)
+            g.add((policy_node, mode_property, mode))
+
+        agent_class_property = acl.agentClass
+        agent_property = acl.agent
+        for index, agent in enumerate(policy.agent):
+            agent = URIRef(agent.iri, acl)
+            g.add((policy_node, agent_class_property, agent))
+
+        if policy.access_to == "default":
+            access_to_property = acl.default
+            access_to_value = fiware_servicepath
+        else:
+            access_to_property = acl.accessTo
+            access_to_value = policy.access_to
+        if default_wac.get(fiware_service) and default_wac[fiware_service]["resourceTypeUrls"].get(
+                policy.resource_type):
+            resource_namespace = Namespace(
+                default_wac[fiware_service]["resourceTypeUrls"][policy.resource_type]["url"])
+            resource_type_namespace = Namespace(
+                default_wac[fiware_service]["resourceTypeUrls"][policy.resource_type]["type_url"])
+            access_to_iri = URIRef(access_to_value, resource_namespace)
+            g.add((policy_node, access_to_property, access_to_iri))
+            access_to_class_iri = URIRef(
+                policy.resource_type, resource_type_namespace)
+            g.add((policy_node, acl.accessToClass, access_to_class_iri))
+        else:
+            access_to_iri = URIRef(access_to_value, n)
+            g.add((policy_node, access_to_property, access_to_iri))
+            access_to_class_iri = URIRef(policy.resource_type, n)
+            g.add((policy_node, acl.accessToClass, access_to_class_iri))
+
+    return g.serialize()
