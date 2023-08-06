51b2ef7dd647e58d8f4eaa21ff0bdcba48e6ffbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:32 +0000

    Add package Pagen/0.0.1/Pagen-0.0.1-py3-none-any.whl

---
+++ packages/Pagen/Pagen-0.0.1-py3-none-any.whl/generator/pagen.py
@@ -0,0 +1,10 @@
+import random
+def gen():
+    lower = "abcdefghijklmnopqrstubwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    NUMBER = "0123456789"
+    symbol = "`!@#$%^&*()_+|}]{[;:'>/?,"
+    all = lower + upper + NUMBER + symbol;
+    length = 9
+    password = "".join(random.sample(all,length));
+    print("Use this password:" ,password);
