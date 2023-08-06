528327f8b21b92ba74d7c27461ea4a65a3face09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:12 +0000

    Add package peaqevcore/3.0.1/peaqevcore-3.0.1.tar.gz

---
+++ packages/peaqevcore/peaqevcore-3.0.1.tar.gz/peaqevcore-3.0.1/peaqevcore/chargertype_service/models/charger_options.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=False)
+class ChargerOptions:
+    powerswitch_controls_charging: bool
+    ampmeter_is_attribute: bool
+    powermeter_factor: int
