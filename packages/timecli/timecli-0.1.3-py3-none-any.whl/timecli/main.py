38ddcd3b24045652c548f26faa085249a6215da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:15 +0000

    Add package timecli/0.1.3/timecli-0.1.3-py3-none-any.whl

---
+++ packages/timecli/timecli-0.1.3-py3-none-any.whl/timecli/main.py
@@ -0,0 +1,24 @@
+import typer
+from timecli.commands import oracle
+
+__version__ = "0.1.3"
+
+app = typer.Typer()
+app.add_typer(oracle.app, name="oracle")
+
+
+def call_version(value: bool):
+    if value:
+        typer.echo(f"timecli: version {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def version_callback(
+        version: bool = typer.Option(None, "--version", callback=call_version),
+):
+    pass
+
+
+if __name__ == '__main__':
+    app()
