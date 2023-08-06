3f874a9cab422b405bad91ed016770699ec9032d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:50:25 +0000

    Add package netbox-vpn-plugin/0.8.1/netbox-vpn-plugin-0.8.1.tar.gz

---
+++ packages/netbox-vpn-plugin/netbox-vpn-plugin-0.8.1.tar.gz/netbox-vpn-plugin-0.8.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-vpn-plugin',
+    version='0.8.1',
+    download_url='https://github.com/hhansen06/netbox-vpn-plugin/archive/refs/tags/0.8.1.tar.gz',
+    description='Manage VPN Connections in Netbox',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+)
