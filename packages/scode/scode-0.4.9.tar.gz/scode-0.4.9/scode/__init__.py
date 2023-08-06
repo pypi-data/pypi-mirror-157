634ce29c8ff0babd3999fda42c2575eb803a5509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:21:26 +0000

    Add package scode/0.4.9/scode-0.4.9.tar.gz

---
+++ packages/scode/scode-0.4.9.tar.gz/scode-0.4.9/scode/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['selenium', 'paramiko', 'telegram', 'dropbox', 'util', 'schedule']
+
+__version__ = '0.4.9'
+
+from . import selenium, paramiko, telegram, dropbox, util, schedule
+
+from .util import *
