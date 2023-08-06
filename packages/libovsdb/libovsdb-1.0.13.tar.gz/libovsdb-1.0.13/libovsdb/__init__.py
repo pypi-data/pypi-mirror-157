0b0f86a970e65c101ad60c84d52e1cbd4d5ce826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:25:22 +0000

    Add package libovsdb/1.0.13/libovsdb-1.0.13.tar.gz

---
+++ packages/libovsdb/libovsdb-1.0.13.tar.gz/libovsdb-1.0.13/libovsdb/__init__.py
@@ -0,0 +1,12 @@
+# $Id: __init__.py,v 1.1 2022/03/17 10:50:34 Sangfor Exp $
+
+"""libraries to manipulate the devices and network."""
+
+__author__ = 'Yongping Guo <guoyoooping@163.com>'
+__copyright__ = 'Copyright (c) 2022 Yongping Guo'
+__license__ = 'GPLv3'
+__url__ = 'https://github.com/guoyoooping/libovsdb'
+__version__ = '1.0'
+
+__all__ = ['libovsdb']
+
