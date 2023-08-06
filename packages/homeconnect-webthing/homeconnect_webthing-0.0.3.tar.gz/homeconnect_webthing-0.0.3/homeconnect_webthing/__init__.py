0086b97e9b254194c18572b6298878db1c32e5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:32 +0000

    Add package homeconnect-webthing/0.0.3/homeconnect_webthing-0.0.3.tar.gz

---
+++ packages/homeconnect-webthing/homeconnect_webthing-0.0.3.tar.gz/homeconnect_webthing-0.0.3/homeconnect_webthing/__init__.py
@@ -0,0 +1,54 @@
+from string import Template
+from homeconnect_webthing.app import App
+from homeconnect_webthing.hc_webthing import run_server
+
+PACKAGENAME = 'homeconnect_webthing'
+ENTRY_POINT = "homeconnect"
+DESCRIPTION = "Homeconnect WebThing adapter"
+
+
+UNIT_TEMPLATE = Template('''
+[Unit]
+Description=$packagename
+After=syslog.target
+
+[Service]
+Type=simple
+ExecStart=$entrypoipython nt --command listen --verbose $verbose --port $port
+SyslogIdentifier=$packagename
+StandardOutput=syslog
+StandardError=syslog
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+''')
+
+
+
+class HomeConnectApp(App):
+
+    def do_process_command(self, command:str, port: int, verbose: bool, args) -> bool:
+        if command == 'listen':
+            print("running " + self.packagename + " on port " + str(port))
+            run_server(port, self.description)
+            return True
+        elif args.command == 'register':
+            print("register " + self.packagename + " on port " + str(port) + " and starting it")
+            unit = UNIT_TEMPLATE.substitute(packagename=self.packagename,
+                                            entrypoint=self.entrypoint,
+                                            port=port,
+                                            verbose=verbose)
+            self.unit.register(port, unit)
+            return True
+        else:
+            return False
+
+
+def main():
+    HomeConnectApp(PACKAGENAME, ENTRY_POINT, DESCRIPTION).handle_command()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/homeconnect-webthing/homeconnect_webthing-0.0.3.tar.gz/homeconnect_webthing-0.0.3/homeconnect_webthing/hc_webthing.py
@@ -0,0 +1,122 @@
+from webthing import (MultipleThings, Property, Thing, Value, WebThingServer)
+import logging
+import tornado.ioloop
+from homeconnect_webthing.hc import HomeConnect, Dishwasher
+
+
+class DishwasherThing(Thing):
+
+    # regarding capabilities refer https://iot.mozilla.org/schemas
+    # there is also another schema registry http://iotschema.org/docs/full.html not used by webthing
+
+    def __init__(self, description: str, dishwasher: Dishwasher):
+        Thing.__init__(
+            self,
+            'urn:dev:ops:Dishwasher-1',
+            'Dishwasher',
+            ['MultiLevelSensor'],
+            description
+        )
+
+        self.ioloop = tornado.ioloop.IOLoop.current()
+
+        self.dishwasher = dishwasher
+        dishwasher.on_updates(self.on_updated)
+
+        self.power = Value("")
+        self.add_property(
+            Property(self,
+                     'power',
+                     self.power,
+                     metadata={
+                         'title': 'Power State',
+                         "type": "string",
+                         'description': 'The power state. See https://api-docs.home-connect.com/settings?#power-state',
+                         'readOnly': True,
+                     }))
+
+        self.operation = Value("")
+        self.add_property(
+            Property(self,
+                     'operation',
+                     self.operation,
+                     metadata={
+                         'title': 'Operation State',
+                         "type": "string",
+                         'description': 'The operation state. See https://api-docs.home-connect.com/states?#operation-state',
+                         'readOnly': True,
+                     }))
+
+        self.remote_start_allowed = Value("")
+        self.add_property(
+            Property(self,
+                     'remote_start_allowed',
+                     self.remote_start_allowed,
+                     metadata={
+                         'title': 'Remote Start Allowed State',
+                         "type": "boolean",
+                         'description': 'Remote Start Allowance State. See https://api-docs.home-connect.com/states?#remote-start-allowance-state',
+                         'readOnly': True,
+                     }))
+
+        self.door = Value("")
+        self.add_property(
+            Property(self,
+                     'door',
+                     self.door,
+                     metadata={
+                         'title': 'Door State',
+                         "type": "string",
+                         'description': 'Door State. See https://api-docs.home-connect.com/states?#door-state',
+                         'readOnly': True,
+                     }))
+
+        self.program = Value("")
+        self.add_property(
+            Property(self,
+                     'program',
+                     self.program,
+                     metadata={
+                         'title': 'Selected Program',
+                         "type": "string",
+                         'description': 'Selected Program. See https://api-docs.home-connect.com/programs-and-options?#cleaning-robot',
+                         'readOnly': True,
+                     }))
+
+        self.remote_start = Value(False, self.dishwasher.start_program)
+        self.add_property(
+            Property(self,
+                     'remote start',
+                     self.remote_start,
+                     metadata={
+                         'title': 'Remote start',
+                         "type": "bool",
+                         'description': 'Remote start state',
+                         'readOnly': False,
+                     }))
+
+    def on_updated(self, reason):
+        self.ioloop.add_callback(self.__on_updated, reason)
+
+    def __on_updated(self, reason):
+        self.power.notify_of_external_update(self.dishwasher.power)
+        self.door.notify_of_external_update(self.dishwasher.door)
+        self.operation.notify_of_external_update(self.dishwasher.operation)
+        self.remote_start_allowed.notify_of_external_update(self.dishwasher.remote_start_allowed)
+        self.remote_start.notify_of_external_update(False)
+
+
+
+def run_server(port: int, description: str):
+    homeappliances = []
+    for device in HomeConnect().devices():
+        if device.is_dishwasher():
+            homeappliances.append(DishwasherThing(description, device))
+    server = WebThingServer(MultipleThings(homeappliances, 'homeappliances'), port=port, disable_host_validation=True)
+    try:
+        logging.info('starting the server')
+        server.start()
+    except KeyboardInterrupt:
+        logging.info('stopping the server')
+        server.stop()
+        logging.info('done')
