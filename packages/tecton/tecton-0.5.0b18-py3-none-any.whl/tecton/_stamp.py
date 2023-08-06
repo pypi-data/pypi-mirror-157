877dc3f9bb8798ecea83bbb7fbbcf40c2afd2ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:52 +0000

    Add package tecton/0.5.0b18/tecton-0.5.0b18-py3-none-any.whl

---
+++ packages/tecton/tecton-0.5.0b18-py3-none-any.whl/tecton/_stamp.py
@@ -0,0 +1,1 @@
+BUILD_STATUS = {'BUILD_ID': 'uncommitted-buildkite-agent-1656614484', 'BUILD_TIMESTAMP': '1656614484', 'GIT_COMMIT': 'a279fd9718fbb1d2a799b2583584f7147884c79b', 'BUILD_EMBED_LABEL': '', 'BUILD_HOST': 'buildkite-agent-medium-75b4c457d9-2p4p7', 'BUILD_USER': 'buildkite-agent'}
---
+++ packages/tecton/tecton-0.5.0b18-py3-none-any.whl/tecton_athena/errors.py
@@ -0,0 +1,4 @@
+class TectonAthenaNotImplementedError(NotImplementedError):
+    """
+    Exception that indicates a feature is not yet implemented with Athena compute.
+    """
