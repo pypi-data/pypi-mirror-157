716e14dff7730fbde9f63435f9d0e4ec190d6dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:55 +0000

    Add package colour-demosaicing/0.2.2/colour_demosaicing-0.2.2-py3-none-any.whl

---
+++ packages/colour-demosaicing/colour_demosaicing-0.2.2-py3-none-any.whl/colour_demosaicing/bayer/demosaicing/tests/test_malvar2004.py
@@ -0,0 +1,65 @@
+# !/usr/bin/env python
+"""
+Define the unit tests for the
+:mod:`colour_demosaicing.bayer.demosaicing.malvar2004` module.
+"""
+
+from __future__ import annotations
+
+import numpy as np
+import os
+import unittest
+
+from colour import read_image
+
+from colour_demosaicing import TESTS_RESOURCES_DIRECTORY
+from colour_demosaicing.bayer import demosaicing_CFA_Bayer_Malvar2004
+
+__author__ = "Colour Developers"
+__copyright__ = "Copyright 2015 Colour Developers"
+__license__ = "New BSD License - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "Colour Developers"
+__email__ = "colour-developers@colour-science.org"
+__status__ = "Production"
+
+__all__ = [
+    "BAYER_DIRECTORY",
+    "TestDemosaicing_CFA_Bayer_Malvar2004",
+]
+
+BAYER_DIRECTORY: str = os.path.join(
+    TESTS_RESOURCES_DIRECTORY, "colour_demosaicing", "bayer"
+)
+
+
+class TestDemosaicing_CFA_Bayer_Malvar2004(unittest.TestCase):
+    """
+    Define :func:`colour_demosaicing.bayer.demosaicing.malvar2004.\
+demosaicing_CFA_Bayer_Malvar2004` definition unit tests methods.
+    """
+
+    def test_demosaicing_CFA_Bayer_Malvar2004(self):
+        """
+        Test :func:`colour_demosaicing.bayer.demosaicing.malvar2004.\
+demosaicing_CFA_Bayer_Malvar2004` definition.
+        """
+
+        for pattern in ("RGGB", "BGGR", "GRBG", "GBRG"):
+            CFA = os.path.join(
+                BAYER_DIRECTORY, f"Lighthouse_CFA_{pattern}.exr"
+            )
+            RGB = os.path.join(
+                BAYER_DIRECTORY, f"Lighthouse_Malvar2004_{pattern}.exr"
+            )
+
+            np.testing.assert_almost_equal(
+                demosaicing_CFA_Bayer_Malvar2004(
+                    read_image(str(CFA))[..., 0], pattern
+                ),
+                read_image(str(RGB)),
+                decimal=7,
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/colour-demosaicing/colour_demosaicing-0.2.2-py3-none-any.whl/colour_demosaicing/bayer/tests/test_masks.py
@@ -0,0 +1,55 @@
+# !/usr/bin/env python
+"""Define the unit tests for the :mod:`colour_demosaicing.bayer.masks` module."""
+
+from __future__ import annotations
+
+import numpy as np
+import os
+import unittest
+
+from colour import read_image
+from colour.utilities import tstack
+
+from colour_demosaicing import TESTS_RESOURCES_DIRECTORY
+from colour_demosaicing.bayer import masks_CFA_Bayer
+
+__author__ = "Colour Developers"
+__copyright__ = "Copyright 2015 Colour Developers"
+__license__ = "New BSD License - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "Colour Developers"
+__email__ = "colour-developers@colour-science.org"
+__status__ = "Production"
+
+__all__ = [
+    "BAYER_DIRECTORY",
+    "TestMasks_CFA_Bayer",
+]
+
+BAYER_DIRECTORY: str = os.path.join(
+    TESTS_RESOURCES_DIRECTORY, "colour_demosaicing", "bayer"
+)
+
+
+class TestMasks_CFA_Bayer(unittest.TestCase):
+    """
+    Define :func:`colour_demosaicing.bayer.masks.masks_CFA_Bayer` definition
+    unit tests methods.
+    """
+
+    def test_masks_CFA_Bayer(self):
+        """
+        Test :func:`colour_demosaicing.bayer.masks.masks_CFA_Bayer`
+        definition.
+        """
+
+        for pattern in ("RGGB", "BGGR", "GRBG", "GBRG"):
+            mask = os.path.join(BAYER_DIRECTORY, f"{pattern}_Masks.exr")
+            np.testing.assert_almost_equal(
+                tstack(masks_CFA_Bayer((8, 8), pattern)),
+                read_image(str(mask)),
+                decimal=7,
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
