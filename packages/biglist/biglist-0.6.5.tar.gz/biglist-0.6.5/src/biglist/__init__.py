aeafaab3a1c619736cb76c02eeca457d8edda844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:20 +0000

    Add package biglist/0.6.5/biglist-0.6.5.tar.gz

---
+++ packages/biglist/biglist-0.6.5.tar.gz/biglist-0.6.5/src/biglist/__init__.py
@@ -0,0 +1,10 @@
+'''Biglist'''
+from ._biglist import Biglist, ListView, FileView, ConcurrentIterStat
+
+
+__version__ = '0.6.5'
+
+
+__all__ = [
+    'Biglist', 'ListView', 'FileView', 'ConcurrentIterStat',
+]
