e36beb0f591c909dda00199d506fb5c023a1fc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:03 +0000

    Add package trainer-py/0.1.1/trainer-py-0.1.1.tar.gz

---
+++ packages/trainer-py/trainer-py-0.1.1.tar.gz/trainer-py-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+      name='trainer-py',
+      version='0.1.1',
+      description='Lightweight context-based library for benchmarking',
+      packages=['trainer']
+)
+
