d570677910853d8ecd83f8db79164948789f44be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:16 +0000

    Add package PaperCrawlerUtil/0.0.52/PaperCrawlerUtil-0.0.52.tar.gz

---
+++ packages/PaperCrawlerUtil/PaperCrawlerUtil-0.0.52.tar.gz/PaperCrawlerUtil-0.0.52/PaperCrawlerUtil/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(__file__))
+sys.path.append("../PaperCrawlerUtil/")
+__all__ = ["common_util", "crawler_util", "document_util"]
+print(sys.path)
+from crawler_util import *
+from document_util import *
+from common_util import *
+
+basic_config(logs_style=LOG_STYLE_PRINT, require_proxy_pool=True)
