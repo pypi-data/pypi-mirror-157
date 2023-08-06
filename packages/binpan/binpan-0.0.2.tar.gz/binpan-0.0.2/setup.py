24ade40cde7405ee423a5436518bad47546749db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:53 +0000

    Add package binpan/0.0.2/binpan-0.0.2.tar.gz

---
+++ packages/binpan/binpan-0.0.2.tar.gz/binpan-0.0.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+readme = open('README.md', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(name='binpan',
+      version='0.0.2',
+      url='https://github.com/nand0san/binpan_studio',
+      license='MIT',
+      install_requires=[
+            'requests~=2.28.0',
+            'pytz~=2022.1',
+            'py-cpuinfo~=8.0.0',
+            'numpy==1.23.0',
+            'pandas~=1.4.2',
+            'plotly~=5.9.0',
+            'tqdm~=4.64.0',
+            'pycryptodome==3.15.0',
+            'pandas-ta==0.3.14b0'
+      ],
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.9",
+      ],
+      author='Fernando Alfonso',
+      author_email='hancaidolosdos@hotmail.com',
+      description='Binance API wrapper with backtesting tools.',
+      long_description=README_TEXT,
+      long_description_content_type="text/markdown",
+      package_dir={"": "."},
+      packages=[
+            ".",
+            "handlers"
+      ]
+      )
