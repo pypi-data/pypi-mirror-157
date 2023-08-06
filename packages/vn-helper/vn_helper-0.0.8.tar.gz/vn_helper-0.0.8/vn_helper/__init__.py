5a07c596f91d834ca705c66f9f67bcb5cc84041c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:52 +0000

    Add package vn-helper/0.0.8/vn_helper-0.0.8.tar.gz

---
+++ packages/vn-helper/vn_helper-0.0.8.tar.gz/vn_helper-0.0.8/vn_helper/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+__author__ = "VanhPham"
+__email__ = "vananh@tinhte.vn"
+
+#TOP-LEVEL MODULES
+from vn_helper.text_preprocessing import text_preprocessing
+from vn_helper.num_preprocessing import num_preprocessing
+
+__all__ = ['text_preprocessing', 'num_preprocessing']
