8d0e92f911edde65b37549e864f354009f42c792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:52 +0000

    Add package jupyterlab-mathjax3-web/0.1.0/jupyterlab-mathjax3-web-0.1.0.tar.gz

---
+++ packages/jupyterlab-mathjax3-web/jupyterlab-mathjax3-web-0.1.0.tar.gz/jupyterlab-mathjax3-web-0.1.0/src/setting.ts
@@ -0,0 +1,7 @@
+
+export namespace Setting {
+
+  export const editConfig = "jupyterlab-mathjax3-web:edit-config";
+  export const viewConfig = "jupyterlab-mathjax3-web:view-config";
+  export const category = "MathJax 3 Config";
+}
