0f7a2f05ad240ebde9ce238ccf98e3af17a4f396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:58 +0000

    Add package date-time-handler/0.0.0.21/date_time_handler-0.0.0.21.tar.gz

---
+++ packages/date-time-handler/date_time_handler-0.0.0.21.tar.gz/date_time_handler-0.0.0.21/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name = "date_time_handler",
+    version = "0.0.0.21",
+    description = "Robust date-time formatter with implicit time-zone conversion, wraps datetime and pytz",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/jmsimons/date_time_handler",
+    author = "Jared Simons",
+    author_email = "jmsimons@lcmail.lcsc.edu",
+    py_modules = ["date_time_handler"],
+    packages = ["date_time_handler"],
+    install_requires = ["time", "datetime", "python-dateutil", "pytz"]
+)
