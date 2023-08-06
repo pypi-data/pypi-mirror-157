010b58f05dcbb999e0067083394b8ec71a874f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:08 +0000

    Add package picovoice/2.1.5/picovoice-2.1.5-py3-none-any.whl

---
+++ packages/picovoice/picovoice-2.1.5-py3-none-any.whl/picovoice/__init__.py
@@ -0,0 +1,24 @@
+#
+# Copyright 2021-2022 Picovoice Inc.
+#
+# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+# file accompanying this source.
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+from .picovoice import Picovoice
+from .picovoice import PicovoiceError
+from .picovoice import PicovoiceMemoryError
+from .picovoice import PicovoiceIOError
+from .picovoice import PicovoiceInvalidArgumentError
+from .picovoice import PicovoiceStopIterationError
+from .picovoice import PicovoiceKeyError
+from .picovoice import PicovoiceInvalidStateError
+from .picovoice import PicovoiceRuntimeError
+from .picovoice import PicovoiceActivationError
+from .picovoice import PicovoiceActivationLimitError
+from .picovoice import PicovoiceActivationThrottledError
+from .picovoice import PicovoiceActivationRefusedError
