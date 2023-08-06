ad34a844bc52389f4a34b6de706e96502db2febd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:33 +0000

    Add package advanced-scrapy-proxies/0.1.3/advanced-scrapy-proxies-0.1.3.tar.gz

---
+++ packages/advanced-scrapy-proxies/advanced-scrapy-proxies-0.1.3.tar.gz/advanced-scrapy-proxies-0.1.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(	name='advanced-scrapy-proxies',
+		version='0.1.3',
+		description='Advanced Scrapy Proxies: random proxy middleware for Scrapy with advanced features',
+		long_description=README,
+		long_description_content_type="text/markdown",
+		author='Pierluigi Vinciguerra',
+		author_email='pierluigivinciguerra@gmail.com',
+		license="GNU GPLv2",
+		url='https://github.com/PVinciguerra/advanced-scrapy-proxies',
+		packages=['advanced-scrapy-proxies'],
+		setup_requires=['wheel', 'requests', 'pip']
+		)
