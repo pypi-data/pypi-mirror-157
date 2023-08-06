b7db1e4c759e36955eb0d68d79e8080f8b6766bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:44 +0000

    Add package marceloapd-ic-toolkit/0.3.8/marceloapd-ic-toolkit-0.3.8.tar.gz

---
+++ packages/marceloapd-ic-toolkit/marceloapd-ic-toolkit-0.3.8.tar.gz/marceloapd-ic-toolkit-0.3.8/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['marceloapd_ic_toolkit']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyMySQL',
+ 'boto3',
+ 'botocore',
+ 'certifi',
+ 'click',
+ 'colorama',
+ 'distlib',
+ 'filelock',
+ 'jmespath',
+ 'numpy',
+ 'pandas',
+ 'pipenv',
+ 'platformdirs',
+ 'python-dateutil',
+ 'pytz',
+ 's3transfer',
+ 'six',
+ 'typer[all]>=0.4.1,<0.5.0',
+ 'urllib3',
+ 'virtualenv',
+ 'virtualenv-clone']
+
+entry_points = \
+{'console_scripts': ['ic-toolkit = marceloapd_ic_toolkit.main:app']}
+
+setup_kwargs = {
+    'name': 'marceloapd-ic-toolkit',
+    'version': '0.3.8',
+    'description': '',
+    'long_description': 'toolkit',
+    'author': 'Marcelo Assis',
+    'author_email': '94455042+marceloapda@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
