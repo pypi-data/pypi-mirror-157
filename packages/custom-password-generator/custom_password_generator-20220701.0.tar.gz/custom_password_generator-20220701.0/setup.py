08d9a6c2380cd0578c151a86033281dad5686d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:26:08 +0000

    Add package custom-password-generator/20220701.0/custom_password_generator-20220701.0.tar.gz

---
+++ packages/custom-password-generator/custom_password_generator-20220701.0.tar.gz/custom_password_generator-20220701.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="custom_password_generator",
+    version="20220701.0",
+    packages=["custom_password_generator"],
+    url="https://gitlab.com/daxm/python-password-generator",
+    license="BSD",
+    author="Dax Mickelson",
+    author_email="dmickelson@zscaler.com",
+    description="Generate a customized complex password",
+)
