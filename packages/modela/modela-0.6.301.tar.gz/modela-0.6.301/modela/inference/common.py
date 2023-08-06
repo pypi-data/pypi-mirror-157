fad0ed86c2b563504477e517f0777487719f3ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:39 +0000

    Add package modela/0.6.301/modela-0.6.301.tar.gz

---
+++ packages/modela/modela-0.6.301.tar.gz/modela-0.6.301/modela/inference/common.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+
+class AccessType(Enum):
+    ClusterIP = "cluster-ip"
+    NodePort = "node-port"
+    LoadBalancer = "load-balancer"
+    Ingress = "ingress"
+    Mesh = "mesh"
+    Nothing = "none"
+
+
+class PredictorConditionType(Enum):
+    PredictorReady = "Ready"
+    PredictorSaved = "Saved"
+
+
+class ModelRole(Enum):
+    Champion = "champion"
+    Challenger = "challenger"
+    Shadow = "shadow"
+
+
+class ModelDeploymentPhase(Enum):
+    Deploying = "Deploying"
+    Deployed = "Deployed"
+    Shadowing = "Shadowing"
+    Releasing = "Releasing"
+    Released = "Released"
+    Failed = "Failed"
+
+
+class PredictorType(Enum):
+    Online = "online"
+    Batch = "batch"
+    Streaming = "streaming"
+
+
+class CanaryMetric(Enum):
+    Cpu = "cpu"
+    Mem = "mem"
+    Latency = "latency"
+    Crash = "crash"
+
+
