18c71f2fe3c90dc659c82e6c5583a0b67fde69b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:33 +0000

    Add package pdal-parallelizer/0.7.2/pdal_parallelizer-0.7.2-py3-none-any.whl

---
+++ packages/pdal-parallelizer/pdal_parallelizer-0.7.2-py3-none-any.whl/pdal_parallelizer/tile.py
@@ -0,0 +1,37 @@
+import sys
+import pdal
+import json
+import os
+
+
+class Tile:
+    def __init__(self, filepath, output_dir, json_pipeline):
+        self.filepath = filepath
+        self.output_dir = output_dir
+        self.json_pipeline = json_pipeline
+
+    def pipeline(self):
+        filename = os.path.basename(self.filepath).split('.')[0]
+        name = 'temp__' + filename
+        output_dir = self.output_dir
+        output_filename = f'{output_dir}/{filename}.las'
+
+        with open(self.json_pipeline, 'r') as pipeline:
+            p = json.load(pipeline)
+            reader = list(filter(lambda x: x['type'] == 'readers.las', p))
+            writer = list(filter(lambda x: x['type'] == 'writers.las', p))
+
+            if not reader:
+                sys.exit("Please add a reader to your pipeline.")
+            elif not writer:
+                sys.exit("Please add a writer to your pipeline.")
+
+            reader[0]['filename'] = self.filepath
+            writer[0]['filename'] = output_filename
+
+            p = pdal.Pipeline(json.dumps(p))
+
+        return p, name
+
+    def __str__(self):
+        return f'{self.filepath}'
