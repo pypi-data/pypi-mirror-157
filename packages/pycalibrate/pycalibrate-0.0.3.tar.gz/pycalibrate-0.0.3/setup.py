988a5ce18b77473468abf0dafdfe24f77d60e745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:20 +0000

    Add package pycalibrate/0.0.3/pycalibrate-0.0.3.tar.gz

---
+++ packages/pycalibrate/pycalibrate-0.0.3.tar.gz/pycalibrate-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pycalibrate",
+    version="0.0.3",
+    author="Peter Xenopoulos, Joao Rulff, Brian Barr, Luis Gustavo Nonato, Claudio Silva",
+    author_email="xenopoulos@nyu.edu",
+    description="pycalibrate. A tool to assess classifier calibration in Jupyter Notebooks",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/VIDA-NYU/pycalibrate',
+    packages=find_packages(exclude=['node_modules']),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'notebook',
+        'notebookjs',
+        'interpret',
+        'pandas'
+    ]
+)
