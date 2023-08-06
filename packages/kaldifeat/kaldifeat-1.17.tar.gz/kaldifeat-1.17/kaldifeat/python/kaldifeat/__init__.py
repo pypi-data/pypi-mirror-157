94ba3b1f58d2474bc95afdef7d53e1f9919dbaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:24 +0000

    Add package kaldifeat/1.17/kaldifeat-1.17.tar.gz

---
+++ packages/kaldifeat/kaldifeat-1.17.tar.gz/kaldifeat-1.17/kaldifeat/python/kaldifeat/__init__.py
@@ -0,0 +1,26 @@
+import torch
+
+from .torch_version import kaldifeat_torch_version
+
+if torch.__version__.split("+")[0] != kaldifeat_torch_version.split("+")[0]:
+    raise ImportError(
+        f"kaldifeat was built using PyTorch {kaldifeat_torch_version}\n"
+        f"But you are using PyTorch {torch.__version__} to run it"
+    )
+
+from _kaldifeat import (
+    FbankOptions,
+    FrameExtractionOptions,
+    MelBanksOptions,
+    MfccOptions,
+    PlpOptions,
+    SpectrogramOptions,
+)
+
+from .fbank import Fbank, OnlineFbank
+from .mfcc import Mfcc, OnlineMfcc
+from .offline_feature import OfflineFeature
+from .online_feature import OnlineFeature
+from .plp import OnlinePlp, Plp
+from .spectrogram import Spectrogram
+__version__ = '1.17'
