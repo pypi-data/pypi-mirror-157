d117ea16ac9551a32b21e12b334e7868a5abe216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:11 +0000

    Add package cao-lang/0.1.66/cao-lang-0.1.66.tar.gz

---
+++ packages/cao-lang/cao-lang-0.1.66.tar.gz/cao-lang-0.1.66/cao-lang/src/compiler/lane.rs
@@ -0,0 +1,35 @@
+use super::Card;
+use crate::VarName;
+use std::str::FromStr;
+
+/// Cao-lang functions
+#[derive(Debug, Clone, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Lane {
+    #[cfg_attr(feature = "serde", serde(default = "Vec::new"))]
+    pub arguments: Vec<VarName>,
+    #[cfg_attr(feature = "serde", serde(default = "Vec::new"))]
+    pub cards: Vec<Card>,
+}
+
+impl Lane {
+    #[must_use]
+    pub fn with_arg(mut self, name: &str) -> Self {
+        let name = VarName::from_str(name).expect("Bad variable name");
+        self.arguments.push(name);
+        self
+    }
+
+    #[must_use]
+    pub fn with_card(mut self, card: Card) -> Self {
+        self.cards.push(card);
+        self
+    }
+
+    /// overrides the existing cards
+    #[must_use]
+    pub fn with_cards<C: Into<Vec<Card>>>(mut self, cards: C) -> Self {
+        self.cards = cards.into();
+        self
+    }
+}
