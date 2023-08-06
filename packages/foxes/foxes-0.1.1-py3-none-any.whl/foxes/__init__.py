8ca941ad0376e40dbac37690c2f24c2656d87b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:55 +0000

    Add package foxes/0.1.1/foxes-0.1.1-py3-none-any.whl

---
+++ packages/foxes/foxes-0.1.1-py3-none-any.whl/foxes/__init__.py
@@ -0,0 +1,16 @@
+"""
+Farm Optimization and eXtended yield Evaluation Software
+"""
+from .core import WindFarm, Turbine
+from .models import ModelBook
+from .data import parse_Pct_file_name, FARM, STATES, PCTCURVE, StaticData
+
+from . import algorithms
+from . import models
+from . import input
+from . import output
+from . import tools
+
+from importlib.resources import read_text
+
+__version__ = read_text(__package__, "VERSION")
