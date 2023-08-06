550f6e08114782b6110f46f0922d83476c4f7f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:02 +0000

    Add package fastboot/0.0.2/fastboot-0.0.2.tar.gz

---
+++ packages/fastboot/fastboot-0.0.2.tar.gz/fastboot-0.0.2/fastboot/web/rest_controller.py
@@ -0,0 +1,55 @@
+from flask import Flask
+
+
+
+class BaseController:
+    instance = None
+    klass = None
+    path = None
+
+    def __init__(self, app: Flask, arguments = []):
+        self.app = app
+        self.arguments = arguments
+
+    def add_get(self):
+        methods_names = self.search_methods('Get')
+        for name in methods_names:
+            method = getattr(self.instance, name)
+            info_method = method()
+            self.app.add_url_rule(self.path + info_method.get('path'), info_method.get('id'), info_method.get('function'))
+
+
+    def add_controller(self):
+        print(*self.arguments)
+        self.instance = self.klass(*self.arguments)
+        self.add_get()
+
+
+
+        ##self.app.add_url_rule(self)
+
+        return self.instance
+
+    def search_methods(self, type) -> list:
+        response = []
+        methods = dir(self.klass)
+        for method_name in methods:
+            method = getattr(self.klass, method_name)
+            try:
+                decorators = getattr(method, 'decorators')
+                if type in decorators:
+                    response.append(method_name)
+            except:
+                pass
+        return response
+
+def RestController(path_controller=''):
+
+    def wrapper(klass_):
+        class Controller(BaseController):
+            path = path_controller
+            klass = klass_
+        return Controller
+
+    return wrapper
+
