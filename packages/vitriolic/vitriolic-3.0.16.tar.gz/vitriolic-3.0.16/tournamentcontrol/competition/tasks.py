d0e5fbdf7bfa15b3dbaaee5b1aa15d9f75f98c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:58 +0000

    Add package vitriolic/3.0.16/vitriolic-3.0.16.tar.gz

---
+++ packages/vitriolic/vitriolic-3.0.16.tar.gz/vitriolic-3.0.16/tournamentcontrol/competition/tasks.py
@@ -0,0 +1,17 @@
+from celery import shared_task
+
+from tournamentcontrol.competition.models import Match, Stage
+from tournamentcontrol.competition.utils import generate_scorecards
+
+
+@shared_task
+def generate_pdf_scorecards(
+    match_pks, templates, extra_context, stage_pk=None, **kwargs
+):
+    matches = Match.objects.filter(pk__in=match_pks)
+    stage = None
+    if stage_pk is not None:
+        stage = Stage.objects.get(pk=stage_pk)
+    return generate_scorecards(
+        matches, templates, "pdf", extra_context, stage, **kwargs
+    )
