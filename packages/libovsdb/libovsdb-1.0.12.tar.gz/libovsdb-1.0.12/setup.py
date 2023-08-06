2e80345fbedbfff07e937dff641614577f54a099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:25:16 +0000

    Add package libovsdb/1.0.12/libovsdb-1.0.12.tar.gz

---
+++ packages/libovsdb/libovsdb-1.0.12.tar.gz/libovsdb-1.0.12/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='libovsdb',
+      version='1.0.12',
+      author='Yongping Guo',
+      author_email='guoyoooping@163.com',
+      description='An OVSDB Library written in python.',
+      long_description=open('README.rst').read(),
+      install_requires = [],
+      url='https://github.com/guoyoooping/libovsdb',
+      license="GPLv3",
+      scripts=[],
+      packages=['libovsdb']
+      )
