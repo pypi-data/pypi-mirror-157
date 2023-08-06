8520e81710f71bf926e5f786c77f0247163ce85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:01 +0000

    Add package msds/0.0.8/msds-0.0.8-py3-none-any.whl

---
+++ packages/msds/msds-0.0.8-py3-none-any.whl/msds/func/Signals.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import random
+
+
+# 将None作为信号返回
+def real_signal_none(df, para):
+    """
+    发出空交易信号
+    :param df:
+    :param para:
+    :return:
+    """
+
+    return None
+
+
+# 随机生成交易信号
+def real_signal_random(df,  para=[200,2]):
+    """
+    随机发出交易信号
+    :param df:
+    :param para:
+    :return:
+    """
+
+    r = random.random()
+    if r <= 0.25:
+        return 1
+    elif r <= 0.5:
+        return -1
+    elif r <= 0.75:
+        return 0
+    else:
+        return None
+
+
+# 布林策略实盘交易信号
+def signal_simple_bolling(df, para=[550, 2.1, 0.05]):
+    """
+    :param df:
+    :param para: n, m
+    :return:
+
+    # 布林线策略
+    # 布林线中轨：n天收盘价的移动平均线
+    # 布林线上轨：n天收盘价的移动平均线 + m * n天收盘价的标准差
+    # 布林线下轨：n天收盘价的移动平均线 - m * n天收盘价的标准差
+    # 当收盘价由下向上穿过上轨的时候，做多；然后由上向下穿过中轨的时候，平仓。
+    # 当收盘价由上向下穿过下轨的时候，做空；然后由下向上穿过中轨的时候，平仓。
+    """
+
+    # ===策略参数
+    n = int(para[0])
+    m = para[1]
+    p = para[2]
+
+    # ===计算指标
+    # 计算均线
+    df['median'] = df['close'].rolling(n, min_periods=1).mean()
+    # 计算上轨、下轨道
+    df['std'] = df['close'].rolling(n, min_periods=1).std(ddof=0)  # ddof代表标准差自由度
+    df['upper'] = df['median'] + m * df['std']
+    df['lower'] = df['median'] - m * df['std']
+    df['abs'] = abs(df['close'] / df['median'] - 1)
+
+    # ===计算信号
+    # 找出做多信号
+    condition1 = df['close'] > df['upper']  # 当前K线的收盘价 > 上轨
+    condition2 = df['close'].shift(1) <= df['upper'].shift(1)  # 之前K线的收盘价 <= 上轨
+    condition3 = df['abs'] <= p
+    df.loc[condition1 & condition3, 'signal_long'] = 1  # 将产生做多信号的那根K线的signal设置为1，1代表做多
+
+    # 找出做多平仓信号
+    condition1 = df['close'] < df['median']  # 当前K线的收盘价 < 中轨
+    condition2 = df['close'].shift(1) >= df['median'].shift(1)  # 之前K线的收盘价 >= 中轨
+    df.loc[condition1 & condition2, 'signal_long'] = 0  # 将产生平仓信号当天的signal设置为0，0代表平仓
+
+    # 找出做空信号
+    condition1 = df['close'] < df['lower']  # 当前K线的收盘价 < 下轨
+    condition2 = df['close'].shift(1) >= df['lower'].shift(1)  # 之前K线的收盘价 >= 下轨
+    condition3 = df['abs'] <= p
+    df.loc[condition1 & condition3, 'signal_short'] = -1  # 将产生做空信号的那根K线的signal设置为-1，-1代表做空
+
+    # 找出做空平仓信号
+    condition1 = df['close'] > df['median']  # 当前K线的收盘价 > 中轨
+    condition2 = df['close'].shift(1) <= df['median'].shift(1)  # 之前K线的收盘价 <= 中轨
+    df.loc[condition1 & condition2, 'signal_short'] = 0  # 将产生平仓信号当天的signal设置为0，0代表平仓
+
+    # 合并做多做空信号，去除重复信号
+    df['signal'] = df[['signal_long', 'signal_short']].sum(axis=1, min_count=1, skipna=True)  # 若你的pandas版本是最新的，请使用本行代码代替上面一行
+    temp = df[df['signal'].notnull()][['signal']]
+    temp = temp[temp['signal'] != temp['signal'].shift(1)]
+    df['signal'] = temp['signal']
+
+    # ===删除无关变量
+    df.drop(['median', 'std', 'upper', 'lower', 'signal_long', 'signal_short'], axis=1, inplace=True)
+
+    return df
